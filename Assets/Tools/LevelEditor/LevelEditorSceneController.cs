19694519a97a96af0bbfbc38588b23da0647e45a
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2015-10-08 17:47:39 +0200

    Level Editor: Development - Grouped all classes under a common namespace.
    Former-commit-id: 67b5bc6283fba6e58caa824329a7b83c2f9e1abe

--- Assets/Tools/LevelEditor/LevelEditorSceneController.cs
+++ Assets/Tools/LevelEditor/LevelEditorSceneController.cs
@@ -12,53 +12,54 @@ using UnityEngine;
 //----------------------------------------------------------------------//
 // CLASSES																//
 //----------------------------------------------------------------------//
-/// <summary>
-/// Scene controller for the level editor scene.
-/// </summary>
-public class LevelEditorSceneController : SceneController {
-	//------------------------------------------------------------------//
-	// CONSTANTS														//
-	//------------------------------------------------------------------//
-	public static readonly string NAME = "SC_LevelEditor";
-
-	//------------------------------------------------------------------//
-	// MEMBERS															//
-	//------------------------------------------------------------------//
-
-	//------------------------------------------------------------------//
-	// GENERIC METHODS													//
-	//------------------------------------------------------------------//
+namespace LevelEditor {
 	/// <summary>
-	/// Initialization.
+	/// Scene controller for the level editor scene.
 	/// </summary>
-	override protected void Awake() {
-		// Load the dragon
-		DragonManager.LoadDragon(LevelEditor.testDragon);
+	public class LevelEditorSceneController : SceneController {
+		//------------------------------------------------------------------//
+		// CONSTANTS														//
+		//------------------------------------------------------------------//
+		public static readonly string NAME = "SC_LevelEditor";
 
-		// Call parent
-		base.Awake();
-	}
+		//------------------------------------------------------------------//
+		// MEMBERS															//
+		//------------------------------------------------------------------//
 
-	/// <summary>
-	/// First update.
-	/// </summary>
-	void Start() {
-	
-	}
-	
-	/// <summary>
-	/// Called every frame.
-	/// </summary>
-	void Update() {
+		//------------------------------------------------------------------//
+		// GENERIC METHODS													//
+		//------------------------------------------------------------------//
+		/// <summary>
+		/// Initialization.
+		/// </summary>
+		override protected void Awake() {
+			// Load the dragon
+			DragonManager.LoadDragon(LevelEditor.testDragon);
 
-	}
+			// Call parent
+			base.Awake();
+		}
 
-	/// <summary>
-	/// Destructor.
-	/// </summary>
-	override protected void OnDestroy() {
-		// Call parent
-		base.OnDestroy();
+		/// <summary>
+		/// First update.
+		/// </summary>
+		void Start() {
+		
+		}
+		
+		/// <summary>
+		/// Called every frame.
+		/// </summary>
+		void Update() {
+
+		}
+
+		/// <summary>
+		/// Destructor.
+		/// </summary>
+		override protected void OnDestroy() {
+			// Call parent
+			base.OnDestroy();
+		}
 	}
 }
-
--- Assets/Tools/LevelEditor/TransformLock.cs
+++ Assets/Tools/LevelEditor/TransformLock.cs
@@ -12,61 +12,63 @@ using UnityEngine;
 //----------------------------------------------------------------------//
 // CLASSES																//
 //----------------------------------------------------------------------//
-/// <summary>
-/// Simple script to lock transformations of an object in the editor.
-/// </summary>
-public class TransformLock : MonoBehaviour {
-	//------------------------------------------------------------------//
-	// MEMBERS															//
-	//------------------------------------------------------------------//
-	public bool[] m_positionLock = new bool[3];
-	public bool[] m_rotationLock = new bool[3];
-	public bool[] m_scaleLock = new bool[3];
-
-	//------------------------------------------------------------------//
-	// GENERIC METHODS													//
-	//------------------------------------------------------------------//
+namespace LevelEditor {
 	/// <summary>
-	/// First update.
+	/// Simple script to lock transformations of an object in the editor.
 	/// </summary>
-	protected void Start() {
-		// Must be included if we want to be able to enable/disable the component
-	}
+	public class TransformLock : MonoBehaviour {
+		//------------------------------------------------------------------//
+		// MEMBERS															//
+		//------------------------------------------------------------------//
+		public bool[] m_positionLock = new bool[3];
+		public bool[] m_rotationLock = new bool[3];
+		public bool[] m_scaleLock = new bool[3];
 
-	/// <summary>
-	/// Sets the position lock.
-	/// </summary>
-	/// <param name="_x">X axis value.</param>
-	/// <param name="_y">Y axis value.</param>
-	/// <param name="_z">Z axis value.</param>
-	public void SetPositionLock(bool _x, bool _y, bool _z) {
-		m_positionLock[0] = _x;
-		m_positionLock[1] = _y;
-		m_positionLock[2] = _z;
-	}
+		//------------------------------------------------------------------//
+		// GENERIC METHODS													//
+		//------------------------------------------------------------------//
+		/// <summary>
+		/// First update.
+		/// </summary>
+		protected void Start() {
+			// Must be included if we want to be able to enable/disable the component
+		}
 
-	/// <summary>
-	/// Sets the rotation lock.
-	/// </summary>
-	/// <param name="_x">X axis value.</param>
-	/// <param name="_y">Y axis value.</param>
-	/// <param name="_z">Z axis value.</param>
-	public void SetRotationLock(bool _x, bool _y, bool _z) {
-		m_rotationLock[0] = _x;
-		m_rotationLock[1] = _y;
-		m_rotationLock[2] = _z;
-	}
+		/// <summary>
+		/// Sets the position lock.
+		/// </summary>
+		/// <param name="_x">X axis value.</param>
+		/// <param name="_y">Y axis value.</param>
+		/// <param name="_z">Z axis value.</param>
+		public void SetPositionLock(bool _x, bool _y, bool _z) {
+			m_positionLock[0] = _x;
+			m_positionLock[1] = _y;
+			m_positionLock[2] = _z;
+		}
 
-	/// <summary>
-	/// Sets the scale lock.
-	/// </summary>
-	/// <param name="_x">X axis value.</param>
-	/// <param name="_y">Y axis value.</param>
-	/// <param name="_z">Z axis value.</param>
-	public void SetScaleLock(bool _x, bool _y, bool _z) {
-		m_scaleLock[0] = _x;
-		m_scaleLock[1] = _y;
-		m_scaleLock[2] = _z;
+		/// <summary>
+		/// Sets the rotation lock.
+		/// </summary>
+		/// <param name="_x">X axis value.</param>
+		/// <param name="_y">Y axis value.</param>
+		/// <param name="_z">Z axis value.</param>
+		public void SetRotationLock(bool _x, bool _y, bool _z) {
+			m_rotationLock[0] = _x;
+			m_rotationLock[1] = _y;
+			m_rotationLock[2] = _z;
+		}
+
+		/// <summary>
+		/// Sets the scale lock.
+		/// </summary>
+		/// <param name="_x">X axis value.</param>
+		/// <param name="_y">Y axis value.</param>
+		/// <param name="_z">Z axis value.</param>
+		public void SetScaleLock(bool _x, bool _y, bool _z) {
+			m_scaleLock[0] = _x;
+			m_scaleLock[1] = _y;
+			m_scaleLock[2] = _z;
+		}
 	}
 }
 
