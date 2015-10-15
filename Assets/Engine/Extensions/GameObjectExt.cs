6cb109707b07b8633b4d1361f6ed0c61c9288b22
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2015-10-15 16:01:39 +0200

    Game Object Extensions: Development - Created class to expand the GameObject class. Added method to generate a unique name to the given GameObject (within its hierarchy level).
    Former-commit-id: 59e00f9dcfb9587b9b879be58ed4b86fce1b07cd

---
+++ Assets/Engine/Extensions/GameObjectExt.cs
@@ -0,0 +1,54 @@
+﻿// GameObjectExt.cs
+// 
+// Created by Alger Ortín Castellví on 15/10/2015.
+// Copyright (c) 2015 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------//
+// INCLUDES																//
+//----------------------------------------------------------------------//
+using UnityEngine;
+using System.Collections.Generic;
+
+//----------------------------------------------------------------------//
+// CLASSES																//
+//----------------------------------------------------------------------//
+/// <summary>
+/// Custom extensions to the GameObject class.
+/// </summary>
+public static class GameObjectExt {
+	//------------------------------------------------------------------//
+	// CONSTANTS														//
+	//------------------------------------------------------------------//
+
+	//------------------------------------------------------------------//
+	// STATIC EXTENSION METHODS											//
+	//------------------------------------------------------------------//
+	/// <summary>
+	/// Generate and apply a unique name for this object in its current parent (no siblings with the same name).
+	/// Costly method, avoid intensive usage (i.e. in the Update call).
+	/// </summary>
+	/// <param name="_obj">The object we're changing.</param>
+	/// <param name="_nameBase">The prefix of the new name. The name will be composed by "prefix_#" where # is a unique numeric identifier.</param>
+	public static void SetUniqueName(this GameObject _obj, string _nameBase) {
+		// Get parent transform
+		Transform parentTr = _obj.transform.parent;
+
+		// We don't want double separators, so clean name base for trailing "_"
+		// [AOC] Gotta love c#
+		_nameBase = _nameBase.TrimEnd('_');
+
+		// Try different names until one is not found in the parent
+		int i = 0;
+		string targetName = "";
+		Transform childTr = null;
+		do {
+			// An object exists with this name?
+			targetName = _nameBase + "_" + i.ToString();
+			childTr = parentTr.Find(targetName);
+			i++;
+		} while(childTr != null);
+
+		// We found our index, set new name to the target object
+		_obj.name = targetName;
+	}
+}
