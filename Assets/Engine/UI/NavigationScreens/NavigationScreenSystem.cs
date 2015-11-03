18b3ccde09b954010a18db8c324d84816e39cc22
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2015-11-03 15:10:38 +0100

    Menu Scene: Development - Moved all the screen navigation logic into a generic class. Grouped all screens under a new prefab so references are kept. Navigation Screen: Development - Generic screen navigation system to easily implement menus.
    Former-commit-id: 486d7324a3f46cfa97e6e47df0bf0468292c0da5

---
+++ Assets/Engine/UI/NavigationScreens/NavigationScreenSystem.cs
@@ -0,0 +1,102 @@
+﻿// NavigationScreenSystem.cs
+// 
+// Created by Alger Ortín Castellví on 03/11/2015.
+// Copyright (c) 2015 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------//
+// INCLUDES																//
+//----------------------------------------------------------------------//
+using UnityEngine;
+using System.Collections.Generic;
+
+//----------------------------------------------------------------------//
+// CLASSES																//
+//----------------------------------------------------------------------//
+/// <summary>
+/// Class to implement a screen navigation system to move between different ui screens.
+/// </summary>
+public class NavigationScreenSystem : MonoBehaviour {
+	//------------------------------------------------------------------//
+	// CONSTANTS														//
+	//------------------------------------------------------------------//
+	public static readonly int SCREEN_NONE = -1;
+
+	//------------------------------------------------------------------//
+	// MEMBERS															//
+	//------------------------------------------------------------------//
+	// Screen references to be set from the inspector
+	[SerializeField] private List<NavigationScreen> m_screens = new List<NavigationScreen>();
+	[SerializeField] private NavigationScreen m_initialScreen = null;
+	private int m_currentScreenIdx = SCREEN_NONE;
+
+	//------------------------------------------------------------------//
+	// GENERIC METHODS													//
+	//------------------------------------------------------------------//
+	/// <summary>
+	/// Initialization.
+	/// </summary>
+	void Awake() {
+		// Initial screen is visible from the beginning
+		for(int i = 0; i < m_screens.Count; i++) {
+			if(m_screens[i] == m_initialScreen) {
+				m_currentScreenIdx = i;
+				m_screens[i].gameObject.SetActive(true);
+			} else {
+				m_screens[i].gameObject.SetActive(false);
+			}
+		}
+	}
+
+	//------------------------------------------------------------------//
+	// PUBLIC METHODS													//
+	//------------------------------------------------------------------//
+	/// <summary>
+	/// Navigate to the target screen. Use an int to be able to directly connect buttons to it.
+	/// </summary>
+	/// <param name="_newScreen">The index of the new screen to go to. Use -1 for NONE.</param>
+	public void GoToScreen(int _newScreen) {
+		// Ignore if screen is already active
+		if(_newScreen == m_currentScreenIdx) return;
+		
+		// If out of bounds, go to none
+		if(_newScreen < 0 || _newScreen >= m_screens.Count) {
+			_newScreen = SCREEN_NONE;
+		}
+		
+		// Figure out animation direction based on current and new screen indexes
+		NavigationScreen.AnimDir dir = NavigationScreen.AnimDir.NEUTRAL;
+		if(m_currentScreenIdx != SCREEN_NONE && _newScreen != SCREEN_NONE) {
+			if(m_currentScreenIdx > _newScreen) {
+				dir = NavigationScreen.AnimDir.BACK;
+			} else {
+				dir = NavigationScreen.AnimDir.FORWARD;
+			}
+		}
+		
+		// Hide current screen (if any)
+		if(m_currentScreenIdx != SCREEN_NONE) {
+			m_screens[m_currentScreenIdx].Hide(dir);
+		}
+		
+		// Show new screen (if any)
+		if(_newScreen != SCREEN_NONE) {
+			m_screens[_newScreen].Show(dir);
+		}
+		
+		// Update screen tracking
+		m_currentScreenIdx = _newScreen;
+	}
+	
+	/// <summary>
+	/// Navigate to the target screen. Screen must be added to the screens array, otherwise nothing will happen.
+	/// </summary>
+	/// <param name="_screen">The screen to go to.</param>
+	public void GoToScreen(NavigationScreen _screen) {
+		for(int i = 0; i < m_screens.Count; i++) {
+			if(m_screens[i] == _screen) {
+				GoToScreen(i);
+				return;
+			}
+		}
+	}
+}
--- Assets/Game/Scenes/MenuSceneController.cs
+++ Assets/Game/Scenes/MenuSceneController.cs
@@ -21,34 +21,9 @@ public class MenuSceneController : SceneController {
 	//------------------------------------------------------------------//
 	public static readonly string NAME = "SC_Menu";
 
-	// Order is relevant! Will define animations
-	public enum Screen {
-		NONE = -1,
-
-		MAIN,
-		DRAGON_SELECTION,
-		LEVEL_SELECTION,
-
-		COUNT
-	}
-
 	//------------------------------------------------------------------//
 	// MEMBERS															//
 	//------------------------------------------------------------------//
-	// Screen references to be set from the inspector
-	[Space(10)]
-	[Comment("Required")]
-	[SerializeField] private MenuScreen m_mainScreen = null;
-	[SerializeField] private MenuScreen m_dragonSelectionScreen = null;
-	[SerializeField] private MenuScreen m_levelSelectionScreen = null;
-
-	[Space(10)]
-	[HideEnumValues(false, true)]
-	[SerializeField] private Screen m_initialScreen = Screen.MAIN;
-
-	// Screen navigation helpers
-	private MenuScreen[] m_screens = null;
-	private Screen m_currentScreen = Screen.NONE;
 
 	//------------------------------------------------------------------//
 	// PROPERTIES														//
@@ -63,25 +38,6 @@ public class MenuSceneController : SceneController {
 	override protected void Awake() {
 		// Call parent
 		base.Awake();
-
-		// Check required members
-		DebugUtils.Assert(m_mainScreen != null, "Required field!");
-		DebugUtils.Assert(m_dragonSelectionScreen != null, "Required field!");
-		DebugUtils.Assert(m_levelSelectionScreen != null, "Required field!");
-
-		// Init screens array
-		m_screens = new MenuScreen[] {
-			m_mainScreen, 
-			m_dragonSelectionScreen, 
-			m_levelSelectionScreen
-		};
-
-		// Initial screen is visible from the beginning
-		m_currentScreen = m_initialScreen;
-		for(int i = 0; i < m_screens.Length; i++) {
-			bool initialScreen = (i == (int)m_currentScreen);
-			m_screens[i].gameObject.SetActive(initialScreen);
-		}
 	}
 
 	/// <summary>
@@ -107,59 +63,23 @@ public class MenuSceneController : SceneController {
 	}
 
 	//------------------------------------------------------------------//
-	// PUBLIC METHODS													//
-	//------------------------------------------------------------------//
-	/// <summary>
-	/// Navigate to the target screen. Use an int to be able to directly connect buttons to it.
-	/// </summary>
-	/// <param name="_newScreen">The index of the new screen to go to. Use -1 for NONE.</param>
-	public void GoToScreen(int _newScreen) {
-		// Ignore if screen is already active
-		if(_newScreen == (int)m_currentScreen) return;
-
-		// Translate int to Screen
-		Screen newScreen = Screen.NONE;
-		if(_newScreen >= 0 && _newScreen < (int)Screen.COUNT) {
-			newScreen = (Screen)_newScreen;
-		}
-
-		// Figure out animation direction based on current and new screen indexes
-		MenuScreen.AnimDir dir = MenuScreen.AnimDir.NEUTRAL;
-		if(m_currentScreen != Screen.NONE && newScreen != Screen.NONE) {
-			if(m_currentScreen > newScreen) {
-				dir = MenuScreen.AnimDir.BACK;
-			} else {
-				dir = MenuScreen.AnimDir.FORWARD;
-			}
-		}
-
-		// Hide current screen (if any)
-		if(m_currentScreen != Screen.NONE) {
-			m_screens[(int)m_currentScreen].Hide(dir);
-		}
-
-		// Show new screen (if any)
-		if(newScreen != Screen.NONE) {
-			m_screens[(int)newScreen].Show(dir);
-		}
-
-		// Update screen tracking
-		m_currentScreen = newScreen;
-	}
-
-	//------------------------------------------------------------------//
 	// CALLBACKS														//
 	//------------------------------------------------------------------//
 	/// <summary>
 	/// Play button has been pressed.
 	/// </summary>
 	public void OnPlayButton() {
-		// Hide current screen
-		GoToScreen((int)Screen.NONE);
-
 		// Go to game!
 		// [AOC] No need to block the button, the GameFlow already controls spamming
 		FlowManager.GoToGame();
+	}
+
+	/// <summary>
+	/// Reset persistence button. Debug purposes only.
+	/// </summary>
+	public void OnResetButton() {
+		PersistenceManager.Clear();
+		FlowManager.Restart();
 	}
 }
 
