929e88cb2bd78bcf7919ae8e9c78b512aa233828
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2015-12-18 12:33:32 +0100

    Automatic generation WIP
    Former-commit-id: 2f19f95bf2e3ed6a901fd2051037ef54e12e0c71

---
+++ generate_build.sh
@@ -0,0 +1,135 @@
+#!/bin/Bash
+# This scripts generates xcode_stage and xcode_prod projects and generates the ipa files
+
+BRANCH="release/0.28"
+BUILD_ANDROID=false
+BUILD_IOS=true
+INCREASE_VERSION_NUMBER=true
+CREATE_TAG=true
+GAME_NAME="Dragon"
+
+USAGE="usage: generate_build.sh [-b branch_name] [-android true|false] [-ios true|false] [-increase_version true|false] [-tag true|false]"
+
+for ((i=1;i<=$#;i++)); 
+do
+    PARAM_NAME=${!i}
+    if [ "$PARAM_NAME" == "--help" ] ; then
+        echo $USAGE
+        exit 1
+    elif [ "$PARAM_NAME" == "-b" ] ; then
+        ((i++))
+        echo ${i}
+        BRANCH=${!i}
+    elif [ "$PARAM_NAME" == "-android" ]; then
+        ((i++))
+        BUILD_ANDROID=${!i}
+    elif [ "$PARAM_NAME" == "-ios" ]; then
+        ((i++))
+        BUILD_IOS=${!i}
+    elif [ "$PARAM_NAME" == "-increase_version" ]; then
+        ((i++))
+        INCREASE_VERSION_NUMBER=${!i}
+    elif [ "$PARAM_NAME" == "-tag" ]; then
+        ((i++))
+        CREATE_TAG=${!i}
+    else 
+        echo "Unknown parameter ${PARAM_NAME}"
+        echo $USAGE
+        exit 1
+    fi
+done;
+
+
+SCRIPT_PATH=$(pwd)/"$(dirname $0)"
+echo $SCRIPT_PATH
+cd ${SCRIPT_PATH}
+
+# UPDATE GIT
+# Revert changes to modified files.
+git reset --hard
+# Remove all untracked files and directories.
+git clean -fdx
+# Chante branch
+git fetch
+git checkout $BRANCH
+success=$?
+if [[ $success -ne 0 ]]; then
+    echo "Something went wrong. Exiting"
+    exit 1
+fi
+#update branch
+git pull origin $BRANCH
+
+if $INCREASE_VERSION_NUMBER; then
+echo "Increasing version numbers"
+#Increase Version Number
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -executeMethod Builder.IncreasePatchVersionNumber -projectPath $SCRIPT_PATH -quit -buildTarget ios
+#Increase Android Version Code
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -executeMethod Builder.IncreaseAndroidVersionCode -projectPath $SCRIPT_PATH -quit -buildTarget ios
+fi
+
+if $BUILD_ANDROID; then
+    #GENERATE APKS
+    echo "Generating APKs"
+    rm "${SCRIPT_PATH}/ss*.apk"    # just in case
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -executeMethod Builder.GenerateAPK -projectPath $SCRIPT_PATH -quit -buildTarget android
+fi
+
+if $BUILD_IOS; then
+    #GENERATE XCODE PROJECTS
+    echo "Generating xCode Projects"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -executeMethod Builder.GenerateXcode -projectPath $SCRIPT_PATH -quit -buildTarget ios
+
+    # GENERATE ARCHIVES AND IPAS
+    PROVISIONING_PROFILE="XC Ad Hoc: com.ubisoft.sandstormdev"
+    PROVISIONING_PROFILE_UUID="8370fe09-5b48-4e0c-9d91-b32a1ff8b26e"
+    SIGNING_ID="iPhone Distribution: Marie Cordon (Y3J3C97LQ8)" # NOT WORKING!!
+    mkdir "${SCRIPT_PATH}/archives/"
+    mkdir "${SCRIPT_PATH}/ipas/"
+
+    #STAGE
+    echo "Archiving"
+    BUNDLE_ID=$(/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" "$SCRIPT_PATH/xcode/Info.plist")
+    ARCHIVE_FILE="${GAME_NAME}_${BUNDLE_ID}.xcarchive"
+    STAGE_IPA_FILE="${GAME_NAME}_${BUNDLE_ID}.ipa"
+    PROJECT_NAME="${SCRIPT_PATH}/xcode/Unity-iPhone.xcodeproj"
+
+    xcodebuild clean -project $PROJECT_NAME -configuration Release -alltargets 
+    xcodebuild archive -project $PROJECT_NAME -configuration Release -scheme "Unity-iPhone" -archivePath "${SCRIPT_PATH}/archives/${ARCHIVE_FILE}" PROVISIONING_PROFILE="${PROVISIONING_PROFILE_UUID}"
+    rm "${SCRIPT_PATH}/ipas/${STAGE_IPA_FILE}"    # just in case
+    xcodebuild -exportArchive -exportFormat IPA -archivePath "${SCRIPT_PATH}/archives/${ARCHIVE_FILE}" -exportPath "${SCRIPT_PATH}/ipas/${STAGE_IPA_FILE}"  -exportProvisioningProfile "${PROVISIONING_PROFILE}" #-exportSigningIdentity "${SIGNING_ID}"
+
+fi
+
+# commit project changes
+echo "Committing changes"
+git add "${SCRIPT_PATH}/Assets/Resources/AndroidVersionCode.txt"
+git add "${SCRIPT_PATH}/Assets/Resources/version.txt"
+git add "${SCRIPT_PATH}/ProjectSettings/ProjectSettings.asset"
+git commit -m "Version Changed To ${BUNDLE_ID} and Android Version Code Increased"
+git push origin ${BRANCH}
+
+if $CREATE_TAG; then
+    # GENERATE TAG
+    git tag $BUNDLE_ID
+    # Share tag
+    git push origin $BUNDLE_ID
+fi
+
+# SEND TO SAMBA SERVER
+SMB_USER="srv_acc_bcn_jenkins"
+SMB_PASS="Lm0%2956jkR%23Tg"
+
+echo "Sending To Server"
+mkdir server
+mount -t smbfs "//${SMB_USER}:${SMB_PASS}@ubisoft.org/BCNStudio/QA/SS" server
+
+if $BUILD_IOS; then
+    cp "${SCRIPT_PATH}/ipas/${STAGE_IPA_FILE}" "server/"
+    cp "${SCRIPT_PATH}/ipas/${PROD_IPA_FILE}" "server/"
+fi
+if $BUILD_ANDROID; then
+    cp "${SCRIPT_PATH}/ss*.apk" "server/"
+fi
+umount server
+rmdir server
