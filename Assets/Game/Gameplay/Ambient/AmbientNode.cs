477fb1c1a21abc113b24691d40685099ed15e760
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2016-01-13 10:59:58 +0100

    Ambient Manager
    Former-commit-id: ac2519e20855d493df7f4a576fb1fdf427b3a040

---
+++ Assets/Game/Gameplay/Ambient/AmbientNode.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientNode : MonoBehaviour 
+{
+	public Color m_ambientColor = Color.white;
+
+	[Range (0,8)]
+	public float m_ambientIntensity;
+
+	[Range (0,1)]
+	public float m_sunSize;
+
+	[Range (0,5)]
+	public float m_atmosphereThickness;
+
+	public Color m_skyTint = Color.white;
+	public Color m_ground = Color.white;
+
+	[Range (0,8)]
+	public float m_exposure;
+
+	void OnDrawGizmos() 
+	{
+		Gizmos.color = new Color(0.09f, 0.69f, 0.12f, 0.5f);
+		Gizmos.DrawSphere(transform.position, 0.5f * transform.localScale.x);
+	}
+
+}
