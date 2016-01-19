7655cb232da7014cf6bad558c42a783dc593bcf6
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2016-01-19 15:07:37 +0100

    rain controller, position
    Former-commit-id: e383525b3fbc25bb53535bad313a58d52872326d

--- Assets/Game/Gameplay/Ambient/RainController.cs
+++ Assets/Game/Gameplay/Ambient/RainController.cs
@@ -3,28 +3,38 @@ using System.Collections;
 
 public class RainController : MonoBehaviour 
 {
+	public float m_rainLocalMove = 2;
 	ParticleSystem m_rainParticle;
-
-
+	DragonMotion m_dragonMotion;
+	Transform m_transform;
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+		m_dragonMotion = InstanceManager.player.GetComponent<DragonMotion>();
+		m_transform = transform;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		Vector3 vel = m_dragonMotion.GetVelocity();
+		Vector3 local = m_transform.localPosition;
+		if ( vel.x  > 0 )
+		{
+			local.x = m_rainLocalMove;
+		}
+		else if ( vel.x < 0 )
+		{
+			local.x = -m_rainLocalMove;
+		}
+		else
+		{
+			local.x = 0;
+		}
+
+
+		m_transform.localPosition = local;
 	}
 
-	void OnParticleCollision(GameObject other) 
-	{
-		Debug.Log("COLLISION!!");
-		/*
-		ParticleSystem particleSystem = other.GetComponent<ParticleSystem>();
- ParticleSystem.CollisionEvent[] collisions = new ParticleSystem.CollisionEvent[particleSystem.safeCollisionEventSize];
- int numberOfCollisions = particleSystem.GetCollisionEvents(this.gameObject, collisions);
- */
-	}
 }
