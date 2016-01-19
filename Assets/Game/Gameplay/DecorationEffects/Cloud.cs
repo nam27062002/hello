67f2d00eaacf94095a8e8cbf745f7a6315f10e4a
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2016-01-19 17:44:17 +0100

    Cloud on exit particle
    Former-commit-id: d01320c6fc0de08b6616047e6266f5acb2364ec1

---
+++ Assets/Game/Gameplay/DecorationEffects/Cloud.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cloud : MonoBehaviour {
+
+	void OnTriggerExit(Collider other) 
+	{
+		DragonPlayer player = other.GetComponent<DragonPlayer>();
+		if ( player != null && other is SphereCollider)
+		{
+			GameObject go = ParticleManager.Spawn("CloudSmoke", player.transform.position);
+			go.transform.rotation = player.transform.rotation;
+			go.transform.Rotate( -90, 0, 0, Space.Self);
+		}
+	}
+
+}
