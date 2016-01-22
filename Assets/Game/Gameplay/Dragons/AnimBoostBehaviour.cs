ca164378bde4db8e41c6d39cf106446c7479170a
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2016-01-22 09:31:02 +0100

    Fix trails start ends transferred to code and fixed
    Former-commit-id: 3b249a62fc9b34589edbcd67ac33fc049d5e8fad

---
+++ Assets/Game/Gameplay/Dragons/AnimBoostBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimBoostBehaviour : StateMachineBehaviour 
+{
+	int loopHash = Animator.StringToHash("Loop");
+
+	void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+	{
+		if (animatorStateInfo.shortNameHash == loopHash)
+		{
+			animator.gameObject.SendMessage("TurboLoopStart");			
+		}
+	}
+
+	void OnStateExit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+	{
+		if (animatorStateInfo.shortNameHash == loopHash)
+		{
+			animator.gameObject.SendMessage("TurboLoopEnd");
+		}
+	}
+}
