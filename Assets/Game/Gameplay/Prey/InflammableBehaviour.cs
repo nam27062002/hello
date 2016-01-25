10aab4517e3a5af17074b63147e809a4c3bdd4b2
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2016-01-25 16:01:20 +0100

    burning motion
    Former-commit-id: 01dd1f05f69eee41e4b4ceb6ea4d9623aa999b50

--- Assets/Game/Gameplay/Prey/InflammableBehaviour.cs
+++ Assets/Game/Gameplay/Prey/InflammableBehaviour.cs
@@ -137,8 +137,10 @@ public class InflammableBehaviour : Initializable {
 				}
 
 				PreyMotion motion = GetComponent<PreyMotion>();
-				if (motion != null) {
-					motion.enabled = false;
+				if (motion != null) 
+				{
+					motion.StartBurning();
+					// motion.enabled = false;
 				}
 
 				m_ashMaterial.SetFloat("_AshLevel", 0);
