6b055d7598281f89bb4880308f07c9d45233bec5
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2016-01-27 11:35:50 +0100

    Transform Extensions: Development - Added extensions to directly set local X, Y, Z of a Transform object. Tweeners Test: Development - Created preview of all ease types and amplitude/period/overshoot parameters. Hungry Dragon Editor Menu: Development - Added shortcut to Tweeners Test scene.
    Former-commit-id: b1ac34cf4e24d7077ba2516ed08c11f42f8f1002

--- Assets/Tests/TestAlger/AOCQuickTest.cs
+++ Assets/Tests/TestAlger/AOCQuickTest.cs
@@ -11,6 +11,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 
 //----------------------------------------------------------------------//
@@ -23,10 +24,19 @@ public class AOCQuickTest : MonoBehaviour {
 	//------------------------------------------------------------------//
 	// CONSTANTS														//
 	//------------------------------------------------------------------//
+	[Serializable]
+	public class EaseCurve {
+		public Ease easeType;
+		public AnimationCurve curve = new AnimationCurve();
+	}
 
 	//------------------------------------------------------------------//
 	// MEMBERS															//
 	//------------------------------------------------------------------//
+	public List<EaseCurve> m_curves = new List<EaseCurve>();
+	public float m_overshoot = 1.7f;
+	public float m_amplitude = 1f;
+	public float m_period = 1f;
 
 	//------------------------------------------------------------------//
 	// PROPERTIES														//
@@ -35,7 +45,6 @@ public class AOCQuickTest : MonoBehaviour {
 	//------------------------------------------------------------------//
 	// GENERIC METHODS													//
 	//------------------------------------------------------------------//
-
 	/// <summary>
 	/// Initialization.
 	/// </summary>
@@ -61,7 +70,41 @@ public class AOCQuickTest : MonoBehaviour {
 	/// Multi-purpose callback.
 	/// </summary>
 	public void OnClick() {
-		//DOTween.Restart("fireRushIn");
-		DOTween.Restart("levelUpIn");
+		/*float duration = 1f;
+		float offsetX = 1000f;
+		for(int i = 0; i < 5; i++) {
+			GameObject go = GameObject.Find("Test" + i);
+			DOTween.Kill(go);
+			go.transform.SetLocalPosX(0f);
+			//go.transform.DOLocalMoveX(offsetX, duration).SetEase(Ease.OutElastic, (float)(i + 1));
+			//go.transform.DOLocalMoveX(offsetX, duration).SetEase(Ease.OutElastic, (float)(i + 1), (float)(i + 1));
+			//go.transform.DOLocalMoveX(offsetX, duration).SetEase(Ease.OutElastic, (float)(i + 1), 1f);
+			go.transform.DOLocalMoveX(offsetX, duration).SetEase(Ease.OutQuart, 1f, (float)(i + 1) * 10f);
+		}*/
+
+		int samples = 100;
+		m_curves = new List<EaseCurve>();
+		int easeCount = Enum.GetNames(typeof(Ease)).Length;
+		for(int i = 0; i < easeCount; i++) {
+			EaseCurve curve;
+			if(i < m_curves.Count) {
+				curve = m_curves[i];
+				curve.curve.keys = new Keyframe[0];
+			} else {
+				curve = new EaseCurve();
+				curve.easeType = (Ease)i;
+				m_curves.Add(curve);
+			}
+
+			float value = 0f;
+			//Tweener t = DOTween.To(x => value = x, 0f, 1f, 1f).SetEase(curve.easeType, m_overshoot);
+			Tweener t = DOTween.To(x => value = x, 0f, 1f, 1f).SetEase(curve.easeType, m_amplitude, m_period);
+			for(int j = 0; j < samples; j++) {
+				float delta = (float)j/(float)samples;
+				t.Goto(delta);
+				curve.curve.AddKey(new Keyframe(delta, value));
+			}
+			DOTween.Kill(value);
+		}
 	}
 }
--- Assets/Tests/TestAlger/Editor/AOCQuickTestEditor.cs
+++ Assets/Tests/TestAlger/Editor/AOCQuickTestEditor.cs
@@ -23,6 +23,8 @@ public class AOCQuickTestEditor : Editor {
 	//------------------------------------------------------------------//
 	// MEMBERS AND PROPERTIES											//
 	//------------------------------------------------------------------//
+	private AOCQuickTest targetTest { get { return target as AOCQuickTest; }}
+	public static float m_curvePreviewSize = 100f;
 
 	//------------------------------------------------------------------//
 	// METHODS															//
@@ -31,14 +33,63 @@ public class AOCQuickTestEditor : Editor {
 	/// 
 	/// </summary>
 	public override void OnInspectorGUI() {
-		// Default
-		DrawDefaultInspector();
+		GUI.changed = false;
+
+		// Params
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("m_overshoot"));
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("m_amplitude"));
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("m_period"));
+
+		// Curve preview size
+		m_curvePreviewSize = EditorGUILayout.FloatField("Curve Preview Size", m_curvePreviewSize);
+		if(m_curvePreviewSize < 1f) m_curvePreviewSize = 1f;
+
+		if(GUI.changed) {
+			serializedObject.ApplyModifiedProperties();
+			targetTest.OnClick();
+		}
 
 		EditorGUILayoutExt.Separator();
 
 		// Test button
 		if(GUILayout.Button("TEST", GUILayout.Height(50))) {
-			
+			targetTest.OnClick();
 		}
+
+		EditorGUILayoutExt.Separator();
+
+		// Curves
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("m_curves"), true);
+
+		// Default
+		//DrawDefaultInspector();
+	}
+
+	//------------------------------------------------------------------//
+	// MENU ENTRIES														//
+	//------------------------------------------------------------------//
+	[MenuItem("Hungry Dragon/AOC/Scene Tweeners", false, 101)]
+	public static void OpenScene1() { HungryDragonEditorMenu.OpenScene("Assets/Tests/TestAlger/Tweeners/TweenersTest.unity"); }
+}
+
+/// <summary>
+/// AOC quick test ease curve editor.
+/// </summary>
+[CustomPropertyDrawer(typeof(AOCQuickTest.EaseCurve))]
+public class AOCQuickTestEaseCurveEditor : ExtendedPropertyDrawer {
+	/// <summary>
+	/// Raises the GUI impl event.
+	/// </summary>
+	/// <param name="_property">Property.</param>
+	/// <param name="_label">Label.</param>
+	protected override void OnGUIImpl(SerializedProperty _property, GUIContent _label) {
+		// Get properties
+		SerializedProperty easeTypeProp = _property.FindPropertyRelative("easeType");
+		SerializedProperty curveProp = _property.FindPropertyRelative("curve");
+
+		// Draw curve - don't assign, we don't want to overwrite values
+		m_pos.height = AOCQuickTestEditor.m_curvePreviewSize;
+		EditorGUI.CurveField(m_pos, easeTypeProp.enumNames[easeTypeProp.enumValueIndex], curveProp.animationCurveValue);
+		AdvancePos();
 	}
 }
