94751f8db9f692b462f201d7ce483a130a86149c
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2016-02-03 15:19:13 +0100

    dragon grab
    Former-commit-id: 000123932e35977fe104d9e6f8b6bad5d12cebfd

---
+++ Assets/Game/Gameplay/Dragons/DragonGrab.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonGrab : MonoBehaviour 
+{
+
+	GrabBehaviour m_grabbed;
+	Transform m_parentTransform;
+	float m_grabbingTimer;
+
+	Transform m_claws = null;
+
+	// Drop if hit, or too high, or to much time, or in water
+	// Once dropped whould we use phisics? does it has to explode always?
+
+	void Awake()
+	{
+		m_claws = transform.FindTransformRecursive("Dragon_RLeg_4");
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_grabbed = null;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( m_grabbed != null )
+		{
+			float lerp = Time.deltaTime * 5;
+			m_grabbed.transform.position = Vector3.Lerp( m_grabbed.transform.position, m_claws.position, lerp );
+			m_grabbed.transform.rotation = Quaternion.Lerp (m_grabbed.transform.rotation,m_claws.rotation*Quaternion.AngleAxis(180,Vector3.right),lerp);
+
+			m_grabbingTimer += Time.deltaTime;
+
+			RaycastHit ground;
+			float flyHeight = 1000;
+			if (Physics.Linecast(transform.position, transform.position + Vector3.down * 10000f, out ground, 1 << LayerMask.NameToLayer("Ground"))) {
+
+				flyHeight =  transform.position.y - ground.point.y;
+			}
+
+			if ( m_grabbingTimer > 2 || flyHeight > 100 )	// or is x high
+			{
+				Drop();
+			}
+		}
+	}
+
+	public Transform GetClaws()
+	{
+		return m_claws;
+	}
+
+	public bool Grab( GrabBehaviour element )
+	{
+		if ( m_grabbed == null )
+		{
+			m_grabbed = element;
+			m_grabbingTimer = 0;
+
+			m_parentTransform = element.transform.parent;
+			element.transform.parent = m_claws;
+			// Animate claws
+
+			return true;	
+		}
+		return false;
+	}
+
+	public void Drop()
+	{
+		m_grabbed.transform.parent = m_parentTransform;
+		m_grabbed.OnDrop();
+
+		// release claws
+
+		m_grabbed = null;
+		m_parentTransform = null;
+	}
+
+	public bool IsGrabbing()
+	{
+		return m_grabbed != null;
+	}
+}
---
+++ Assets/Game/Gameplay/Prey/GrabBehaviour.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabBehaviour : MonoBehaviour 
+{
+	private DragonMotion m_dragon;
+	private Transform m_dragonClaws;
+	private CircleArea2D m_bounds;
+	private DragonGrab m_dragonGrab;
+	private Rigidbody m_rigidbody;
+
+	float m_sleepMinTimer;
+
+	enum State
+	{
+		READY,
+		GRABBED,
+		FALLING,
+		EXPLODING,
+		WAITING_TO_RESPAWN
+	};
+	private State m_state;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_bounds = GetComponent<CircleArea2D>();
+		m_dragon = InstanceManager.player.GetComponent<DragonMotion>();
+		m_dragonGrab = m_dragon.GetComponent<DragonGrab>();
+		m_dragonClaws = m_dragonGrab.GetClaws();
+		m_state = State.READY;
+		m_rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		switch( m_state )
+		{
+			case State.READY:
+			{
+				if ( !m_dragonGrab.IsGrabbing() )
+				{
+					
+					float distanceSqr = 100000;
+					if (m_bounds != null)
+					 	distanceSqr = m_bounds.DistanceSqr( m_dragonClaws.position );
+					 else
+						distanceSqr = (m_dragonClaws.position - transform.position).sqrMagnitude;
+
+					if (distanceSqr <= 50) 
+					{
+						bool grabbed = m_dragonGrab.Grab( this );
+						if ( grabbed )
+						{
+							ChangeState( State.GRABBED );
+						}
+					}
+				}
+			}break;
+			case State.FALLING:
+			{
+				if (m_rigidbody != null)
+				{
+					if (m_rigidbody.IsSleeping() && m_sleepMinTimer <= 0 )
+						ChangeState(State.READY);
+					m_sleepMinTimer -= Time.deltaTime;
+				}
+			}break;
+		}
+	}
+
+	private void ChangeState( State newState )
+	{
+		if ( newState != m_state )
+		{
+			/*
+			switch( m_state )
+			{
+
+			}
+			*/
+			m_state = newState;
+			switch( m_state )
+			{
+				case State.GRABBED:
+				{
+					if ( m_rigidbody != null )
+					{
+						m_rigidbody.isKinematic = true;
+					}
+				}break;
+				case State.FALLING:
+				{
+					// Activate rigidbody and collider /  Activate gravity?
+					if ( m_rigidbody != null )
+					{
+						m_rigidbody.isKinematic = false;
+					}
+						
+					m_sleepMinTimer = 1;
+				}break;
+			}		
+		}
+	}
+
+	public void OnDrop()
+	{
+		ChangeState( State.FALLING );
+	}
+}
