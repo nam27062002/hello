363f38c5909fab8eb9e74dab10e28434d887a5bc
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2016-02-05 09:35:45 +0100

    Level Editor: Development - Added primitive collision shapes circle and triangle. Ease Curve Test: Development - Separated from AOCQuickTest.
    Former-commit-id: f91ce8870b6cc8af66b31012749bca6a7ac3cc4c

---
+++ Assets/Tests/TestAlger/Tweeners/Editor/EaseCurveTest.cs
@@ -0,0 +1,95 @@
+﻿// EaseCurveTestEditor.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 02/10/2015.
+// Copyright (c) 2015 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------//
+// INCLUDES																//
+//----------------------------------------------------------------------//
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections.Generic;
+
+//----------------------------------------------------------------------//
+// CLASSES																//
+//----------------------------------------------------------------------//
+/// <summary>
+/// 
+/// </summary>
+[CustomEditor(typeof(EaseCurveTest))]
+public class EaseCurveTestEditor : Editor {
+	//------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES											//
+	//------------------------------------------------------------------//
+	private EaseCurveTest targetTest { get { return target as EaseCurveTest; }}
+	public static float m_curvePreviewSize = 100f;
+
+	//------------------------------------------------------------------//
+	// METHODS															//
+	//------------------------------------------------------------------//
+	/// <summary>
+	/// 
+	/// </summary>
+	public override void OnInspectorGUI() {
+		GUI.changed = false;
+
+		// Params
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("m_overshoot"));
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("m_amplitude"));
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("m_period"));
+
+		// Curve preview size
+		m_curvePreviewSize = EditorGUILayout.FloatField("Curve Preview Size", m_curvePreviewSize);
+		if(m_curvePreviewSize < 1f) m_curvePreviewSize = 1f;
+
+		if(GUI.changed) {
+			serializedObject.ApplyModifiedProperties();
+			targetTest.OnClick();
+		}
+
+		EditorGUILayoutExt.Separator();
+
+		// Test button
+		if(GUILayout.Button("TEST", GUILayout.Height(50))) {
+			targetTest.OnClick();
+		}
+
+		EditorGUILayoutExt.Separator();
+
+		// Curves
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("m_curves"), true);
+
+		// Default
+		//DrawDefaultInspector();
+	}
+
+	//------------------------------------------------------------------//
+	// MENU ENTRIES														//
+	//------------------------------------------------------------------//
+	[MenuItem("Hungry Dragon/AOC/Scene Tweeners", false, 101)]
+	public static void OpenScene1() { HungryDragonEditorMenu.OpenScene("Assets/Tests/TestAlger/Tweeners/TweenersTest.unity"); }
+}
+
+/// <summary>
+/// AOC quick test ease curve editor.
+/// </summary>
+[CustomPropertyDrawer(typeof(EaseCurveTest.EaseCurve))]
+public class EaseCurveTestEaseCurveEditor : ExtendedPropertyDrawer {
+	/// <summary>
+	/// Raises the GUI impl event.
+	/// </summary>
+	/// <param name="_property">Property.</param>
+	/// <param name="_label">Label.</param>
+	protected override void OnGUIImpl(SerializedProperty _property, GUIContent _label) {
+		// Get properties
+		SerializedProperty easeTypeProp = _property.FindPropertyRelative("easeType");
+		SerializedProperty curveProp = _property.FindPropertyRelative("curve");
+
+		// Draw curve - don't assign, we don't want to overwrite values
+		m_pos.height = EaseCurveTestEditor.m_curvePreviewSize;
+		EditorGUI.CurveField(m_pos, easeTypeProp.enumNames[easeTypeProp.enumValueIndex], curveProp.animationCurveValue);
+		AdvancePos();
+	}
+}
