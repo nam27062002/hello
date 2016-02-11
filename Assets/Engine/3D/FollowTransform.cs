025787eb5474e82b3646e94b34c2354ef29bc907
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2016-02-11 17:36:25 +0100

    Audio Management
    Former-commit-id: 1b90ef1c919ad4ac6bf427cf5483df54cc2f6ffd

---
+++ Assets/Engine/3D/FollowTransform.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTransform : MonoBehaviour 
+{
+	public Vector3 m_offset;
+	public Transform m_follow;
+	private Transform m_transform;
+
+	void Start()
+	{
+		m_transform = transform;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		if (m_follow != null)
+			m_transform.position = m_follow.position + m_offset;
+	}
+}
