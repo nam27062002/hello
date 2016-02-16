84af602a11e7e3318652478e4d415c09ca23d124
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2016-02-16 11:45:46 +0100

    Probability Set: Development - Full rework, moved the logic to the actual class rather than the editor script. All functionality can be run from code as well as inspector now. Chest Manager: Development - Re-saved asset with new probability set structure.
    Former-commit-id: 0235c0d54421b1ec33e295d19c00913225fc14fb

--- Assets/Engine/Generic/Editor/ProbabilitySetEditor.cs
+++ Assets/Engine/Generic/Editor/ProbabilitySetEditor.cs
@@ -24,7 +24,9 @@ public class ProbabilitySetEditor : ExtendedPropertyDrawer {
 	//------------------------------------------------------------------//
 	// MEMBERS															//
 	//------------------------------------------------------------------//
-	SerializedProperty m_elementsProp = null;	// To keep the reference between methods
+	// References to keep between methods
+	// Will be null outside the OnGUIImpl call
+	ProbabilitySet m_target = null;
 
 	//------------------------------------------------------------------//
 	// GENERIC METHODS													//
@@ -46,6 +48,9 @@ public class ProbabilitySetEditor : ExtendedPropertyDrawer {
 	/// <param name="_property">The property we're drawing.</param>
 	/// <param name="_label">The label of the property.</param>
 	override protected void OnGUIImpl(SerializedProperty _property, GUIContent _label) {
+		// Register undo
+		Undo.RecordObject(_property.serializedObject.targetObject, "Probability Set");
+
 		// Draw property label + foldout widget
 		m_pos.height = EditorStyles.largeLabel.lineHeight;	// Advance pointer just the size of the label
 		_property.isExpanded = EditorGUI.Foldout(m_pos, _property.isExpanded, _label);
@@ -56,28 +61,27 @@ public class ProbabilitySetEditor : ExtendedPropertyDrawer {
 			// Indent in
 			EditorGUI.indentLevel++;
 
-			// Get the values and labels arrays
-			m_elementsProp = _property.FindPropertyRelative("m_elements");
+			// Get the reference to both the set and the elements serialized property
+			m_target = fieldInfo.GetValue(_property.serializedObject.targetObject) as ProbabilitySet;	// [AOC] Small trick from http://answers.unity3d.com/questions/425012/get-the-instance-the-serializedproperty-belongs-to.html
 
 			// Elements
 			DoElements();
 
-			// Spacing
-			AdvancePos(5f);
-
 			// Add/Remove buttons
+			AdvancePos(5f);		// Spacing
 			DoAddRemoveButtons();
 
-			// Spacing
-			AdvancePos(5f);
-
 			// Reset Buttons
+			AdvancePos(5f);		// Spacing
 			DoResetButtons();
 
 			// Indent back out and make sure gui is enabled
 			EditorGUI.indentLevel--;
 			GUI.enabled = true;
 		}
+
+		// Make sure all changes are applied
+		_property.serializedObject.ApplyModifiedProperties();
 	}
 
 	/// <summary>
@@ -98,20 +102,22 @@ public class ProbabilitySetEditor : ExtendedPropertyDrawer {
 	/// </summary>
 	private void DoElements() {
 		// Draw line by line
-		for(int i = 0; i < m_elementsProp.arraySize; i++) {
+		bool locked;
+		string label;
+		float probability;
+		for(int i = 0; i < m_target.numElements; i++) {
 			// Aux
-			SerializedProperty currentElementProp = m_elementsProp.GetArrayElementAtIndex(i);
 			m_pos.height = EditorStyles.numberField.lineHeight + EditorStyles.numberField.margin.vertical;
 			Rect pos = new Rect(m_pos);
 
 			// Lock?
-			// [AOC] We will use the isExpanded property to store the status (so dirty xD)
-			SerializedProperty lockedProp = currentElementProp.FindPropertyRelative("locked");
 			GUI.enabled = true;
 			pos.width = 40;
-			lockedProp.boolValue = !EditorGUI.Toggle(pos, !lockedProp.boolValue);	// [AOC] A little weird: what actually makes sense is that the toggle is marked when the element is not locked
+			EditorGUI.BeginChangeCheck();
+			locked = !EditorGUI.Toggle(pos, !m_target.IsLocked(i));	// [AOC] A little weird: what actually makes sense is that the toggle is marked when the element is not locked
+			if(EditorGUI.EndChangeCheck()) m_target.SetLocked(i, locked);
 			pos.x += pos.width;
-			GUI.enabled = !lockedProp.boolValue;
+			GUI.enabled = !locked;
 
 			// [AOC] Temporarily remove indentation so following controls are next to each other
 			int indentBackup = EditorGUI.indentLevel;
@@ -120,18 +126,16 @@ public class ProbabilitySetEditor : ExtendedPropertyDrawer {
 			// Label - Editable
 			// [AOC] Draw a textfield using the label style
 			pos.width = 120;
-			SerializedProperty labelProp = currentElementProp.FindPropertyRelative("label");
-			labelProp.stringValue = EditorGUI.TextField(pos, labelProp.stringValue, EditorStyles.label);
+			EditorGUI.BeginChangeCheck();
+			label = EditorGUI.TextField(pos, m_target.GetLabel(i), EditorStyles.label);
+			if(EditorGUI.EndChangeCheck()) m_target.SetLabel(i, label);
 			pos.x += pos.width;
 
 			// Use a slider to draw it and store new value
-			EditorGUI.BeginChangeCheck();
 			pos.width = m_pos.width - (pos.x - m_pos.x);	// [AOC] Fill the rest of the space (Original reserved width minus the advance of the cursor from the original cursor)
-			currentElementProp.FindPropertyRelative("value").floatValue = EditorGUI.Slider(pos, currentElementProp.FindPropertyRelative("value").floatValue, 0f, 1f);
-			if(EditorGUI.EndChangeCheck()) {
-				// If the value has changed, redistribute probabilities
-				Redistribute(i);
-			}
+			EditorGUI.BeginChangeCheck();
+			probability = EditorGUI.Slider(pos, m_target.GetProbability(i), 0f, 1f);
+			if(EditorGUI.EndChangeCheck()) m_target.SetProbability(i, probability);
 
 			// Move to next line for the next element
 			AdvancePos();
@@ -152,19 +156,7 @@ public class ProbabilitySetEditor : ExtendedPropertyDrawer {
 
 		// Reset button - don't respect locks
 		if(GUI.Button(pos, "Add Element")) {
-			// This will add a new element at the end cloning the last element on the list
-			m_elementsProp.arraySize++;
-
-			// Set initial values
-			// Give it 0 value to avoid affecting the global balance, unless there's only one element, in which case we'll give it value 1
-			SerializedProperty newElementProp = m_elementsProp.GetArrayElementAtIndex(m_elementsProp.arraySize - 1);
-			if(m_elementsProp.arraySize == 1) {
-				newElementProp.FindPropertyRelative("value").floatValue = 1f;
-			} else {
-				newElementProp.FindPropertyRelative("value").floatValue = 0f;
-			}
-			newElementProp.FindPropertyRelative("label").stringValue = "NewElement";
-			newElementProp.FindPropertyRelative("locked").boolValue = false;
+			m_target.AddElement("Element " + (m_target.numElements + 1).ToString());
 		}
 
 		// Move cursor
@@ -172,17 +164,9 @@ public class ProbabilitySetEditor : ExtendedPropertyDrawer {
 
 		// Remove element button
 		// Disable if there are no elements to remove
-		GUI.enabled = m_elementsProp.arraySize > 0;
+		GUI.enabled = m_target.numElements > 0;
 		if(GUI.Button(pos, "Remove Element")) {
-			// Before removing, redistribute values simulating that the element we will remove goes to 0
-			SerializedProperty lastElement = m_elementsProp.GetArrayElementAtIndex(m_elementsProp.arraySize - 1);
-			lastElement.FindPropertyRelative("value").floatValue = 0f;
-			Redistribute(m_elementsProp.arraySize - 1, true);
-
-			// Just remove last element (if enough elements)
-			if(m_elementsProp.arraySize > 0) {
-				m_elementsProp.arraySize--;
-			}
+			m_target.RemoveElement();
 		}
 		GUI.enabled = true;
 
@@ -201,11 +185,7 @@ public class ProbabilitySetEditor : ExtendedPropertyDrawer {
 
 		// Reset button - don't respect locks
 		if(GUI.Button(pos, "Reset")) {
-			for(int i = 0; i < m_elementsProp.arraySize; i++) {
-				SerializedProperty currentElementProp = m_elementsProp.GetArrayElementAtIndex(i);
-				currentElementProp.FindPropertyRelative("value").floatValue = 1f/(float)m_elementsProp.arraySize;
-				currentElementProp.FindPropertyRelative("locked").boolValue = false;
-			}
+			m_target.Reset(false);
 		}
 
 		// Move cursor
@@ -213,113 +193,10 @@ public class ProbabilitySetEditor : ExtendedPropertyDrawer {
 
 		// Reset button - respect locks
 		if(GUI.Button(pos, "Reset (respect locks)")) {
-			// Count non-locked elements
-			float nonLockedCount = 0f;
-			float toDistribute = 1f;
-			for(int i = 0; i < m_elementsProp.arraySize; i++) {
-				SerializedProperty currentElementProp = m_elementsProp.GetArrayElementAtIndex(i);
-				if(!currentElementProp.FindPropertyRelative("locked").boolValue) {
-					nonLockedCount++;
-				} else {
-					toDistribute -= currentElementProp.FindPropertyRelative("value").floatValue;
-				}
-			}
-
-			// Distribute uniformly among non-locked elements
-			for(int i = 0; i < m_elementsProp.arraySize; i++) {
-				SerializedProperty currentElementProp = m_elementsProp.GetArrayElementAtIndex(i);
-				if(!currentElementProp.FindPropertyRelative("locked").boolValue) {
-					currentElementProp.FindPropertyRelative("value").floatValue = toDistribute/nonLockedCount;
-				}
-			}
+			m_target.Reset(true);
 		}
 
 		// Move to next line
 		AdvancePos();
-	}
-
-	//------------------------------------------------------------------//
-	// INTERNAL AUX														//
-	//------------------------------------------------------------------//
-	/// <summary>
-	/// Re-distribute probability within all elements.
-	/// </summary>
-	/// <param name="_changedIdx">The index of the element that has been changed.</param>
-	/// <param name="_overrideLocks">If set to <c>true</c>, locked sliders will be modified too.</param> 
-	private void Redistribute(int _changedIdx, bool _overrideLocks = false) {
-		// [AOC] From HumbleBundle's web page code:
-		// Calculate the splits for all the siblings.
-		// Conceptually, we remove the active slider from the mix. Then we normalize the siblings to 1 to
-		// determine their weights relative to each other. Then we divide the split that is left over from the moved
-		// slider with these relative weights.
-
-		// Get changed element
-		SerializedProperty changedElementProp = m_elementsProp.GetArrayElementAtIndex(_changedIdx);
-
-		// Amount to distribute between the siblings
-		float totalToDistribute = (1f - changedElementProp.FindPropertyRelative("value").floatValue);
-
-		// Compute total amount of unlocked siblings to distribute proportionally
-		// Adjust amount to distribute by ignoring locked sliders
-		float unlockedSiblingsTotal = 0f;
-		float unlockedSiblingsCount = 0f;	// [AOC] Use float directly to avoid casting later on
-		for(int j = 0; j < m_elementsProp.arraySize; j++) {
-			// Skip current
-			if(j == _changedIdx) continue;
-
-			// Is it locked?
-			SerializedProperty siblingProp = m_elementsProp.GetArrayElementAtIndex(j);
-			if(_overrideLocks || !siblingProp.FindPropertyRelative("locked").boolValue) {
-				unlockedSiblingsTotal += siblingProp.FindPropertyRelative("value").floatValue;
-				unlockedSiblingsCount++;
-			} else {
-				totalToDistribute -= siblingProp.FindPropertyRelative("value").floatValue;
-			}
-		}
-
-		// [AOC] Extra
-		// Locked sliders may limit new value, check it here
-		// If the amount to distribute is negative, add that amount to changed slider value
-		if(!_overrideLocks && totalToDistribute < 0f) {
-			changedElementProp.FindPropertyRelative("value").floatValue += totalToDistribute;
-			totalToDistribute = 0;
-		}
-
-		// Compute and assign new value to each sibling based on its weight
-		float remainingToDistribute = totalToDistribute;
-		for(int i = 0; i < m_elementsProp.arraySize; i++) {
-			// Skip ourselves
-			if(i == _changedIdx) continue;
-
-			// Skip if locked
-			SerializedProperty siblingProp = m_elementsProp.GetArrayElementAtIndex(i);
-			if(!_overrideLocks && siblingProp.FindPropertyRelative("locked").boolValue) continue;
-
-			// Store previous value
-			float oldSiblingValue = siblingProp.FindPropertyRelative("value").floatValue;
-
-			// Compute relative weight of this sibling in relation to all active siblings (or something like that :s)
-			float siblingWeight = 0;
-			if(unlockedSiblingsTotal == 0f) {
-				// If all sliders except the one being moved are at 0, we split the movement evently amongst them
-				siblingWeight = 1f/unlockedSiblingsCount;
-			} else {
-				siblingWeight = oldSiblingValue/unlockedSiblingsTotal;
-			}
-
-			// Compute new value for the sibling and store it to the property
-			siblingProp.FindPropertyRelative("value").floatValue = totalToDistribute * siblingWeight;
-			remainingToDistribute -= totalToDistribute * siblingWeight;
-		}
-
-		// [AOC] Extra
-		// If not everything could be distributed, limit the movement of the target slider.
-		// This could happen when all siblings are either locked or already 0
-		if(remainingToDistribute > 0f) {
-			changedElementProp.FindPropertyRelative("value").floatValue += remainingToDistribute;
-		}
-
-		// Make sure changes are stored
-		m_elementsProp.serializedObject.ApplyModifiedProperties();
 	}
 }
--- Assets/Engine/Generic/ProbabilitySet.cs
+++ Assets/Engine/Generic/ProbabilitySet.cs
@@ -8,6 +8,8 @@
 //----------------------------------------------------------------------//
 using UnityEngine;
 using System;
+using UnityEngine.Events;
+using System.Collections.Generic;
 
 //----------------------------------------------------------------------//
 // CLASSES																//
@@ -30,13 +32,37 @@ public class ProbabilitySet {
 		//--------------------------------------------------------------------//
 		// MEMBERS AND PROPERTIES											  //
 		//--------------------------------------------------------------------//
-		public float value = 0f;
-		public string label = "";
-		public bool locked = false;
+		// The weight of this element within the probability set. [0..1], always use the setter.
+		[SerializeField] private float m_probability = 0f;
+		public float probability {
+			get { return m_probability; }
+			set { m_probability = Mathf.Clamp01(value); }
+		}
+
+		// Label in Unity's editor inspector
+		[SerializeField] private string m_label = "";
+		public string label {
+			get { return m_label; }
+			set { m_label = value; }
+		}
+
+		// Whether to adjust this element's probabilty when other elements in the set change
+		[SerializeField] private bool m_locked = false;
+		public bool locked {
+			get { return m_locked; }
+			set { m_locked = value; }
+		}
 
 		//--------------------------------------------------------------------//
 		// GENERIC METHODS													  //
 		//--------------------------------------------------------------------//
+		/// <summary>
+		/// Empty constructor with default values.
+		/// </summary>
+		public Element() {
+			// Using default values
+		}
+
 		/// <summary>
 		/// Parametrized constructor.
 		/// </summary>
@@ -45,19 +71,31 @@ public class ProbabilitySet {
 		public Element(string _label, bool _locked = false) {
 			label = _label;
 			locked = _locked;
+			m_probability = 0f;
 		}
 	};
 
 	//------------------------------------------------------------------------//
 	// MEMBERS AND PROPERTIES												  //
 	//------------------------------------------------------------------------//
-	[SerializeField] private Element[] m_elements = new Element[0];
+	// Collection of elements in the set
+	[SerializeField] private List<Element> m_elements = new List<Element>();
 
-	public int numElements { get { return m_elements.Length; }}
+	// Amount of elements in the set
+	public int numElements { 
+		get { return m_elements.Count; }
+	}
 
 	//------------------------------------------------------------------------//
 	// GENERIC METHODS														  //
 	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Empty constructor.
+	/// </summary>
+	public ProbabilitySet() {
+
+	}
+
 	/// <summary>
 	/// Initializes a new instance of the <see cref="ProbabilitySet"/> class.
 	/// All values will be reset.
@@ -74,29 +112,295 @@ public class ProbabilitySet {
 	/// <param name="_elements">Elements.</param>
 	public ProbabilitySet(Element[] _elements) {
 		// Store elements
-		if(_elements != null) m_elements = _elements;
+		if(_elements != null) m_elements = new List<Element>(_elements);
 
 		// Distribute probability uniformly at start
-		for(int i = 0; i < m_elements.Length; i++) {
-			m_elements[i].value = 1f/(float)(m_elements.Length);
-		}
+		Reset(false);
 	}
 
+	//------------------------------------------------------------------------//
+	// ELEMENT MANAGEMENT METHODS											  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Add a new element at the end of the set. Will be initialized with probability 0.
+	/// </summary>
+	/// <returns>The newly added element.</returns>
+	/// <param name="_label">Label for the new element.</param>
+	public Element AddElement(string _label) {
+		// Create a new element with the given label
+		Element newElement = new Element(_label);
+
+		// Give it 0 value to avoid affecting the global balance, unless there's only one element, in which case we'll give it value 1
+		if(numElements == 0) {
+			newElement.probability = 1f;
+		} else {
+			newElement.probability = 0f;
+		}
+
+		// Add it to the list and return it - no need to redistribute since default probability is 0
+		m_elements.Add(newElement);
+		return newElement;
+	}
+
+	/// <summary>
+	/// Removes last element of the set.
+	/// All other elements probabilities will be readjusted, regardless of their lock status.
+	/// </summary>
+	/// <returns>The newly added element.</returns>
+	/// <param name="_label">Label for the new element.</param>
+	public void RemoveElement() {
+		// Ignore if there are no elements to remove
+		if(numElements <= 0) return;
+
+		// Get element to be removed
+		Element toRemove = GetElement(numElements - 1);
+
+		// Before removing, redistribute values simulating that the element we will remove goes to 0
+		toRemove.probability = 0f;		// Set probability to 0
+		Redistribute(true, toRemove);	// Redistribute, including locked elements for the case where all remaining elements are locked
+
+		// Remove element from the list
+		m_elements.Remove(toRemove);
+	}
+
+	/// <summary>
+	/// Obtain the element at the given index.
+	/// </summary>
+	/// <returns>The element at index <paramref name="_idx"/>. <c>null</c> if index not valid.</returns>
+	/// <param name="_idx">The index of the element we want.</param>
+	private Element GetElement(int _idx) {
+		if(_idx < 0 || _idx >= numElements) return null;
+		return m_elements[_idx];
+	}
+
+	//------------------------------------------------------------------------//
+	// ELEMENTS ACCESSORS													  //
+	// We want to control any change on the elements, so don't allow direct   //
+	// access to them														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Obtain the probability value of an element in this set.
+	/// </summary>
+	/// <returns>The probability [0..1] of the element with index <paramref name="_elementIdx"/>. <c>-1</c> if given index is not valid.</returns>
+	/// <param name="_elementIdx">The index of the element to be checked.</param>
+	public float GetProbability(int _elementIdx) {
+		// Check params
+		if(_elementIdx < 0 || _elementIdx >= numElements) return -1;
+
+		// Return requested value
+		return m_elements[_elementIdx].probability;
+	}
+
+	/// <summary>
+	/// Define the probability of the target element.
+	/// The probabilities of the whole set will be redistributed accordingly.
+	/// Locked status will be ignored when calling this method.
+	/// </summary>
+	/// <param name="_elementIdx">The index of the element to be modified.</param>
+	/// <param name="_probability">The new probability [0..1] for the target element.</param>
+	public void SetProbability(int _elementIdx, float _probability) {
+		// Check params
+		if(_elementIdx < 0 || _elementIdx >= numElements) return;
+
+		// Store new value and redistribute
+		m_elements[_elementIdx].probability = _probability;
+		Redistribute(false, m_elements[_elementIdx]);
+	}
+
+	/// <summary>
+	/// Obtain the label of an element in this set.
+	/// </summary>
+	/// <returns>The label of the element with index <paramref name="_elementIdx"/>.</returns>
+	/// <param name="_elementIdx">The index of the element to be checked.</param>
+	public string GetLabel(int _elementIdx) {
+		// Check params
+		if(_elementIdx < 0 || _elementIdx >= numElements) return "";
+
+		// Return requested value
+		return m_elements[_elementIdx].label;
+	}
+
+	/// <summary>
+	/// Define the label of the target element.
+	/// </summary>
+	/// <param name="_elementIdx">The index of the element to be modified.</param>
+	/// <param name="_label">The new label for the target element.</param>
+	public void SetLabel(int _elementIdx, string _label) {
+		// Check params
+		if(_elementIdx < 0 || _elementIdx >= numElements) return;
+
+		// Set new value
+		m_elements[_elementIdx].label = _label;
+	}
+
+	/// <summary>
+	/// Whether an element is locked or not.
+	/// </summary>
+	/// <returns><c>true</c> if this element with index <paramref name="_elementIdx"/> is locked; otherwise, <c>false</c>.</returns>
+	/// <param name="_elementIdx">The index of the element to be checked.</param>
+	public bool IsLocked(int _elementIdx) {
+		// Check params
+		if(_elementIdx < 0 || _elementIdx >= numElements) return false;
+
+		// Return requested value
+		return m_elements[_elementIdx].locked;
+	}
+
+	/// <summary>
+	/// Lock/Unlock the target element.
+	/// </summary>
+	/// <param name="_elementIdx">The index of the element to be modified.</param>
+	/// <param name="_locked">Whether to lock or unlock the target element.</param>
+	public void SetLocked(int _elementIdx, bool _locked) {
+		// Check params
+		if(_elementIdx < 0 || _elementIdx >= numElements) return;
+
+		// Set new value
+		m_elements[_elementIdx].locked = _locked;
+	}
+
+	//------------------------------------------------------------------------//
+	// OPERATIONS															  //
+	//------------------------------------------------------------------------//
 	/// <summary>
 	/// Gets the index of a random element weighted following the probability value assigned to it.
 	/// </summary>
 	/// <returns>The index of the selected element.</returns>
-	public int GetWeightedRandomElement() {
+	public int GetWeightedRandomElementIdx() {
 		// Select a random value [0..1]
 		// Since the weights of all elements sum exactly 1, iterate through elements until the selected value is reached
 		// This should match weighted probability distribution
 		float targetValue = UnityEngine.Random.Range(0f, 1f);
-		for(int i = 0; i < m_elements.Length; i++) {
-			targetValue -= m_elements[i].value;
+		for(int i = 0; i < m_elements.Count; i++) {
+			targetValue -= m_elements[i].probability;
 			if(targetValue <= 0f) return i;
 		}
 
 		// Should never reach this point unless there are no elements on the set
 		return -1;
+	}
+
+	/// <summary>
+	/// Get a random element weighted following the probability value assigned to it.
+	/// </summary>
+	/// <returns>The selected random element.</returns>
+	public Element GetWeightedRandomElement() {
+		return GetElement(GetWeightedRandomElementIdx());
+	}
+
+	/// <summary>
+	/// Reset all probabilities to be equally distributed.
+	/// </summary>
+	/// <param name="_respectLocks">Whether to respect or ignore the <c>locked</c> flag of the elements.</param>
+	public void Reset(bool _respectLocks) {
+		// Respect locks
+		if(_respectLocks) {
+			// Count non-locked elements and actual amount to distribute
+			float nonLockedCount = 0f;
+			float toDistribute = 1f;
+			for(int i = 0; i < numElements; i++) {
+				if(m_elements[i].locked) {
+					toDistribute -= m_elements[i].probability;
+				} else {
+					nonLockedCount++;
+				}
+			}
+
+			// Distribute remaining amount uniformly among non-locked elements
+			float newProbabiltyPerElement = toDistribute/nonLockedCount;
+			for(int i = 0; i < numElements; i++) {
+				if(!m_elements[i].locked) {
+					m_elements[i].probability = newProbabiltyPerElement;
+				}
+			}
+		}
+
+		// Don't respect locks
+		else {
+			// Distribute uniformly among all elements
+			for(int i = 0; i < numElements; i++) {
+				m_elements[i].probability = 1f/(float)numElements;
+			}
+		}
+	}
+
+	//------------------------------------------------------------------------//
+	// INTERNAL																  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Redistribute probability proportionally between all elements.
+	/// </summary>
+	/// <param name="_overrideLocks">If set to <c>true</c>, locked sliders will be modified too.</param> 
+	/// <param name="_skipElement">Single element to be skipped from the redistribution (i.e. the element that has just been modified).</param>
+	private void Redistribute(bool _overrideLocks, Element _skipElement) {
+		// [AOC] From HumbleBundle's web page code:
+		// Calculate the splits for all the elements.
+		// Conceptually, we remove the active slider from the mix. Then we normalize the siblings to 1 to
+		// determine their weights relative to each other. Then we divide the split that is left over from the moved
+		// slider with these relative weights.
+		if(_skipElement == null) return;	// Redistribution should always be caused by an element being modified
+
+		// Amount to distribute between the siblings
+		float totalToDistribute = 1f - _skipElement.probability;
+
+		// Compute total amount of unlocked siblings to distribute proportionally
+		// Adjust amount to distribute by ignoring locked sliders
+		float unlockedSiblingsTotal = 0f;
+		float unlockedSiblingsCount = 0f;	// [AOC] Use float directly to avoid casting later on
+		Element element = null;
+		for(int i = 0; i < numElements; i++) {
+			// Get element
+			element = m_elements[i];
+
+			// Skip current
+			if(element == _skipElement) continue;
+
+			// Is it locked?
+			if(_overrideLocks || !element.locked) {
+				unlockedSiblingsTotal += element.probability;
+				unlockedSiblingsCount++;
+			} else {
+				totalToDistribute -= element.probability;
+			}
+		}
+
+		// Locked sliders may limit new value, check it here
+		// If the amount to distribute is negative, add (subtract) that amount to changed slider value
+		if(!_overrideLocks && totalToDistribute < 0f) {
+			_skipElement.probability += totalToDistribute;
+			totalToDistribute = 0;
+		}
+
+		// Compute and assign new value to each sibling based on its weight
+		float remainingToDistribute = totalToDistribute;
+		for(int i = 0; i < numElements; i++) {
+			// Get element
+			element = m_elements[i];
+
+			// Skip current
+			if(element == _skipElement) continue;
+
+			// Skip if locked
+			if(!_overrideLocks && element.locked) continue;
+
+			// Compute relative weight of this sibling in relation to all active siblings (or something like that :s)
+			float siblingWeight = 0;
+			if(unlockedSiblingsTotal == 0f) {
+				// If all sliders except the one being moved are at 0, we split the movement evently amongst them
+				siblingWeight = 1f/unlockedSiblingsCount;
+			} else {
+				siblingWeight = element.probability/unlockedSiblingsTotal;
+			}
+
+			// Compute new value for the sibling and store it to the property
+			element.probability = totalToDistribute * siblingWeight;
+			remainingToDistribute -= element.probability;
+		}
+
+		// If not everything could be distributed, limit the movement of the target slider.
+		// This could happen when all siblings are either locked or already 0
+		if(remainingToDistribute > 0f) {
+			_skipElement.probability += remainingToDistribute;
+		}
 	}
 }
