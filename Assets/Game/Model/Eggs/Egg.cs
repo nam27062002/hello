e22d7fe1d92adf8f2fae69f6aab7d692a626b614
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2016-02-17 12:45:31 +0100

    Eggs: Development - Created basic infrastructure - TO BE IMPLEMENTED!
    Former-commit-id: e6f2943d4cdcdac9a6ecf991801ae3413d58554d

---
+++ Assets/Game/Model/Eggs/Egg.cs
@@ -0,0 +1,60 @@
+﻿// Egg.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 15/02/2016.
+// Copyright (c) 2016 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------//
+// INCLUDES																//
+//----------------------------------------------------------------------//
+using UnityEngine;
+using System;
+
+//----------------------------------------------------------------------//
+// CLASSES																//
+//----------------------------------------------------------------------//
+/// <summary>
+/// Single Egg object.
+/// </summary>
+public class Egg {
+	//------------------------------------------------------------------//
+	// CONSTANTS														//
+	//------------------------------------------------------------------//
+	public enum State {
+		INIT,		// Init state
+		STORED,		// Egg is in storage, waiting for incubation
+		INCUBATING,	// Egg is in the incubator
+		READY,		// Egg has finished incubation period and is ready to be collected
+		COLLECTED	// Egg has been collected
+	};
+
+	//------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES											//
+	//------------------------------------------------------------------//
+	// Data
+	private DefinitionNode m_def = null;
+	public DefinitionNode def {
+		get { return m_def; }
+		set { m_def = value; }
+	}
+
+	// Logic
+	private State m_state = State.INIT;
+	public State state { 
+		get { return m_state; }
+	}
+
+	public bool collected { 
+		get { return m_state == State.COLLECTED; }
+	}
+
+	//------------------------------------------------------------------//
+	// GENERIC METHODS													//
+	//------------------------------------------------------------------//
+	/// <summary>
+	/// Default constructor.
+	/// </summary>
+	public Egg() {
+
+	}
+}
