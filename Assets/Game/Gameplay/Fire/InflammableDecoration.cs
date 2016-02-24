bab9a65d392521e4a607db9b0233a91a804bedfd
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2016-02-24 16:34:55 +0100

    crumble WIP
    Former-commit-id: 8c3d745de6e0d5b3bb53e54f029ba417562a3833

--- Assets/Game/Gameplay/Fire/InflammableDecoration.cs
+++ Assets/Game/Gameplay/Fire/InflammableDecoration.cs
@@ -11,8 +11,11 @@ public class InflammableDecoration : Initializable {
 
 	private FireNode[] m_fireNodes;
 	private bool m_burned;
+	private DeltaTimer m_timer = new DeltaTimer();
 
 	private AutoSpawnBehaviour m_autoSpawner;
+	public bool m_crumble = false;
+	private Vector3 m_startPosition;
 
 	// Use this for initialization
 	void Start () {
@@ -35,12 +38,14 @@ public class InflammableDecoration : Initializable {
 		}
 
 		m_fireNodes[m_fireNodes.Length - 1].Init(coins - (coinsPerNode * (m_fireNodes.Length - 1)));
+
+		m_startPosition = transform.position;
 	}
 
 	public override void Initialize() {
 		m_view.SetActive(true);
 		m_viewBurned.SetActive(false);
-
+		transform.localScale = Vector3.one;
 		m_burned = false;
 
 		for (int i = 0; i < m_fireNodes.Length; i++) {
@@ -48,19 +53,37 @@ public class InflammableDecoration : Initializable {
 		}
 
 		enabled = true;
+
+		transform.position = m_startPosition;
 	}
 
 	// Update is called once per frame
 	void Update() {	
-		if (m_burned) {
-			m_view.SetActive(false);
-			m_viewBurned.SetActive(true);
 
-			if (m_autoSpawner) {
+		if ( m_autoSpawner.state == AutoSpawnBehaviour.State.Respawning )	// if respawning we wait
+			return;
+
+		if (m_burned) 
+		{
+			// Wait burn animation to end
+			Vector3 scale = transform.localScale;
+			scale.y = 0.5f + (0.5f - (m_timer.GetDelta() * 0.5f));
+			transform.localScale = scale;
+
+			Vector3 pos = m_startPosition;
+			float size = 0.1f;
+			pos.x += Random.Range( -size, size );
+			pos.y += Random.Range( -size, size );
+			pos.z += Random.Range( -size, size );
+			transform.position = pos;
+
+			if ( m_timer.Finished() )
+			{
 				m_autoSpawner.Respawn();
 			}
-			enabled = false;
-		} else {
+		} 
+		else 
+		{
 			m_burned = true;
 			for (int i = 0; i < m_fireNodes.Length && m_burned; i++) {
 				m_burned = m_burned && m_fireNodes[i].IsBurned();
@@ -74,6 +97,16 @@ public class InflammableDecoration : Initializable {
 			{
 				for (int i = 0; i < m_fireNodes.Length && m_burned; i++) {
 					m_fireNodes[i].StartSmoke();
+				}
+				m_view.SetActive(false);
+				m_viewBurned.SetActive(true);
+				if (m_crumble)
+				{
+					m_timer.Start( 2f );
+				}
+				else
+				{
+					m_autoSpawner.Respawn();
 				}
 			}
 		}
