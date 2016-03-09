600170554453d8c1774e499f32e240f2a2500b69
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2016-03-09 14:05:46 +0100

    ENGINE 	UI 3D View: Development - Implemented UIScene3D and UIScene3DManager to easily integrate 3D into the UI.
    GAME
    	Chest Manager: Development - Implemented egg reward.
    	Chest Manager: Content - Set egg probability to 100% for the play test.
    	Egg: Development - Added "isNew" flag.
    	Chest Popup: Development - Chest is now a 3D model, added egg reward 3D preview.
    
    
    Former-commit-id: f350d7b67c7ac7a5bdababc22077549ef5a6e732

---
+++ Assets/Engine/UI/View3D/UIScene3D.cs
@@ -0,0 +1,105 @@
+﻿// UIScene3D.cs
+// 
+// Created by Alger Ortín Castellví on 09/03/2016.
+// Copyright (c) 2016 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Full 3D scene setup to be rendered on a RenderTexture to be integrated into
+/// the UI.
+/// The game object using this component should have the following structure:
+/// <c>
+/// + UIScene3D
+///   - Camera
+///   - HierarchyToRender
+/// </c>
+/// If a camera is not found, a new one will be created with default parameters.
+/// Either way, a new render texture will be created and assigned to the camera.
+/// </summary>
+public class UIScene3D : MonoBehaviour {
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
+	
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
+	// References
+	private Camera m_camera = null;
+	public Camera camera {
+		get { return m_camera; }
+	}
+
+	private RenderTexture m_renderTexture = null;
+	public RenderTexture renderTexture {
+		get { return m_renderTexture; }
+	}
+
+	// Persist through scenes
+	[SerializeField] private bool m_persistent = false;
+	public bool persistent {
+		get { return m_persistent; }
+		set { m_persistent = value; }
+	}
+	
+	//------------------------------------------------------------------------//
+	// GENERIC METHODS														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Initialization.
+	/// </summary>
+	private void Awake() {
+		// Find camera within the hierarchy
+		// If not found, create a new one with default parameters
+		m_camera = GetComponentInChildren<Camera>(true);
+		if(m_camera == null) {
+			// Create container object
+			GameObject cameraObj = new GameObject("Camera");
+			cameraObj.transform.SetParent(this.transform, false);
+			cameraObj.transform.localPosition = Vector3.back * 10f;
+			cameraObj.transform.LookAt(this.transform.position);
+
+			// Create and initialize camera component
+			m_camera = cameraObj.AddComponent<Camera>();
+			m_camera.orthographic = false;
+			m_camera.clearFlags = CameraClearFlags.Color;
+			m_camera.backgroundColor = new Color(1f, 0f, 0f, 0f);
+			m_camera.cullingMask = (1 << LayerMask.NameToLayer(UIScene3DManager.LAYER_NAME));
+			m_camera.nearClipPlane = 1f;
+			m_camera.farClipPlane = 100f;
+		}
+
+		// Create a new render texture and set it as the camera render target
+		m_renderTexture = new RenderTexture(512, 512, 24, RenderTextureFormat.ARGB32);	// Might need a bigger one
+		m_renderTexture.Create();
+		m_camera.targetTexture = m_renderTexture;
+	}
+
+	/// <summary>
+	/// Destructor.
+	/// </summary>
+	private void OnDestroy() {
+		// Destroy created render texture as well
+		m_camera.targetTexture = null;
+		if(m_renderTexture != null) {
+			m_renderTexture.Release();
+			Destroy(m_renderTexture);
+			m_renderTexture = null;
+		}
+	}
+
+	//------------------------------------------------------------------------//
+	// OTHER METHODS														  //
+	//------------------------------------------------------------------------//
+
+	//------------------------------------------------------------------------//
+	// CALLBACKS															  //
+	//------------------------------------------------------------------------//
+}
