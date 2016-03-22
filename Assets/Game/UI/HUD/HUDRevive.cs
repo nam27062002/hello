5212f97123141ed94d5a1f0bd3ad7cdc448621f9
Alger Ortin Castellvi <alger.ortin@ubisoft.com>
2016-03-22 11:32:47 +0100

    GAME 	Revive HUD: Development - A little bit of love given to the revive experience.
    Former-commit-id: 58d169c5e15f138b762a53ec63319387b1f1bf1b

--- Assets/Game/UI/HUD/HUDRevive.cs
+++ Assets/Game/UI/HUD/HUDRevive.cs
@@ -1,51 +1,150 @@
-﻿using UnityEngine;
+﻿// HUDRevive.cs
+// Hungry Dragon
+// 
+// Marc Saña Forrellach, Alger Ortín Castellví
+// Copyright (c) 2016 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using DG.Tweening;
 
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Revive logic and UI controller.
+/// </summary>
+[RequireComponent(typeof(ShowHideAnimator))]
 public class HUDRevive : MonoBehaviour {
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
 
-	[SerializeField] private GameObject m_reviveButton;
-	[SerializeField] private float m_reviveAvailableSecs = 5f;
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
+	// Exposed references
+	[SerializeField] private Text m_pcText = null;
+	[SerializeField] private Text m_reviveText = null;
 
-	private float m_timer;
+	// Exposed setup
+	[SerializeField] private float m_reviveAvailableSecs = 5f;	// [AOC] TODO!! From content
 
-	// Use this for initialization
-	void Start() {
+	// Internal references
+	private ShowHideAnimator m_animator = null;
+
+	// Internal logic
+	private int m_reviveCount = 0;
+	private DeltaTimer m_timer = new DeltaTimer();
+	private bool m_allowCurrencyPopup = true;
+
+	//------------------------------------------------------------------------//
+	// GENERIC METHODS														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Initialization.
+	/// </summary>
+	void Awake() {
+		// Get references
+		m_animator = GetComponent<ShowHideAnimator>();
+
+		// Subscribe to external events
 		Messenger.AddListener(GameEvents.PLAYER_KO, OnPlayerKo);
-		m_timer = 0;
+		m_timer.Stop();
+		m_reviveCount = 0;
 	}
 
-	void OnEnable() {
-		m_reviveButton.SetActive(false);
+	/// <summary>
+	/// First update call.
+	/// </summary>
+	void Start() {
+		m_animator.Hide(false);	// Start hidden
 	}
 
-	void OnDisable() {
+	/// <summary>
+	/// Destructor.
+	/// </summary>
+	void OnDestroy() {
+		// Unsubscribe from external events
 		Messenger.RemoveListener(GameEvents.PLAYER_KO, OnPlayerKo);
+
+		// Restore timescale
+		Time.timeScale = 1f;
 	}
 
-	// Update is called once per frame
+	/// <summary>
+	/// Called once per frame.
+	/// </summary>
 	void Update() {
-		if (m_timer > 0) {
-			m_timer -= Time.deltaTime;
-			if (m_timer <= 0) {
-				m_timer = 0;
-				m_reviveButton.SetActive(false);
+		if(!m_timer.IsStopped()) {
+			m_reviveText.text = Localization.Localize("Revive %U0", StringUtils.FormatNumber(Mathf.CeilToInt(m_timer.GetTimeLeft())));	// [AOC] HARDCODED!!
+			if(m_timer.Finished()) {
+				m_timer.Stop();
+				m_animator.Hide();
 				Messenger.Broadcast(GameEvents.PLAYER_DIED);
 			}
 		}
 	}
 
-	//------------------------------------------------------------------//
-	// CALLBACKS														//
-	//------------------------------------------------------------------//
+	//------------------------------------------------------------------------//
+	// CALLBACKS															  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// The revive button has been clicked.
+	/// </summary>
 	public void OnRevive() {
-		InstanceManager.player.ResetStats(true);
-		m_reviveButton.SetActive(false);
-		m_timer = 0;
+		// Perform transaction
+		// If not enough funds, pause timer and open PC shop popup
+		long costPC = m_reviveCount + 1;	// [AOC] TODO!! Actual revive cost formula
+		if(UserProfile.pc >= costPC) {
+			// Revive!
+			m_reviveCount++;
+			InstanceManager.player.ResetStats(true);
+			m_timer.Stop();
+
+			// Perform transaction
+			UserProfile.AddPC(-costPC);
+			PersistenceManager.Save();
+
+			// Hide button
+			m_animator.Hide();
+
+			// Restore timescale
+			Time.timeScale = 1f;
+		} else if(m_allowCurrencyPopup) {
+			// Open PC shop popup
+			PopupController popup = PopupManager.OpenPopupInstant(PopupCurrencyShop.PATH);
+			popup.OnClosePostAnimation.AddListener(OnRevive);	// [AOC] Quick'n'dirty: try to revive again, but don't show the popup twice if we're out of currency!
+			m_allowCurrencyPopup = false;
+
+			// Pause timer
+			m_timer.Stop();
+		} else {
+			// Currency popup closed, no funds purchased
+			// If player tries to revive again, show popup
+			m_allowCurrencyPopup = true;
+
+			// Resume timer
+			m_timer.Stop();
+		}
 	}
 
+	/// <summary>
+	/// The player is KO.
+	/// </summary>
 	private void OnPlayerKo() {
-		m_reviveButton.SetActive(true);
-		m_timer = m_reviveAvailableSecs;
-	}
+		m_pcText.text = StringUtils.FormatNumber(m_reviveCount + 1);	// [AOC] TODO!! Actual revive cost formula
 
+		m_timer.Start(m_reviveAvailableSecs);
+		m_allowCurrencyPopup = true;
+
+		m_animator.Show();
+
+		// Slow motion
+		Time.timeScale = 0.25f;
+	}
 }
