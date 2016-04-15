709b0af6d52120d788a1fa3049307158ee68d898
Marc Saña <marc.sana@ubisoft.com>
2016-04-15 14:23:45 +0200

    working on villagers behavior
    Former-commit-id: 6c14c924116b50046886063abc8f1c333c345aa6

---
+++ Assets/Game/Gameplay/Prey/Animation/AnimRandom.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimRandom : StateMachineBehaviour {
+
+	[SerializeField] private string m_versionATriggerKey;
+	[SerializeField] private string m_versionBTriggerKey;
+
+	// OnStateMachineEnter is called when entering a statemachine via its Entry Node
+	override public void OnStateMachineEnter(Animator animator, int stateMachinePathHash){
+		float rnd = Random.Range(0, 100);
+		animator.SetBool(m_versionATriggerKey, rnd <= 50);
+		animator.SetBool(m_versionBTriggerKey, rnd > 50);
+	}
+}
---
+++ Assets/Game/Gameplay/Prey/FleePathBehaviour.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(PreyMotion))]
+public class FleePathBehaviour : Initializable {
+
+	protected enum State {
+		None = 0,
+		Run,		// we'll start always running from the dragon, if it gets too close
+		Scared,		// then we'll start the scared animations until we have the dragon
+		Panic   // over us and we'll stop moving.
+	};
+
+	private PathController m_path;
+	public PathController path { set { m_path = value; } }
+
+	private PreyMotion m_motion;
+	private Animator m_animator;
+	private SensePlayer m_sensor;
+
+	private Vector3 m_target;
+
+	private State m_state;
+	private State m_nextState;
+
+	//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+	// Use this for initialization
+	void Awake () {
+		m_motion = GetComponent<PreyMotion>();
+		m_animator = transform.FindChild("view").GetComponent<Animator>();
+		m_sensor = GetComponent<SensePlayer>();
+	}
+
+	public override void Initialize() {			
+		if (m_path != null) {
+			m_target = m_path.GetNext();
+		}	
+
+		m_state = State.None;
+		m_nextState = State.Run;
+	}
+
+	void OnEnable() {
+		if (m_path != null) {
+			m_target = m_path.GetNearestTo(m_motion.position);
+		}
+
+		m_state = State.None;
+		m_nextState = State.Run; 
+	}
+
+	void OnDisable() {
+		if (m_path != null) {
+			m_target = m_path.GetNearestTo(m_motion.position);
+		}
+
+		m_animator.SetBool("scared", false);
+	}
+
+	public void SetPath(PathController _path) {
+		m_path = _path;
+		if (m_path != null) {
+			m_target = m_path.GetNext();
+		}
+	}
+
+	//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+	void Update() {
+		if (m_state != m_nextState) {
+			ChangeState();
+		}
+
+		switch (m_state) {
+			case State.Run:
+				ChooseTarget();
+
+				if (m_sensor.isInsideMinArea) {
+					m_nextState = State.Scared;
+				}
+				break;
+
+			case State.Scared:
+				ChooseTarget();
+
+				if (m_sensor.distanceSqr < m_sensor.sensorMinRadiusSqr * 0.5f){
+					m_nextState = State.Panic;
+				}
+				break;
+
+			case State.Panic:
+				if (!m_sensor.isInsideMinArea) {
+					m_nextState = State.Scared;
+				}
+				break;
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+		if (m_state != State.Panic) {
+			m_motion.RunTo(m_target);
+		}
+	}
+
+	private void ChangeState() {
+		m_animator.SetBool("move", m_nextState != State.Panic);
+		m_animator.SetBool("scared", m_nextState != State.Run);
+		m_state = m_nextState;
+	}
+
+	private void ChooseTarget() {
+
+	}
+}
