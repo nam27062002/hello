835610c12c43cbc555976f3caa957371c0debd37
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2016-04-18 09:14:48 +0200

    fps counter always visible
    Former-commit-id: c8bde65d7d69725bc5f57a63544f2fb0bfc1a1a9

--- Assets/Game/Debug/ControlPanel/ControlPanel.cs
+++ Assets/Game/Debug/ControlPanel/ControlPanel.cs
@@ -8,7 +8,7 @@
 // INCLUDES																//
 //----------------------------------------------------------------------//
 using UnityEngine;
-
+using UnityEngine.UI;
 //----------------------------------------------------------------------//
 // CLASSES																//
 //----------------------------------------------------------------------//
@@ -21,8 +21,12 @@ public class ControlPanel : SingletonMonoBehaviour<ControlPanel> {
 	//------------------------------------------------------------------//
 	[SerializeField] private GameObject m_panel;
 	public GameObject m_toggleButton;
+	public Text m_fpsCounter;
 	private float m_activateTimer;
 
+	const int m_NumDeltaTimes = 30;
+	float[] m_DeltaTimes;
+	int m_DeltaIndex;
 	//------------------------------------------------------------------//
 	// MEMBERS															//
 	//------------------------------------------------------------------//
@@ -33,7 +37,20 @@ public class ControlPanel : SingletonMonoBehaviour<ControlPanel> {
 		// Start disabled
 		m_panel.SetActive(false);
 		m_toggleButton.SetActive( Debug.isDebugBuild );
+		m_fpsCounter.gameObject.SetActive( Debug.isDebugBuild );
 		m_activateTimer = 0;
+	}
+
+	void Start()
+	{
+		// FPS Initialization
+		m_DeltaTimes = new float[ m_NumDeltaTimes ];
+		m_DeltaIndex = 0;
+		float initValue = 1.0f / 30.0f;
+		if ( Application.targetFrameRate > 0 )
+			initValue = 1.0f / Application.targetFrameRate;
+		for( int i = 0; i<m_NumDeltaTimes; i++ )
+			m_DeltaTimes[i] = initValue;
 	}
 
 	protected void Update()
@@ -64,8 +81,44 @@ public class ControlPanel : SingletonMonoBehaviour<ControlPanel> {
 
 		if ( Input.GetKeyDown(KeyCode.Tab) )
 			Toggle();
+
+
+		// Update FPS
+		m_DeltaTimes[ m_DeltaIndex ] = Time.deltaTime;
+		m_DeltaIndex++;
+		if ( m_DeltaIndex >= m_NumDeltaTimes )
+			m_DeltaIndex = 0;
+
+		if ( m_fpsCounter != null )
+		{
+			float fps = GetFPS();
+			if ( fps < 15 )
+			{
+				m_fpsCounter.color = Color.red;
+			}
+			else if ( fps < 25 )
+			{
+				m_fpsCounter.color = Color.yellow;
+			}
+			else
+			{
+				m_fpsCounter.color = Color.green;
+			}
+			m_fpsCounter.text = ((int)fps).ToString("D");
+		}
 	}
-	
+
+	public float GetFPS()
+	{
+		float median = 0;
+		for( int i = 0; i<m_NumDeltaTimes; i++ )
+		{
+			median += m_DeltaTimes[i];
+		}
+		median = median / m_NumDeltaTimes;
+		return 1.0f / median;
+	}
+
 	//------------------------------------------------------------------//
 	// CALLBACKS														//
 	//------------------------------------------------------------------//
