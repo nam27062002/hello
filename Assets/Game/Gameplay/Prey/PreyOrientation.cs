a4d36e267539ce9b2555fbfa50b85dcbaffdcfae
Marc Saña <marc.sana@ubisoft.com>
2016-04-20 10:30:02 +0200

    - fixes on ground spawners, they no longer run to dragon's mouth at the beginning  - fixes on prey motion class to allow slow speeds  - added ghost prey
    Former-commit-id: 6e4639f0ab48080a8977ce92b4e0dd0247dd942e

--- Assets/Game/Gameplay/Prey/PreyOrientation.cs
+++ Assets/Game/Gameplay/Prey/PreyOrientation.cs
@@ -2,21 +2,23 @@
 
 [DisallowMultipleComponent]
 public class PreyOrientation : Orientation {
-	
+
+	[SeparatorAttribute]
 	[SerializeField] private bool m_faceDirection;
 	public bool faceDirection { get { return m_faceDirection; } }
+
+	[SeparatorAttribute]
+	[SerializeField] private float m_faceLeftAngleY = 180f;
+	[SerializeField] private float m_faceRightAngleY = 0f;
 
 	private Vector3 m_direction;
 
 	private Quaternion m_targetRotation;
 	private Quaternion m_rotation;
 
-	private Animator m_animator;
-
 
 	// Use this for initialization
 	void Awake() {
-		m_animator = transform.FindChild("view").GetComponent<Animator>();
 		m_targetRotation = transform.rotation;
 		m_rotation = transform.rotation;
 		m_direction = Vector3.right;
@@ -46,10 +48,10 @@ public class PreyOrientation : Orientation {
 			m_targetRotation = Quaternion.Euler(eulerRot);		
 		} else {
 			// Rotate so it faces the right direction (replaces 2D sprite flip)
-			float angleY = 0f;
+			float angleY = m_faceRightAngleY;
 
 			if (m_direction.x < 0f) {
-				angleY = 180f;
+				angleY = m_faceLeftAngleY;
 			}
 
 			m_targetRotation = Quaternion.Euler(0, angleY, 0);
