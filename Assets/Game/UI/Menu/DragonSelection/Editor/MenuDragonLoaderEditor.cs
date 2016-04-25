b8a9a046236597497ac1f4849a0dff21da7276e5
Marc Saña <marc.sana@ubisoft.com>
2016-04-25 11:13:25 +0200

    Dragons have new Skus based on concept art.
    Former-commit-id: ab295cdde65cbf6e950b15dd80abd0ca5a91d8b9

---
+++ Assets/Game/UI/Menu/DragonSelection/Editor/MenuDragonLoaderEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MenuDragonLoader))]
+public class MenuDragonLoaderEditor : Editor {
+
+	/// <summary>
+	/// Draw the inspector.
+	/// </summary>
+	public override void OnInspectorGUI() {
+		DrawDefaultInspector();
+
+		if (GUILayout.Button("Load Dragon Preview")) {
+			((MenuDragonLoader)target).LoadDragonPreview();
+		}
+	}
+}
