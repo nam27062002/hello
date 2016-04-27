dfcd95eaaa5d7a13bb170f8c8a8c8b941b5bb5f2
Marc Saña <marc.sana@ubisoft.com>
2016-04-27 09:53:57 +0200

    new scripts to manage the results scene and new version of the UI prefab
    Former-commit-id: 9dd6c85018d88571c6441d4b981318cc9acbb64d

---
+++ Assets/Game/UI/Menu/ResultsScreen/ResultsScreenController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultsScreenController : MonoBehaviour {
+
+	//------------------------------------------------------------------//
+	// MEMBERS															//
+	//------------------------------------------------------------------//
+	// Exposed members
+	[SerializeField] private Text m_timeLabel = null;
+	[SerializeField] private NumberTextAnimator m_scoreAnimator = null;
+	[SerializeField] private NumberTextAnimator m_coinsAnimator = null;
+	[SerializeField] private NumberTextAnimator m_bonusCoinsAnimator = null;
+
+	[SerializeField] private Text m_highScoreLabel = null;
+	[SerializeField] private GameObject m_newHighScoreDeco = null;
+
+
+	//------------------------------------------------------------------//
+	// GENERIC METHODS													//
+	//------------------------------------------------------------------//
+
+	void Awake() {
+		// Check required fields
+		DebugUtils.Assert(m_timeLabel != null, "Required field not initialized!");
+		DebugUtils.Assert(m_scoreAnimator != null, "Required field not initialized!");
+		DebugUtils.Assert(m_coinsAnimator != null, "Required field not initialized!");
+		DebugUtils.Assert(m_bonusCoinsAnimator != null, "Required field not initialized!");
+		DebugUtils.Assert(m_highScoreLabel != null, "Required field not initialized!");
+		DebugUtils.Assert(m_newHighScoreDeco != null, "Required field not initialized!");
+
+		// Initialize some of the popup's members
+		// Set initial values
+		m_scoreAnimator.SetValue(0, 0);
+		m_coinsAnimator.SetValue(0, 0);
+		m_bonusCoinsAnimator.SetValue(0, 0);
+		m_highScoreLabel = "0";
+
+		// Set time - format to MM:SS
+		GameSceneController game = InstanceManager.GetSceneController<GameSceneController>();
+		m_timeLabel.text = TimeUtils.FormatTime(game.elapsedSeconds, TimeUtils.EFormat.ABBREVIATIONS, 2, TimeUtils.EPrecision.MINUTES);
+
+		// Hide high score decoration
+		m_newHighScoreDeco.SetActive(false);
+	}
+
+	void OnEnable() {
+		// set values from score manager
+
+	}
+
+
+	//------------------------------------------------------------------//
+	// DELEGATES														//
+	//------------------------------------------------------------------//
+
+	public void OnGoToMenu() {
+		FlowManager.GoToMenu();
+	}
+}
