c6e3500abf78768e69b72028e9f5200b0cb1ea24
Marc Saña <marc.sana@ubisoft.com>
2016-05-13 12:17:46 +0200

    improvements on fire propagation
    Former-commit-id: ca5e7932e3ffce23be74258457268fb15f212e7d

--- Assets/Engine/Generic/DisableInSeconds.cs
+++ Assets/Engine/Generic/DisableInSeconds.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class DisableInSeconds : MonoBehaviour {
 
@@ -8,9 +9,16 @@ public class DisableInSeconds : MonoBehaviour {
 	};
 
 	[SerializeField] private float m_activeTime = 1f;
+	public float activeTime { set { m_activeTime = value; m_activeTimer = m_activeTime; } }
 	[SerializeField] private PoolType m_returnTo = PoolType.PoolManager;
 
 	private float m_activeTimer;
+	private ParticleSystem m_particleSystem;
+
+	void Start() {
+		// lets grab the particle system if it exists. 
+		m_particleSystem = GetComponent<ParticleSystem>();
+	}
 
 	void OnEnable() {
 		m_activeTimer = m_activeTime;
@@ -19,11 +27,32 @@ public class DisableInSeconds : MonoBehaviour {
 	void Update() {		
 		m_activeTimer -= Time.deltaTime;
 		if (m_activeTimer < 0f) {
-			gameObject.SetActive(false);
-			switch(m_returnTo) {
-				case PoolType.PoolManager: 		PoolManager.ReturnInstance(gameObject); 	break;
-				case PoolType.ParticleManager: 	ParticleManager.ReturnInstance(gameObject); break;
+			if (m_particleSystem != null) {
+				// we are disabling a particle system
+				m_particleSystem.Stop();
+				StartCoroutine(WaitEndEmissionToDeactivate());
+			} else {
+				// it's a simple game object
+				Disable();
 			}
 		}
+	}
+
+	private void Disable() {
+		gameObject.SetActive(false);
+		switch(m_returnTo) {
+			case PoolType.PoolManager: 		PoolManager.ReturnInstance(gameObject); 	break;
+			case PoolType.ParticleManager: 	ParticleManager.ReturnInstance(gameObject); break;
+		}
+	}
+
+	IEnumerator WaitEndEmissionToDeactivate() {
+		while (m_particleSystem.particleCount > 0) {
+			yield return null;
+		}
+
+		Disable();
+
+		yield return null;
 	}
 }
