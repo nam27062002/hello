1da40715e2b5d222488c5ee2353f32c8ae62a269
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2016-06-09 18:14:53 +0200

    HUD Messaging System: Development - Full rework for easier scalability and to avoid conflicts between different types of message. HUD Messages: Development - Full rework of the animations. No more tweens. HUD Multiplier: Development - Full review and rework of the animations.
    Former-commit-id: 7e38f0a2ffdfc9de0a28e47deac2e399955b4986

---
+++ Assets/Game/UI/HUD/Messages/HUDMessageSystem.cs
@@ -0,0 +1,121 @@
+﻿// HUDMessageSystem.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 08/06/2016.
+// Copyright (c) 2016 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+using System.Collections.Generic;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Manager for HUD messages, avoiding overlapping between them.
+/// If a message with higher priority is triggered, previous message will be closed.
+/// Works similar to Unity's ToggleGroup.
+/// </summary>
+public class HUDMessageSystem : MonoBehaviour {
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
+	
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
+	// Exposed setup
+	private List<HUDMessage> m_messages = new List<HUDMessage>();
+
+	// Internal logic
+	private HUDMessage m_currentMessage = null;
+	
+	//------------------------------------------------------------------------//
+	// GENERIC METHODS														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Component has been enabled.
+	/// </summary>
+	private void OnEnable() {
+		// Subscribe to external events
+		Messenger.AddListener(GameEvents.PLAYER_KO, OnPlayerKo);
+	}
+
+	/// <summary>
+	/// Component has been disabled.
+	/// </summary>
+	private void OnDisable() {
+		// Unsubscribe from external events
+		Messenger.RemoveListener(GameEvents.PLAYER_KO, OnPlayerKo);
+	}
+
+	//------------------------------------------------------------------------//
+	// OTHER METHODS														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Register a message to work with this system.
+	/// </summary>
+	/// <param name="_msg">The message to be registered.</param>
+	public void Add(HUDMessage _msg) {
+		// Skip invalid params
+		if(_msg == null) return;
+
+		// Skip if message is already registered
+		if(m_messages.Contains(_msg)) return;
+
+		// Register and subscribe to events
+		m_messages.Add(_msg);
+		_msg.OnShow.AddListener(OnMessageShow);
+		_msg.OnHide.AddListener(OnMessageHide);
+	}
+
+	/// <summary>
+	/// Check whether a given message can be displayed or not within the current system.
+	/// </summary>
+	/// <returns><c>true</c>, if show was requested, <c>false</c> otherwise.</returns>
+	/// <param name="_msg">Message.</param>
+	public bool RequestShow(HUDMessage _msg) {
+		// Don't allow if there is an open message of higher priority
+		if(m_currentMessage != null) {
+			if(m_currentMessage.priority > _msg.priority) return false;
+		}
+
+		// All checks passed!
+		return true;
+	}
+
+	//------------------------------------------------------------------------//
+	// CALLBACKS															  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// A HUD message has been triggered.
+	/// </summary>
+	/// <param name="_msg">The message that triggered the event.</param>
+	private void OnMessageShow(HUDMessage _msg) {
+		// Hide current message (if any)
+		if(m_currentMessage != null && m_currentMessage != _msg) m_currentMessage.Hide();
+
+		// Store as the current message
+		m_currentMessage = _msg;
+	}
+
+	/// <summary>
+	/// A HUD message has been hidden.
+	/// </summary>
+	/// <param name="_msg">The message that triggered the event.</param>
+	private void OnMessageHide(HUDMessage _msg) {
+		// Clear current message ref
+		m_currentMessage = null;
+	}
+
+	/// <summary>
+	/// A HUD message has been triggered.
+	/// </summary>
+	/// <param name="_msg">The message that triggered the event.</param>
+	private void OnPlayerKo() {
+		// Hide current message (if any)
+		if(m_currentMessage != null) m_currentMessage.Hide();
+	}
+}
---
+++ Assets/Game/UI/HUD/Multiplier/HUDMultiplier.cs
@@ -0,0 +1,115 @@
+﻿// HUDMultiplier.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 26/10/2015.
+// Copyright (c) 2015 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------//
+// INCLUDES																//
+//----------------------------------------------------------------------//
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+//----------------------------------------------------------------------//
+// CLASSES																//
+//----------------------------------------------------------------------//
+/// <summary>
+/// Simple controller for the score multiplier display in the hud.
+/// </summary>
+[RequireComponent(typeof(Animator))]
+public class HUDMultiplier : MonoBehaviour {
+	//------------------------------------------------------------------//
+	// PROPERTIES														//
+	//------------------------------------------------------------------//
+	[SerializeField] private Text m_text = null;
+	[SerializeField] private Text m_maskText = null;
+	[SerializeField] private Image m_progressFill = null;
+	private Animator m_anim = null;
+	
+	//------------------------------------------------------------------//
+	// GENERIC METHODS													//
+	//------------------------------------------------------------------//
+	/// <summary>
+	/// Initialization.
+	/// </summary>
+	private void Awake() {
+		m_anim = GetComponent<Animator>();
+	}
+
+	/// <summary>
+	/// First update call.
+	/// </summary>
+	private void Start() {
+		UpdateText(RewardManager.currentScoreMultiplier);
+	}
+
+	/// <summary>
+	/// The spawner has been enabled.
+	/// </summary>
+	private void OnEnable() {
+		// Subscribe to external events
+		Messenger.AddListener<ScoreMultiplier, ScoreMultiplier>(GameEvents.SCORE_MULTIPLIER_CHANGED, OnMultiplierChanged);
+	}
+	
+	/// <summary>
+	/// The spawner has been disabled.
+	/// </summary>
+	private void OnDisable() {
+		// Unsubscribe from external events
+		Messenger.RemoveListener<ScoreMultiplier, ScoreMultiplier>(GameEvents.SCORE_MULTIPLIER_CHANGED, OnMultiplierChanged);
+	}
+
+	/// <summary>
+	/// Called every frame.
+	/// </summary>
+	private void Update() {
+		// If we have a valid multiplier, show progress to reach next one
+		if(RewardManager.currentScoreMultiplier != RewardManager.defaultScoreMultiplier) {
+			Vector3 scale = m_progressFill.rectTransform.localScale;
+			scale.y = RewardManager.scoreMultiplierProgress;
+			m_progressFill.rectTransform.localScale = scale;
+		}
+	}
+
+	//------------------------------------------------------------------//
+	// INTERNAL UTILS													//
+	//------------------------------------------------------------------//
+	/// <summary>
+	/// Updates the displayed multiplier.
+	/// </summary>
+	/// <param name="_mult">The multiplier we want to display.</param>
+	private void UpdateText(ScoreMultiplier _mult) {
+		// Do it! Except if going back to "no multiplier"
+		if(_mult != null && _mult != RewardManager.defaultScoreMultiplier) {
+			m_text.text = StringUtils.FormatNumber(_mult.multiplier, 0);
+			m_maskText.text = m_text.text;
+		}
+	}
+
+	//------------------------------------------------------------------//
+	// CALLBACKS														//
+	//------------------------------------------------------------------//
+	/// <summary>
+	/// Current score multiplier has changed.
+	/// </summary>
+	/// <param name="_oldMultiplier">The previous multiplier.</param>
+	/// <param name="_newMultiplier">The new multiplier.</param>
+	private void OnMultiplierChanged(ScoreMultiplier _oldMultiplier, ScoreMultiplier _newMultiplier) {
+		// Update text
+		UpdateText(_newMultiplier);
+
+		// Launch anim
+		if(m_anim != null) {
+			// If it's the default multiplier, fade out
+			if(_newMultiplier == RewardManager.defaultScoreMultiplier) {
+				m_anim.ResetTrigger("in");
+				m_anim.SetTrigger("out");
+			} else {
+				m_anim.ResetTrigger("out");
+				m_anim.SetTrigger("in");
+				m_anim.SetTrigger("changed");
+			}
+		}
+	}
+}
