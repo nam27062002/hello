cfc620115a142841c1192fba74d43ab244f67e4e
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2016-07-14 12:57:18 +0200

    Menu Egg Loader: Development - Created auxiliar component to simplify showing 3D eggs on scene.
    Former-commit-id: 5413d452c68a8db70661f838d0a8935793c1e74f

---
+++ Assets/Game/UI/Menu/Eggs/MenuEggLoader.cs
@@ -0,0 +1,77 @@
+﻿// MenuEggLoader.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 14/07/2016.
+// Copyright (c) 2016 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+using UnityEngine.UI;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Auxiliar component to easily load a 3D egg preview into the UI.
+/// </summary>
+public class MenuEggLoader : MonoBehaviour {
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
+	[SkuList(DefinitionsCategory.EGGS)]
+	[SerializeField] private string m_eggSku = "";
+	[SerializeField] private RawImage m_targetRawImage = null;
+
+	private EggUIScene3D m_eggScene3D = null;
+	
+	//------------------------------------------------------------------------//
+	// GENERIC METHODS														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Intialization.
+	/// </summary>
+	private void Awake() {
+		
+	}
+
+	/// <summary>
+	/// Initialization.
+	/// </summary>
+	private void OnEnable() {
+		// Instantiate the 3D scene and initialize the raw image
+		if(m_eggScene3D == null) {
+			m_eggScene3D = EggUIScene3D.CreateEmpty();
+			m_eggScene3D.InitRawImage(ref m_targetRawImage);
+		}
+
+		// If a egg sku was defined from inspector and we have no loaded egg, create it now
+		if(m_eggScene3D.egg == null && !string.IsNullOrEmpty(m_eggSku)) {
+			// Create a new egg
+			Egg newEgg = Egg.CreateFromSku(m_eggSku);
+
+			// Load it into this scene!
+			m_eggScene3D.SetEgg(newEgg);
+		}
+	}
+
+	/// <summary>
+	/// Default destructor.
+	/// </summary>
+	private void OnDestroy() {
+		// Destroy egg 3D scene
+		if(m_eggScene3D != null) {
+			UIScene3DManager.Remove(m_eggScene3D);
+			m_eggScene3D = null;
+		}
+	}
+
+	//------------------------------------------------------------------------//
+	// OTHER METHODS														  //
+	//------------------------------------------------------------------------//
+
+	//------------------------------------------------------------------------//
+	// CALLBACKS															  //
+	//------------------------------------------------------------------------//
+}
