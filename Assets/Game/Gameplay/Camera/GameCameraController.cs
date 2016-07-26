7d11a9c2a07a4eb99c4a9e44b91a36db1fc13ab9
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2016-07-26 16:41:05 +0200

    let old version seated
    Former-commit-id: 626bd943f5a0b93fe59ae000214a00215be87ead

--- Assets/Game/Gameplay/Camera/GameCameraController.cs
+++ Assets/Game/Gameplay/Camera/GameCameraController.cs
@@ -221,8 +221,8 @@ public class GameCameraController : MonoBehaviour {
 			}break;
 			case State.PLAY:
 			{
-				NewVersion();
-				// OldVersion();
+				// NewVersion();
+				OldVersion();
 			}break;
 		}
 
