dffedd46dc8d6143b3d65dffe2f4e367a44f5706
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2016-07-27 10:49:24 +0200

    Numeric Range Attribute: Fix - Wasn't working for integer fields.
    Former-commit-id: 6aececb22c8935c9af6fe3a737a6d712aa134ec4

--- Assets/Engine/EditorUtils/NumericRangeAttribute.cs
+++ Assets/Engine/EditorUtils/NumericRangeAttribute.cs
@@ -21,8 +21,28 @@ public class NumericRangeAttribute : PropertyAttribute {
 	//------------------------------------------------------------------//
 	// MEMBERS															//
 	//------------------------------------------------------------------//
-	public float m_min = float.MinValue;
-	public float m_max = float.MaxValue;
+	// Internal
+	private float m_floatMin = float.MinValue;
+	private float m_floatMax = float.MaxValue;
+	private int m_intMin = int.MinValue;
+	private int m_intMax = int.MaxValue;
+
+	// Float getters
+	public float floatMin { 
+		get { return m_floatMin; }
+	}
+	public float floatMax { 
+		get { return m_floatMax; }
+	}
+
+	// Int getters
+	// We must make sure that values don't 
+	public int intMin { 
+		get { return m_intMin; }
+	}
+	public int intMax { 
+		get { return m_intMax; }
+	}
 
 	//------------------------------------------------------------------//
 	// GENERIC METHODS													//
@@ -33,8 +53,14 @@ public class NumericRangeAttribute : PropertyAttribute {
 	/// <param name="_min">The minimum allowed value for this property.</param>
 	/// <param name="_max">The maximum allowed value for this property.</param>
 	public NumericRangeAttribute(float _min = float.MinValue, float _max = float.MaxValue) {
-		m_min = _min;
-		m_max = _max;
+		// Store float values
+		m_floatMin = _min;
+		m_floatMax = _max;
+
+		// Make sure we don't overreach int limits
+		// We have to add some margin 
+		m_intMin = (int)Mathf.Max((float)(int.MinValue + 100), _min);
+		m_intMax = (int)Mathf.Min((float)(int.MaxValue - 100), _max);
 	}
 }
 
