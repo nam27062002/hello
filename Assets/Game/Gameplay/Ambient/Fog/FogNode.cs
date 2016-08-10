49fd2a378df00d3369ef262c894efc18cd153c67
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2016-08-10 16:45:28 +0200

    lerp fog setter and show fog nodes when setter selected
    Former-commit-id: 25c5dfb9e39286e50a1c4b45482c202ea1f69297

--- Assets/Game/Gameplay/Ambient/Fog/FogNode.cs
+++ Assets/Game/Gameplay/Ambient/Fog/FogNode.cs
@@ -7,9 +7,21 @@ public class FogNode : MonoBehaviour
 	public float m_fogStart = 0;
 	public float m_fogEnd = 100;
 
+	private bool m_used = false;
+	public bool used
+	{
+		get{ return m_used; }
+		set { m_used = value; }
+	}
+
 
 	public void CustomGuizmoDraw()
 	{
+		if ( m_used )
+		{
+			Gizmos.color = Color.white;
+			Gizmos.DrawSphere( transform.position, 1.25f);	
+		}
 		Gizmos.color = m_fogColor;
 		Gizmos.DrawSphere( transform.position, 1);
 	}
