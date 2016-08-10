f854313ff8db4753c34ca0f7b8a7a718d454d7e3
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2016-08-10 11:09:32 +0200

    QuadTree: Development - Added methods to perform a search to a given list rather than creating a new one. QuadTree: Documentation - Full class documentation.
    Former-commit-id: 0eb4814cbdc894efb8925783965471b780817aff

--- Assets/Engine/Generic/QuadTree.cs
+++ Assets/Engine/Generic/QuadTree.cs
@@ -1,6 +1,18 @@
-﻿using UnityEngine;
+﻿// QuadTree.cs
+// Hungry Dragon
+// 
+// Created by Marc Saña Forrellach, Alger Ortín Castellví
+// Copyright (c) 2016 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
 using System.Collections.Generic;
 
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
 /// <summary>
 /// All items to be inserted in a quad tree must implement this interface
 /// </summary>
@@ -8,18 +20,37 @@ public interface IQuadTreeItem {
 	Transform transform { get; }
 }
 
+/// <summary>
+/// Quad tree structure used to partition a two-dimensional space by recursively 
+/// subdividing it into four quadrants or regions.
+/// Optimizes search by position within the area.
+/// See https://en.wikipedia.org/wiki/Quadtree
+/// </summary>
 public class QuadTree<T> where T : IQuadTreeItem {
-
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
 	public const uint MAX_ELEMENTS = 4;
 	public const uint MAX_DEPTH = 6;
 
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
 	private QuadTreeNode<T> m_root;
 	private List<T> m_items;
 	private List<QuadTreeNode<T>> m_nodes;
 	private Dictionary<T, QuadTreeNode<T>> m_indexTable;
 
-	///
-
+	//------------------------------------------------------------------------//
+	// METHODS																  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Create a new QuadTree with the given dimensions.
+	/// </summary>
+	/// <param name="_x">X position in world coords.</param>
+	/// <param name="_y">Y position in world coords.</param>
+	/// <param name="_w">Wdith in world coords.</param>.</param>
+	/// <param name="_h">Height in world coords.</param>
 	public QuadTree(float _x, float _y, float _w, float _h) {
 		m_root = new QuadTreeNode<T>();
 		m_items = new List<T>();
@@ -29,14 +60,35 @@ public class QuadTree<T> where T : IQuadTreeItem {
 		m_root.Init(0, null, new Rect(_x, _y, _w, _h));
 	}
 
+	/// <summary>
+	/// Insert a new item to the QuadTree.
+	/// </summary>
+	/// <param name="_item">The item to be inserted.</param>
 	public void Insert(T _item) {
 		m_root.Insert(_item, ref m_indexTable);
 	}
 
+	/// <summary>
+	/// Remove an item from the QuadTree.
+	/// </summary>
+	/// <param name="_item">The item to be removed.</param>
+	public void Remove(T _item) {
+		if (m_indexTable.ContainsKey(_item)) {
+			QuadTreeNode<T> node = m_indexTable[_item];
+			node.Remove(_item, ref m_indexTable);
+			m_indexTable.Remove(_item);
+		}
+	}
+
+	/// <summary>
+	/// Update the QuadTree indexing of an item.
+	/// Call this if an item's position has changed.
+	/// </summary>
+	/// <param name="_item">The item to be updated.</param>
 	public void Update(T _item) {
 		if (m_indexTable.ContainsKey(_item)) {
 			QuadTreeNode<T> node = m_indexTable[_item];
-			
+
 			if (!node.Contains(_item.transform.position)) {
 				QuadTreeNode<T> parent = node.parent;
 				node.Remove(_item, ref m_indexTable);
@@ -46,45 +98,72 @@ public class QuadTree<T> where T : IQuadTreeItem {
 		}
 	}
 
-	public void Remove(T _item) {
-		if (m_indexTable.ContainsKey(_item)) {
-			QuadTreeNode<T> node = m_indexTable[_item];
-			node.Remove(_item, ref m_indexTable);
-			m_indexTable.Remove(_item);
-		}
-	}
-
+	/// <summary>
+	/// Find all items within the given rectangle.
+	/// </summary>
+	/// <returns>An array with all the items in range.</returns>
+	/// <param name="_rect">The rectangle to be checked.</param>
 	public T[] GetItemsInRange(Rect _rect) {
 		m_items.Clear();
-		PreOrderInRange(m_root, _rect);
+		PreOrderInRange(m_root, _rect, ref m_items);
 		return m_items.ToArray();
 	}
 
-	private void PreOrderInRange(QuadTreeNode<T> _node, Rect _rect) {
+	/// <summary>
+	/// Find all items within the given rectangle and add them to the given list.
+	/// </summary>
+	/// <param name="_rect">Rectangle to be checked.</param>
+	/// <param name="_list">List where the selected items will be added.</param>
+	public void AddItemsInRange(Rect _rect, ref List<T> _list) {
+		PreOrderInRange(m_root, _rect, ref _list);
+	}
+
+	//------------------------------------------------------------------------//
+	// INTERNAL METHODS														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Recursively find all items within the given rect in the given node and
+	/// add them to the given list.
+	/// </summary>
+	/// <param name="_node">Node to be checked.</param>
+	/// <param name="_rect">Rectangle to be checked.</param>
+	/// <param name="_list">List where the selected items will be added.</param>
+	private void PreOrderInRange(QuadTreeNode<T> _node, Rect _rect, ref List<T> _list) {
 		if (_node.IsLeaf()) {
-			m_items.AddRange(_node.items);
+			_list.AddRange(_node.items);
 		} else {
 			for (int i = 0; i < 4; i++) {
 				if (_node.child[i].Intersects(_rect)) {
-					PreOrderInRange(_node.child[i], _rect);
+					PreOrderInRange(_node.child[i], _rect, ref _list);
 				}
 			}
 		}
 	}
 
-	private void PreOrderGetNodes(QuadTreeNode<T> _node) {
-		m_nodes.Add(_node);
+	/// <summary>
+	/// Get all the items in a node and store them in the target list.
+	/// </summary>
+	/// <param name="_node">The node to be checked.</param>
+	/// <param name="_list">The list where to store the values.</param>
+	private void PreOrderGetNodes(QuadTreeNode<T> _node, ref List<QuadTreeNode<T>> _list) {
+		_list.Add(_node);
 		if (!_node.IsLeaf()) {
 			for (int i = 0; i < 4; i++) {
-				PreOrderGetNodes(_node.child[i]);
+				PreOrderGetNodes(_node.child[i], ref _list);
 			}
 		}
 	}
 
-	// Debug
+	//------------------------------------------------------------------------//
+	// DEBUG METHODS														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Draw helper debug stuff on the scene.
+	/// </summary>
+	/// <param name="_color">The color to be used to draw the quad tree's grid.</param>
 	public void DrawGizmos(Color _color) {
 		m_nodes.Clear();
-		PreOrderGetNodes(m_root);
+		PreOrderGetNodes(m_root, ref m_nodes);
 
 		Gizmos.color = _color;
 		for (int i = 0; i < m_nodes.Count; i++) {
--- Assets/Game/Gameplay/Spawners/SpawnerManager.cs
+++ Assets/Game/Gameplay/Spawners/SpawnerManager.cs
@@ -37,6 +37,9 @@ public class SpawnerManager : SingletonMonoBehaviour<SpawnerManager> {
 	}
 
 	public void EnableSpawners() {
+		// This is called at the start of the game, so use it for other initializations as well
+
+		// Set flag
 		m_enabled = true;
 
 		// Make sure camera reference is valid
@@ -47,6 +50,9 @@ public class SpawnerManager : SingletonMonoBehaviour<SpawnerManager> {
 	}
 
 	public void DisableSpawners() {
+		// This is called at the end of the game, so use it for other finalizations as well
+
+		// Clear spawners
 		m_enabled = false;
 		for (int i = 0; i < m_spawners.Count; i++) {
 			m_spawners[i].ForceRemoveEntities();
@@ -58,92 +64,89 @@ public class SpawnerManager : SingletonMonoBehaviour<SpawnerManager> {
 	}
 
 	void Update() {
-		if (m_enabled) {
-			// Update timer (optimization to avoid performing the update every frame)
-			// Pre-optimization: ~10% CPU usage, 36-39 FPS
-			// 0.2s: 37-40 FPS
-			m_updateTimer += Time.deltaTime;
-			if(m_updateTimer > UPDATE_INTERVAL) {
-				// Reset timer
-				m_updateTimer = 0f;
+		// Only if enabled!
+		if(!m_enabled) return;
 
-				// Only update those spawners closer to the dragon
-				// Use the camera detection area for that
-				// Since the detection area has a hole in it, divide it in 4 rectangle sub-areas
+		// Update timer (optimization to avoid performing the update every frame)
+		// Pre-optimization: ~10% CPU usage, 36-39 FPS
+		// 0.2s: 37-40 FPS
+		m_updateTimer += Time.deltaTime;
+		if(m_updateTimer > UPDATE_INTERVAL) {
+			// Reset timer
+			m_updateTimer = 0f;
 
-				//            min.x0                  min.x1
-				//              |                       |
-				//           _______________________________  _ max.y0
-				//          |               1               |
-				// min.y0 _ |_______________________________|
-				//          |   |                       |   |
-				//          |   |                       |   |
-				//          | 4 |                       | 2 |
-				//          |   |                       |   |
-				// min.y1 _ |___|_______________________|___|
-				//          |               3               |
-				//          |_______________________________| _ max.y1
-				//
-				//          |                               |
-				//        max.x0                         max.x1
-				//
-				m_selectedSpawners.Clear();
+			// Only update those spawners closer to the dragon
+			// Use the camera detection area for that
+			// Since the detection area has a hole in it, divide it in 4 rectangle sub-areas
 
-				// Get activation bounds
-				if(DebugSettings.newCameraSystem && m_newCamera != null) {
-					m_minRect = m_newCamera.activationMinRect;
-					m_maxRect = m_newCamera.activationMaxRect;
-				} else if(m_camera != null) {
-					m_minRect = m_camera.activationMinRect;
-					m_maxRect = m_camera.activationMaxRect;
-				}
+			//            min.x0                  min.x1
+			//              |                       |
+			//           _______________________________  _ max.y0
+			//          |               1               |
+			// min.y0 _ |_______________________________|
+			//          |   |                       |   |
+			//          |   |                       |   |
+			//          | 4 |                       | 2 |
+			//          |   |                       |   |
+			// min.y1 _ |___|_______________________|___|
+			//          |               3               |
+			//          |_______________________________| _ max.y1
+			//
+			//          |                               |
+			//        max.x0                         max.x1
+			//
+			m_selectedSpawners.Clear();
 
-				// Split it in 4 rectangles that the quadtree can process
-				// 1: top sub-rect
-				m_subRect.Set(
-					m_maxRect.x0, 
-					m_maxRect.y0,
-					m_maxRect.w,
-					m_minRect.y0 - m_maxRect.y0
-				);
-				m_selectedSpawners.AddRange(m_spawnersTree.GetItemsInRange(m_subRect));
+			// Get activation bounds
+			// Update every frame in case camera bounds change (i.e. zoom in/out)
+			if(DebugSettings.newCameraSystem && m_newCamera != null) {
+				m_minRect = m_newCamera.activationMinRect;
+				m_maxRect = m_newCamera.activationMaxRect;
+			} else if(m_camera != null) {
+				m_minRect = m_camera.activationMinRect;
+				m_maxRect = m_camera.activationMaxRect;
+			}
 
-				// 2: right sub-rect
-				m_subRect.Set(
-					m_minRect.x1, 
-					m_minRect.y0,
-					m_maxRect.x1 - m_minRect.x1,
-					m_minRect.h
-				);
-				m_selectedSpawners.AddRange(m_spawnersTree.GetItemsInRange(m_subRect));
+			// Split it in 4 rectangles that the quadtree can process
+			// 1: top sub-rect
+			m_subRect.Set(
+				m_maxRect.x0, 
+				m_maxRect.y0,
+				m_maxRect.w,
+				m_minRect.y0 - m_maxRect.y0
+			);
+			m_spawnersTree.AddItemsInRange(m_subRect, ref m_selectedSpawners);
 
-				// 3: bottom sub-rect
-				m_subRect.Set(
-					m_maxRect.x0,
-					m_minRect.y1,
-					m_maxRect.w,
-					m_maxRect.y1 - m_minRect.y1
-				);
-				m_selectedSpawners.AddRange(m_spawnersTree.GetItemsInRange(m_subRect));
+			// 2: right sub-rect
+			m_subRect.Set(
+				m_minRect.x1, 
+				m_minRect.y0,
+				m_maxRect.x1 - m_minRect.x1,
+				m_minRect.h
+			);
+			m_spawnersTree.AddItemsInRange(m_subRect, ref m_selectedSpawners);
 
-				// 4: left sub-rect
-				m_subRect.Set(
-					m_maxRect.x0,
-					m_minRect.y0,
-					m_minRect.x0 - m_maxRect.x0,
-					m_minRect.h
-				);
-				m_selectedSpawners.AddRange(m_spawnersTree.GetItemsInRange(m_subRect));
+			// 3: bottom sub-rect
+			m_subRect.Set(
+				m_maxRect.x0,
+				m_minRect.y1,
+				m_maxRect.w,
+				m_maxRect.y1 - m_minRect.y1
+			);
+			m_spawnersTree.AddItemsInRange(m_subRect, ref m_selectedSpawners);
 
-				// Process all selected spawners!
-				for(int i = 0; i < m_selectedSpawners.Count; i++) {
-					m_selectedSpawners[i].CheckRespawn();
-				}
+			// 4: left sub-rect
+			m_subRect.Set(
+				m_maxRect.x0,
+				m_minRect.y0,
+				m_minRect.x0 - m_maxRect.x0,
+				m_minRect.h
+			);
+			m_spawnersTree.AddItemsInRange(m_subRect, ref m_selectedSpawners);
 
-				// Perform the update on all the spawners
-				/*for (int i = 0; i < m_spawners.Count; i++) {
-					m_spawners[i].CheckRespawn();
-				}*/
+			// Process all selected spawners!
+			for(int i = 0; i < m_selectedSpawners.Count; i++) {
+				m_selectedSpawners[i].CheckRespawn();
 			}
 		}
 	}
