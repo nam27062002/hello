69d339cbad44182562d6f75111590946e7de4ad4
Marc Saña <marc.sana@ubisoft.com>
2016-08-10 10:29:34 +0200

    - WIP: Grab and Bite behavior  - fixed, again, bird rotation
    Former-commit-id: 518edcf9e90cbb6b9d5adb2cc69184c0471b00b8

---
+++ Assets/Game/Gameplay/Entities/AI/Brain/Behaviours/Attack/GrabAndBite.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AI {
+	namespace Behaviour {
+		[System.Serializable]
+		public class GrabAndBiteData : StateComponentData {
+			public float time = 0f;
+			public float damagePerSecond = 0f;
+			public float retreatTime = 0f;
+		}
+
+		[CreateAssetMenu(menuName = "Behaviour/Attack/Grab and Bite")]
+		public class GrabAndBite : StateComponent {
+
+			[StateTransitionTrigger]
+			private static string OnRelease = "onRelease";
+
+
+			private GrabAndBiteData m_data;
+			private object[] m_transitionParam;
+
+			private DragonHealthBehaviour m_dragon;
+
+			private Transform m_parent;
+			private float m_timer;
+
+
+			//--------------------------------------------------------
+			public override StateComponentData CreateData() {
+				return new GrabAndBiteData();
+			}
+
+			public override System.Type GetDataType() {
+				return typeof(GrabAndBiteData);
+			}
+
+			protected override void OnInitialise() {
+				m_data = m_pilot.GetComponentData<GrabAndBiteData>();
+
+				m_transitionParam = new object[1];
+				m_transitionParam[0] = m_data.retreatTime; // retreat time
+			}
+
+			protected override void OnEnter(State oldState, object[] param) {
+				m_parent = m_machine.transform.parent;
+
+				if (m_machine.enemy != null) {
+					m_machine.transform.parent = m_machine.enemy.transform;
+				}
+
+				m_dragon = InstanceManager.player.GetComponent<DragonHealthBehaviour>();
+
+				m_timer = m_data.time;
+
+				m_machine.SetSignal(Signals.Type.Biting, true);
+			}
+
+			protected override void OnExit(State _newState) {
+				m_machine.transform.parent = m_parent;
+				m_machine.position = m_machine.transform.position;
+
+				m_machine.SetSignal(Signals.Type.Biting, false);
+			}
+
+			protected override void OnUpdate() {
+				m_timer -= Time.deltaTime;
+				if (m_timer <= 0 || m_machine.enemy == null) {
+					Transition(OnRelease, m_transitionParam);
+				} else {
+					m_dragon.ReceiveDamage(m_data.damagePerSecond * Time.deltaTime, DamageType.NORMAL, m_machine.transform);
+				}
+			}
+		}
+	}
+}
--- Assets/Game/Gameplay/Entities/AI/Machine/Components/MachineMotion.cs
+++ Assets/Game/Gameplay/Entities/AI/Machine/Components/MachineMotion.cs
@@ -90,6 +90,10 @@ namespace AI {
 		}
 
 		public override void Update() {
+			if (m_machine.GetSignal(Signals.Type.Biting)) {
+				return;
+			}
+
 			if (m_machine.GetSignal(Signals.Type.Panic)) {
 				m_viewControl.Panic(true, m_machine.GetSignal(Signals.Type.Burning));
 				return;
@@ -209,13 +213,21 @@ namespace AI {
 					m_targetRotation = Quaternion.LookRotation(m_direction, m_upVector);
 				}
 
-				float angle = 0f;
-				if (m_direction.x > 0)			angle = Vector3.Angle(Vector3.right, m_direction);
-				else if (m_direction.x < 0)		angle = Vector3.Angle(Vector3.left, m_direction);
+				float angle = Vector3.Angle(Vector3.right, m_direction);
 
-				angle = Mathf.Min(35f, angle);
+				if (angle > 10f && angle < 90f) {
+					angle = Mathf.Min(35f, angle);
+				} else if (angle > 90f && angle < 170f) {
+					angle = Mathf.Min(35f, 180f - angle);
+				} else {
+					angle = 0f;
+				}
 
-				m_targetRotation = Quaternion.AngleAxis(-angle, m_direction) * m_targetRotation;
+				if (m_direction.x < 0f && m_direction.z > 0f
+				||  m_direction.x > 0f && m_direction.z < 0f)
+					angle *= -1;
+				
+				m_targetRotation = Quaternion.AngleAxis(angle, m_direction) * m_targetRotation;
 
 			} else {
 				if (m_pilot.speed > 0.01f) {
