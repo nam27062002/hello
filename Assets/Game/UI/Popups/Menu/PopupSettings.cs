3c32f1e05a3f5346cb2e8ebba645c6fa92132eb0
David Germade <david.germade@ubisoft.com>
2016-08-30 14:11:58 +0200

    POPUP SETTINGS: Changed to use tabs. Only two tabs so far: "Options" and "Save". "Options" tab contains what it was shown on popup settings before.
    Former-commit-id: 4f625ef73582f920ffa3847bfd7c563eaa8bc6bb

--- Assets/Game/UI/Popups/Menu/PopupSettings.cs
+++ Assets/Game/UI/Popups/Menu/PopupSettings.cs
@@ -7,10 +7,6 @@
 // INCLUDES																	  //
 //----------------------------------------------------------------------------//
 using UnityEngine;
-using System;
-using System.Collections.Generic;
-using UnityEngine.UI;
-
 //----------------------------------------------------------------------------//
 // CLASSES																	  //
 //----------------------------------------------------------------------------//
@@ -25,89 +21,4 @@ public class PopupSettings : MonoBehaviour {
 	public static readonly string PATH = "UI/Popups/Settings/PF_PopupSettings";
 
     public const string KEY_SETTINGS_LANGUAGE = "SETTINGS_LANGUAGE";
-
-	//------------------------------------------------------------------------//
-	// MEMBERS AND PROPERTIES												  //
-	//------------------------------------------------------------------------//
-	// Exposed
-	[SerializeField] private Localizer m_languageText = null;
-
-	// Internal
-	private List<DefinitionNode> m_languageDefs;
-	private int m_selectedIdx = -1;
-
-	//------------------------------------------------------------------------//
-	// GENERIC METHODS														  //
-	//------------------------------------------------------------------------//
-	/// <summary>
-	/// Initialization.
-	/// </summary>
-	public void Awake() {
-		// Check required params
-		Debug.Assert(m_languageText != null, "Required field");
-
-		// Cache language definitions and init with selected language
-		// [AOC] TODO!! Exclude languages marked to be excluded based on platform
-		m_languageDefs = DefinitionsManager.SharedInstance.GetDefinitionsByVariable(DefinitionsCategory.LOCALIZATION, "iOS", "true");
-		DefinitionsManager.SharedInstance.SortByProperty(ref m_languageDefs, "order", DefinitionsManager.SortType.NUMERIC);
-
-		// Find current language
-		for(int i = 0; i < m_languageDefs.Count; i++) {
-            if(m_languageDefs[i].sku == LocalizationManager.SharedInstance.GetCurrentLanguageSKU ()) {
-				m_selectedIdx = i;
-				break;
-			}
-		}
-
-		// Update text
-		RefreshText();
-	}
-
-	/// <summary>
-	/// Update textfield with currently selected language
-	/// </summary>
-	private void RefreshText() {
-		m_languageText.Localize(m_languageDefs[m_selectedIdx].Get("tidName"));	// [AOC] CHECK!! Each language in its own language
-	}
-
-	//------------------------------------------------------------------------//
-	// CALLBACKS															  //
-	//------------------------------------------------------------------------//
-	/// <summary>
-	/// Select previous language.
-	/// </summary>
-	public void OnSelectPreviousLanguage() {
-		// Change selected index
-		m_selectedIdx--;
-		if(m_selectedIdx < 0) m_selectedIdx = m_languageDefs.Count - 1;
-
-		// Change localization!
-        if (LocalizationManager.SharedInstance.SetLanguage (m_languageDefs [m_selectedIdx].sku))
-        {
-            PlayerPrefs.SetString(KEY_SETTINGS_LANGUAGE, m_languageDefs [m_selectedIdx].sku);
-        }
-
-		Messenger.Broadcast(EngineEvents.LANGUAGE_CHANGED);
-		// Update text!
-		RefreshText();
-	}
-
-	/// <summary>
-	/// Select next language.
-	/// </summary>
-	public void OnSelectNextLanguage() {
-		// Change selected index
-		m_selectedIdx++;
-		if(m_selectedIdx >= m_languageDefs.Count) m_selectedIdx = 0;
-
-		// Change localization!
-        if (LocalizationManager.SharedInstance.SetLanguage (m_languageDefs [m_selectedIdx].sku))
-        {
-            PlayerPrefs.SetString(KEY_SETTINGS_LANGUAGE, m_languageDefs [m_selectedIdx].sku);
-        }
-
-		Messenger.Broadcast(EngineEvents.LANGUAGE_CHANGED);
-		// Update text!
-		RefreshText();
-	}
 }
