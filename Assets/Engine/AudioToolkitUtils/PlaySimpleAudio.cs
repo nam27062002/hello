f050136b91d2d9a5228b71896196593f849ca497
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2016-09-27 15:12:51 +0200

    New Audio system WIP
    Former-commit-id: 54337719952baef3bc3045b306fd455fb591d956

--- Assets/Engine/AudioToolkitUtils/PlaySimpleAudio.cs
+++ Assets/Engine/AudioToolkitUtils/PlaySimpleAudio.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlaySimpleAudio : MonoBehaviour {
+namespace UbiBCN{
+	public class PlaySimpleAudio : MonoBehaviour {
 
-	public string m_audioId;
-	public void Play () {
-		AudioController.Play(m_audioId);
+		public string m_audioId;
+		public void Play () {
+			if ( !string.IsNullOrEmpty(m_audioId) )
+				AudioController.Play(m_audioId);
+		}
 	}
 }
