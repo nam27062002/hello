e964bcbe5b6ed08ac6bc395156c4d4597b3d764c
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2016-09-28 09:24:24 +0200

    Results Screen: Development - New results screen logic, using predefined setups in the art scene to show a nice, cool animated 3d view.
    Former-commit-id: dcd66745207095de4dd1e16a911cb2ce910bd8da

--- Assets/Game/UI/Menu/ResultsScreen/ResultsSceneController.cs
+++ Assets/Game/UI/Menu/ResultsScreen/ResultsSceneController.cs
@@ -24,10 +24,17 @@ public class ResultsSceneController : MonoBehaviour {
 	//------------------------------------------------------------------------//
 	// Exposed references
 	[SerializeField] private Camera m_mainCamera;
-	[SerializeField] private Camera m_resultsCamera;
-	
+
+	[Space]
 	[SerializeField] private GameObject m_gameUI;
 	[SerializeField] private GameObject m_resultsUI;
+
+	[Space]
+	[Tooltip("Default scene setup prefab to be used in levels where no setup can be found.")]
+	[SerializeField] private GameObject m_defaultSetupPrefab = null;
+
+	// Internal
+	private ResultsSceneSetup[] m_sceneSetups = null;
 
 	//------------------------------------------------------------------------//
 	// GENERIC METHODS														  //
@@ -36,9 +43,19 @@ public class ResultsSceneController : MonoBehaviour {
 	/// Initialization.
 	/// </summary>
 	private void Awake() {
-		// Disable results camera and UI
-		m_resultsCamera.gameObject.SetActive(false);
+		// Disable results UI
 		m_resultsUI.SetActive(false);
+
+		// Subscribe to external events
+		Messenger.AddListener(GameEvents.GAME_LEVEL_LOADED, OnLevelLoaded);
+	}
+
+	/// <summary>
+	/// Destructor.
+	/// </summary>
+	private void OnDestroy() {
+		// Unsubscribe to external events
+		Messenger.RemoveListener(GameEvents.GAME_LEVEL_LOADED, OnLevelLoaded);
 	}
 
 	//------------------------------------------------------------------------//
@@ -49,17 +66,43 @@ public class ResultsSceneController : MonoBehaviour {
 	/// dragon and UI.
 	/// </summary>
 	public void Show() {
-		// Switch cameras
-		m_mainCamera.gameObject.SetActive(false);
-		m_resultsCamera.gameObject.SetActive(true);
-
 		// Activate and initialize UI
 		m_gameUI.SetActive(false);
 		m_resultsUI.SetActive(true);
 		m_resultsUI.GetComponentInChildren<ResultsScreenController>().Initialize();
+
+		// Turn off main camera
+		m_mainCamera.gameObject.SetActive(false);
+
+		// Select a random scene setup and launch it!
+		ResultsSceneSetup targetSetup = m_sceneSetups.GetRandomValue();
+		targetSetup.gameObject.SetActive(true);
+		targetSetup.LaunchAnim();
 	}
 
 	//------------------------------------------------------------------------//
 	// CALLBACKS															  //
 	//------------------------------------------------------------------------//
+	/// <summary>
+	/// The game level has been loaded.
+	/// </summary>
+	private void OnLevelLoaded() {
+		// Find all suitable result scene setups in the loaded level
+		// We have a special component for that, look for it
+		ResultsSceneSetupList setupList = GameObject.FindObjectOfType<ResultsSceneSetupList>();
+		if(setupList != null && setupList.scenesList.Length > 0) {
+			m_sceneSetups = setupList.scenesList;
+		} else {
+			// If no setup was found (i.e. test levels), use the placeholder prefab
+			// Create a new instance and add it to the array
+			GameObject newSetupObj = GameObject.Instantiate<GameObject>(m_defaultSetupPrefab);
+			m_sceneSetups = new ResultsSceneSetup[1];
+			m_sceneSetups[0] = newSetupObj.GetComponent<ResultsSceneSetup>();
+		}
+
+		// Disable all found setups
+		for(int i = 0; i < m_sceneSetups.Length; i++) {
+			m_sceneSetups[i].gameObject.SetActive(false);
+		}
+	}
 }
