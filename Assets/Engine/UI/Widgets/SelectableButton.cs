e221db14ad5e2693c02e37f0a76cbb0fe590d22d
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2016-10-03 17:37:27 +0200

    Selectable Button: Development - Created as complement to the standard button class. Adds an extra state "Selected", non-interactable, and adds support to transitions to it. Usually used with tabs. Tab System: Development - Added support for disabled tabs. Now works with Selectable buttons rather than default buttons.
    Equipment Screen: Development - Changed tabs to the new system. Pets and photo tabs are disabled if the selected dragon is not owned.
    Goals Screen: Development - Changed tabs to the new system.
    Pause Popup: Development - Changed tabs to the new system.
    Settings Popup: Development - Changed tabs to the new system.
    Control Panel: Development  Changed tabs to the new system.
    
    Results Screen Default Prefab: Development - Added audio listener, refactored.
    
    
    Former-commit-id: a1dc792f6f90a5411e208344ed635c2adeb27265

---
+++ Assets/Engine/UI/Widgets/SelectableButton.cs
@@ -0,0 +1,106 @@
+﻿// SelectableButton.cs
+// 
+// Created by Alger Ortín Castellví on 25/05/2016.
+// Copyright (c) 2016 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Add an extra state to the Button component (selected), mostly used for tab-like behaviour.
+/// Will override the "Disabled" state (aka a button cannot be selected AND disabled at the same time).
+/// Simple implementation, won't work properly if misused.
+/// </summary>
+[RequireComponent(typeof(Button))]
+public class SelectableButton : MonoBehaviour {
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
+	
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
+	// Exposed setup
+	// Support the 3 transition types, store original value
+	[SerializeField] private Color m_transitionColor = Colors.green;
+	private Color m_transitionColorDisabled = Colors.gray;
+
+	[SerializeField] private string m_transitionAnimationTrigger = "Selected";
+	private string m_transitionAnimationTriggerDisabled = "Disabled";
+
+	[SerializeField] private Sprite m_transitionSprite = null;
+	private Sprite m_transitionSpriteDisabled = null;
+
+	// Internal
+	private bool m_selected = false;
+	public bool selected {
+		get { return m_selected; }
+	}
+
+	private Button m_button = null;
+	public Button button {
+		get { 
+			if(m_button == null) {
+				// Store button ref
+				m_button = GetComponent<Button>();
+
+				// Init backup values
+				if(m_button != null) {
+					m_transitionColorDisabled = m_button.colors.disabledColor;
+					m_transitionAnimationTriggerDisabled = m_button.animationTriggers.disabledTrigger;
+					m_transitionSpriteDisabled = m_button.spriteState.disabledSprite;
+				}
+			}
+			return m_button;
+		}
+	}
+
+	//------------------------------------------------------------------------//
+	// GENERIC METHODS														  //
+	//------------------------------------------------------------------------//
+
+	//------------------------------------------------------------------------//
+	// OTHER METHODS														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Sets the selected state.
+	/// </summary>
+	/// <param name="_selected">Whether to select or unselect the button.</param>
+	/// <param name="_stayDisabled">If leaving selection state, whether to go to normal state or keep the button disabled. Doesn't apply if <paramref name="_selected"/> is <c>true</c></param>
+	public void SetSelected(bool _selected, bool _stayDisabled = false) {
+		// Change button's transition
+		// Color
+		ColorBlock newColors = button.colors;
+		newColors.disabledColor = _selected ? m_transitionColor : m_transitionColorDisabled;
+		button.colors = newColors;
+
+		// Trigger
+		AnimationTriggers newTriggers = button.animationTriggers;
+		newTriggers.disabledTrigger = _selected ? m_transitionAnimationTrigger : m_transitionAnimationTriggerDisabled;
+		button.animationTriggers = newTriggers;
+
+		// Sprite
+		SpriteState newSprites = button.spriteState;
+		newSprites.disabledSprite = _selected ? m_transitionSprite : m_transitionSpriteDisabled;
+		button.spriteState = newSprites;
+
+		// Consume trigger to reset animation state
+		if(button.transition == Selectable.Transition.Animation) {
+			button.animator.ResetTrigger(m_transitionAnimationTrigger);
+		}
+
+		// Set button's disabled state
+		// Delay it until the end of frame, otherwise the transition setup wont be updated in time
+		// Make it interactable by default, will be re-applied on the coroutine
+		bool willBeInteractable = _selected ? false : !_stayDisabled;	// Disabled if selected or forced by flag
+		button.interactable = willBeInteractable;
+		m_selected = _selected;
+	}
+}
