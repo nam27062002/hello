ccb1bca131a1753a4953dbe2964cd4a31b6d3ef8
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2016-10-19 12:47:36 +0200

    Changes
    Former-commit-id: ee2d5a1508ea70cf0cefe1c4581de090e697d3d9

--- Assets/Game/Gameplay/Dragons/GlideBehaviour.cs
+++ Assets/Game/Gameplay/Dragons/GlideBehaviour.cs
@@ -5,10 +5,13 @@ public class GlideBehaviour : StateMachineBehaviour {
 
 	public Range m_glidingTime = new Range(4f, 6f);
 	private float m_timer = 4f;
-
+	private Animator m_animator;
+	private FlyLoopBehaviour m_flyLoopBehaviour;
 
 	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		if (m_flyLoopBehaviour == null)
+			m_flyLoopBehaviour = animator.GetBehaviour<FlyLoopBehaviour>();
 		ResetTimer();
 	}
 	
@@ -20,7 +23,10 @@ public class GlideBehaviour : StateMachineBehaviour {
 	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {		
 		m_timer -= Time.deltaTime;
 		if (m_timer <= 0) {
+			if (m_flyLoopBehaviour)
+				m_flyLoopBehaviour.ResetTimer();
 			animator.SetBool("glide", false);
+
 		}
 	}
 
