4d442fd718e4d5f90ec91b8e1cd63d49633e5c9c
David Germade <david.germade@ubisoft.com>
2016-11-07 12:16:58 +0100

    UPGRADE VERSION: 426 code is treated as if it was 200 (SUCCESS) for all game commands.
    Former-commit-id: 1fc384100b4f406426bf1d94ab82c1ccdae604e7

--- Assets/Game/Core/GameServerManagerCalety.cs
+++ Assets/Game/Core/GameServerManagerCalety.cs
@@ -525,6 +525,13 @@ public class GameServerManagerCalety : GameServerManager
         Error error = null;
         Dictionary<string, object> response = null;
 
+        // 426 code means that there's a new version of the application available. We simulate that the response was 200 (SUCCESS) because we don't want to force the
+        // user to upgrade
+        if (statusCode == 426)
+        {
+            statusCode = 200;
+        }
+
         int status = (int)Math.Floor(statusCode / 100.0);
 
         switch (status)
@@ -741,7 +748,7 @@ public class GameServerManagerCalety : GameServerManager
     private void CaletyExtensions_Init()
     {
 		// All codes need to be handled in order to be sure that the game will continue regardless the network error
-		int[] codes = new int[] { 200, 301, 302, 303, 304, 305, 306, 307, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 500, 501, 502, 503, 504, 505 };
+		int[] codes = new int[] { 200, 301, 302, 303, 304, 305, 306, 307, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 426, 500, 501, 502, 503, 504, 505 };
         NetworkManager.SharedInstance.RegistryEndPoint(COMMAND_PING, NetworkManager.EPacketEncryption.E_ENCRYPTION_AES_NONE, codes, CaletyExtensions_OnPing);
         NetworkManager.SharedInstance.RegistryEndPoint(COMMAND_TIME, NetworkManager.EPacketEncryption.E_ENCRYPTION_AES_NONE, codes, CaletyExtensions_OnGetTime);
         NetworkManager.SharedInstance.RegistryEndPoint(COMMAND_GET_PERSISTENCE, NetworkManager.EPacketEncryption.E_ENCRYPTION_AES_NONE, codes, CaletyExtensions_OnGetPersistenceResponse);
