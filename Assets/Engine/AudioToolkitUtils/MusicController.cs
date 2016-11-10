f863c58cfba066878d9175fb755c5b896c562257
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2016-11-10 11:05:01 +0100

    remove old audio controller from camera, and add a music controller with a test
    Former-commit-id: 467e812408f10170fafa92deb58dc617e8de4139

---
+++ Assets/Engine/AudioToolkitUtils/MusicController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour {
+
+	public string m_music = "Music_Test";
+	public float m_volume = 0.1f;
+	// Use this for initialization
+	void Start () {
+		AudioController.PlayMusic(m_music, m_volume);
+	}
+	
+	void OnDestroy(){
+		AudioController.StopMusic(0.3f);
+	}
+}
