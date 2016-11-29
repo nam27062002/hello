6ab4debabdf831af072567aa2344979bef3e44c0
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2016-11-29 10:46:37 +0100

    menu random animations for classic
    Former-commit-id: a1e30ab009bc81cc60af6a81f1f657c54df00c8a

---
+++ Assets/Game/Gameplay/Dragons/AltAnimationBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class AltAnimationBehaviour : StateMachineBehaviour {
+
+	
+
+	public Range m_altAnimationTimer = new Range(4f, 6f);
+	public int m_numAlternativeAnim;
+	private float m_timer = 4f;
+	private int m_currentAnimIndex = -1;
+
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		m_timer = m_altAnimationTimer.GetRandom();
+		m_currentAnimIndex = -1;
+	}
+	
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	}
+
+	// OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {		
+		// if main looping animation
+		if ( m_currentAnimIndex < 0 )	
+		{
+			m_timer -= Time.deltaTime;
+			if (m_timer <= 0) 
+			{
+				// Set alternative animation
+				m_currentAnimIndex = Random.Range(0,m_numAlternativeAnim);
+				animator.SetInteger("AltAnimation", m_currentAnimIndex);
+			}	
+		}
+	}
+
+
+}
