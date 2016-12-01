0aad3351a74fbbc79d7bee64e49f5e2f5ce1f33d
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2016-12-01 19:09:47 +0100

    Goals Screen: Development - Dragon is now flying on the background! Menu Screen Activator: Development - Class created to easily activate/deactivate objects based on current menu screen.
    Former-commit-id: dd738808921d19ec7b0a3affc12ff6df6cd99616

---
+++ Assets/Game/UI/Menu/Shared/MenuScreenActivator.cs
@@ -0,0 +1,90 @@
+﻿// MenuScreenActivator.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 01/12/2016.
+// Copyright (c) 2016 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------//
+// INCLUDES																//
+//----------------------------------------------------------------------//
+using UnityEngine;
+
+//----------------------------------------------------------------------//
+// CLASSES																//
+//----------------------------------------------------------------------//
+/// <summary>
+/// Activate object only on specific screens.
+/// </summary>
+public class MenuScreenActivator : MonoBehaviour {
+	//------------------------------------------------------------------//
+	// CONSTANTS														//
+	//------------------------------------------------------------------//
+
+	//------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES											//
+	//------------------------------------------------------------------//
+	// Exposed setup
+	[SerializeField] private MenuScreens m_screen = MenuScreens.NONE;
+
+	// Internal
+	private MenuScreensController m_screensController = null;
+
+	//------------------------------------------------------------------//
+	// GENERIC METHODS													//
+	//------------------------------------------------------------------//
+	/// <summary>
+	/// Initialiation.
+	/// </summary>
+	private void Awake() {
+		
+	}
+
+	/// <summary>
+	/// First update call.
+	/// </summary>
+	private void Start() {
+		// Get required references
+		m_screensController = InstanceManager.GetSceneController<MenuSceneController>().screensController;
+
+		// Subscribe to external events
+		Messenger.AddListener<NavigationScreenSystem.ScreenChangedEventData>(EngineEvents.NAVIGATION_SCREEN_CHANGED, OnScreenChanged);
+
+		// Apply initial state
+		Apply();
+	}
+
+	/// <summary>
+	/// Destructor.
+	/// </summary>
+	private void OnDestroy() {
+		// Subscribe to external events
+		Messenger.RemoveListener<NavigationScreenSystem.ScreenChangedEventData>(EngineEvents.NAVIGATION_SCREEN_CHANGED, OnScreenChanged);
+	}
+
+	//------------------------------------------------------------------//
+	// OTHER METHODS													//
+	//------------------------------------------------------------------//
+	private void Apply() {
+		// Check whether active screen is the target one
+		if(m_screensController == null) return;
+
+		// Special case if NONE
+		if(m_screen == MenuScreens.NONE) {
+			gameObject.SetActive(true);
+		} else {
+			gameObject.SetActive(m_screensController.currentScene == m_screensController.GetScene((int)m_screen));
+		}
+	}
+
+	//------------------------------------------------------------------//
+	// CALLBACKS														//
+	//------------------------------------------------------------------//
+	/// <summary>
+	/// A navigation screen has changed.
+	/// </summary>
+	/// <param name="_data">The event's data.</param>
+	public void OnScreenChanged(NavigationScreenSystem.ScreenChangedEventData _data) {
+		// Refresh
+		Apply();
+	}
+}
