a635e234545252b00e95ec2d32173dc0899b2e26
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2016-12-22 12:47:01 +0100

    Gacha: Development - Integration of the new Egg assets, animations and FX.
    Former-commit-id: c0c9980419209aa14b0b345fa41f0b692caee81a

--- Assets/Game/UI/Menu/Eggs/Behaviours/OpenEggBehaviour.cs
+++ Assets/Game/UI/Menu/Eggs/Behaviours/OpenEggBehaviour.cs
@@ -61,6 +61,9 @@ public class OpenEggBehaviour : MonoBehaviour, IPointerClickHandler {
 		if(!this.enabled) return;
 		if(m_tapCount >= TAPS_TO_OPEN) return;
 
+		// Aux vars
+		EggController eggController = GetComponent<EggController>();
+
 		// Increase tap count!
 		m_tapCount++;
 
@@ -68,19 +71,25 @@ public class OpenEggBehaviour : MonoBehaviour, IPointerClickHandler {
 		if(m_tapCount >= TAPS_TO_OPEN) {
 			// Do it!
 			// Open FX are managed externally
-			GetComponent<EggController>().eggData.Collect();			
+			eggController.eggData.Collect();
+
+			// Reset count
+			m_tapCount = 0;
 		}
 
 		// Otherwise show some FX
 		else {
-			// Kill any existing tween
+			/*// Kill any existing tween
 			DOTween.Kill("eggTap");
 
 			// Scale down a bit every time xD
 			// Use relative scaling
 			Vector3 targetScale = new Vector3(transform.localScale.x * 1.1f, transform.localScale.y * 0.9f, transform.localScale.z * 1.1f);
-			transform.DOScale(targetScale, 0.5f).SetEase(Ease.OutElastic).SetId("eggTap");
+			transform.DOScale(targetScale, 0.5f).SetEase(Ease.OutElastic).SetId("eggTap");*/
 		}
+
+		// Refresh view
+		eggController.Refresh();
 	}
 }
 
