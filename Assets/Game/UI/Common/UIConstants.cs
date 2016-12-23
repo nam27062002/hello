5af01bbcb3b4a8f3143523138b88c8e46a34f2dc
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2016-12-23 11:31:18 +0100

    Egg: Fix - Fixed issue when egg was stored in a temporal state. Eggs: Assets - Tuned colors and animations. Rarity colors are now Yellow-Blue-Cyan. UIConstants: Development - Converted into a ScriptableObject to be able to visually tune values.
    Former-commit-id: 406fbe7afdfa7c67d120b23c52c10a8e6af07493

--- Assets/Game/UI/Common/UIConstants.cs
+++ Assets/Game/UI/Common/UIConstants.cs
@@ -15,21 +15,60 @@ using UnityEngine;
 /// <summary>
 /// Static class for some global UI constants and utils.
 /// </summary>
-public static class UIConstants {
+public class UIConstants : SingletonScriptableObject<UIConstants> {
 	//------------------------------------------------------------------------//
 	// CONSTANTS															  //
 	//------------------------------------------------------------------------//
 	// Text Mesh Pro shortcuts
-	public const string TMP_SPRITE_SC = "<sprite name=\"icon_sc\">";
-	public const string TMP_SPRITE_PC = "<sprite name=\"icon_pc\">";
+	[Separator("Text Mesh Pro shortcuts")]
+	[SerializeField] private string m_tmpSpriteSC = "<sprite name=\"icon_sc\">";
+	public static string TMP_SPRITE_SC {
+		get { return instance.m_tmpSpriteSC; }
+	}
+
+	[SerializeField] private string m_tmpSpritePC = "<sprite name=\"icon_pc\">";
+	public static string TMP_SPRITE_PC {
+		get { return instance.m_tmpSpritePC; }
+	}
 
 	// Colors
-	public static readonly Color COINS_TEXT_COLOR = new Color(1f, 0.8f, 0.1f);
-	public static readonly Color PC_TEXT_COLOR = new Color(0.9f, 0.5f, 0.8f);
+	[Separator("Colors")]
+	[SerializeField] private Color m_coinsTextColor = new Color(1f, 0.8f, 0.1f);
+	public static Color COINS_TEXT_COLOR {
+		get { return instance.m_coinsTextColor; }
+	}
 
-	public static readonly Color RARITY_COMMON_COLOR = new Color(1f, 0.8f, 0.1f);	// Yellow-ish
-	public static readonly Color RARITY_RARE_COLOR = new Color(0f, 1f, 0.5f);		// Cyan-ish
-	public static readonly Color RARITY_EPIC_COLOR = new Color(0.5f, 0f, 0.5f);		// Purple-ish
+	[SerializeField] private Color m_pcTextColor = new Color(0.9f, 0.5f, 0.8f);
+	public static Color PC_TEXT_COLOR {
+		get { return instance.m_pcTextColor; }
+	}
+
+	[Space]
+	[SerializeField] private Color m_rarityCommonColor = new Color(1f, 0.8f, 0.1f);	// Yellow-ish
+	public static Color RARITY_COMMON_COLOR {
+		get { return instance.m_rarityCommonColor; }
+	}
+
+	[SerializeField] private Color m_rarityRareColor = new Color(0f, 0.5f, 1f);	// Blue-ish
+	public static Color RARITY_RARE_COLOR {
+		get { return instance.m_rarityRareColor; }
+	}
+
+	[SerializeField] private Color m_rarityEpicColor = new Color(0f, 1f, 0.5f);		// Cyan-ish
+	public static Color RARITY_EPIC_COLOR {
+		get { return instance.m_rarityEpicColor; }
+	}
+
+	// Asset paths in Resources
+	[Separator("Asset paths in Resources")]
+	[SerializeField] private string m_uiSpritesheetPath = "UI/Common/SpriteSheets/UI_spritesheet";
+	public static string UI_SPRITESHEET_PATH {
+		get { return instance.m_uiSpritesheetPath; }
+	}
+
+	//------------------------------------------------------------------------//
+	// METHODS																  //
+	//------------------------------------------------------------------------//
 	public static Color GetRarityColor(string _raritySku) {
 		switch(_raritySku) {
 			case "common":	return RARITY_COMMON_COLOR;		break;
@@ -38,7 +77,4 @@ public static class UIConstants {
 		}
 		return Color.white;
 	}
-
-	// Asset paths in Resources
-	public const string UI_SPRITESHEET_PATH = "UI/Common/SpriteSheets/UI_spritesheet";
 }
