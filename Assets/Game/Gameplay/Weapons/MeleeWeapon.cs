28af81d5aeb81fa9cb6778eb179bededb10cdaf6
Marc Saña <marc.sana@ubisoft.com>
2017-01-04 10:27:51 +0100

    placeholder for goblins
    Former-commit-id: d926b89c23afc308b883dd7b5005add8efd6e0bd

--- Assets/Game/Gameplay/Weapons/MeleeWeapon.cs
+++ Assets/Game/Gameplay/Weapons/MeleeWeapon.cs
@@ -4,20 +4,24 @@ using System.Collections;
 public class MeleeWeapon : MonoBehaviour {
 
 	private Collider m_weapon;
+	private TrailRenderer m_trail;
 
 	private float m_damage;
 	public float damage { set { m_damage = value; } }
 
 	void Awake() {
 		m_weapon = GetComponent<Collider>();
+		m_trail = GetComponentInChildren<TrailRenderer>();
 	}
 
 	void OnEnable() {
 		m_weapon.enabled = true;
+		if (m_trail) m_trail.enabled = true;
 	}
 
 	void OnDisable() {
 		m_weapon.enabled = false;
+		if (m_trail) m_trail.enabled = false;
 	}
 
 	void OnTriggerEnter(Collider _other) {
