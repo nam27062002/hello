557fd663a2acfae2155b88b376b349236f34a36a
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2017-01-09 17:00:51 +0100

    Script Templates: Assets - Updated to year 2017.
    Former-commit-id: d3d75c6806e83e89a0f87ae82731477e60ca8f2d

--- Assets/Tools/Templates/MonoBehaviourTemplate.cs
+++ Assets/Tools/Templates/MonoBehaviourTemplate.cs
@@ -1,8 +1,8 @@
 ﻿// MonoBehaviourTemplate.cs
 // Hungry Dragon
 // 
-// Created by Alger Ortín Castellví on DD/MM/2016.
-// Copyright (c) 2016 Ubisoft. All rights reserved.
+// Created by Alger Ortín Castellví on DD/MM/2017.
+// Copyright (c) 2017 Ubisoft. All rights reserved.
 
 //----------------------------------------------------------------------------//
 // INCLUDES																	  //
