e429804d71b54db584427a55579d2279d2409e63
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2017-01-11 15:14:15 +0100

    public root on dragon part follow
    Former-commit-id: 4a220646e230efaae046b4e3215856910779c565

--- Assets/Game/Gameplay/Dragons/DragonPartFollow.cs
+++ Assets/Game/Gameplay/Dragons/DragonPartFollow.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class DragonPartFollow : MonoBehaviour {
 
 	
-	private Transform m_follow;
+	public Transform m_root;
 	public List<Transform> m_parts;
 	public float springSpeed = 1.0f;
 	private class PartInfo
@@ -21,7 +21,8 @@ public class DragonPartFollow : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-		m_follow = transform;
+		if ( m_root == null )
+			m_root = transform;
 		m_partInfos = new List<PartInfo>();
 		for( int i = 0; i<m_parts.Count; i++ )
 		{
@@ -79,7 +80,7 @@ public class DragonPartFollow : MonoBehaviour {
 			Transform follow;
 			if ( i == 0)
 			{
-				follow = m_follow;
+				follow = m_root;
 			}
 			else
 			{
