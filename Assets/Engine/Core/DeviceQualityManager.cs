e9cfb1012be9e59ca13be4f996f6795ea417a0e1
David Germade <david.germade@ubisoft.com>
2017-01-19 10:51:14 +0100

    FEATURE SETTINGS: Code refactored.
    Former-commit-id: baf48d5f98c21fd171a8a4242220cee1bbe0b8bb

--- Assets/Engine/Core/DeviceQualityManager.cs
+++ Assets/Engine/Core/DeviceQualityManager.cs
@@ -18,47 +18,7 @@ using System.Collections.Generic;
 /// Quality profiles are used to define the values for some parameters that configure how some features perform.
 /// </summary>
 public class DeviceQualityManager
-{
-    public class FeatureSettings
-    {
-        private const string RATING_KEY = "rating";
-
-        public float Rating { get; set; }
-
-        private void Reset()
-        {
-            Rating = 0f;
-        }
-
-        protected virtual void ExtendedReset() {}
-        
-        public void FromJSON(SimpleJSON.JSONNode json)
-        {
-            Reset();
-
-            string key = RATING_KEY;
-            if (json.ContainsKey(key))
-            {
-                Rating = json[key].AsFloat;
-            }
-
-            ExtendedFromJSON(json);
-        }
-
-        protected virtual void ExtendedFromJSON(SimpleJSON.JSONNode json) {}
-
-        public SimpleJSON.JSONNode ToJSON()
-        {
-            SimpleJSON.JSONClass returnValue = new SimpleJSON.JSONClass();
-            returnValue.Add(RATING_KEY, Rating.ToString());
-            ExtendedToJSON(returnValue);            
-
-            return returnValue;
-        }
-
-        protected virtual void ExtendedToJSON(SimpleJSON.JSONNode json) {}
-    }  
-    
+{        
     public DeviceQualityManager()
     {
         Clear();
