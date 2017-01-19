c8e1258a535115c1466502f51eb88eab386401ec
David Germade <david.germade@ubisoft.com>
2017-01-19 12:48:59 +0100

    FEATURE SETTINGS: Profile settings can be overriden per device.
    Former-commit-id: abfd05d024f2cd408d6f6a8033f9ab41ef647a1a

--- Assets/Engine/Core/FeatureSettings.cs
+++ Assets/Engine/Core/FeatureSettings.cs
@@ -1,35 +1,50 @@
-﻿public class FeatureSettings
+﻿using SimpleJSON;
+public class FeatureSettings
 {
-    private const string RATING_KEY = "rating";
+    public const string KEY_RATING = "rating";
 
     public float Rating { get; set; }
 
-    private void Reset()
+    public void Reset()
     {
         Rating = 0f;
+        ExtendedReset();
     }
 
     protected virtual void ExtendedReset() { }
 
-    public void FromJSON(SimpleJSON.JSONNode json)
+    /// <summary>
+    /// Loads this object with the data defined by <c>json</c>. Default values are set for the keys that are not defined in <c>json</c>.
+    /// </summary>    
+    public void FromJSON(JSONNode json)
+    {        
+        Reset();        
+        OverrideFromJSON(json); 
+    }
+    
+    /// <summary>
+    /// Overrides the current data of this object with the data defined in <c>json</c>. Only the keys defined in <c>json</c> are overriden.
+    /// </summary>    
+    public void OverrideFromJSON(JSONNode json)
     {
-        Reset();
-
-        string key = RATING_KEY;
-        if (json.ContainsKey(key))
+        if (json != null)
         {
-            Rating = json[key].AsFloat;
-        }
+            string key = KEY_RATING;
+            if (json.ContainsKey(key))
+            {
+                Rating = json[key].AsFloat;
+            }
 
-        ExtendedFromJSON(json);
+            ExtendedOverrideFromJSON(json);
+        }
     }
 
-    protected virtual void ExtendedFromJSON(SimpleJSON.JSONNode json) { }
+    protected virtual void ExtendedOverrideFromJSON(JSONNode json) { }
 
     public SimpleJSON.JSONNode ToJSON()
     {
         SimpleJSON.JSONClass returnValue = new SimpleJSON.JSONClass();
-        returnValue.Add(RATING_KEY, Rating.ToString());
+        returnValue.Add(KEY_RATING, Rating.ToString());
         ExtendedToJSON(returnValue);
 
         return returnValue;
