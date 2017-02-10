d590c10a68b5c05ea79c32e75249d8a8791bf4e1
Marc Saña <marc.sana@ubisoft.com>
2017-02-10 16:43:46 +0100

    improvements on bird flocking behaviour
    Former-commit-id: 4fe5db44e35f11108f437e80540513ba9da5d6fe

---
+++ Assets/Game/Gameplay/Entities/AI/Brain/Behaviours/BirdLeader.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AI {
+	namespace Behaviour {
+		[System.Serializable]
+		public class BirdLeaderData : FollowLeaderData {}
+
+
+		[CreateAssetMenu(menuName = "Behaviour/Bird Leader")]
+		public class BirdLeader : FollowLeader {
+
+			public override StateComponentData CreateData() {
+				return new BirdLeaderData();
+			}
+
+			public override System.Type GetDataType() {
+				return typeof(BirdLeaderData);
+			}
+
+			protected override void OnInitialise() {
+				m_data = m_pilot.GetComponentData<BirdLeaderData>();
+			}
+					
+			protected override void OnUpdate() {
+				// Update guide function
+				if (m_pilot.guideFunction != null) {
+					Vector3 target = m_pilot.guideFunction.NextPositionAtSpeed(m_pilot.speed);
+					m_pilot.GoTo(target);
+				} else {
+					m_pilot.GoTo(m_pilot.homePosition);
+				}
+
+				base.OnUpdate();
+			}
+		}
+	}
+}
