10af60b52498e24dc241ffb1b9f1f64d70c4f73d
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2017-02-14 10:47:35 +0100

    ambient area particles
    Former-commit-id: 878e29298dbe7d959b784c246375d9d7b5ae77fe

--- Assets/Game/Gameplay/Entities/AI/View/ParticleData.cs
+++ Assets/Game/Gameplay/Entities/AI/View/ParticleData.cs
@@ -7,6 +7,13 @@ public class ParticleData {
 	public string path = "";
 	public Vector3 offset = Vector3.zero;
 
+	public ParticleData()
+	{
+		name = "";
+		path = "";
+		offset = Vector3.zero;
+	}
+
 	public ParticleData( string n, string p, Vector3 o)
 	{
 		name = n;
