528b392586c4788443521299558b70997ba4ccb1
Marc Saña <marc.sana@ubisoft.com>
2017-02-17 18:45:23 +0100

    WIP Spartakus, final model
    Former-commit-id: 1b0c550cf6c2c2b940538f76520cccde7f3929de

---
+++ Assets/Game/Gameplay/Entities/AI/View/SpartakusViewControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpartakusViewControl : ViewControl {
+
+	[SeparatorAttribute("Dizzy")]
+	[SerializeField] private GameObject m_stars;
+
+
+
+	public override void Spawn(ISpawner _spawner) {
+		base.Spawn(_spawner);
+
+		m_stars.SetActive(false);
+	}
+
+	protected override void OnSpecialAnimationEnter(SpecialAnims _anim) {
+		switch(_anim) {
+			case SpecialAnims.A: break;
+			case SpecialAnims.B: m_stars.SetActive(true); break;
+		}
+	}
+
+	protected override void OnSpecialAnimationExit(SpecialAnims _anim) {
+		switch(_anim) {
+			case SpecialAnims.A: break;
+			case SpecialAnims.B: m_stars.SetActive(false); break;
+		}
+	}
+}
