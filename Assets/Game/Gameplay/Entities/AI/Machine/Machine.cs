4b04cbbb40fdef647818fb8c7991841c5473dbf9
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2017-02-21 17:24:45 +0100

    pet dog WIP
    Former-commit-id: 5bda9b2c828e89548f03d8fd0dce8b6ddbdf1537

--- Assets/Game/Gameplay/Entities/AI/Machine/Machine.cs
+++ Assets/Game/Gameplay/Entities/AI/Machine/Machine.cs
@@ -98,6 +98,9 @@ namespace AI {
 		private bool m_freezing = false;
 		private float m_freezingMultiplier = 1;
 
+
+		// Activating
+		UnityEngine.Events.UnityAction m_deactivateCallback;
 		//---------------------------------------------------------------------------------
 
 		// Use this for initialization
@@ -667,6 +670,20 @@ namespace AI {
 			if (m_sensor != null) {
 				m_sensor.OnDrawGizmosSelected(transform);
 			}
+		}
+
+		public void Deactivate( float duration, UnityEngine.Events.UnityAction _action)
+		{
+			gameObject.SetActive(false);
+			m_deactivateCallback = _action;
+			Invoke("Activate", duration);
+		}
+
+		void Activate()
+		{
+			gameObject.SetActive(true);
+			if ( m_deactivateCallback != null )
+				m_deactivateCallback();
 		}
 	}
 }
