a3e4fec4a2700663b51f2f8c839fbe73ec22490f
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2017-03-16 14:27:28 +0100

    Custom Event System: Development - Created custom event system inheriting from Unity’s default one to be able to expand from it. Not used for now. Custom Input Module: Development - Created custom input module inheriting from Unity’s default one to be able to expand from it. Not used for now. Mouse Events Handler: Development - Simple component to help dealing better with OnMouseUp, OnMouseDown and OnMouseUpAsButton events on GameObjects.
    Former-commit-id: a47bee145085b99cfff01a47424461ab9236604e

---
+++ Assets/Engine/Events/CustomEventSystem.cs
@@ -0,0 +1,56 @@
+﻿// CustomEventSystem.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 16/03/2017.
+// Copyright (c) 2017 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Specialization of Unity's EventSystem to do some custom stuff.
+/// </summary>
+public class CustomEventSystem : EventSystem {
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
+	
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
+	// Cache raycast results every time a RaycastAll is triggered
+	private List<RaycastResult> m_lastRaycastResults = new List<RaycastResult>();
+	public List<RaycastResult> lastRaycastResults {
+		get { return m_lastRaycastResults; }
+	}
+
+	//------------------------------------------------------------------------//
+	// METHODS																  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Override RayCastAll method to cache last raycast results.
+	/// </summary>
+	/// <param name="_eventData">Current pointer event data.</param>
+	/// <param name="_raycastResults">List of "hits" to populate.</param>
+	new public void RaycastAll(PointerEventData _eventData, List<RaycastResult> _raycastResults) {
+		Debug.Log("Raycasting all from " + _eventData.currentInputModule.name);
+
+		// Do default Unity's stuff
+		base.RaycastAll(_eventData, _raycastResults);
+
+		// Store results for future access
+		m_lastRaycastResults.Clear();
+		m_lastRaycastResults.AddRange(_raycastResults);
+	}
+
+	//------------------------------------------------------------------------//
+	// CALLBACKS															  //
+	//------------------------------------------------------------------------//
+}
