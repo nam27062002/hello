094547cd446ca2dcf2a62d46de36969c4ed92177
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2017-03-17 11:47:07 +0100

    Menu Show Conditionally: Development - Merged both MenuDragonShowConditionally and MenuScreenActivator into a single class to manage all show conditions. Made some improvements. Improved the editor as well. Photo Button: Development - Now it’s hidden for not-owned dragons.
    Former-commit-id: 07dce0584473e2d702d9ff26bf4a92e0f7de7f84

---
+++ Assets/Game/UI/Menu/Shared/Editor/MenuShowConditionallyEditor.cs
@@ -0,0 +1,118 @@
+﻿// MenuShowConditionallyEditor.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 17/03/2017.
+// Copyright (c) 2017 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+using UnityEditor;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Custom editor for the MenuShowConditionally class.
+/// </summary>
+[CustomEditor(typeof(MenuShowConditionally), true)]	// True to be used by heir classes as well
+[CanEditMultipleObjects]
+public class MenuShowConditionallyEditor : Editor {
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
+
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
+	// Casted target object
+	MenuShowConditionally m_targetMenuShowConditionally = null;
+
+	//------------------------------------------------------------------------//
+	// METHODS																  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// The editor has been enabled - target object selected.
+	/// </summary>
+	private void OnEnable() {
+		// Get target object
+		m_targetMenuShowConditionally = target as MenuShowConditionally;
+	}
+
+	/// <summary>
+	/// The editor has been disabled - target object unselected.
+	/// </summary>
+	private void OnDisable() {
+		// Clear target object
+		m_targetMenuShowConditionally = null;
+	}
+
+	/// <summary>
+	/// Draw the inspector.
+	/// </summary>
+	public override void OnInspectorGUI() {
+		// Instead of modifying script variables directly, it's advantageous to use the SerializedObject and 
+		// SerializedProperty system to edit them, since this automatically handles private fields, multi-object 
+		// editing, undo, and prefab overrides.
+
+		// Update the serialized object - always do this in the beginning of OnInspectorGUI.
+		serializedObject.Update();
+
+		// Loop through all serialized properties and work with special ones
+		SerializedProperty p = serializedObject.GetIterator();
+		p.Next(true);	// To get first element
+		do {
+			// Properties requiring special treatment
+			// Unity's "script" property
+			if(p.name == "m_Script") {
+				// Draw the property, disabled
+				bool wasEnabled = GUI.enabled;
+				GUI.enabled = false;
+				EditorGUILayout.PropertyField(p, true);
+				GUI.enabled = wasEnabled;
+			}
+
+			// Dragon ownership variables
+			else if(p.name == "m_targetDragonSku") {
+				// Disable the whole group if m_hideForDragons property is set to either NONE or ALL
+				SerializedProperty hideForDragonsProp = serializedObject.FindProperty("m_hideForDragons");
+				bool wasEnabled = GUI.enabled;
+				GUI.enabled = (hideForDragonsProp.enumValueIndex != (int)MenuShowConditionally.HideForDragons.NONE) 
+					&& (hideForDragonsProp.enumValueIndex != (int)MenuShowConditionally.HideForDragons.ALL);
+
+				// Draw all ownership properties
+				EditorGUILayout.PropertyField(p, true);
+				EditorGUILayout.PropertyField(serializedObject.FindProperty("m_showIfLocked"), true);
+				EditorGUILayout.PropertyField(serializedObject.FindProperty("m_showIfAvailable"), true);
+				EditorGUILayout.PropertyField(serializedObject.FindProperty("m_showIfOwned"), true);
+
+				// Restore enabled state
+				GUI.enabled = wasEnabled;
+			}
+
+			// Properties we don't want to show (or that are processed elsewhere)
+			else if(p.name == "m_ObjectHideFlags"
+				|| p.name == "m_showIfLocked"
+				|| p.name == "m_showIfAvailable"
+				|| p.name == "m_showIfOwned") {
+				// Do nothing
+			}
+
+			// Default property display
+			else {
+				EditorGUILayout.PropertyField(p, true);
+			}
+		} while(p.NextVisible(false));		// Only direct children, not grand-children (will be drawn by default if using the default EditorGUI.PropertyField)
+
+		// Apply changes to the serialized object - always do this in the end of OnInspectorGUI.
+		serializedObject.ApplyModifiedProperties();
+	}
+
+	/// <summary>
+	/// The scene is being refreshed.
+	/// </summary>
+	public void OnSceneGUI() {
+		// Scene-related stuff
+	}
+}
---
+++ Assets/Game/UI/Menu/Shared/MenuShowConditionally.cs
@@ -0,0 +1,290 @@
+// MenuShowConditionally.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 19/11/2015.
+// Copyright (c) 2015 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------//
+// INCLUDES																//
+//----------------------------------------------------------------------//
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+
+//----------------------------------------------------------------------//
+// CLASSES																//
+//----------------------------------------------------------------------//
+/// <summary>
+/// Simple behaviour to show/hide the target game object depending on several
+/// conditions.
+/// </summary>
+[RequireComponent(typeof(ShowHideAnimator))]
+public class MenuShowConditionally : MonoBehaviour {
+	//------------------------------------------------------------------//
+	// CONSTANTS														//
+	//------------------------------------------------------------------//
+	public enum HideForDragons {
+		IGNORE,
+		FIRST,	// Hide for first dragon
+		LAST,	// Hide for last dragon
+		FIRST_AND_LAST,	// Hide for first and last dragons
+		ALL,	// Hide for all dragons
+		NONE	// Never hide based on dragon
+	}
+
+	public enum ScreenVisibilityMode {
+		SHOW_ON_TARGET_SCREENS,
+		HIDE_ON_TARGET_SCREENS
+	}
+
+	//------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES											//
+	//------------------------------------------------------------------//
+	// Config
+	[Separator("Dragon-based visibility")]
+	
+	[Comment("Higher priority than ownership status (unless set to IGNORE)")]
+	[SerializeField] private HideForDragons m_hideForDragons = HideForDragons.IGNORE;
+
+	[Comment("Ownership of the target dragon", 5f)]
+	[Comment("Leave target dragon sku empty to use the current selected dragon on the menu as target.")]
+	[SkuList(DefinitionsCategory.DRAGONS, true)]
+	[SerializeField] private string m_targetDragonSku = "";
+	[SerializeField] private bool m_showIfLocked = false;
+	[SerializeField] private bool m_showIfAvailable = false;
+	[SerializeField] private bool m_showIfOwned = true;
+
+	[Separator("Screen-based visibility")]
+	[SerializeField] private ScreenVisibilityMode m_mode = ScreenVisibilityMode.HIDE_ON_TARGET_SCREENS;
+	[SerializeField] private List<MenuScreens> m_screens = new List<MenuScreens>();
+
+	[Separator("Animation options")]
+	[SerializeField] private bool m_restartShowAnimation = false;
+
+	// Internal references
+	private ShowHideAnimator m_animator = null;
+
+	// Extra Properties
+	public string targetDragonSku {
+		get {
+			// If no specific dragon is defined, use currently selected dragon
+			if(string.IsNullOrEmpty(m_targetDragonSku)) {
+				return InstanceManager.menuSceneController.selectedDragon;
+			}
+			return m_targetDragonSku;
+		}
+	}
+
+	private MenuScreens currentMenuScreen {
+		get { return InstanceManager.menuSceneController.screensController.currentMenuScreen; }
+	}
+
+	//------------------------------------------------------------------//
+	// GENERIC METHODS													//
+	//------------------------------------------------------------------//
+	/// <summary>
+	/// Initialization.
+	/// </summary>
+	private void Awake() {
+		// Get external references
+		m_animator = GetComponent<ShowHideAnimator>();
+	}
+
+	/// <summary>
+	/// First update.
+	/// </summary>
+	private void Start() {
+		// Subscribe to external events
+		Messenger.AddListener<string>(GameEvents.MENU_DRAGON_SELECTED, OnDragonSelected);
+		Messenger.AddListener<DragonData>(GameEvents.DRAGON_ACQUIRED, OnDragonAcquired);
+		Messenger.AddListener<NavigationScreenSystem.ScreenChangedEventData>(EngineEvents.NAVIGATION_SCREEN_CHANGED, OnScreenChanged);
+
+		// Apply for the first time with current values and without animation
+		Apply(targetDragonSku, currentMenuScreen, false, false);
+	}
+	
+	/// <summary>
+	/// Called every frame.
+	/// </summary>
+	private void Update() {
+
+	}
+
+	/// <summary>
+	/// Destructor.
+	/// </summary>
+	private void OnDestroy() {
+		// Unsubscribe from external events
+		Messenger.RemoveListener<string>(GameEvents.MENU_DRAGON_SELECTED, OnDragonSelected);
+		Messenger.RemoveListener<DragonData>(GameEvents.DRAGON_ACQUIRED, OnDragonAcquired);
+		Messenger.RemoveListener<NavigationScreenSystem.ScreenChangedEventData>(EngineEvents.NAVIGATION_SCREEN_CHANGED, OnScreenChanged);
+	}
+
+	//------------------------------------------------------------------//
+	// PUBLIC METHODS													//
+	//------------------------------------------------------------------//
+	/// <summary>
+	/// Check whether the object must be visible with a given param set.
+	/// </summary>
+	/// <returns>Whether the visibility checks are passed or not with the given parameters.</returns>
+	/// <param name="_dragonSku">Dragon sku to be considered.</param>
+	/// <param name="_screen">Menu screen to be considered.</param>
+	public bool Check(string _dragonSku, MenuScreens _screen) {
+		// Both conditions must be satisfied
+		return CheckDragon(_dragonSku) && CheckScreen(_screen);
+	}
+
+	/// <summary>
+	/// Check whether the object must be visible with a given dragon.
+	/// </summary>
+	/// <returns>Whether the visibility checks are passed or not with the given parameters.</returns>
+	/// <param name="_dragonSku">Dragon sku to be considered.</param>
+	public bool CheckDragon(string _dragonSku) {
+		// Check whether the object should be visible or not
+		bool show = false;
+		DragonData dragon = DragonManager.GetDragonData(_dragonSku);
+		if(dragon == null) return true;
+
+		// Ownership status
+		switch(dragon.lockState) {
+			case DragonData.LockState.LOCKED:		show = m_showIfLocked;		break;
+			case DragonData.LockState.AVAILABLE:	show = m_showIfAvailable;	break;
+			case DragonData.LockState.OWNED:		show = m_showIfOwned;		break;
+		}
+
+		// Dragon ID (overrides ownership status)
+		int dragonIdx = dragon.def.GetAsInt("order");
+		switch(m_hideForDragons) {
+			case HideForDragons.IGNORE:	break;	// Nothing to change
+			case HideForDragons.FIRST:	show &= (dragonIdx != 0);	break;
+			case HideForDragons.LAST:	show &= (dragonIdx != (DragonManager.dragonsByOrder.Count - 1));	break;
+			case HideForDragons.FIRST_AND_LAST:	{
+				show &= (dragonIdx != 0);
+				show &= (dragonIdx != (DragonManager.dragonsByOrder.Count - 1));
+			} break;
+			case HideForDragons.ALL:	show = false;	break;	// Force hiding
+			case HideForDragons.NONE:	show = true;	break;	// Force showing
+		}
+
+		return show;
+	}
+
+	/// <summary>
+	/// Check whether the object must be visible with a given screen.
+	/// </summary>
+	/// <returns>Whether the visibility checks are passed or not with the given parameters.</returns>
+	/// <param name="_screen">Menu screen to be considered.</param>
+	public bool CheckScreen(MenuScreens _screen) {
+		// Is screen in the list?
+		bool isScreenOnTheList = m_screens.IndexOf(_screen) >= 0;
+
+		// Determine visibility
+		bool show = true;
+		switch(m_mode) {
+			case ScreenVisibilityMode.SHOW_ON_TARGET_SCREENS: {
+				show = isScreenOnTheList;
+			} break;
+
+			case ScreenVisibilityMode.HIDE_ON_TARGET_SCREENS: {
+				show = !isScreenOnTheList;
+			} break;
+		}
+
+		return show;
+	}
+
+	//------------------------------------------------------------------//
+	// INTERNAL METHODS													//
+	//------------------------------------------------------------------//
+	/// <summary>
+	/// Apply visibility based on given parameters.
+	/// </summary>
+	/// <param name="_show">Whether to show or hide.</param>
+	/// <param name="_useAnims">Whether to animate or not.</param>
+	/// <param name="_resetAnim">Optionally force the animation to be played, even if going to the same state.</param>
+	private void Apply(bool _show, bool _useAnims, bool _resetAnim) {
+		// Let animator do its magic
+		// If forcing animation and going from visible to visible, hide before showing again
+		if(_resetAnim 
+		&& _show 
+		&& m_animator.visible 
+		&& isActiveAndEnabled 
+		&& m_animator.tweenType != ShowHideAnimator.TweenType.NONE) {
+			// Go to opposite of the target state
+			m_animator.Hide(_useAnims, false);
+
+			// Program the animation to the target state in sync with the dragon scroll animation (more or less)
+			StartCoroutine(LaunchDelayedAnimation(_show, _useAnims));
+		} else {
+			m_animator.Set(_show, _useAnims);
+		}
+	}
+
+	/// <summary>
+	/// Apply visibility based on given parameters.
+	/// </summary>
+	/// <param name="_dragonSku">Dragon sku to be considered.</param>
+	/// <param name="_screen">Menu screen to be considered.</param>
+	/// <param name="_useAnims">Whether to animate or not.</param>
+	/// <param name="_resetAnim">Optionally force the animation to be played, even if going to the same state.</param>
+	private void Apply(string _dragonSku, MenuScreens _screen, bool _useAnims, bool _resetAnim) {
+		Apply(Check(_dragonSku, _screen), true, m_restartShowAnimation);
+	}
+
+	//------------------------------------------------------------------//
+	// CALLBACKS														//
+	//------------------------------------------------------------------//
+	/// <summary>
+	/// The selected dragon on the menu has changed.
+	/// </summary>
+	/// <param name="_sku">The sku of the newly selected dragon.</param>
+	public void OnDragonSelected(string _sku) {
+		// Just update visibility
+		Apply(
+			string.IsNullOrEmpty(m_targetDragonSku) ? _sku : m_targetDragonSku,	// MenuSceneController.selectedDragon is not necessarily updated yet
+			currentMenuScreen,
+			true, m_restartShowAnimation
+		);
+	}
+
+	/// <summary>
+	/// A dragon has been acquired
+	/// </summary>
+	/// <param name="_data">The data of the acquired dragon.</param>
+	public void OnDragonAcquired(DragonData _data) {
+		// Is it our target dragon?
+		// It should be the selected dragon, but check anyway
+		if(_data.def.sku != targetDragonSku) {
+			return;
+		}
+
+		// Update visibility
+		Apply(targetDragonSku, currentMenuScreen, true, false);
+	}
+
+	/// <summary>
+	/// A navigation screen has changed.
+	/// </summary>
+	/// <param name="_data">The event's data.</param>
+	public void OnScreenChanged(NavigationScreenSystem.ScreenChangedEventData _data) {
+		// Refresh
+		Apply(targetDragonSku, currentMenuScreen, true, false);
+	}
+
+	/// <summary>
+	/// Use it to set the animator's visibility after a delay via StartCoroutine().
+	/// </summary>
+	/// <returns>The coroutine function.</returns>
+	/// <param name="_toShow">Whether to show or hide the object.</param>
+	/// <param name="_useAnims">Whether to use anims or not.</param>
+	private IEnumerator LaunchDelayedAnimation(bool _toShow, bool _useAnims) {
+		// Delay
+		yield return new WaitForSeconds(m_animator.tweenDuration);
+
+		// Do it!
+		m_animator.Set(_toShow, _useAnims);
+	}
+}
+
