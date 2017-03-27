1fc7859f1ea0c3f136d8aca28a4729f8bcb8960a
David Germade <david.germade@ubisoft.com>
2017-03-27 15:59:15 +0200

    PROFILER LOAD SCENES: Added a scene used to load scenes per area.
    Former-commit-id: 741eec6dd8ec3354add30d33a6b56a052274cd3f

---
+++ Assets/Tools/Profiler/ProfilerLoadScenesController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ProfilerLoadScenesController : MonoBehaviour
+{  
+    public List<string> m_scenesAreaToLoad;
+
+    public List<string> m_ScenesCommonToLoad;
+    
+    void Awake()
+    {
+        ScenesAreLoaded = false;
+        if (!ContentManager.ready) ContentManager.InitContent(true);
+    }
+
+    // Use this for initialization
+    void Update()
+    {
+        if (ContentManager.ready && !ScenesAreLoaded)
+        {
+            ScenesAreLoaded = true;
+
+            int count = m_scenesAreaToLoad.Count;
+            for (int i = 0; i < count; i++)
+            {
+                SceneManager.LoadSceneAsync(m_scenesAreaToLoad[i], LoadSceneMode.Additive);
+            }
+
+            count = m_ScenesCommonToLoad.Count;
+            for (int i = 0; i < count; i++)
+            {
+                SceneManager.LoadSceneAsync(m_ScenesCommonToLoad[i], LoadSceneMode.Additive);
+            }
+        }
+    }	
+
+    private bool ScenesAreLoaded { get; set; }
+}
