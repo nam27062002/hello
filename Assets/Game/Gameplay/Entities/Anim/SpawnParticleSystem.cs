b6984123b38207b5f7d006a92a3b0f7f0d8644c0
Marc Saña <marc.sana@ubisoft.com>
2017-03-30 12:58:45 +0200

    Soldier effects!
    Former-commit-id: 103eadd5ec67e4bf03a2f7de700d7ba77200ca62

---
+++ Assets/Game/Gameplay/Entities/Anim/SpawnParticleSystem.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class SpawnParticleSystem : StateMachineBehaviour {
+
+	[SerializeField] private ParticleData m_particleSystemData;
+	[SerializeField] private bool m_attach = true;
+
+	Transform m_Transform;
+
+	// Use this for initialization
+	void Start () {		
+		ParticleManager.CreatePool(m_particleSystemData);
+	}
+	
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		Transform transform = animator.transform;
+		GameObject ps = ParticleManager.Spawn(m_particleSystemData, transform.position + m_particleSystemData.offset);
+		if (m_attach && ps != null) {
+			ps.transform.parent = transform;
+			ps.transform.localRotation = Quaternion.identity;
+		}
+	}
+}
