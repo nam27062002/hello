8d5b85801d3fa2d671545c230c2c22bedc765624
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2017-04-04 11:48:05 +0200

    Currency Shop Popup: Development - Added init method to open the popup in different modes (default, coins only, pc only). Popup Launcher: Development - Implemented specialization for the Currency Shop to be able to initialize it in a specific mode before opening it.
    Former-commit-id: c8829534908fd47f9a2a424e26bf31b8ea6971ab

---
+++ Assets/Game/UI/Common/CurrencyShopPopupLauncher.cs
@@ -0,0 +1,60 @@
+// CurrencyShopPopupLauncher.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on DD/MM/2016.
+// Copyright (c) 2016 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Specialization of the popup launcher meant for the currency popup.
+/// </summary>
+public class CurrencyShopPopupLauncher : PopupLauncher {
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
+	
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
+	[SerializeField] private PopupCurrencyShop.Mode m_mode = PopupCurrencyShop.Mode.DEFAULT;
+
+	//------------------------------------------------------------------------//
+	// GENERIC METHODS														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Initialization.
+	/// </summary>
+	private void Awake() {
+		OnPopupInit.AddListener(OnInitPopup);
+	}
+
+	/// <summary>
+	/// Destructor.
+	/// </summary>
+	private void OnDestroy() {
+		OnPopupInit.RemoveListener(OnInitPopup);
+	}
+
+	//------------------------------------------------------------------------//
+	// OTHER METHODS														  //
+	//------------------------------------------------------------------------//
+
+	//------------------------------------------------------------------------//
+	// CALLBACKS															  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// A popup has been destroyed.
+	/// </summary>
+	/// <param name="_popup">The popup that has just been destroyed.</param>
+	private void OnInitPopup() {
+		// Initialize target popup with the requested mode
+		m_popup.GetComponent<PopupCurrencyShop>().Init(m_mode);
+	}
+}
