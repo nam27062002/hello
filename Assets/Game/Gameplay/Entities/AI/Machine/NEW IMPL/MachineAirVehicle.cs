4585d77b858ded804f2fd0f9f50c31c77d94eeaf
Marc Saña <marc.sana@ubisoft.com>
2017-04-27 16:54:20 +0200

    Goblin Boat
    Former-commit-id: 700d36f0cfc7b1cf6a2db9d5aa2158e09fe48727

--- Assets/Game/Gameplay/Entities/AI/Machine/NEW IMPL/MachineAirVehicle.cs
+++ Assets/Game/Gameplay/Entities/AI/Machine/NEW IMPL/MachineAirVehicle.cs
@@ -7,12 +7,12 @@ namespace AI {
 		[SeparatorAttribute("Armor")]
 		[SerializeField] private HitsPerDragonTier m_armorDurabilityPerTier;
 
-		private DeviceOperatorSpawner m_operatorSpawner;	
+		private DevicePassengersSpawner m_operatorSpawner;	
 		private Hit m_armorDurability = new Hit();
 
 
 		protected override void Awake() {
-			m_operatorSpawner = GetComponent<DeviceOperatorSpawner>();
+			m_operatorSpawner = GetComponent<DevicePassengersSpawner>();
 			m_operatorSpawner.Initialize();
 			base.Awake();
 		}
@@ -26,20 +26,17 @@ namespace AI {
 			m_armorDurability.needBoost = originalHits.needBoost;
 
 			m_operatorSpawner.Respawn();
-			m_operatorSpawner.OperatorEnterDevice();
+			m_operatorSpawner.PassengersEnterDevice();
 
 			base.Spawn(_spawner);
 		}
 
 		public bool ReduceDurability(bool _boost) {
-			if (m_armorDurability.count > 0) {
+			if (m_armorDurability.count > 0 && !GetSignal(Signals.Type.Burning)) {
 				if (!m_armorDurability.needBoost || _boost) {
 					m_armorDurability.count--;
 					if (m_armorDurability.count <= 0) {
-						if (!m_operatorSpawner.IsOperatorDead()) {
-							m_operatorSpawner.OperatorLeaveDevice();
-						}
-
+						m_operatorSpawner.PassengersLeaveDevice();
 						SetSignal(Signals.Type.Destroyed, true);
 					}
 					return true;
@@ -54,10 +51,8 @@ namespace AI {
 		}
 
 		public override bool Burn(Transform _transform) {			
-			if (base.Burn(_transform)) {
-				if (!m_operatorSpawner.IsOperatorDead()) {
-					m_operatorSpawner.OperatorBurn();
-				}
+			if (base.Burn(_transform)) {				
+				m_operatorSpawner.PassengersBurn();
 				return true;
 			}
 
