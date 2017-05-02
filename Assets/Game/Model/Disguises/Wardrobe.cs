b6ad19e0292054675e234eacbac9fb17c6fadf38
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2017-05-02 12:35:19 +0200

    Skins: Development - Skins now have different states. Added the “NEW” state to show the notification. Skins: Development - Added “NEW” skin notification! UI Notification: Assets - Moved anchor point, fixed double -1 scaling.
    Former-commit-id: c5307b9f4fda5a558de7b9f5722988134dfb414d

--- Assets/Game/Model/Disguises/Wardrobe.cs
+++ Assets/Game/Model/Disguises/Wardrobe.cs
@@ -8,20 +8,24 @@ public class Wardrobe
 	//------------------------------------------------------------------//
 	// CONSTANTS														//
 	//------------------------------------------------------------------//
+	public enum SkinState {
+		LOCKED = 0,
+		NEW = 1,		// Same as "AVAILABLE", but showing the "new" notification
+		AVAILABLE = 2,
+		OWNED = 3
+	};
 
 	//------------------------------------------------------------------//
 	// MEMBERS AND PROPERTIES											//
 	//------------------------------------------------------------------//
-	// [AOC] Disguises no longer have levels. We'll keep the int value to track owned disguises though. 0 - not owned, 1 - owned.
-	private Dictionary<string, int> m_disguises;
-
+	private Dictionary<string, SkinState> m_disguises = null;
 
 	//------------------------------------------------------------------//
 	// GENERIC METHODS													//
 	//------------------------------------------------------------------//
 	public Wardrobe()
 	{
-		m_disguises = new Dictionary<string, int>();
+		m_disguises = new Dictionary<string, SkinState>();
 	}
 
 	/// <summary>
@@ -33,24 +37,64 @@ public class Wardrobe
 		Dictionary<string, DefinitionNode> defs = DefinitionsManager.SharedInstance.GetDefinitions(DefinitionsCategory.DISGUISES);
 		m_disguises.Clear();
 		foreach(KeyValuePair<string, DefinitionNode> kvp in defs) {
-			// Special case: if unlock level is 0, mark it as owned!
+			// Special case: if unlock level is 0, mark it as owned! (probably dragon's default skins)
 			if(kvp.Value.GetAsInt("unlockLevel") <= 0) {
-				m_disguises.Add(kvp.Key, 1);
+				m_disguises.Add(kvp.Key, SkinState.OWNED);
 			} else {
-				m_disguises.Add(kvp.Key, 0);
+				m_disguises.Add(kvp.Key, SkinState.LOCKED);
 			}
 		}
 	}
 
-	public bool IsDisguiseOwned(string _sku) {
-		if ( m_disguises != null && m_disguises.ContainsKey(_sku)) {
-			return m_disguises[_sku] > 0;
+	/// <summary>
+	/// Get the state of a specific skin.
+	/// </summary>
+	/// <returns>The state of the skin.</returns>
+	/// <param name="_skinSku">Skin to be checked. LOCKED if sku was not found or wardrobe wasn't initialized.</param>
+	public SkinState GetSkinState(string _skinSku) {
+		// Is wardrobe initialized and skin sku valid?
+		if(m_disguises != null && m_disguises.ContainsKey(_skinSku)) {
+			return m_disguises[_skinSku];
 		}
-		return false;
+		return SkinState.LOCKED;
 	}
 
-	public void UnlockDisguise(string _sku) {
-		m_disguises[_sku] = 1;
+	/// <summary>
+	/// Set the state of a specific skin.
+	/// </summary>
+	/// <param name="_skinSku">Skin to be modified. No checks will be performed.</param>
+	/// <param name="_newSkinState">New skin state. No checks will be performed.</param>
+	public void SetSkinState(string _skinSku, SkinState _newSkinState) {
+		// Skip if wardrobe not initialized
+		if(m_disguises == null) return;
+
+		// Just do it!
+		m_disguises[_skinSku] = _newSkinState;
+	}
+
+	/// <summary>
+	/// Detect unlocked sking based on given dragon data.
+	/// Will check dragon's XP level and move all its locked skins matching the level
+	/// to the "NEW" state.
+	/// </summary>
+	/// <param name="_targetDragon">Target dragon.</param>
+	public void ProcessUnlockedSkins(DragonData _targetDragon) {
+		// Skip if wardrobe not initialized
+		if(m_disguises == null) return;
+		if(_targetDragon == null) return;
+
+		// Get all the skins for the given dragon
+		List<DefinitionNode> skinDefs = DefinitionsManager.SharedInstance.GetDefinitionsByVariable(DefinitionsCategory.DISGUISES, "dragonSku", _targetDragon.def.sku);
+		for(int i = 0; i < skinDefs.Count; i++) {
+			// Is the skin locked
+			if(GetSkinState(skinDefs[i].sku) == SkinState.LOCKED) {
+				// Have we reached the unlock level for this skin?
+				if(_targetDragon.progression.level >= skinDefs[i].GetAsInt("unlockLevel")) {
+					// Yes!! Mark it as "NEW"
+					SetSkinState(skinDefs[i].sku, SkinState.NEW);
+				}
+			}
+		}
 	}
 
 	//------------------------------------------------------------------//
@@ -65,7 +109,7 @@ public class Wardrobe
 		SimpleJSON.JSONArray diguisesArr = _data.AsArray;
 		int disguisesLength = diguisesArr.Count;
 		for (int i = 0; i < disguisesLength; i++) {
-			m_disguises[ diguisesArr[i]["disguise"] ] = diguisesArr[i]["level"].AsInt;
+			m_disguises[ diguisesArr[i]["disguise"] ] = (SkinState)diguisesArr[i]["level"].AsInt;
 		}
 	}
 
@@ -77,16 +121,15 @@ public class Wardrobe
 	{
 		SimpleJSON.JSONArray diguisesArr = new SimpleJSON.JSONArray();
 		if(m_disguises != null) {
-			foreach (KeyValuePair<string, int> pair in m_disguises) {
-				if (pair.Value > 0) 
-				{
-					SimpleJSON.JSONClass dl = new SimpleJSON.JSONClass();
+			foreach (KeyValuePair<string, SkinState> pair in m_disguises) {
+				// Don't store locked disguises (no need to make savefile that big!)
+				if(pair.Value == SkinState.LOCKED) continue;
 
-					dl.Add("disguise", pair.Key.ToString());
-					dl.Add("level", pair.Value.ToString());
-
-					diguisesArr.Add(dl);
-				}
+				// We're reusing the old "level" field ^^
+				SimpleJSON.JSONClass dl = new SimpleJSON.JSONClass();
+				dl.Add("disguise", pair.Key.ToString());
+				dl.Add("level", ((int)pair.Value).ToString(System.Globalization.CultureInfo.InvariantCulture));
+				diguisesArr.Add(dl);
 			}
 		}
 		return diguisesArr;
