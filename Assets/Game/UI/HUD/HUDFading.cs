4aa70f932031f5c7697301a8108fe2c618c16058
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2017-05-08 15:44:58 +0200

    fade out/in when changing areas
    Former-commit-id: eb9e24c1973b9139db500028e09aac8068751361

---
+++ Assets/Game/UI/HUD/HUDFading.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class HUDFading : MonoBehaviour {
+
+	public Image m_blackImage;
+	enum State
+	{
+		NONE,
+		FADE_OUT,
+		WAITING_FADE_IN,
+		FADE_IN
+	};
+	private State m_state = State.NONE;
+
+	const float FADE_DURATION = 0.5f;
+	float m_timer = 0;
+	Color m_color = Color.black;
+
+	bool m_skipFrame = false;
+	float m_waitingTime = 0;
+
+	public GameObject m_text;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		m_blackImage =  GetComponent<Image>();
+		m_blackImage.enabled = false;
+		m_text.SetActive(false);
+		m_color.a = 0;
+		enabled = false;
+
+		Messenger.AddListener(GameEvents.PLAYER_LEAVING_AREA, PlayerLeavingArea);
+		Messenger.AddListener(GameEvents.GAME_AREA_ENTER, OnAreaStart);
+	}
+	
+	// Update is called once per frame
+	void OnDestroy () 
+	{
+		Messenger.RemoveListener(GameEvents.PLAYER_LEAVING_AREA, PlayerLeavingArea);
+		Messenger.RemoveListener(GameEvents.GAME_AREA_ENTER, OnAreaStart);
+	}
+
+	void Update()
+	{
+		if ( m_skipFrame )
+		{
+			m_skipFrame = false;
+			return;
+		}
+		switch( m_state )
+		{
+			case State.FADE_OUT:
+			{
+				m_timer += Time.deltaTime;
+
+				float alpha = m_timer / FADE_DURATION;
+				if ( alpha > 1.0f )
+					alpha = 1.0f;
+				m_color.a = alpha;
+				m_blackImage.color = m_color;
+
+				if ( m_timer >= FADE_DURATION )
+				{
+					m_waitingTime = 0;
+					m_timer = FADE_DURATION;
+					m_state = State.WAITING_FADE_IN;
+				}
+			}break;
+			case State.WAITING_FADE_IN:
+			{
+				m_waitingTime += Time.deltaTime;
+				if (m_waitingTime > 1.0f )
+				{
+					// Enable Loading time
+					m_text.SetActive(true);
+				}
+			}break;
+			case State.FADE_IN:
+			{
+				m_timer -= Time.deltaTime;
+				float alpha = m_timer / FADE_DURATION;
+				if ( alpha <= 0f )
+					alpha = 0f;
+				m_color.a = alpha;
+				m_blackImage.color = m_color;
+				if ( m_timer <= 0 )
+				{
+					m_timer = 0;
+					m_state = State.NONE;
+					enabled = false;
+				}
+
+			}break;
+		}
+	}
+
+	void PlayerLeavingArea()
+	{
+		StartFadeOut();
+	}
+
+	void OnAreaStart()
+	{
+		StartFadeIn();
+	}
+
+	// Screen to black
+	void StartFadeOut()
+	{
+		enabled = true;
+		m_blackImage.enabled = true;
+		m_state = State.FADE_OUT;
+	}
+
+	// Screen to transparent
+	void StartFadeIn()
+	{
+		m_text.SetActive(false);
+		m_skipFrame = true;
+		enabled = true;
+		m_state = State.FADE_IN;
+
+	}
+
+}
