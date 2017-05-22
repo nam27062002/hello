ed6b266dc5ab187cebc13e8ad6e9167e2ff951cd
Marc Saña <marc.sana@ubisoft.com>
2017-05-22 11:46:57 +0200

    Ghosts Smoke is spawned when the entity is visible
    Former-commit-id: 07cbb22cc2db96af3bd557e7919d82bc05aa85d8

---
+++ Assets/Game/Gameplay/Entities/AI/View/ViewParticleSpanwer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewParticleSpanwer : MonoBehaviour {
+	[SerializeField] private Renderer m_view;
+	[SerializeField] private ParticleData[] m_particleDatas;
+
+
+	private GameCamera m_camera;
+
+	private Transform m_parent;
+	private GameObject[] m_particleSytems;
+
+	private bool m_spawned;
+
+	// Use this for initialization
+	void Awake () {
+		m_camera = Camera.main.GetComponent<GameCamera>();
+
+		m_parent = transform;
+		m_particleSytems = new GameObject[m_particleDatas.Length];
+
+		for (int i = 0; i < m_particleDatas.Length; ++i) {
+			ParticleManager.CreatePool(m_particleDatas[i]);
+		}
+
+		m_spawned = false;
+	}
+
+	void Update() {
+		// Show / Hide fire effect if thfis node is inside Camera or not
+		bool isInsideActivationMaxArea = false;
+
+		if (m_view != null) {
+			isInsideActivationMaxArea = m_camera.IsInsideFrustrum(m_view.bounds);
+		} else {
+			isInsideActivationMaxArea = m_camera.IsInsideFrustrum(m_parent.position);
+		}
+
+		if (isInsideActivationMaxArea) {
+			if (!m_spawned) {
+				Spawn();
+			}
+		} else {
+			if (m_spawned) {
+				Return();
+			}
+		}
+	}
+	
+	void Spawn() {
+		for (int i = 0; i < m_particleDatas.Length; ++i) {
+			m_particleSytems[i] = ParticleManager.Spawn(m_particleDatas[i]);
+			if (m_particleSytems[i] != null) {
+				Transform t = m_particleSytems[i].transform;
+				t.SetParent(m_parent, false);
+				t.localPosition = Vector3.zero;
+			}
+		}
+
+		m_spawned = true;
+	}
+
+	void Return() {
+		for (int i = 0; i < m_particleSytems.Length; ++i) {
+			if (m_particleSytems[i] != null) {
+				ParticleManager.ReturnInstance(m_particleSytems[i]);
+			}
+			m_particleSytems[i] = null;
+		}
+
+		m_spawned = false;
+	}
+}
