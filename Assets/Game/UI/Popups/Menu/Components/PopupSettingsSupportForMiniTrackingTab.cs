4bdd740d9b98e9957e68c384085783994b7de79d
David Germade <david.germade@ubisoft.com>
2017-05-25 17:21:23 +0200

    MINI TRACKING: A new support tab with a button to send the mini tracking data has been added to settings popup.
    Former-commit-id: 5e8508af721b1bbb51e8a459408713d446e3583d

---
+++ Assets/Game/UI/Popups/Menu/Components/PopupSettingsSupportForMiniTrackingTab.cs
@@ -0,0 +1,48 @@
+﻿// PopupSettingsSupportTab.cs
+// Hungry Dragon
+// 
+// Created by David Germade on 25th May 2017.
+// Copyright (c) 201y Ubisoft. All rights reserved.
+
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is responsible for handling the support tab in the settings popup when minitracking is enabled
+/// </summary>
+public class PopupSettingsSupportForMiniTrackingTab : MonoBehaviour
+{      
+    //------------------------------------------------------------------------//
+    // GENERIC METHODS														  //
+    //------------------------------------------------------------------------//
+    /// <summary>
+    /// Initialization.
+    /// </summary>
+    public void Awake() {	
+        if (!FeatureSettingsManager.instance.IsMiniTrackingEnabled) {
+            gameObject.SetActive(false);
+        }	
+    }
+
+    //------------------------------------------------------------------------//
+    // CALLBACKS															  //
+    //------------------------------------------------------------------------//
+	/// <summary>
+	/// Mini Tracking file is sent to our server
+	/// </summary>	
+	public void OnSendTracking() {
+        MiniTrackingEngine.SendTrackingFile(false,
+           delegate (FGOL.Server.Error error, Dictionary<string, object> response)
+           {
+               if (error == null)
+               {
+
+                   Debug.Log("Play test tracking sent successfully");
+               }
+               else
+               {
+                   Debug.Log("Error when sending play test tracking");
+               }
+           });        
+	}
+}
