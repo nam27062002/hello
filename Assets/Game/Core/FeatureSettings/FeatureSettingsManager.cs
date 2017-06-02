a2c6326a9508eecd08e8aceada0058011ff9d077
David Germade <david.germade@ubisoft.com>
2017-06-02 10:02:23 +0200

    FEATURE SETTINGS: Control panel enabled again after the build for the play test has already been made.
    Former-commit-id: 008de6bb523b340a87fd114dce20835ddc9ec55f

--- Assets/Game/Core/FeatureSettings/FeatureSettingsManager.cs
+++ Assets/Game/Core/FeatureSettings/FeatureSettingsManager.cs
@@ -1031,9 +1031,7 @@ public class FeatureSettingsManager : UbiBCN.SingletonMonoBehaviour<FeatureSetti
             return true;
 #else
             ServerManager.ServerConfig kServerConfig = ServerManager.SharedInstance.GetServerConfig();
-            return (kServerConfig != null && kServerConfig.m_eBuildEnvironment != CaletyConstants.eBuildEnvironments.BUILD_PRODUCTION &&
-                // Disabled in STAGE too for the play test as the users play in STAGE. TO REMOVE once the play test is done
-                kServerConfig.m_eBuildEnvironment != CaletyConstants.eBuildEnvironments.BUILD_STAGE); 
+            return (kServerConfig != null && kServerConfig.m_eBuildEnvironment != CaletyConstants.eBuildEnvironments.BUILD_PRODUCTION);               
 #endif
         }
     }
