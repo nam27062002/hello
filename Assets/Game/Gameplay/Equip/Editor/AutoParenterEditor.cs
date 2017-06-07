a38630b512116af6e19c9bfa3e52332d6a2ed431
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2017-06-07 18:39:12 +0200

    auto partner utility
    Former-commit-id: c6b698bf1bbaae9602e14cf95a4de0dab3b8057d

---
+++ Assets/Game/Gameplay/Equip/Editor/AutoParenterEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AutoParenter))]
+public class AutoParenterEditor : Editor {
+
+	AutoParenter m_target = null;
+
+	private void OnEnable() {
+		if ( !Application.isPlaying ){
+			m_target = target as AutoParenter;
+		}
+	}
+
+	public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+		if ( GUILayout.Button("Copy Target Position And Rotation") )
+		{
+			if (!string.IsNullOrEmpty(m_target.parentName)) {
+				Transform t = m_target.transform;
+				Transform p;
+				if (m_target.parentRoot == null)
+					p = t.parent.FindTransformRecursive(m_target.parentName);
+				else
+					p = m_target.parentRoot.FindTransformRecursive(m_target.parentName);
+
+				if (p != null) {
+					t.position = p.position;
+					t.rotation = p.rotation;
+				} 
+			}
+		}
+    }
+}
