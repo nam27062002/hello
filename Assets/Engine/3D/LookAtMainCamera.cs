f98c37f27cde6a55d5d86b3412d1f53e181f1dec
David Germade <david.germade@ubisoft.com>
2017-06-07 15:09:53 +0200

    PERFORMANCE: LookAtMainCamera: rotation is updated in Update() instead of in LateUpdate() in order to avoid to wait for particle systems updates.
    Former-commit-id: 2512c4abf33c4928d6dc1b2d0de3648241aa91f5

--- Assets/Engine/3D/LookAtMainCamera.cs
+++ Assets/Engine/3D/LookAtMainCamera.cs
@@ -12,30 +12,22 @@ public class LookAtMainCamera : MonoBehaviour {
 	}
 
 	private Transform m_overrideCameraTransform;
-	private Transform m_transform;
-
-	// The position where the transform has to look at. It's calculated in LateUpdate() to be sure that the camera has the latest position, but it's applied in Update()
-	// instead of in LateUpdate() for performance reasons as a long (2ms) Unity's WaitingForJob task because of ParticleSystems.Update() was found when profiling.
-	private Vector3 m_lookAtPosition;
+	private Transform m_transform;	
 
 	void Awake() {
 		m_transform = transform;
 	}
 
-	private void Update() {
-		m_transform.LookAt(m_lookAtPosition);
-	}
-
-	/// <summary>
-	/// Called every frame after all Updates have been called.
-	/// </summary>
-	private void LateUpdate () 
-	{
-		Transform targetCamera = GetTargetCameraTransform();
-		if(targetCamera != null) {
-			m_lookAtPosition = targetCamera.position;
-		}
-	}
+	private void Update() {        
+        Transform targetCamera = GetTargetCameraTransform();
+        if (targetCamera != null)
+        {            
+            // This is not done in LateUpdate() because rotating a game object that is the parent of a particle system prevents this
+            // stuff from being parallelized with particle system updates introducing a Unity WaitingForJob task that takes 1-2ms. 
+            // This camera position might be not the latest one but we consider that the magnitude of the error is negligible
+            m_transform.LookAt(targetCamera.position);
+        }        
+	}	
 
 	/// <summary>
 	/// Check all conditions and define the target camera to look at.
