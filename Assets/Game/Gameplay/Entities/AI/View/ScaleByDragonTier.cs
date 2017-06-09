a3bafdd1271772f9d132a051a8756ecabca3ca2a
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2017-06-09 11:02:33 +0200

    scale every time we spawn
    Former-commit-id: 281e5efe437734865751ee1331d577ba15b7e51b

--- Assets/Game/Gameplay/Entities/AI/View/ScaleByDragonTier.cs
+++ Assets/Game/Gameplay/Entities/AI/View/ScaleByDragonTier.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScaleByDragonTier : MonoBehaviour {
+public class ScaleByDragonTier : MonoBehaviour, ISpawnable {
 	public float[] m_scaleByTier = new float[ (int) DragonTier.COUNT ];
-	void Start () {
+	public void Spawn(ISpawner _spawner)
+	{
 		if ( InstanceManager.player )
 		{
 			transform.localScale *= m_scaleByTier[ (int)InstanceManager.player.data.tier ];
 		}
 	}
+	public void CustomUpdate(){}
 	
 
 }
