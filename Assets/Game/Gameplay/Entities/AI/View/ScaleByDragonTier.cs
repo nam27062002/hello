0f3238a85a07aa241d3f793f2edc532e8ff0af1a
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2017-06-09 09:57:28 +0200

    scale enemy dragons by current player tier
    Former-commit-id: a094cf61fed6dfce6f980dfca33daf444f612c55

---
+++ Assets/Game/Gameplay/Entities/AI/View/ScaleByDragonTier.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleByDragonTier : MonoBehaviour {
+	public float[] m_scaleByTier = new float[ (int) DragonTier.COUNT ];
+	void Start () {
+		if ( InstanceManager.player )
+		{
+			transform.localScale *= m_scaleByTier[ (int)InstanceManager.player.data.tier ];
+		}
+	}
+	
+
+}
