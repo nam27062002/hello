2eca39cac877a4bab24e9aee9bf79fcce88dc368
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2017-06-19 14:42:11 +0200

    group scaler
    Former-commit-id: 347ca9b3bbb3f373603982c65c8c1d299028407b

--- Assets/Engine/Utils/Editor/SceneParticlePoolingGroupEditor.cs
+++ Assets/Engine/Utils/Editor/SceneParticlePoolingGroupEditor.cs
@@ -87,6 +87,13 @@ public class SceneParticlePoolingGroupEditor : Editor {
 			_particleInstance.transform.position += particle.m_particle.offset;
 			_particleInstance.transform.rotation = particle.transform.rotation;
 			_particleInstance.hideFlags = HideFlags.DontSaveInEditor;
+			ParticleScaler scaler = _particleInstance.GetComponent<ParticleScaler> ();
+			if (scaler != null) {
+				if (scaler.m_scale != particle.m_particle.scale) {
+					scaler.m_scale = particle.m_particle.scale;
+					scaler.DoScale();
+				}
+			}
 
 			m_particlesCreated.Add( particle );
 			m_particlesCreatedObj.Add( _particleInstance );
