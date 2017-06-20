b6e4da73d533d801603995fa5513ad00e3e3e70c
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2017-06-20 15:17:07 +0200

    pet find eggs test
    Former-commit-id: 0712946cb65f03c6b3bd2438cd31c47bf2e883d9

--- Assets/Game/Gameplay/Entities/AI/Brain/Behaviours/Pet/PetPointingCollectible.cs
+++ Assets/Game/Gameplay/Entities/AI/Brain/Behaviours/Pet/PetPointingCollectible.cs
@@ -14,8 +14,10 @@ namespace AI {
 			private static string OnCollected = "onCollected";
 
 
-			float m_playerRangeDistance;
-			float m_playerRangeDistanceSqr;
+			float m_playerDistance;
+			float m_closeRange;
+			float m_farRange;
+
 			DragonPlayer m_player;
 			GameObject m_pointToObject;
 			Transform m_pointToTransform;
@@ -26,8 +28,14 @@ namespace AI {
 
 			protected override void OnInitialise() {
 				m_player = InstanceManager.player;
-				m_playerRangeDistance = m_player.data.GetScaleAtLevel( m_player.data.progression.maxLevel ) * 11;
-				m_playerRangeDistanceSqr = m_playerRangeDistance * m_playerRangeDistance;
+				float maxLevelScale = m_player.data.GetScaleAtLevel( m_player.data.progression.maxLevel );
+				m_playerDistance = maxLevelScale * 5;
+
+				m_closeRange = maxLevelScale * 10;
+				m_closeRange = m_closeRange * m_closeRange;
+
+				m_farRange = maxLevelScale * 30;
+				m_farRange = m_farRange * m_farRange;
 			}
 
 			protected override void OnEnter(State oldState, object[] param){
@@ -61,13 +69,13 @@ namespace AI {
 
 				Vector3 diff = (m_pointToTransform.position - m_player.transform.position);
 				float sqrMagnitude = diff.sqrMagnitude;
-				if ( sqrMagnitude < m_playerRangeDistanceSqr )	// if player is close enough
+				if ( sqrMagnitude < m_closeRange )	// if player is close enough
 				{
 					// Just go for the collectible
-					Vector3 destPos = m_pointToTransform.position + (Vector3.up * 2);
+					Vector3 destPos = m_pointToTransform.position + (Vector3.up * 4);
 					m_pilot.GoTo(destPos);
 				}
-				else if ( sqrMagnitude > m_playerRangeDistanceSqr * 2 )
+				else if ( sqrMagnitude > m_farRange )
 				{
 					// Stop pointing
 					Transition(OnTooFarPointing);
@@ -76,7 +84,7 @@ namespace AI {
 				{
 					// show player where to go
 					diff.Normalize();
-					Vector3 destPos = m_player.transform.position + diff * m_playerRangeDistance;
+					Vector3 destPos = m_player.transform.position + diff * m_playerDistance;
 					m_pilot.GoTo(destPos);
 				}
 
