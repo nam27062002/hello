95383536ad5ad8a18f5cc3cfca6577513d172e00
Marc Saña <marc.sana@ubisoft.com>
2017-07-21 12:52:51 +0200

    Gear proto
    Former-commit-id: 092cc0be03d78cb4b689ab00cc42f2b2a7b2b670

---
+++ Assets/Game/Gameplay/Decorations/Devices/Hazards/HazardPassiveDamage.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardPassiveDamage : MonoBehaviour {
+
+	[SerializeField] private float m_damage = 10f;
+	[SerializeField] private DamageType m_type = DamageType.NORMAL;
+	[SerializeField] private float m_delay = 1f;
+
+	private Transform m_transform;
+	private DragonHealthBehaviour m_dragon;
+	private float m_timer;
+
+
+	// Use this for initialization
+	void Start() {
+		m_transform = transform;
+
+		m_timer = 0f;
+		m_dragon = InstanceManager.player.dragonHealthBehaviour;
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		if (m_timer > 0f) {
+			m_timer -= Time.deltaTime;
+			if (m_timer <= 0f) {
+				m_timer = 0f;
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider _other) {
+		if (m_timer <= 0f) {			
+			if (_other.CompareTag("Player")) {
+				m_dragon.ReceiveDamage(m_damage, m_type, m_transform);
+			}
+			m_timer = m_delay;
+		}
+	}
+}
