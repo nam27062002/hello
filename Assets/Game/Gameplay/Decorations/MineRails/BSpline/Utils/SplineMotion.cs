710c9ec86080055ead1dca5e3b7978e0bba57269
Marc Saña <marc.sana@ubisoft.com>
2017-08-10 14:58:42 +0200

    moving wagons!
    Former-commit-id: a8aee3ec83e2105a27d301da0d6f4adf6586666d

--- Assets/Game/Gameplay/Decorations/MineRails/BSpline/Utils/SplineMotion.cs
+++ Assets/Game/Gameplay/Decorations/MineRails/BSpline/Utils/SplineMotion.cs
@@ -10,6 +10,7 @@ public class SplineMotion : MonoBehaviour {
 
 	[SerializeField] private BSpline.BezierSpline m_spline;
 	[SerializeField] private float m_speed = 10f;
+	[SerializeField] private float m_delay = 2f;
 	[SerializeField] private SplineWalkerMode m_mode = SplineWalkerMode.Loop;
 
 	private Transform m_transform;
@@ -18,31 +19,42 @@ public class SplineMotion : MonoBehaviour {
 
 	private bool m_goingForward = true;
 	private float m_distance;
+	private float m_timer;
 
 	private void Start() {
 		m_transform = transform;
 		m_distance = 0f;
 		m_acceleration = 0f;
+
+		m_timer = m_delay;
 	}
 
 	private void Update() {
-		if (m_goingForward) {
-			m_distance += (m_speed + m_acceleration) * Time.deltaTime;
-			if (m_distance > m_spline.length) {
-				if (m_mode == SplineWalkerMode.Once) {
-					m_distance = m_spline.length;
-				} else if (m_mode == SplineWalkerMode.Loop) {
-					m_distance -= m_spline.length;
-				} else {
-					m_distance = 2f * m_spline.length - m_distance;
-					m_goingForward = false;
-				}
-			}
+		float dt = Time.smoothDeltaTime;
+
+		if (m_timer > 0f) {
+			m_timer -= dt;
 		} else {
-			m_distance -= (m_speed + m_acceleration) * Time.deltaTime;
-			if (m_distance < 0f) {
-				m_distance = -m_distance;
-				m_goingForward = true;
+			if (m_goingForward) {
+				m_distance += (m_speed + m_acceleration) * dt;
+				if (m_distance > m_spline.length) {
+					if (m_mode == SplineWalkerMode.Once) {
+						m_distance = m_spline.length;
+					} else if (m_mode == SplineWalkerMode.Loop) {
+						m_distance -= m_spline.length;
+					} else {
+						m_distance = 2f * m_spline.length - m_distance;
+						m_goingForward = false;
+					}
+					m_timer = m_delay;
+				}
+			} else {
+				m_distance -= (m_speed + m_acceleration) * dt;
+				if (m_distance < 0f) {
+					m_distance = -m_distance;
+					m_goingForward = true;
+					m_timer = m_delay;
+				}
 			}
 		}
 
@@ -55,11 +67,11 @@ public class SplineMotion : MonoBehaviour {
 			dir *= -1;
 		}
 
-		if (dir.y < -0.15f) 	m_acceleration = Mathf.MoveTowards(m_acceleration, m_speed * 2f, m_speed * 0.1f * Mathf.Abs(dir.y));
-		else if (dir.y > 0.15f)	m_acceleration = Mathf.MoveTowards(m_acceleration, -m_speed * 0.25f, m_speed * 0.05f);
-		else 					m_acceleration = Mathf.MoveTowards(m_acceleration, 0f, m_speed * 0.05f);
+		if (dir.y < -0.15f) 	m_acceleration = Mathf.MoveTowards(m_acceleration, m_speed * 2f, m_speed * 1.5f * Mathf.Abs(dir.y) * dt);
+		else if (dir.y > 0.15f)	m_acceleration = Mathf.MoveTowards(m_acceleration, -m_speed * 0.25f, m_speed * 0.75f * dt);
+		else 					m_acceleration = Mathf.MoveTowards(m_acceleration, 0f, m_speed * 1.0f * dt);
 
-		m_transform.localPosition = position;
+		m_transform.position = position;
 		m_transform.localRotation = Quaternion.LookRotation(dir, up);
 	}
 }
