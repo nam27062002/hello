1e3de8affb327a780d9e4eb8d653f3bb53b8ab74
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2017-08-21 14:43:00 +0200

    Back Button Manager: Development - Added classes to handle back button in Android.
    Former-commit-id: 034855ed4413bb84d524346d8caabf9579063a7a

---
+++ Assets/Game/UI/Navigation/BackButtonManager.cs
@@ -0,0 +1,86 @@
+// BackButtonManager.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 21/08/2017.
+// Copyright (c) 2017 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+using System.Collections.Generic;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Global manager to control back button handlers.
+/// </summary>
+public class BackButtonManager : UbiBCN.SingletonMonoBehaviour<BackButtonManager> {
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
+	
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
+	// Registered handlers
+	// [AOC] Test if a Stack works fine or is too restrictive
+	private Stack<BackButtonHandler> m_handlers = new Stack<BackButtonHandler>();
+	
+	//------------------------------------------------------------------------//
+	// GENERIC METHODS														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Initialization.
+	/// </summary>
+	private void Awake() {
+
+	}
+
+	/// <summary>
+	/// First update call.
+	/// </summary>
+	private void Start() {
+
+	}
+
+	/// <summary>
+	/// Component has been enabled.
+	/// </summary>
+	private void OnEnable() {
+
+	}
+
+	/// <summary>
+	/// Component has been disabled.
+	/// </summary>
+	private void OnDisable() {
+
+	}
+
+	/// <summary>
+	/// Called every frame.
+	/// </summary>
+	private void Update() {
+		// Back button pressed?
+		if(Input.GetKeyUp(KeyCode.Escape)) {
+			// Peek and trigger
+		}
+	}
+
+	/// <summary>
+	/// Destructor.
+	/// </summary>
+	private void OnDestroy() {
+
+	}
+
+	//------------------------------------------------------------------------//
+	// OTHER METHODS														  //
+	//------------------------------------------------------------------------//
+
+	//------------------------------------------------------------------------//
+	// CALLBACKS															  //
+	//------------------------------------------------------------------------//
+}
