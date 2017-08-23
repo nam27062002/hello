77e5f4d24a20d4da5857475f7c3caab1322e76aa
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2017-08-23 08:50:18 +0200

    Global Events: Development - Implemented Top % reward. Social info now comes included in the leaderboard data.
    Former-commit-id: 86aa5b6e4b0b75b112e0df4eb3002539e62a485a

--- Assets/Game/Model/MissionsAndEvents/GlobalEvents/GlobalEventUserData.cs
+++ Assets/Game/Model/MissionsAndEvents/GlobalEvents/GlobalEventUserData.cs
@@ -31,6 +31,10 @@ public class GlobalEventUserData {
 	public long endTimestamp = 0;		 // End time
 	public bool rewardCollected = false; // User collected the reward 
 
+	// Social info - probably will go elsewhere in the future
+	public string name = "";
+	public string pictureUrl = "";
+
 	//------------------------------------------------------------------------//
 	// GENERIC METHODS														  //
 	//------------------------------------------------------------------------//
@@ -82,8 +86,6 @@ public class GlobalEventUserData {
 		rewardCollected = _rewardCollected;
 	}
 
-
-
 	//------------------------------------------------------------------------//
 	// PERSISTENCE															  //
 	//------------------------------------------------------------------------//
@@ -92,6 +94,8 @@ public class GlobalEventUserData {
 	/// </summary>
 	public void Reset(){
 		Init(-1, "", 0, -1, 0, false);
+		name = "";
+		pictureUrl = "";
 	}
 
 	/// <summary>
@@ -103,9 +107,13 @@ public class GlobalEventUserData {
 		if(_data.ContainsKey("eventId")) eventID = _data["eventId"].AsInt;	// Event ID is optional
 		userID = _data["userId"];
 		score = _data["score"].AsInt;
-		if(_data.ContainsKey("position")) position = _data["position"].AsInt;
+		if(_data.ContainsKey("rank")) position = _data["rank"].AsInt;
 		if(_data.ContainsKey("endTimestamp")) endTimestamp = _data["endTimestamp"].AsLong;
 		if(_data.ContainsKey("rewardCollected")) rewardCollected = _data["rewardCollected"].AsBool;
+
+		// Social info
+		if(_data.ContainsKey("name")) name = _data["name"];
+		if(_data.ContainsKey("pic")) pictureUrl = _data["pic"];
 	}
 
 	/// <summary>
@@ -113,14 +121,14 @@ public class GlobalEventUserData {
 	/// </summary>
 	/// <returns>A new data object to be stored to persistence by the PersistenceManager.</returns>
 	/// <param name="_includeEventId">Whether to save the event ID as well.</param>
-	public SimpleJSON.JSONNode Save(bool _includeEventID, bool includePosition = true, bool includeRewardCollected = true) {
+	public SimpleJSON.JSONNode Save(bool _includeEventID, bool _includePosition = true, bool _includeRewardCollected = true) {
 		// Create a new json object for this event
 		SimpleJSON.JSONClass data = new SimpleJSON.JSONClass();
 		if(_includeEventID) data.Add("eventId", eventID.ToString(PersistenceManager.JSON_FORMATTING_CULTURE));
 		data.Add("userId", userID);
 		data.Add("score", score.ToString(PersistenceManager.JSON_FORMATTING_CULTURE));
-		if ( includePosition ) data.Add("position", position.ToString(PersistenceManager.JSON_FORMATTING_CULTURE));
-		if (includeRewardCollected) data.Add("rewardCollected", rewardCollected);
+		if(_includePosition) data.Add("rank", position.ToString(PersistenceManager.JSON_FORMATTING_CULTURE));
+		if(_includeRewardCollected) data.Add("rewardCollected", rewardCollected);
 		return data;
 	}
 }
