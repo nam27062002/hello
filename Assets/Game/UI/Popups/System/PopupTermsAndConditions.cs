b56363d7d2b43453690fef0d67718f7356f36c89
Marc Saña <marc.sana@ubisoft.com>
2017-08-31 12:56:45 +0200

    [W.I.P.] Terms and Conditions popup
    Former-commit-id: bb1cab565fb10a8c246b34e4e114d592d57dc4f1

---
+++ Assets/Game/UI/Popups/System/PopupTermsAndConditions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PopupController))]
+public class PopupTermsAndConditions : MonoBehaviour {
+
+	public void OnPrivacyPolicyButton() {
+
+	}
+
+	public void OnEulaButton() {
+
+	}
+
+	public void OnTermsOfUseButton() {
+
+	}
+	/*
+	public void OnCancelButton() {
+		https://legal.ubi.com/privacypolicy/
+		https://legal.ubi.com/eula/
+		https://legal.ubi.com/termsofuse/
+	}*/
+}
