8c31eb9dabac0db866885b6d217168c3d97b5cfb
David Germade <david.germade@ubisoft.com>
2017-09-12 18:59:36 +0200

    PERSISTENCE: Fixed some problems with unresponsive x in persistence errors popups.
    Former-commit-id: adb78124c4d82b61158c8bd6b3641968b4567382

--- Assets/Game/Core/Persistence/PersistenceFacade.cs
+++ Assets/Game/Core/Persistence/PersistenceFacade.cs
@@ -503,15 +503,14 @@ public class PersistenceFacade
     /// https://mdc-web-tomcat17.ubisoft.org/confluence/display/ubm/29%29Sync+Conflict
     /// </summary>    
     public static void Popups_OpenMergeConfirmation(Action onConfirm)
-    {
-        /*
+    {        
 		PopupMessage.Config config = PopupMessage.GetConfig();
         config.TitleTid = "TID_SAVE_WARN_CLOUD_WRONG_CHOICE_NAME";
         config.MessageTid = "TID_SAVE_WARN_CLOUD_WRONG_CHOICE_DESC";                
         config.ButtonMode = PopupMessage.Config.EButtonsMode.ConfirmAndCancel;
         config.OnConfirm = onConfirm;
-        PopupManager.PopupMessage_Open(config);
-        */
+        config.IsButtonCloseVisible = false;
+        PopupManager.PopupMessage_Open(config);       
     }
 
 	public static void Popup_OpenErrorWhenSyncing(Action onContinue, Action onRetry)
@@ -525,7 +524,8 @@ public class PersistenceFacade
         config.ExtraButtonTid = "TID_GEN_UPLOAD";
         config.ButtonMode = PopupMessage.Config.EButtonsMode.ConfirmAndCancel;
         config.OnConfirm = onRetry;
-        config.OnCancel = onContinue;        
+        config.OnCancel = onContinue;
+        config.IsButtonCloseVisible = false;
         PopupManager.PopupMessage_Open(config);            
 
         /*
@@ -555,6 +555,7 @@ public class PersistenceFacade
         PopupMessage.Config config = PopupMessage.GetConfig();
         config.TitleTid = "TID_SAVE_ERROR_CLOUD_CORRUPTED_NAME";
         config.MessageTid = msg;
+        config.IsButtonCloseVisible = false;
         config.OnConfirm = onContinue;
 
         if (canOverride)
@@ -577,6 +578,7 @@ public class PersistenceFacade
         PopupMessage.Config config = PopupMessage.GetConfig();
         config.TitleTid = "TID_SAVE_ERROR_LOCAL_CORRUPTED_NAME";
         config.MessageTid = msg;
+        config.IsButtonCloseVisible = false;
         config.OnConfirm = onReset;
 
         if (logInSocialEver)
@@ -610,6 +612,7 @@ public class PersistenceFacade
         config.TitleTid = "TID_SAVE_ERROR_BOTH_SAVE_CORRUPTED_NAME";
         config.MessageTid = "TID_SAVE_ERROR_BOTH_SAVE_CORRUPTED_DESC";
         config.ButtonMode = PopupMessage.Config.EButtonsMode.Confirm;
+        config.IsButtonCloseVisible = false;
         config.OnConfirm = onReset;
         PopupManager.PopupMessage_Open(config);
 
