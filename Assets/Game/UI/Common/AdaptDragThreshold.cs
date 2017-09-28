5b7c2f55664c3593b4610f0a8cd253bbfe76c152
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2017-09-28 17:33:51 +0200

    adapt drag threshold depending on screen dpi. Fix HDK-585
    Former-commit-id: f06f5592945e913ded3452617c4e97b8543c3f28

---
+++ Assets/Game/UI/Common/AdaptDragThreshold.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(UnityEngine.EventSystems.EventSystem))]
+public class AdaptDragThreshold : MonoBehaviour {
+
+	void Awake()
+	{
+		float inchToMove = 0.5f / 2.54f;// We want 0.5 cm to move before dragging
+		int pixels = Mathf.CeilToInt( inchToMove * Screen.dpi );
+		GetComponent<EventSystem>().pixelDragThreshold = pixels;
+	}
+}
