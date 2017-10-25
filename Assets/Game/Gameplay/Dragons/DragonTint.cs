716ccbcbc976a5f8aebdd2f7d453b8b6f7638b50
Marc Saña <marc.sana@ubisoft.com>
2017-10-25 14:22:30 +0200

    fixing tint: DOT as normal damage
    Former-commit-id: d7d3fc76dc0433fb361e2e497fd5b710cf0d11f1

--- Assets/Game/Gameplay/Dragons/DragonTint.cs
+++ Assets/Game/Gameplay/Dragons/DragonTint.cs
@@ -147,7 +147,7 @@ public class DragonTint : MonoBehaviour
 
 		// Other color
 		Color otherColor = Color.black;
-		if ( m_health.HasDOT() )
+		if ( m_health.HasDOT(DamageType.POISON) )
 		{
 			m_otherColorTimer += Time.deltaTime * 5;
 			otherColor = m_curseColor * (Mathf.Sin( m_otherColorTimer) + 1) * 0.5f;
