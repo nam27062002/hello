4f2cdb582efc9468a7ab31ec0c9046eae7110330
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2017-11-15 15:30:29 +0100

    HUD Energy Bar: Development - Changed size factor so all dragons have the same size.
    Former-commit-id: 16df37969765dbfaa2da29d59285df6fa77f161b

--- Assets/Game/UI/HUD/HUDStatBar.cs
+++ Assets/Game/UI/HUD/HUDStatBar.cs
@@ -431,7 +431,7 @@ public class HUDStatBar : MonoBehaviour {
 		if ( InstanceManager.player )
 		switch (m_type) {
 			case Type.Health: 	return InstanceManager.player.data.def.GetAsFloat("statsBarRatio");
-			case Type.Energy:	return InstanceManager.player.data.def.GetAsFloat("statsBarRatio");
+			case Type.Energy:	return 0.01f;//return InstanceManager.player.data.def.GetAsFloat("statsBarRatio");
 		}
 		return 0.01f;
 	}
