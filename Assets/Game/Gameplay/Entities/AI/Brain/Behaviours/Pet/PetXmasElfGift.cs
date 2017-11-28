24d0c257c5f7a9fc0affa8e4acba0724d55fb0b9
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2017-11-28 15:53:03 +0100

    xmas elf WIP
    Former-commit-id: c0e14e0fa3a199b9a0292683c0649f7ab51afc68

--- Assets/Game/Gameplay/Entities/AI/Brain/Behaviours/Pet/PetXmasElfGift.cs
+++ Assets/Game/Gameplay/Entities/AI/Brain/Behaviours/Pet/PetXmasElfGift.cs
@@ -18,6 +18,7 @@ namespace AI {
 			protected PetXmasElfGiftData m_data;
 			protected float m_timer;
 			protected PetXmasElfSpawner m_spawner;
+			protected PreyAnimationEvents m_animEvents;
 
 			public override StateComponentData CreateData() {
 				return new PetXmasElfGiftData();
@@ -32,20 +33,32 @@ namespace AI {
 				m_data = m_pilot.GetComponentData<PetXmasElfGiftData>();
 				m_timer =  Time.time + m_data.m_timeSecondAction.GetRandom();
 				m_spawner = m_pilot.GetComponent<PetXmasElfSpawner>();
-
+				m_animEvents = m_pilot.FindComponentRecursive<PreyAnimationEvents>();
 			}
 
+			protected override void OnEnter(State _oldState, object[] _param) {
+				m_animEvents.onAttachProjectile += new PreyAnimationEvents.OnAttachprojectile(OnAttachProjectile);
+			}
+
+			protected override void OnExit(State _newState) {
+				m_animEvents.onAttachProjectile -= new PreyAnimationEvents.OnAttachprojectile(OnAttachProjectile);
+				m_pilot.ReleaseAction( Pilot.Action.Button_A );
+			}
+
+			private void OnAttachProjectile() {
+				m_spawner.RamdomizeEntity();
+				m_spawner.Respawn();
+				m_pilot.ReleaseAction( Pilot.Action.Button_A );
+			}
 
 			protected override void OnUpdate()
 			{
 				if ( m_timer <= Time.time && m_spawner.HasAvailableEntities())
 				{
 					m_timer =  Time.time + m_data.m_timeSecondAction.GetRandom();
-					m_spawner.RamdomizeEntity();
-					m_spawner.Respawn();
+					m_pilot.PressAction(Pilot.Action.Button_A);
 				}
 			}
-
 		}
 	}
 }
