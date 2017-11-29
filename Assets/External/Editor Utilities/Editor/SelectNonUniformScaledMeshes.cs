e9a06eae896ae3a155210e3f4ac0347628d00a13
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2017-11-29 14:16:28 +0100

    non uniform meshes selector
    Former-commit-id: 9fdb4e2c7b5cb1df9b91b12bd3370229b35814ed

---
+++ Assets/External/Editor Utilities/Editor/SelectNonUniformScaledMeshes.cs
@@ -0,0 +1,54 @@
+﻿using Assets.Code.Game.Spline;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+class SelectNonUniformScaledMeshes
+{
+    
+	[MenuItem("Hungry Dragon/Tools/Highlight Non uniform scaled meshes")]
+	static void HighlightNonUniformScale()
+    {
+		MeshFilter[] meshesFilters = Object.FindObjectsOfType<MeshFilter>();
+		List<GameObject> toSelect = new List<GameObject>();
+		for (int i = 0; i < meshesFilters.Length; i++)
+        {
+        	bool badScale = false;
+			Vector3 scale = meshesFilters[i].transform.localScale;
+			if ( !MathUtils.FuzzyEquals(scale.x, scale.y) || !MathUtils.FuzzyEquals(scale.x, scale.z) || !MathUtils.FuzzyEquals(scale.y, scale.z) )
+			{
+				badScale = true;
+			}
+
+			if ( badScale )
+			{
+				// Highlight object
+				toSelect.Add( meshesFilters[i].gameObject );
+			}
+        }
+
+		SkinnedMeshRenderer[] skinnedRenderers = Object.FindObjectsOfType<SkinnedMeshRenderer>();
+		for (int i = 0; i < skinnedRenderers.Length; i++)
+        {
+        	bool badScale = false;
+			Vector3 scale = skinnedRenderers[i].transform.localScale;
+			if ( !MathUtils.FuzzyEquals(scale.x, scale.y) || !MathUtils.FuzzyEquals(scale.x, scale.z) || !MathUtils.FuzzyEquals(scale.y, scale.z) )
+			{
+				badScale = true;
+			}
+
+			if ( badScale )
+			{
+				// Highlight object
+				toSelect.Add( skinnedRenderers[i].gameObject );
+			}
+        }
+
+        Selection.objects = toSelect.ToArray();
+    }
+
+	
+}
