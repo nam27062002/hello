22958c85b6350e058c63c8d14b35562e9ad9b74c
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2018-01-10 08:43:06 +0100

    alien pet
    Former-commit-id: 8cf641973ad8b483f0b65a7ee5415d3eabac1a99

---
+++ Assets/Game/Gameplay/Entities/AI/Brain/Behaviours/Attack/AttackStunArch.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AI {
+	namespace Behaviour {	
+		[System.Serializable]
+		public class AttackStunArchData : AttackData {
+			public float m_archIntro;
+			public float m_archOutro;
+			public float m_archDuration;
+
+			public float m_archLength;
+			public float m_archAngle;
+
+			public string m_beamAnchorPoint;
+			public float m_stunDuration;
+
+		}
+		
+
+		[CreateAssetMenu(menuName = "Behaviour/Attack/Stun Arch")]
+		public class AttackStunArch : Attack {
+
+			[StateTransitionTrigger]
+			private static string OnAttackDone = "onAttackDone";
+
+			private ViewControl m_viewControl;
+
+			private AttackStunArchData m_stunData;
+			private int m_numCheckEntities = 0;
+			private Entity[] m_checkEntities = new Entity[30];
+			private Transform m_stunAnchor;
+			private bool m_attacking = false;
+			private float m_attackingTimer = 0;
+
+			public override StateComponentData CreateData() {
+				return new AttackStunArchData();
+			}
+
+			public override System.Type GetDataType() {
+				return typeof(AttackStunArchData);
+			}
+
+			protected override void OnInitialise() {
+				m_stunData = m_pilot.GetComponentData<AttackStunArchData>();
+				m_data = m_stunData;
+				m_viewControl = m_pilot.GetComponent<ViewControl>();
+				base.OnInitialise();
+
+				m_stunAnchor = m_pilot.FindTransformRecursive( m_stunData.m_beamAnchorPoint );
+			}
+
+			protected override void StartAttack() 
+			{
+				m_attacking = true;
+				m_attackingTimer = m_stunData.m_archDuration + m_stunData.m_archIntro + m_stunData.m_archOutro;
+
+				base.StartAttack();
+				if (m_data.forceFaceToShoot && m_viewControl != null) {
+					// Tell view position to attack
+					m_viewControl.attackTargetPosition = m_facingTarget;
+				}
+
+
+			}
+
+			protected override void OnEnter(State _oldState, object[] _param) {
+				base.OnEnter(_oldState, _param);
+				m_pilot.PressAction(Pilot.Action.Aim);
+				m_machine.SetSignal(Signals.Type.Ranged, true);
+
+			}
+
+			protected override void OnUpdate() {
+				base.OnUpdate();
+
+				if ( m_attacking && m_attackingTimer > 0 )
+				{
+					m_attackingTimer -= Time.deltaTime;
+					if ( m_attackingTimer <= 0 )
+					{
+						m_pilot.ReleaseAction(Pilot.Action.Attack);
+						m_machine.DisableSensor(m_data.retreatTime);
+						Transition(OnAttackDone);
+					}
+					else
+					{
+
+							// Intro Outro
+						float delta = 1;
+						if ( m_attackingTimer > (m_stunData.m_archDuration + m_stunData.m_archOutro) )
+						{
+							// intro
+							delta = m_attackingTimer - (m_stunData.m_archDuration + m_stunData.m_archOutro);
+							delta = 1.0f - delta / m_stunData.m_archIntro;
+						}else if ( m_attackingTimer < m_stunData.m_archOutro){
+							// outro
+							delta = (m_stunData.m_archOutro - m_attackingTimer) / m_stunData.m_archOutro;
+						}
+
+						float arcLength = m_stunData.m_archLength * delta;
+						float arcAngle = m_stunData.m_archAngle * delta;
+
+						Vector3 arcOrigin = m_stunAnchor.position;
+						Vector3 arcOrigin_0 = arcOrigin;
+						arcOrigin_0.z = 0;
+						Vector3 arcDir = m_stunAnchor.forward;
+						arcDir.z = 0;
+						arcDir.Normalize();
+						m_numCheckEntities =  EntityManager.instance.GetOverlapingEntities(arcOrigin, arcLength, m_checkEntities);
+
+						for (int e = 0; e < m_numCheckEntities; e++) 
+						{
+							Entity entity = m_checkEntities[e];
+							// Start bite attempt
+							Vector3 heading = (entity.transform.position - arcOrigin);
+							float dot = Vector3.Dot(heading, arcDir);
+							if ( dot > 0)
+							{
+								// Check arc
+								Vector3 circleCenter = entity.circleArea.center;
+								circleCenter.z = 0;
+								if (MathUtils.TestArcVsCircle( arcOrigin_0, arcAngle, arcLength, arcDir, circleCenter, entity.circleArea.radius))
+								{
+									// stun entity
+									(entity.machine as Machine).Stun( m_stunData.m_stunDuration );
+								}
+							}
+						}
+					}
+				}
+			}
+
+			protected override void OnExit(State _newState) {
+				base.OnExit(_newState);
+				m_pilot.ReleaseAction(Pilot.Action.Aim);
+				m_machine.SetSignal(Signals.Type.Ranged, false);
+			}
+		}
+	}
+}
---
+++ Assets/Game/Gameplay/Entities/AI/Brain/Behaviours/Pet/PetSearchStunTarget.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace AI {
+	namespace Behaviour {	
+
+		[System.Serializable]
+		public class PetSearchStunTargetData : StateComponentData {
+			[Tooltip("Max tier this pet will consider target.")]
+			public DragonTier maxValidTier = DragonTier.TIER_4;
+			[Tooltip("Min tier this pet will consider target.")]
+			public DragonTier minValidTier = DragonTier.TIER_0;
+			public float dragonSizeRangeMultiplier = 10;
+			public Range m_shutdownRange = new Range(10,20);
+		}
+
+		[CreateAssetMenu(menuName = "Behaviour/Pet/Search Stun Target")]
+		public class PetSearchStunTarget : StateComponent {
+
+			[StateTransitionTrigger]
+			private static string onEnemyTargeted = "onEnemyTargeted";
+
+			private float m_shutdownSensorTime;
+			private float m_timer;
+			private object[] m_transitionParam;
+
+			private Entity[] m_checkEntities = new Entity[50];
+			private int m_numCheckEntities = 0;
+
+			private int m_collidersMask;
+
+			DragonPlayer m_owner;
+			float m_range;
+			MachineSensor m_sensor;
+
+			private PetSearchStunTargetData m_data;
+
+
+			public override StateComponentData CreateData() {
+				return new PetSearchStunTargetData();
+			}
+
+			public override System.Type GetDataType() {
+				return typeof(PetSearchStunTargetData);
+			}
+
+			protected override void OnInitialise() {
+				m_timer = 0f;
+				m_shutdownSensorTime = 0f;
+
+				m_transitionParam = new object[1];
+
+				m_collidersMask = 1<<LayerMask.NameToLayer("Ground") | 1<<LayerMask.NameToLayer("Obstacle");
+
+				base.OnInitialise();
+
+				m_owner = InstanceManager.player;
+				m_data = m_pilot.GetComponentData<PetSearchStunTargetData>();
+				m_range = m_owner.data.GetScaleAtLevel(m_owner.data.progression.maxLevel) * m_data.dragonSizeRangeMultiplier;
+
+				m_sensor = (m_machine as Machine).sensor;
+			}
+
+			// The first element in _param must contain the amount of time without detecting an enemy
+			protected override void OnEnter(State _oldState, object[] _param) {
+				m_shutdownSensorTime = m_data.m_shutdownRange.GetRandom();
+				if (m_shutdownSensorTime > 0f) {
+					m_timer = m_shutdownSensorTime;
+				} else {
+					m_timer = 0f;
+				}
+			}
+
+			protected override void OnUpdate() {
+				if (m_timer > 0f) {
+					m_timer -= Time.deltaTime;
+				} else {
+					Vector3 centerPos = m_owner.transform.position;
+
+					m_numCheckEntities = EntityManager.instance.GetOverlapingEntities( centerPos , m_range, m_checkEntities);
+					for (int e = 0; e < m_numCheckEntities; e++) 
+					{
+						Entity entity = m_checkEntities[e];
+						Machine machine = entity.GetComponent<Machine>();
+						if (machine != null && !machine.isPetTarget )
+						{
+							bool isViable = false;
+
+							if ( entity.IsEdible( m_data.maxValidTier ) && entity.edibleFromTier >= m_data.minValidTier)
+							{
+								// Test if in front of player!
+								Vector3 entityDir = machine.position - m_owner.dragonMotion.position;
+								if( Vector2.Dot( m_owner.dragonMotion.direction, entityDir) > 0)
+								{
+									// Check if physics reachable
+									RaycastHit hit;
+									Vector3 dir = entity.circleArea.center - m_machine.position;
+									bool hasHit = Physics.Raycast(m_machine.position, dir.normalized, out hit, dir.magnitude, m_collidersMask);
+									if ( !hasHit )
+									{
+										// Check if closed? Not for the moment
+										m_transitionParam[0] = entity.transform;
+										if ( entity.circleArea )
+											m_sensor.SetupEnemy(entity.transform, entity.circleArea.radius * entity.circleArea.radius, null);
+										else
+											m_sensor.SetupEnemy(entity.transform, 0, null);
+										m_machine.SetSignal(Signals.Type.Warning, true);
+										Transition( onEnemyTargeted, m_transitionParam);
+										break;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
