58eab955afeb7c2acf3725af450b9fa794217434
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2018-01-15 14:52:35 +0100

    Survey Popup: Development - Setup and flow implementation. TODO: Tracking and actually linking to the survey.
    Former-commit-id: e6cc8a1270aa8518e03e27711f53401728b00ac8

---
+++ Assets/Game/UI/Popups/System/PopupAskSurvey.cs
@@ -0,0 +1,128 @@
+// PopupAskSurvey.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 15/01/2018.
+// Copyright (c) 2018 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// https://mdc-web-tomcat17.ubisoft.org/confluence/display/ubm/%5BHD%5D+In-Game+Survey
+/// </summary>
+public class PopupAskSurvey : MonoBehaviour {
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
+	public const string PATH = "UI/Popups/Message/PF_PopupSurvey";
+
+	public const string PREF_CHECK = "PopupAskSurvey.Check";
+	public const string PREF_LAST_DISPLAYED_SESSION = "PopupAskSurvey.LastDisplayedSession";
+
+	public const string MIN_OWNED_DRAGON = "dragon_reptile";
+	public const int MIN_RUNS = 3;
+	public const int MIN_SESSIONS = 3;
+
+	private enum Action {
+		NO,
+		LATER,
+		YES
+	}
+	
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
+	
+	//------------------------------------------------------------------------//
+	// GENERIC METHODS														  //
+	//------------------------------------------------------------------------//
+
+	//------------------------------------------------------------------------//
+	// OTHER METHODS														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Check whether the popup must be triggered considering the current profile.
+	/// If all checks are passed, opens the popup.
+	/// </summary>
+	/// <returns><c>true</c> if all conditions to display the popup are met and the popup will be opened.</returns>
+	public static bool Check() {
+		// Not if already checked!
+		if(!Prefs.GetBoolPlayer(PREF_CHECK, true)) return false;
+
+		// Not if target dragon not yet owned
+		DragonData targetDragon = DragonManager.GetDragonData(MIN_OWNED_DRAGON);
+		if(targetDragon == null) return false;
+		if(!targetDragon.isOwned) return false;
+
+		// Not if we haven't reached the minimum runs with target dragon!
+		if(targetDragon.gamesPlayed < MIN_RUNS) return false;
+
+		// Only after a run!
+		if(GameSceneManager.prevScene.CompareTo(ResultsScreenController.NAME) != 0 
+		&& GameSceneManager.prevScene.CompareTo(GameSceneController.NAME) != 0) {
+			return false;
+		}
+
+		// Not if not enough sessions have passed since last time we showed the popup
+		if(HDTrackingManager.Instance.TrackingPersistenceSystem.SessionCount < Prefs.GetIntPlayer(PREF_LAST_DISPLAYED_SESSION, 0) + MIN_SESSIONS) return false;
+
+		// All checks passed! Popup can be displayed!
+		PopupManager.OpenPopupInstant(PATH);
+		return true;
+	}
+
+	/// <summary>
+	/// Sends the tracking and close the popup.
+	/// </summary>
+	/// <param name="_action">Tha chosen action.</param>
+	private void CloseAndSendTracking(Action _action) {
+		// [AOC] TODO!! Tracking
+
+		// Close the popup!
+		GetComponent<PopupController>().Close(true);
+	}
+
+	//------------------------------------------------------------------------//
+	// CALLBACKS															  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Poppup is about to be displayed.
+	/// </summary>
+	public void OnShowPreAnimation() {
+		// Update tracking vars
+		Prefs.SetIntPlayer(PREF_LAST_DISPLAYED_SESSION, HDTrackingManager.Instance.TrackingPersistenceSystem.SessionCount);
+	}
+
+	/// <summary>
+	/// Yes button has been pressed.
+	/// </summary>
+	public void OnYes() {
+		// [AOC] TODO!! Open external survey
+
+		// Close the popup and set to never show again
+		Prefs.SetBoolPlayer(PREF_CHECK, false);
+		CloseAndSendTracking(Action.YES);
+	}
+
+	/// <summary>
+	/// No button has been pressed.
+	/// </summary>
+	public void OnNo() {
+		// Close the popup and set to never show again
+		Prefs.SetBoolPlayer(PREF_CHECK, false);
+		CloseAndSendTracking(Action.NO);
+	}
+
+	/// <summary>
+	/// Later button has been pressed.
+	/// </summary>
+	public void OnLater() {
+		// Close the popup
+		CloseAndSendTracking(Action.LATER);
+	}
+}
