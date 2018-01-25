23534755e92cf13accfcf79d69005eabc247d4d4
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2018-01-25 11:22:38 +0100

    Settings Popup: Development - Implemented graphic settings slider.
    Former-commit-id: 4aa82ff50b3817fcfd80a5621adb76eff94adf48

--- Assets/Game/UI/Popups/Menu/Components/PopupSettingsOptionsTab.cs
+++ Assets/Game/UI/Popups/Menu/Components/PopupSettingsOptionsTab.cs
@@ -7,6 +7,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 /// <summary>
 /// This class is responsible for handling the options tab in the settings popup.
@@ -19,6 +20,11 @@ public class PopupSettingsOptionsTab : MonoBehaviour
     // Exposed
     [SerializeField] private GameObject m_languagePillPrefab = null;
 	[SerializeField] private SnappingScrollRect m_languageScrollList = null;
+	[Space]
+	[SerializeField] private Slider m_graphicsQualitySlider = null;
+	[SerializeField] private CanvasGroup m_graphicsQualityCanvasGroup = null;
+	[SerializeField] private TextMeshProUGUI m_graphicsQualityCurrentValueText = null;
+	[SerializeField] private GameObject[] m_graphicsQualitySeparators = new GameObject[4];
 	[Space]
 	[SerializeField] private GameObject m_googlePlayGroup = null;
 	[SerializeField] private GameObject m_googlePlayLoginButton = null;
@@ -37,6 +43,9 @@ public class PopupSettingsOptionsTab : MonoBehaviour
 
 	private PopupController m_loadingPopupController = null;
 	private bool m_dirty = false;
+
+	private int m_graphicsMaxLevel = 4;
+	private int m_initialGraphicsQualityLevel = -1;
 
     //------------------------------------------------------------------------//
     // GENERIC METHODS														  //
@@ -114,6 +123,15 @@ public class PopupSettingsOptionsTab : MonoBehaviour
 		}
 	}
 
+	/// <summary>
+	/// Update the graphics quality textfield with the current selected value.
+	/// </summary>
+	private void RefreshGraphicsQualityText() {
+		// Just display the number
+		int value = Mathf.Min(m_graphicsMaxLevel, (int)m_graphicsQualitySlider.value);	// Special case for when max level is 0
+		m_graphicsQualityCurrentValueText.text = StringUtils.FormatNumber(value + 1);
+	}
+
     //------------------------------------------------------------------------//
     // CALLBACKS															  //
     //------------------------------------------------------------------------//
@@ -142,11 +160,79 @@ public class PopupSettingsOptionsTab : MonoBehaviour
 	}
 
 	/// <summary>
-	/// The popup has just finished open.
+	/// The graphics quality slider has changed its value.
 	/// </summary>
-	public void OnOpenPostAnimation() {
-		// SelectCurrentLanguage(false);
-		// UbiBCN.CoroutineManager.DelayedCallByFrames(() => { SelectCurrentLanguage(true); }, 5);
+	/// <param name="_newValue">New value.</param>
+	public void OnGraphicsQualityChanged(float _newValue) {
+		// Ignore if max quality level is 0 (shouldn't even get here)
+		if(m_graphicsMaxLevel == 0) return;
+
+		// Store new value
+		FeatureSettingsManager.instance.SetUserProfileLevel((int)_newValue);
+
+		// Update text
+		RefreshGraphicsQualityText();
+	}
+
+	/// <summary>
+	/// The popup is about to open.
+	/// </summary>
+	public void OnOpenPreAnimation() {
+		// Initialize Graphics Quality Slider
+		m_initialGraphicsQualityLevel = FeatureSettingsManager.instance.GetUserProfileLevel();
+		m_graphicsMaxLevel = FeatureSettingsManager.instance.GetMaxProfileLevelSupported();
+		int userLevel = m_initialGraphicsQualityLevel;
+		if(userLevel < 0) {
+			userLevel = FeatureSettingsManager.instance.GetCurrentProfileLevel();
+		}
+
+		// Special case when only 1 level is available
+		m_graphicsQualitySlider.minValue = 0;
+		if(m_graphicsMaxLevel == 0) {
+			m_graphicsQualitySlider.maxValue = 1;
+			m_graphicsQualitySlider.value = 1;
+			m_graphicsQualityCanvasGroup.interactable = false;
+		} else {
+			m_graphicsQualitySlider.maxValue = m_graphicsMaxLevel;
+			m_graphicsQualitySlider.value = userLevel;
+			m_graphicsQualityCanvasGroup.interactable = true;
+		}
+		m_graphicsQualitySlider.onValueChanged.AddListener(OnGraphicsQualityChanged);
+
+		// Adjust number of separators according to slider's max value
+		for(int i = 0; i < m_graphicsQualitySeparators.Length; ++i) {
+			m_graphicsQualitySeparators[i].SetActive(i < (int)m_graphicsQualitySlider.maxValue);	// Last level doesn't have a separator (it's the end of the slider)
+		}
+
+		// Initialize text
+		RefreshGraphicsQualityText();
+	}
+
+	/// <summary>
+	/// The popup is about to be closed.
+	/// </summary>
+	public void OnClosePreAnimation() {
+		// If the graphics quality setting has changed, apply new value
+		// Ignore if max quality level is 0 (shouldn't even get here)
+		if(m_graphicsMaxLevel > 0 && FeatureSettingsManager.instance.GetUserProfileLevel() != m_initialGraphicsQualityLevel) {
+			// Show busy screen
+			BusyScreen.Setup(true, LocalizationManager.SharedInstance.Localize("TID_QUALITY_SLIDER_APPLYING"));
+			BusyScreen.Show(this, false);
+
+			// Apply new quality setting
+			FeatureSettingsManager.instance.RecalculateAndApplyProfile();
+		}
+
+		// Remove listener
+		m_graphicsQualitySlider.onValueChanged.RemoveListener(OnGraphicsQualityChanged);
+	}
+
+	/// <summary>
+	/// The popup has been closed.
+	/// </summary>
+	public void OnClosePostAnimation() {
+		// Hide busy screen
+		BusyScreen.Hide(this, true);
 	}
 
 	public void RefreshGooglePlayView(){
