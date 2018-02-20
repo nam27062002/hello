cf50a8bb751d1f7f845c04507c9fc312e77d4b29
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2018-02-20 15:49:29 +0100

    convict chain test
    Former-commit-id: 9575ddb4d6cad139ff04d8510ea88d2e16b00928

---
+++ Assets/Game/Gameplay/Equip/EquipableChain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipableChain : MonoBehaviour {
+
+	public GameObject m_ChainPrefab;
+	public Rigidbody m_ChainAnchor;
+	public string m_ChainLinkName;
+
+	public void Start()
+	{
+		GameObject go = Instantiate<GameObject>(m_ChainPrefab);
+		go.transform.position = m_ChainAnchor.transform.position;
+		go.transform.rotation = m_ChainAnchor.transform.rotation;
+		go.transform.localScale = m_ChainAnchor.transform.lossyScale;
+
+		Transform link = go.transform.Find(m_ChainLinkName);
+		link.GetComponent<ConfigurableJoint>().connectedBody = m_ChainAnchor;
+		link.transform.position = m_ChainAnchor.transform.position;
+		link.transform.rotation = m_ChainAnchor.transform.rotation;
+		ConfigurableJoint[] joints = go.transform.GetComponentsInChildren<ConfigurableJoint>();
+		for (int i = 0; i < joints.Length; ++i) {
+			joints[i].anchor = joints[i].anchor * m_ChainAnchor.transform.lossyScale.x;
+		}
+
+	}
+	
+}
