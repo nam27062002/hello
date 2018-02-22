da6a225ecc57c0c5cd9b37596d8218b6fa737f51
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2018-02-22 11:45:51 +0100

    sheep disguise for Jawfrey
    Former-commit-id: 5711faae6ae8342d0ea9da1c8f089961aca88261

--- Assets/Game/Gameplay/Equip/EquipableChain.cs
+++ Assets/Game/Gameplay/Equip/EquipableChain.cs
@@ -8,12 +8,15 @@ public class EquipableChain : MonoBehaviour {
 	public Rigidbody m_ChainAnchor;
 	public string m_ChainLinkName;
 
+	private GameObject m_chainInstance;
+
 	public void Start()
 	{
 		GameObject go = Instantiate<GameObject>(m_ChainPrefab);
 		go.transform.position = m_ChainAnchor.transform.position;
 		go.transform.rotation = m_ChainAnchor.transform.rotation;
 		go.transform.localScale = m_ChainAnchor.transform.lossyScale;
+		m_chainInstance = go;
 
 		Transform link = go.transform.Find(m_ChainLinkName);
 		link.GetComponent<ConfigurableJoint>().connectedBody = m_ChainAnchor;
@@ -23,7 +26,13 @@ public class EquipableChain : MonoBehaviour {
 		for (int i = 0; i < joints.Length; ++i) {
 			joints[i].anchor = joints[i].anchor * m_ChainAnchor.transform.lossyScale.x;
 		}
+	}
 
+	void OnDestroy()
+	{
+		if ( m_chainInstance )	
+			Destroy(m_chainInstance);
+		m_chainInstance = null;
 	}
 	
 }
