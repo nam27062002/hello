f5901c72e59a698be80b96b1b30a09ed1dfe6261
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2018-03-20 16:48:28 +0100

    pet faune WIP
    Former-commit-id: f5155b84ce0c378b2b7106f834ebd531304aaccd

---
+++ Assets/Game/Gameplay/Entities/AI/Brain/Behaviours/Pet/PetScreenGold.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AI {
+	namespace Behaviour {
+		/*
+		[System.Serializable]
+		public class PetScreenGoldData : StateComponentData {
+			public float m_duration;
+		}
+		*/
+
+		[CreateAssetMenu(menuName = "Behaviour/Pet/Screen Gold")]
+		public class PetScreenGold : StateComponent {
+
+			[StateTransitionTrigger]
+			private static string OnAnimFinished = "onAnimFinished";
+
+			float m_timer;
+
+			public override StateComponentData CreateData() {
+				return new PetPlayerInfiniteBoostData();
+			}
+
+			public override System.Type GetDataType() {
+				return typeof(PetPlayerInfiniteBoostData);
+			}
+
+			protected override void OnInitialise() {
+				// m_data = m_pilot.GetComponentData<PetPlayerInfiniteBoostData>();
+			}
+
+			protected override void OnEnter(State oldState, object[] param) {
+				m_pilot.PressAction(Pilot.Action.Button_A);
+				m_timer = 2.0f;
+				EntityManager.instance.ForceOnScreenEntitiesGolden();
+			}
+
+			protected override void OnUpdate(){
+				m_timer -= Time.deltaTime;
+				if ( m_timer <= 0 )	
+				{
+					Transition( OnAnimFinished );
+				}
+			}
+
+			protected override void OnExit(State _newState){
+				m_pilot.ReleaseAction(Pilot.Action.Button_A);
+			}
+		}
+	}
+}
