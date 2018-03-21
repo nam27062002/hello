cc631dd170bec1e97dceae2fb36c3387b327bfa9
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2018-03-21 09:51:30 +0100

    prepare audio
    Former-commit-id: 2ffcac150a75dd92b7b422462d6b98270aa2c247

--- Assets/Game/Gameplay/Entities/AI/Brain/Behaviours/Pet/PetScreenGold.cs
+++ Assets/Game/Gameplay/Entities/AI/Brain/Behaviours/Pet/PetScreenGold.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 namespace AI {
 	namespace Behaviour {
-		/*
+		
 		[System.Serializable]
 		public class PetScreenGoldData : StateComponentData {
-			public float m_duration;
+			public string m_audio;
 		}
-		*/
+
 
 		[CreateAssetMenu(menuName = "Behaviour/Pet/Screen Gold")]
 		public class PetScreenGold : StateComponent {
@@ -16,6 +16,7 @@ namespace AI {
 			[StateTransitionTrigger]
 			private static string OnAnimFinished = "onAnimFinished";
 
+			PetScreenGoldData m_data;
 			float m_timer;
 
 			public override StateComponentData CreateData() {
@@ -27,12 +28,16 @@ namespace AI {
 			}
 
 			protected override void OnInitialise() {
-				// m_data = m_pilot.GetComponentData<PetPlayerInfiniteBoostData>();
+				m_data = m_pilot.GetComponentData<PetScreenGoldData>();
 			}
 
 			protected override void OnEnter(State oldState, object[] param) {
 				m_pilot.PressAction(Pilot.Action.Button_A);
 				m_timer = 1.0f;
+				if (!string.IsNullOrEmpty(m_data.m_audio))
+				{
+					AudioController.Play(m_data.m_audio);
+				}
 			}
 
 			protected override void OnUpdate(){
