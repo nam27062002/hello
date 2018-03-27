996a4dfb1530f19a8ad2ffb5d576312445f04bdb
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2018-03-27 15:30:02 +0200

    seasonal spawner
    Former-commit-id: a358c95ce3aca241d18c7e4d308c356fb3c17de9

---
+++ Assets/Game/Gameplay/Spawners/Editor/SeasonalSpawnerEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(SeasonalSpawner), true)]	// True to be used by heir classes as well
+[CanEditMultipleObjects]
+public class SeasonalSpawnerEditor : SpawnerEditor {
+
+	public override bool HasCustomPropertyDraw(SerializedProperty _p) {
+		if (_p.name == "m_entityPrefabList") {
+
+			SerializedProperty sc = serializedObject.FindProperty("m_spawnConfigs");
+			EditorGUILayout.PropertyField(sc, true);
+
+			return true;
+		} else if (_p.name == "m_spawnConfigs") {
+
+			return true;
+		}
+		return false;
+	}
+}
