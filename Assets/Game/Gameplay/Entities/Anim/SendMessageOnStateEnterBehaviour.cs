4b0e8c33f3bc4377891f9e8faaf14a716982e7b6
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2018-04-17 15:55:50 +0200

    strong flap sound on chinese and reptile dragons
    Former-commit-id: 1a4a8efe8be9832858c72e779e82cfe5d7e53d5a

---
+++ Assets/Game/Gameplay/Entities/Anim/SendMessageOnStateEnterBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendMessageOnStateEnterBehaviour : StateMachineBehaviour {
+
+	public string m_messageName = ""; 
+
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+	{
+		base.OnStateEnter(animator, animatorStateInfo,layerIndex);
+		animator.SendMessage( m_messageName );
+	}
+}
