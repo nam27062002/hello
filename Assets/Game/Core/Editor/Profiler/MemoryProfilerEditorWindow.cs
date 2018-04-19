0c101496496966bd89e178ef89dd369b6b2aeb05
dcampos <dcampos@ffelius-imac.ubisoft.org>
2018-04-19 11:33:24 +0200

    [PROFILER] Deleted obsolete code that doesn’t compile in Unity 2017.
    Former-commit-id: f31a57fe491d62f71a0c3bc6bc2bb640377d61ee

--- Assets/Game/Core/Editor/Profiler/MemoryProfilerEditorWindow.cs
+++ Assets/Game/Core/Editor/Profiler/MemoryProfilerEditorWindow.cs
@@ -963,12 +963,6 @@ public class MemoryProfilerEditorWindow : EditorWindow
                 return 8;
             case TextureFormat.BGRA32://	 Format returned by iPhone camera
                 return 32;
-#if !UNITY_5
-			case TextureFormat.ATF_RGB_DXT1://	 Flash-specific RGB DXT1 compressed color texture format.
-			case TextureFormat.ATF_RGBA_JPG://	 Flash-specific RGBA JPG-compressed color texture format.
-			case TextureFormat.ATF_RGB_JPG://	 Flash-specific RGB JPG-compressed color texture format.
-			return 0; //Not supported yet  
-#endif
         }
         return 0;
     }
