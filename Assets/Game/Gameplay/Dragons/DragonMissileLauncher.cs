1cf8fbeff2a28af11b5f78ae024e8f6335adcc8d
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2018-04-23 15:26:37 +0200

    improve missile targeting
    Former-commit-id: 2c721b3aba4a4e3260db706e4181c0518d3b6063

--- Assets/Game/Gameplay/Dragons/DragonMissileLauncher.cs
+++ Assets/Game/Gameplay/Dragons/DragonMissileLauncher.cs
@@ -9,7 +9,6 @@ public class DragonMissileLauncher : MonoBehaviour {
 
 	DragonBoostBehaviour m_playerBoost;
 	private PoolHandler m_poolHandler;
-	private GameObject m_projectile;
 	public List<Transform> m_firePositions = new List<Transform>();
 	private int m_fireAnchorIndex = 0;
 	public string m_projectileName;
@@ -44,38 +43,37 @@ public class DragonMissileLauncher : MonoBehaviour {
 	private void Fire()
 	{
 		// Fire!!
-		if (m_projectile == null) {
-			m_projectile = m_poolHandler.GetInstance();
+		Transform originTransform = m_firePositions[m_fireAnchorIndex];
 
-			if (m_projectile != null) {
-				
-				Transform originTransform = m_firePositions[m_fireAnchorIndex];
-
-				// Search target!
-				Transform target = null;
-				Entity[] preys = EntityManager.instance.GetEntitiesInRange2D(originTransform.position, m_rangeSize);
-				for (int i = 0; i < preys.Length && target == null; i++) 
-				{
-					// if (preys[i].IsBurnable(m_fireTier)) 
-					{
-						target = preys[i].transform;
-					}
+		// Search target!
+		Transform alternateTarget = null;
+		Transform target = null;
+		Entity[] preys = EntityManager.instance.GetEntitiesInRange2D(originTransform.position, m_rangeSize);
+		for (int i = 0; i < preys.Length && target == null; i++) 
+		{
+			// if (preys[i].IsBurnable(m_fireTier)) 
+			{
+				if ( alternateTarget == null ){
+					alternateTarget = preys[i].transform;
+				}else if ( Vector3.Dot( originTransform.forward, preys[i].transform.position - originTransform.position) > 0 ){
+					target = preys[i].transform;
 				}
-
-				if ( target != null )
-				{
-					IProjectile projectile = m_projectile.GetComponent<IProjectile>();
-					projectile.AttachTo(originTransform);	
-					projectile.Shoot(target, transform.forward, 9999, originTransform);
-					m_fireAnchorIndex++;
-					if ( m_fireAnchorIndex >= m_firePositions.Count )
-						m_fireAnchorIndex = 0;
-				}
-			} else {
-				Debug.LogError("Projectile not available");
 			}
-			m_projectile = null;
 		}
+		if ( target == null && alternateTarget != null )
+			target = alternateTarget;
+
+		if ( target != null )
+		{
+			GameObject go = m_poolHandler.GetInstance();
+			IProjectile projectile = go.GetComponent<IProjectile>();
+			projectile.AttachTo(originTransform);	
+			projectile.Shoot(target, transform.forward, 9999, originTransform);
+			m_fireAnchorIndex++;
+			if ( m_fireAnchorIndex >= m_firePositions.Count )
+				m_fireAnchorIndex = 0;
+		}
+			
 	}
 
 	void CreatePool() {
