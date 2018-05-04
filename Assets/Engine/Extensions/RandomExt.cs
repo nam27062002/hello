21ea5b1e0b805515c996e90c15ccadf4f8006f7c
David Germade <david.germade@ubisoft.com>
2018-05-04 13:01:22 +0200

    [FIX] Fixed an issue causing a crash when Range(min, max) was called with min equals to max.
    Former-commit-id: 8fe8e885786b784c3376fb0d457c05f3b644998b

--- Assets/Engine/Extensions/RandomExt.cs
+++ Assets/Engine/Extensions/RandomExt.cs
@@ -5,9 +5,13 @@ public static class RandomExt {
 	private static System.Random s_rand = new System.Random();
 	private static byte[] s_buf = new byte[8];
 
-	public static long Range(long _min, long _max) {		
-		s_rand.NextBytes(s_buf);
-		long longRand = BitConverter.ToInt64(s_buf, 0);
-		return (Math.Abs(longRand % (_max - _min)) + _min);
+	public static long Range(long _min, long _max) {
+        if (_min == _max) {
+            return _min;
+        } else {
+            s_rand.NextBytes(s_buf);
+            long longRand = BitConverter.ToInt64(s_buf, 0);
+            return (Math.Abs(longRand % (_max - _min)) + _min);
+        }
 	}
 }
