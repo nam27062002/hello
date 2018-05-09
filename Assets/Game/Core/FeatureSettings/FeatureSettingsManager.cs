532d883b50a841bbc88d76180e024dec3961fe41
David Germade <david.germade@ubisoft.com>
2018-05-09 15:18:44 +0200

    [LOADING] Added a flag to make the first loading wait for the customizer. This flag has been set to FALSE.
    Former-commit-id: 63363daa3ab81d97be858e1db3d7dd367dcb98d4

--- Assets/Game/Core/FeatureSettings/FeatureSettingsManager.cs
+++ Assets/Game/Core/FeatureSettings/FeatureSettingsManager.cs
@@ -1490,7 +1490,18 @@ public class FeatureSettingsManager : UbiBCN.SingletonMonoBehaviour<FeatureSetti
         {
             return Device_CurrentFeatureSettings.GetValueAsBool(FeatureSettings.KEY_CONTENT_DELTAS_CACHED);
         }
-    }    
+    }
+
+    /// <summary>
+    /// When <c>true</c> the first loading screen has to wait for customizer to be applied before keeping on loading so we can be sure that the latest rules are loaded when the user starts playing
+    /// </summary>
+    public bool IsCustomizerBlocker
+    {
+        get
+        {
+            return Device_CurrentFeatureSettings.GetValueAsBool(FeatureSettings.KEY_CUSTOMIZER_BLOCKER);
+        }
+    }
 
     public bool IsGlowEffectEnabled
     {
