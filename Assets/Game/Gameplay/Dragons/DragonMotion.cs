2ac4a8d5ce50e14509ac8da21df6e3aa78c26c58
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2018-05-22 09:34:07 +0200

    minor optimization
    Former-commit-id: 157bb8c1b7487c9cc0a7ff09932cde5aa681a245

--- Assets/Game/Gameplay/Dragons/DragonMotion.cs
+++ Assets/Game/Gameplay/Dragons/DragonMotion.cs
@@ -1601,13 +1601,10 @@ public class DragonMotion : MonoBehaviour, IMotion {
 			float angle = dir.ToAngleDegrees();
 			float roll = angle;
 			float pitch = angle;
-			// float yaw = 0;
-
 
 			Quaternion qRoll = Quaternion.Euler(0.0f, 0.0f, roll);
-			// Quaternion qYaw = Quaternion.Euler(0.0f, yaw, 0.0f);
 			Quaternion qPitch = Quaternion.Euler(pitch, 0.0f, 0.0f);
-			// m_desiredRotation = qYaw * qRoll * qPitch;
+
 			m_desiredRotation = qRoll * qPitch;
 			Vector3 eulerRot = m_desiredRotation.eulerAngles;
 			if (m_capVerticalRotation) 
