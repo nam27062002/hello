aa586f32caba8d50e88b7bf27862b6a8c05df720
Marc Saña <marc.sana@ubisoft.com>
2018-05-30 09:23:21 +0200

    remove duplicated scripts
    Former-commit-id: 3b84d60c3f44ea7cb92145a1f1dadf679d127db5

--- Assets/Game/UI/Menu/LiveEvents/Tournament/TournamentInfoScreen.cs
+++ Assets/Game/UI/Menu/LiveEvents/Tournament/TournamentInfoScreen.cs
@@ -31,7 +31,7 @@ public class TournamentInfoScreen : MonoBehaviour {
 	//----------------------------------------------------------------//
 
 
-	void Awake() {
+	void OnEnable() {
 		Refresh();
 	}
 
@@ -40,54 +40,54 @@ public class TournamentInfoScreen : MonoBehaviour {
 		HDTournamentManager tournament = HDLiveEventsManager.instance.m_tournament;
 		m_definition = tournament.data.definition as HDTournamentDefinition;
 
-		//TITLE
-		m_titleText.Localize(m_definition.m_name);
-		m_timerText.text = "w.i.p.";
+		if (m_definition != null) {
+			//TITLE
+			m_titleText.Localize(m_definition.m_name);
 
-		m_timerText.text = "w.i.p: ";
+			//GOALS
+			m_goalText.Localize(m_definition.m_goal.m_desc);
+			m_goalIcon.sprite = Resources.Load<Sprite>(UIConstants.LIVE_EVENTS_ICONS_PATH + m_definition.m_goal.m_icon);
 
-
-		//GOALS
-		m_goalText.Localize(m_definition.m_goal.m_desc);
-		m_goalIcon.sprite = Resources.Load<Sprite>(UIConstants.LIVE_EVENTS_ICONS_PATH + m_definition.m_goal.m_icon);
-
-		//MODIFIERS
-		List<IModifierDefinition> mods = new List<IModifierDefinition>();
-		for (int i = 0; i < m_definition.m_dragonMods.Count; ++i) {
-			mods.Add(m_definition.m_dragonMods[i]);
-		}
-
-		for (int i = 0; i < m_definition.m_otherMods.Count; ++i) {
-			mods.Add(m_definition.m_otherMods[i]);
-		}
-
-		for (int i = 0; i < m_modifier.Length; ++i) {
-			if (i < mods.Count) {
-				m_modifier[i].InitFromDefinition(mods[i]);
-			} else {
-				m_modifier[i].gameObject.SetActive(false);
+			//MODIFIERS
+			List<IModifierDefinition> mods = new List<IModifierDefinition>();
+			for (int i = 0; i < m_definition.m_dragonMods.Count; ++i) {
+				mods.Add(m_definition.m_dragonMods[i]);
 			}
+
+			for (int i = 0; i < m_definition.m_otherMods.Count; ++i) {
+				mods.Add(m_definition.m_otherMods[i]);
+			}
+
+			for (int i = 0; i < m_modifier.Length; ++i) {
+				if (i < mods.Count) {
+					m_modifier[i].InitFromDefinition(mods[i]);
+				} else {
+					m_modifier[i].gameObject.SetActive(false);
+				}
+			}
+
+			//MAP
+			m_areaText.text = m_definition.m_goal.m_area;
+
+			//
+			m_elapsedTime = 0f;
 		}
-
-		//MAP
-		m_areaText.text = m_definition.m_goal.m_area;
-
-		//
-		m_elapsedTime = 0f;
 	}
 
 	// Update timers!
 	void Update() {
-		m_elapsedTime += Time.deltaTime;
+		if (m_definition != null) {
+			m_elapsedTime += Time.deltaTime;
 
-		if (m_elapsedTime >= 1f) {
-			System.DateTime end = m_definition.m_endTimestamp;
-			System.DateTime now = System.DateTime.Now;
-			System.TimeSpan delta = end - now;
+			if (m_elapsedTime >= 1f) {
+				System.DateTime end = m_definition.m_endTimestamp;
+				System.DateTime now = System.DateTime.Now;
+				System.TimeSpan delta = end - now;
 
-			m_timerText.text = "End in: " + TimeUtils.FormatTime(delta.TotalSeconds, TimeUtils.EFormat.DIGITS, 4, TimeUtils.EPrecision.DAYS, true);
+				m_timerText.text = "End in: " + TimeUtils.FormatTime(delta.TotalSeconds, TimeUtils.EFormat.DIGITS, 4, TimeUtils.EPrecision.DAYS, true);
 
-			m_elapsedTime -= 1f;
+				m_elapsedTime -= 1f;
+			}
 		}
 	}
 }
