cbe3cefd7a0185459e5b6a62ce6ac18077d7a6d7
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2018-06-07 11:59:34 +0200

    score and timer for the tournament
    Former-commit-id: 8c521e5135fe063c4781c0cb7f9bf45e776acb29

--- Assets/Game/UI/HUD/HUDTournamentScore.cs
+++ Assets/Game/UI/HUD/HUDTournamentScore.cs
@@ -18,14 +18,15 @@ using TMPro;
 /// <summary>
 /// Simple controller for a time counter in the hud.
 /// </summary>
-[RequireComponent(typeof(TextMeshProUGUI))]
-public class HUDTournamentScore : MonoBehaviour {
+public class HUDTournamentScore : HudWidget {
 	//------------------------------------------------------------------//
 	// PROPERTIES														//
 	//------------------------------------------------------------------//
 	private TextMeshProUGUI m_valueTxt;
 	private long m_lastScorePrinted;
 	private TrackerBase m_tracker;
+	private long m_targetScore = 0;
+	private HDTournamentDefinition.TournamentGoal.TournamentMode m_mode = HDTournamentDefinition.TournamentGoal.TournamentMode.NORMAL;
 
 	//------------------------------------------------------------------//
 	// GENERIC METHODS													//
@@ -34,21 +35,21 @@ public class HUDTournamentScore : MonoBehaviour {
 	/// Initialization.
 	/// </summary>
 	private void Awake() {
+		base.Awake();
 		if ( SceneController.s_mode == SceneController.Mode.TOURNAMENT )
 		{
 			HDTournamentData _data = HDLiveEventsManager.instance.m_tournament.GetEventData() as HDTournamentData;
 			HDTournamentDefinition _def = _data.definition as HDTournamentDefinition;
 
-			if ( ( _def.m_goal.m_mode == HDTournamentDefinition.TournamentGoal.TournamentMode.NORMAL && _def.m_goal.m_type == "survive_time" ) || _def.m_goal.m_type == "score")
+			m_mode = _def.m_goal.m_mode;
+			if ( ( m_mode == HDTournamentDefinition.TournamentGoal.TournamentMode.NORMAL && _def.m_goal.m_type == "survive_time" ) || _def.m_goal.m_type == "score")
 			{
 				gameObject.SetActive(false);
 			}
 			else
 			{
-				// Get external references
-				m_valueTxt = GetComponent<TextMeshProUGUI>();
-				m_valueTxt.text = "0";
-				m_lastScorePrinted = -1;	
+				m_lastScorePrinted = 0;	
+				m_targetScore = _def.m_goal.m_targetAmount;
 				m_tracker = HDLiveEventsManager.instance.m_tournament.m_tracker;
 			}
 		}
@@ -61,16 +62,28 @@ public class HUDTournamentScore : MonoBehaviour {
 	/// <summary>
 	/// First update call.
 	/// </summary>
-	private void Start() {
+	override protected void Start() {
+		base.Start();
 		UpdateScore();
 	}
 
 	/// <summary>
 	/// Called every frame.
 	/// </summary>
-	private void Update() {
+	override protected void Update() {
+		base.Update();
 		UpdateScore();
 	}
+
+	protected override string GetValueAsString() {
+		string ret = "";
+		if ( m_mode == HDTournamentDefinition.TournamentGoal.TournamentMode.TIME_ATTACK ){
+			ret = m_lastScorePrinted + "/" + m_targetScore;
+		}else{
+			ret = m_lastScorePrinted + "";
+		}
+		return ret;
+    }
 
 	//------------------------------------------------------------------//
 	// INTERNAL UTILS													//
@@ -82,7 +95,7 @@ public class HUDTournamentScore : MonoBehaviour {
 
 		long _score = m_tracker.currentValue;
 		if(_score != m_lastScorePrinted) {
-			m_valueTxt.text = _score.ToString();
+			UpdateValue(_score, true); 
 			m_lastScorePrinted = _score;
 		}
 	}
