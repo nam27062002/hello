a83ae8648ae9edd90d376ff54f3b34be7b00b06d
Diego Campos <diego.campos@ubisoft.com>
2018-07-04 14:40:59 +0200

    BakedlightDestroyer
    Former-commit-id: 6dc6d7ff4ac325b55979973e58a87c77a488fcd9

---
+++ Assets/Engine/Utils/BakedLightDestroyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BakedLightDestroyer : MonoBehaviour {
+
+    void Awake()
+    {
+        Light[] lights = GetComponentsInChildren<Light>();
+        foreach (Light light in lights)
+        {
+            if (light.lightmapBakeType == LightmapBakeType.Baked)
+            {
+                Destroy(light.gameObject);
+            }
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        Destroy(this);
+	}
+	
+}
