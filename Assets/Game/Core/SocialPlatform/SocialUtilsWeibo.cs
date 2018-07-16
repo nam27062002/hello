b6725c542e1c78a2106d1af2294bd72ef633298a
David Germade <david.germade@ubisoft.com>
2018-07-16 16:47:03 +0200

    [SOCIAL] - SocialUtilsWeibo placeholder added - All social stuff has been put into a folder (SocialPlatform) - SocialPlatformManager is handled like any other manager in order to ease its initialization when age protection configuration changes.
    Former-commit-id: 3d0b5264bb3ece723cfd2e0d7b5fcdd419aba9e6

---
+++ Assets/Game/Core/SocialPlatform/SocialUtilsWeibo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class SocialUtilsWeibo : SocialUtils
+{    
+    public override void Init(SocialPlatformManager manager) { }
+
+    public override string GetPlatformNameTID()
+    {
+        return "TID_SOCIAL_NETWORK_WEIBO_NAME";
+    }
+
+    public override string GetSocialID() { return null; }
+
+    public override string GetAccessToken() { return null; }
+
+    public override string GetUserName() { return null; }
+
+    public override bool IsLoggedIn()
+    {
+        return WeiboManager.SharedInstance.IsLoggedIn();
+    }
+
+    public override void GetProfileInfoFromPlatform(Action<ProfileInfo> onGetProfileInfo)
+    {
+        if (onGetProfileInfo != null)
+        {
+            onGetProfileInfo(null);
+        }
+    }
+
+    protected override void ExtendedGetProfilePicture(string socialID, string storagePath, Action<bool> onGetProfilePicture, int width = 256, int height = 256) { }
+}
