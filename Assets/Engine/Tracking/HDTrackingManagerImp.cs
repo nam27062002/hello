56d19f2d2ccff9ff137dfb4ba68034b7a1d92eef
David Germade <david.germade@ubisoft.com>
2018-08-07 16:56:48 +0200

    [FIX] HDK-2410 [All Platforms][All Devices][DNA] nbAdsLtd and nbAdsSession attributes from custom.game.start.ad event sends the value "0"
    Former-commit-id: c1be30b13829ea6ea1abd3647a73b02e9601d3d7

--- Assets/Engine/Tracking/HDTrackingManagerImp.cs
+++ Assets/Engine/Tracking/HDTrackingManagerImp.cs
@@ -1528,8 +1528,17 @@ public class HDTrackingManagerImp : HDTrackingManager
         {
             if (TrackingPersistenceSystem != null)
             {
-                e.SetParameterValue(TRACK_PARAM_NB_ADS_LTD, TrackingPersistenceSystem.AdsCount);
-                e.SetParameterValue(TRACK_PARAM_NB_ADS_SESSION, TrackingPersistenceSystem.AdsSessions);
+                // According to specification we need to include the current ad
+                e.SetParameterValue(TRACK_PARAM_NB_ADS_LTD, TrackingPersistenceSystem.AdsCount + 1);
+
+                // According to specification we need to include the current session is it hasn't been condisered yet
+                int adsSessions = TrackingPersistenceSystem.AdsSessions;
+                if (!Session_IsAdSession)
+                {
+                    adsSessions++;
+                }
+
+                e.SetParameterValue(TRACK_PARAM_NB_ADS_SESSION, adsSessions);
             }
 
             Track_AddParamBool(e, TRACK_PARAM_AD_IS_AVAILABLE, adIsAvailable);
