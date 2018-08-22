3080336033d229963769ecf966a9748f42e571e0
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2018-08-22 11:26:21 +0200

    UI Safe Area Setter: Development - Migrated to new multi-action mode.
    Former-commit-id: dae9040d0b40dee6de76d2d0ca57f54301cb3bf9

--- Assets/Engine/UI/Utils/UISafeAreaSetter.cs
+++ Assets/Engine/UI/Utils/UISafeAreaSetter.cs
@@ -8,8 +8,8 @@
 // INCLUDES																	  //
 //----------------------------------------------------------------------------//
 using UnityEngine;
-
 using System;
+using System.Collections.Generic;
 
 //----------------------------------------------------------------------------//
 // CLASSES																	  //
@@ -29,18 +29,24 @@ public class UISafeAreaSetter : MonoBehaviour {
 	}
 
 	[Serializable]
-	public class OverrideData {
-		[HideEnumValues(false, true)]
-		public UIConstants.SpecialDevice device = UIConstants.SpecialDevice.NONE;
-		public UISafeArea safeArea = new UISafeArea();
+	public class Action {
+		public Mode mode = Mode.POSITION;
+		public UISafeArea scale = new UISafeArea(1f, 1f, 1f, 1f);
+
+		public override string ToString() {
+			return mode.ToString() + " | " + scale.ToString();
+		}
 	}
-	
+
 	//------------------------------------------------------------------------//
 	// MEMBERS AND PROPERTIES												  //
 	//------------------------------------------------------------------------//
 	// Exposed
-	[SerializeField] private Mode m_adjustMode = Mode.POSITION;
-	[SerializeField] private UISafeArea m_scale = new UISafeArea(1f, 1f, 1f, 1f);
+	[Reorderable("ACTION", true, false)]
+	[SerializeField] private List<Action> m_actions = new List<Action>();
+	public List<Action> actions {
+		get { return m_actions;  }
+	}
 	
 	//------------------------------------------------------------------------//
 	// GENERIC METHODS														  //
@@ -79,77 +85,80 @@ public class UISafeAreaSetter : MonoBehaviour {
 		// Get safe area
 		UISafeArea safeArea = UIConstants.safeArea;
 
-		// Apply scale
-		UISafeArea scaledSafeArea = new UISafeArea(
-			safeArea.left * m_scale.left,
-			safeArea.top * m_scale.top,
-			safeArea.right * m_scale.right,
-			safeArea.bottom * m_scale.bottom
-		);
+		// Perform all actions by order
+		for(int i = 0; i < m_actions.Count; ++i) {
+			// Apply scale
+			UISafeArea scaledSafeArea = new UISafeArea(
+				safeArea.left * m_actions[i].scale.left,
+				safeArea.top * m_actions[i].scale.top,
+				safeArea.right * m_actions[i].scale.right,
+				safeArea.bottom * m_actions[i].scale.bottom
+			);
 
-		// Apply based on mode
-		switch(m_adjustMode) {
-			case Mode.SIZE_DECREASE: {
-				// Adjust both offsets
-				rt.offsetMin = new Vector2(
-					rt.offsetMin.x + scaledSafeArea.left,
-					rt.offsetMin.y + scaledSafeArea.bottom
-				);
+			// Apply based on mode
+			switch(m_actions[i].mode) {
+				case Mode.SIZE_DECREASE: {
+					// Adjust both offsets
+					rt.offsetMin = new Vector2(
+						rt.offsetMin.x + scaledSafeArea.left,
+						rt.offsetMin.y + scaledSafeArea.bottom
+					);
 
-				rt.offsetMax = new Vector2(
-					rt.offsetMax.x - scaledSafeArea.right,
-					rt.offsetMax.y - scaledSafeArea.top
-				);
-			} break;
+					rt.offsetMax = new Vector2(
+						rt.offsetMax.x - scaledSafeArea.right,
+						rt.offsetMax.y - scaledSafeArea.top
+					);
+				} break;
 
-			case Mode.SIZE_INCREASE: {
-				// Adjust both offsets
-				rt.offsetMin = new Vector2(
-					rt.offsetMin.x - scaledSafeArea.left,
-					rt.offsetMin.y - scaledSafeArea.bottom
-				);
+				case Mode.SIZE_INCREASE: {
+					// Adjust both offsets
+					rt.offsetMin = new Vector2(
+						rt.offsetMin.x - scaledSafeArea.left,
+						rt.offsetMin.y - scaledSafeArea.bottom
+					);
 
-				rt.offsetMax = new Vector2(
-					rt.offsetMax.x + scaledSafeArea.right,
-					rt.offsetMax.y + scaledSafeArea.top
-				);
-			} break;
+					rt.offsetMax = new Vector2(
+						rt.offsetMax.x + scaledSafeArea.right,
+						rt.offsetMax.y + scaledSafeArea.top
+					);
+				} break;
 
-			case Mode.POSITION: {
-				// Select which margins to apply in each axis based on anchors, and do it
-				Vector2 newAnchoredPos = rt.anchoredPosition;
+				case Mode.POSITION: {
+					// Select which margins to apply in each axis based on anchors, and do it
+					Vector2 newAnchoredPos = rt.anchoredPosition;
 
-				// [AOC] TODO!! Research interpolating offset based on actual anchor value
+					// [AOC] TODO!! Research interpolating offset based on actual anchor value
 
-				// X
-				/*Vector2 anchorMin = rt.anchorMin;
-				Vector2 anchorMax = rt.anchorMax;
-				if(anchorMin.x < 0.5f && anchorMax.x < 0.5f) {
+					// X
+					/*Vector2 anchorMin = rt.anchorMin;
+					Vector2 anchorMax = rt.anchorMax;
+					if(anchorMin.x < 0.5f && anchorMax.x < 0.5f) {
+						newAnchoredPos.x += scaledSafeArea.left;
+					} else if(anchorMin.x > 0.5f && anchorMax.x > 0.5f) {
+						newAnchoredPos.x -= scaledSafeArea.right;
+					} else {
+						// Don't move!
+					}
+
+					// Y
+					if(anchorMin.y < 0.5f && anchorMax.y < 0.5f) {
+						newAnchoredPos.y += scaledSafeArea.bottom;
+					} else if(anchorMin.y > 0.5f && anchorMax.y > 0.5f) {
+						newAnchoredPos.y -= scaledSafeArea.top;
+					} else {
+						// Don't move!!
+					}*/
+
+					// Define the weights using the inspector
 					newAnchoredPos.x += scaledSafeArea.left;
-				} else if(anchorMin.x > 0.5f && anchorMax.x > 0.5f) {
 					newAnchoredPos.x -= scaledSafeArea.right;
-				} else {
-					// Don't move!
-				}
-
-				// Y
-				if(anchorMin.y < 0.5f && anchorMax.y < 0.5f) {
 					newAnchoredPos.y += scaledSafeArea.bottom;
-				} else if(anchorMin.y > 0.5f && anchorMax.y > 0.5f) {
 					newAnchoredPos.y -= scaledSafeArea.top;
-				} else {
-					// Don't move!!
-				}*/
 
-				// Define the weights using the inspector
-				newAnchoredPos.x += scaledSafeArea.left;
-				newAnchoredPos.x -= scaledSafeArea.right;
-				newAnchoredPos.y += scaledSafeArea.bottom;
-				newAnchoredPos.y -= scaledSafeArea.top;
-
-				// Apply!
-				rt.anchoredPosition = newAnchoredPos;
-			} break;
+					// Apply!
+					rt.anchoredPosition = newAnchoredPos;
+				} break;
+			}
 		}
 	}
 
