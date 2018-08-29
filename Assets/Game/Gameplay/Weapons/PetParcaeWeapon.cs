7c18d0ad04c2f6c67dae629d915af6de109f6177
Marc Saña <marc.sana@ubisoft.com>
2018-08-29 10:34:12 +0200

    Parcae passive skill WIP
    Former-commit-id: 8586985a2a643199997daf05ca2fc9ffa6e0687c

---
+++ Assets/Game/Gameplay/Weapons/PetParcaeWeapon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetParcaeWeapon : PetMeleeWeapon {
+
+    [Serializable]
+    public class ModData {
+        public float hpPercentage;
+        public float xpPercentage;
+        public float coinsPercentage;
+    }
+
+    [Serializable]
+    public class ParcaeModifierDictionary : SerializableDictionary<string, ModData> { }
+
+    [SerializeField] private ParcaeModifierDictionary m_modifiers;
+}
