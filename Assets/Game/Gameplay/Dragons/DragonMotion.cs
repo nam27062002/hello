01eb2604471096fde6674acc2ef6db44089245f8
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2018-09-07 09:29:32 +0200

    Dragon Data: Development - Added mass, friction and maxSpeed properties. Dragon Motion: Development - Now using mass, friction and maxSpeed from DragonData.
    Former-commit-id: 93d5cbc1ad0a437331bb1e45bc6a0fd1dd9a7568

--- Assets/Game/Gameplay/Dragons/DragonMotion.cs
+++ Assets/Game/Gameplay/Dragons/DragonMotion.cs
@@ -407,8 +407,8 @@ public class DragonMotion : MonoBehaviour, IMotion {
 		// Movement Setup
 		RecalculateDragonForce();
 		// m_dargonAcceleration = m_dragon.data.def.GetAsFloat("speedBase");
-		m_dragonMass = m_dragon.data.def.GetAsFloat("mass");
-		m_dragonFricction = m_dragon.data.def.GetAsFloat("friction");
+		m_dragonMass = m_dragon.data.mass;
+		m_dragonFricction = m_dragon.data.friction;
 		m_dragonGravityModifier = m_dragon.data.def.GetAsFloat("gravityModifier");
 		m_dragonAirGravityModifier = m_dragon.data.def.GetAsFloat("airGravityModifier");
         m_dragonWaterGravityModifier = m_dragon.data.def.GetAsFloat("waterGravityModifier");
