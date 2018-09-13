fe751e70645e3b604be088cb7d1456c48cd25bde
Marc Saña <marc.sana@ubisoft.com>
2018-09-13 12:44:11 +0200

    Added Parcae particles
    Former-commit-id: 8d5993e8eca960a77013ef084b089527866dd13a

---
+++ Assets/Game/Gameplay/Entities/AI/View/PetParcaeViewControl.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using UnityEngine;
+
+
+public class PetParcaeViewControl : ViewControl {
+    //--------------------------------------------------------------------------
+    [Serializable]
+    public class PetParcaeColorsDictionary : SerializableDictionary<string, ColorRange> { }
+    //--------------------------------------------------------------------------
+
+
+    //--------------------------------------------------------------------------
+    [Separator("Pet Parcae")]
+    [SerializeField] private PetParcaeColorsDictionary m_colors;
+    [SerializeField] private string m_idleKey;
+    [SerializeField] private ParticleSystem m_smoke;
+    //--------------------------------------------------------------------------
+
+
+    //--------------------------------------------------------------------------
+    private ColorRange m_currentColor;
+    private bool m_applyColor;
+    //--------------------------------------------------------------------------
+
+
+    protected override void Awake() {
+        base.Awake();
+
+        m_currentColor = m_colors.Get(m_idleKey);
+        ApplyColor();
+    }
+
+    public override void CustomUpdate() {
+        base.CustomUpdate();
+
+        if (m_applyColor) {
+            ApplyColor();
+        }
+    }
+
+
+    public void SetIdleColor() {
+        SetColor(m_idleKey);
+    }
+
+    public void SetColor(string _key) {
+        ColorRange color = m_colors.Get(_key);
+        if (m_currentColor != color) {
+            m_currentColor = color;
+            m_applyColor = true;
+        }
+    }
+
+    private void ApplyColor() {
+        ParticleSystem.MainModule main = m_smoke.main;
+        ParticleSystem.MinMaxGradient gradient = main.startColor;
+        if (gradient.mode == ParticleSystemGradientMode.TwoColors) {
+            gradient.colorMin = m_currentColor.GetA();
+            gradient.colorMax = m_currentColor.GetB();
+        } else {
+            gradient.color = m_currentColor.GetRandom();
+        }
+        main.startColor = gradient;
+
+        m_applyColor = false;
+    }
+}
