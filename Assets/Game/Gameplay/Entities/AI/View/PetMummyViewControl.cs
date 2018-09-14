92fec409bebf39c93a975d7c5add4524ea8a5334
Marc Saña <marc.sana@ubisoft.com>
2018-09-14 15:55:43 +0200

    new material for the mummy in the menu
    Former-commit-id: 48200bd6cfd80a3d9778e0a5e6e7c0b1ef313045

--- Assets/Game/Gameplay/Entities/AI/View/PetMummyViewControl.cs
+++ Assets/Game/Gameplay/Entities/AI/View/PetMummyViewControl.cs
@@ -45,6 +45,10 @@ public class PetMummyViewControl : ViewControl {
 
     private void OnInit() {
         m_powerStatus = 1f;
+        for (int i = 0; i < m_materialList.Count; ++i) {
+            m_materialList[i].SetFloat("_DissolveAmount", 0f);
+        }
+        m_machine.SetSignal(AI.Signals.Type.FallDown, false);
         m_effect.Stop(true);
         m_state = State.IDLE;
     }
@@ -61,12 +65,12 @@ public class PetMummyViewControl : ViewControl {
         base.CustomUpdate();
 
         if (m_state == State.POWER_ACTIVE) {
-            m_powerStatus = 1f - (m_dragon.health / m_dragon.mummyHealthMax);
+            m_powerStatus = (m_dragon.health / m_dragon.mummyHealthMax);
 
-            m_effectTransform.localPosition = Vector3.Lerp(m_basePosition, m_topPosition, m_powerStatus);
+            m_effectTransform.localPosition = Vector3.Lerp(m_basePosition, m_topPosition, 1f - m_powerStatus);
 
             for (int i = 0; i < m_materialList.Count; ++i) {
-                m_materialList[i].SetFloat("_DissolveAmount", m_powerStatus);
+                m_materialList[i].SetFloat("_DissolveAmount", 1f - m_powerStatus);
             }
 
             if (m_powerStatus <= float.Epsilon) {
