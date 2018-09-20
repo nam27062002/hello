72d704988b66b0c1c54b76d7e7dbef92c7c9f72e
David Germade <david.germade@ubisoft.com>
2018-09-20 11:18:50 +0200

    [CONTROL PANEL] Fixed an issue preventing Reset Prefs button from working.
    Former-commit-id: 6aefe618d1edb6cbcaee7980f30afbab4db35a8a

---
+++ Assets/Game/Debug/ControlPanel/CPTabCheats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CPTabCheats : MonoBehaviour
+{
+    /// <summary>
+	/// Clear all prefs.
+	/// </summary>
+	public void OnResetPrefs()
+    {
+        ControlPanel.instance.OnResetPrefs();
+    }
+
+}
