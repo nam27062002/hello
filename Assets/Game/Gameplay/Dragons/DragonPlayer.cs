6e3a5c3d5e84d4cc1b20a2b30b6be984d126cede
Marc Saña <marc.sana@ubisoft.com>
2018-09-25 11:59:51 +0200

    Mummy revive has to be the last one.
    Former-commit-id: 6e83bbe2b0f373f350e5f4ca89233df753e70ba3

--- Assets/Game/Gameplay/Dragons/DragonPlayer.cs
+++ Assets/Game/Gameplay/Dragons/DragonPlayer.cs
@@ -865,11 +865,11 @@ public class DragonPlayer : MonoBehaviour {
     }
 
     public bool CanUseMummyPower() {
-        return HasMummyPowerAvailable();
+        return !CanUseFreeRevives() && HasMummyPowerAvailable();
     }
 
     public bool CanUseFreeRevives() {
-        return !CanUseMummyPower() && GetReminingLives() > 0;
+        return GetReminingLives() > 0;
     }
 
 	/// <summary>
