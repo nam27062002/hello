165e9f34e54ed9f748c0a65c2c960875a90ac551
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2018-09-27 14:44:52 +0200

    Lab: Development - Created lab's main screens placeholders and set up basic navigation.
    Former-commit-id: a82a611c0d1657ca3a293a86ede248ec129f64e8

---
+++ Assets/Game/UI/Menu/Lab/LabDragonSelectionScreen.cs
@@ -0,0 +1,79 @@
+// LabDragonSelectionScreen.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 27/09/2018.
+// Copyright (c) 2018 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Screen controller for the Lab Dragon Selection screen.
+/// </summary>
+public class LabDragonSelectionScreen : MonoBehaviour {
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
+	
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
+	
+	//------------------------------------------------------------------------//
+	// GENERIC METHODS														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Initialization.
+	/// </summary>
+	private void Awake() {
+
+	}
+
+	/// <summary>
+	/// First update call.
+	/// </summary>
+	private void Start() {
+
+	}
+
+	/// <summary>
+	/// Component has been enabled.
+	/// </summary>
+	private void OnEnable() {
+
+	}
+
+	/// <summary>
+	/// Component has been disabled.
+	/// </summary>
+	private void OnDisable() {
+
+	}
+
+	/// <summary>
+	/// Called every frame.
+	/// </summary>
+	private void Update() {
+
+	}
+
+	/// <summary>
+	/// Destructor.
+	/// </summary>
+	private void OnDestroy() {
+
+	}
+
+	//------------------------------------------------------------------------//
+	// OTHER METHODS														  //
+	//------------------------------------------------------------------------//
+
+	//------------------------------------------------------------------------//
+	// CALLBACKS															  //
+	//------------------------------------------------------------------------//
+}
---
+++ Assets/Tools/Templates/LabGoalsScreen.cs
@@ -0,0 +1,90 @@
+// LabGoalsScreen.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 27/09/2018.
+// Copyright (c) 2018 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Screen controller for the Lab Goals screen.
+/// </summary>
+public class LabGoalsScreen : MonoBehaviour {
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
+	
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
+	
+	//------------------------------------------------------------------------//
+	// GENERIC METHODS														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Initialization.
+	/// </summary>
+	private void Awake() {
+
+	}
+
+	/// <summary>
+	/// First update call.
+	/// </summary>
+	private void Start() {
+
+	}
+
+	/// <summary>
+	/// Component has been enabled.
+	/// </summary>
+	private void OnEnable() {
+
+	}
+
+	/// <summary>
+	/// Component has been disabled.
+	/// </summary>
+	private void OnDisable() {
+
+	}
+
+	/// <summary>
+	/// Called every frame.
+	/// </summary>
+	private void Update() {
+
+	}
+
+	/// <summary>
+	/// Destructor.
+	/// </summary>
+	private void OnDestroy() {
+
+	}
+
+	//------------------------------------------------------------------------//
+	// OTHER METHODS														  //
+	//------------------------------------------------------------------------//
+
+	//------------------------------------------------------------------------//
+	// CALLBACKS															  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// The Play button has been pressed.
+	/// </summary>
+	public void OnPlayButton() {
+		// Tracking
+		// [AOC] TODO!! Tournament as reference:
+		//HDTrackingManager.Instance.Notify_TournamentClickOnEnter(m_definition.m_name, _flow.currency);
+
+		// Go to play!
+		InstanceManager.menuSceneController.OnPlayButton();
+	}
+}
