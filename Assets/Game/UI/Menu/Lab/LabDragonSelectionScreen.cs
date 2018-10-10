3d390c129829c5d1768f0b7d9552050dd44e8bd8
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2018-10-10 09:34:59 +0200

    Lab Main Screen: Development - Implementing data initialization and refresh.
    Former-commit-id: bdd0019d280525308f1013a581e0d6919bd89171

--- Assets/Game/UI/Menu/Lab/LabDragonSelectionScreen.cs
+++ Assets/Game/UI/Menu/Lab/LabDragonSelectionScreen.cs
@@ -36,6 +36,9 @@ public class LabDragonSelectionScreen : MonoBehaviour {
 	[SerializeField] private LabStatUpgrader[] m_stats = new LabStatUpgrader[0];
 	[Space]
 	[SerializeField] private GameObject m_loadingUI = null;
+
+	// Cache some data
+	private DragonDataSpecial m_dragonData = null;
 	
 	//------------------------------------------------------------------------//
 	// GENERIC METHODS														  //
@@ -72,7 +75,7 @@ public class LabDragonSelectionScreen : MonoBehaviour {
 		Messenger.AddListener<string>(MessengerEvents.MENU_DRAGON_SELECTED, OnDragonSelected);
 
 		// Do a first refresh
-		RefreshDragonInfo(InstanceManager.menuSceneController.selectedDragonData);
+		InitWithDragon(InstanceManager.menuSceneController.selectedDragonData, false);
 	}
 
 	/// <summary>
@@ -133,25 +136,63 @@ public class LabDragonSelectionScreen : MonoBehaviour {
 	/// Refresh with data from a target dragon.
 	/// </summary>
 	/// <param name="_dragonData">Data to be used to initialize the dragon info.</param>
-	private void RefreshDragonInfo(IDragonData _dragonData) {
+	/// <param name="_animate">Trigger animations?</param>
+	private void InitWithDragon(IDragonData _dragonData, bool _animate) {
+		// Store new dragon data
+		m_dragonData = _dragonData as DragonDataSpecial;
+
 		// Skip if dragon data is not valid
-		if(_dragonData == null) return;
+		if(m_dragonData == null) return;
 
 		// Dragon name
 		if(m_dragonNameText != null) {
-			//m_dragonNameText.Localize(_dragonData.def.GetAsString("tidName"));
-			// [AOC] TODO!! TIDs not already defined, use sku for now
-			m_dragonNameText.Localize(_dragonData.sku);
+			m_dragonNameText.Localize(m_dragonData.def.GetAsString("tidName"));
 		}
 
 		// Dragon desc
 		if(m_dragonDescText != null) {
-			m_dragonDescText.Localize(_dragonData.def.GetAsString("tidDesc"));
+			m_dragonDescText.Localize(m_dragonData.def.GetAsString("tidDesc"));
 		}
 
 		// Dragon stats
 		for(int i = 0; i < m_stats.Length; ++i) {
-			m_stats[i].InitFromData(_dragonData as DragonDataSpecial);
+			m_stats[i].InitFromData(m_dragonData);
+		}
+
+		// First refresh
+		Refresh(_animate);
+	}
+
+	/// <summary>
+	/// Soft refresh of things that can change while the same dragon is selected.
+	/// </summary>
+	/// <param name="_animate">Trigger animations?</param>
+	private void Refresh(bool _animate) {
+		// Skip if dragon data is not valid
+		if(m_dragonData == null) return;
+
+		// Dragon description
+		if(m_dragonDescText != null) {
+			// Show only if dragon is not owned
+			m_dragonDescText.gameObject.SetActive(!m_dragonData.isOwned);
+		}
+
+		// Upgrade locked info
+		if(m_upgradeLockedInfoText != null) {
+			// Show if no more upgrades are possible
+			// [AOC] Unless the cause is because all stats are already maxed!
+			DragonTier nextRequiredTier = m_dragonData.GetNextRequiredTier();
+			bool show = !m_dragonData.CanUpgradeStats() && !m_dragonData.allStatsMaxed && nextRequiredTier != DragonTier.COUNT;
+			m_upgradeLockedInfoText.gameObject.SetActive(show);
+
+			// Update text
+			if(show) {
+				m_upgradeLockedInfoText.Localize(	// Unlock a %U0 dragon to keep upgrading %U1!
+					m_upgradeLockedInfoText.tid,
+                    UIConstants.GetSpriteTag(UIConstants.GetDragonTierIcon(nextRequiredTier)),
+                 	m_dragonData.def.GetLocalized("tidName")
+				);
+			}
 		}
 	}
 
@@ -171,6 +212,6 @@ public class LabDragonSelectionScreen : MonoBehaviour {
 	/// <param name="_sku">The sku of the selected dragon.</param>
 	private void OnDragonSelected(string _sku) {
 		// Get new dragon's data from the dragon manager and do the refresh logic
-		RefreshDragonInfo(DragonManager.GetDragonData(_sku));
+		InitWithDragon(DragonManager.GetDragonData(_sku), true);
 	}
 }
