607a55f6d9c4b9f3ee7ca3b83af8543454e16f9d
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2018-10-15 11:25:36 +0200

    use same function
    Former-commit-id: f035a3efc2a502a1a5cb809ff5324a3a96409ffe

--- Assets/Engine/Generic/MathTest.cs
+++ Assets/Engine/Generic/MathTest.cs
@@ -2,9 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Math test. This class will contain math test of the kind AvsB to know if they touch
+/// </summary>
 public class MathTest 
 {
-
+    
+    /// <summary>
+    /// Tests the circle vs circle.
+    /// </summary>
+    /// <returns><c>true</c>, if the circles touch, <c>false</c> otherwise.</returns>
+    /// <param name="center1">Center1.</param>
+    /// <param name="radius1">Radius1.</param>
+    /// <param name="center2">Center2.</param>
+    /// <param name="radius2">Radius2.</param>
     public static bool TestCircleVsCircle( Vector2 center1, float radius1, Vector2 center2, float radius2 )
     {
         bool ret = false;
