eb2021bbcccd1a4d9f9499da9c93bc8ee9d0f009
Marc Saña <marc.sana@ubisoft.com>
2018-10-23 12:37:34 +0200

    Tube man place holder
    Former-commit-id: 62bfb92a7c46ab499932301a9c7a0503b63d0e2d

---
+++ Assets/Game/Gameplay/Entities/AI/Brain/Behaviours/Actions/Alert.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AI {
+	namespace Behaviour {
+		
+		[CreateAssetMenu(menuName = "Behaviour/Actions/Alert")]
+        public class Alert : StateComponent {
+			
+			protected override void OnEnter(State oldState, object[] param) {
+                m_machine.SetSignal(Signals.Type.Alert, true);				
+			}
+
+			protected override void OnExit(State _newState) {
+                m_machine.SetSignal(Signals.Type.Alert, false);
+			}
+		}
+	}
+}
