7492c7f13eba7c1486f1baa1d00b26a56bc53631
Marc Saña <marc.sana@ubisoft.com>
2018-11-09 14:37:12 +0100

    fixed gear rotation
    Former-commit-id: bc5e6c4fbd39cef3828f973920e54a67307a63eb

--- Assets/Game/Gameplay/Decorations/Devices/Doors/DoorGearOpenDevice.cs
+++ Assets/Game/Gameplay/Decorations/Devices/Doors/DoorGearOpenDevice.cs
@@ -17,19 +17,21 @@ public class DoorGearOpenDevice : MonoBehaviour {
        
 
     private State m_state;
-    private Vector3[] m_rotations;
-
+    private float[] m_rotations;
+    private Vector3[] m_initialRotations;
 
 
     private void Awake() {
         m_state = State.IDLE;
-        m_rotations = new Vector3[m_gears.Length];
+        m_rotations = new float[m_gears.Length];
+        m_initialRotations = new Vector3[m_gears.Length];
         m_breakableTrigger.onBreak += OnTriggerBreak;
     }
 
     private void OnTriggerBreak() {
         for (int i = 0; i < m_gears.Length; ++i) {
-            m_rotations[i] = m_gears[i].localRotation.eulerAngles;
+            m_rotations[i] = 0f;
+            m_initialRotations[i] = m_gears[i].localRotation.eulerAngles;
         }
         m_door.Open();
         m_state = State.ACTIVE;
@@ -47,17 +49,20 @@ public class DoorGearOpenDevice : MonoBehaviour {
 
     private void Rotate(float _dt) {
         for (int i = 0; i < m_gears.Length; ++i) {
-            m_rotations[i] += (1 - (2 * (i % 2))) * m_rotationAxis * m_gearsRotationSpeed[i] * _dt;
-            m_gears[i].localRotation = Quaternion.Euler(m_rotations[i]);
+            m_rotations[i] += m_gearsRotationSpeed[i] * _dt;
+            m_gears[i].localRotation = Quaternion.Euler(m_initialRotations[i]) *  Quaternion.AngleAxis(m_rotations[i], (1 - (2 * (i % 2))) * m_rotationAxis);
         }
     }
 
     public void DebugAnimation(float _dt) {
         if (m_rotations == null || m_rotations.Length < m_gears.Length) {
-            m_rotations = new Vector3[m_gears.Length];
-            for (int i = 0; i < m_gears.Length; ++i) {
-                m_rotations[i] = m_gears[i].localRotation.eulerAngles;
-            }
+            m_rotations = new float[m_gears.Length];
+            m_initialRotations = new Vector3[m_gears.Length];
+        }
+
+        for (int i = 0; i < m_gears.Length; ++i) {
+            m_rotations[i] = 0f;
+            m_initialRotations[i] = m_gears[i].localRotation.eulerAngles;
         }
 
         Rotate(_dt);
