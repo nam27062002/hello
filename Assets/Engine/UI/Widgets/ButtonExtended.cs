7630c7a0e1ae2ab3399ecc47b9346c06e342254f
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2018-11-12 14:31:03 +0100

    fixes
    Former-commit-id: 94080d93a621e4d9fe4c4648579b1b93052a7469

--- Assets/Engine/UI/Widgets/ButtonExtended.cs
+++ Assets/Engine/UI/Widgets/ButtonExtended.cs
@@ -65,6 +65,7 @@ public class ButtonExtended : Button {
     /// <summary>
     /// First update call.
     /// </summary>
+    /*
     private void Start()
     {
         for (int i = 0; i < onClick.GetPersistentEventCount(); i++)
@@ -75,7 +76,7 @@ public class ButtonExtended : Button {
         onClick.RemoveAllListeners();
         onClick.AddListener(safeOnclick);
     }
-
+    */
     void safeOnclick()
     {
         if (m_MultiTouchDisable && !checkMultitouchAvailability()) return;
