e2855dc4d43af347d610972c5af62ce8abe39a24
Marc Saña <marc.sana@ubisoft.com>
2018-11-21 18:00:24 +0100

    added seasonal trigger
    Former-commit-id: 14f3cc971b0523ed7974db9348d23d306752422c

--- Assets/Game/Gameplay/Prey/Air/LinearMotion.cs
+++ Assets/Game/Gameplay/Prey/Air/LinearMotion.cs
@@ -7,22 +7,23 @@ public class LinearMotion : Initializable, ISpawnable {
 	[SerializeField] private float m_speed = 1;
 
 	private float m_time;
+    private Transform m_transform;
 	private Vector3 m_originalPostion;
 	public Vector3 originalPostion { get { return m_originalPostion; } }
 
 	void Start() {
-		m_originalPostion = transform.position;
+        m_transform = transform;
+        m_originalPostion = m_transform.position;
 	}
-
 	
 	public override void Initialize() {		
 		m_time = 0f;
-		m_originalPostion = transform.position;
+		m_originalPostion = m_transform.position;
 	}
 
 	public void Spawn(ISpawner _spawner) {
 		m_time = 0f;
-		m_originalPostion = transform.position;
+		m_originalPostion = m_transform.position;
 	}
 
 	public void CustomUpdate() {}
@@ -31,9 +32,9 @@ public class LinearMotion : Initializable, ISpawnable {
 	// Update is called once per frame
 	void Update() {	
 		if (m_speed > 0) {
-			Vector3 position = transform.position;
+			Vector3 position = m_transform.position;
             position = m_originalPostion + m_direction * m_speed * m_time;
-			transform.position = position;
+            m_transform.position = position;
 			m_time += Time.deltaTime;
 		}
 	}
