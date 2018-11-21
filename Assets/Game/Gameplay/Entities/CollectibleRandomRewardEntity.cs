76b290bd127fc07f2ee1bc110acb124d0399be99
Marc Saña <marc.sana@ubisoft.com>
2018-11-21 15:37:12 +0100

    New seasonal collectible: xmas present
    Former-commit-id: da37acc077fe7de047e9daca9c0b87963a2fc43a

---
+++ Assets/Game/Gameplay/Entities/CollectibleRandomRewardEntity.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class CollectibleRandomRewardEntity : CollectibleEntity {
+    //
+    [SerializeField] private ProbabilitySet m_probability;
+
+
+    //
+    private Reward[] m_rewards;
+
+
+    //
+
+    protected override void Awake() {
+        m_rewards = new Reward[m_probability.numElements];
+        base.Awake();
+    }
+
+    protected override void OnRewardCreated() {
+        for (int i = 0; i < m_probability.numElements; ++i) {
+            m_rewards[i] = new Reward();
+
+            switch (m_probability.GetLabel(i)) {
+                case "pc":      m_rewards[i].pc     = reward.pc;    break;
+                case "coins":   m_rewards[i].coins  = reward.coins; break;
+                case "score":   m_rewards[i].score  = reward.score; break;
+                case "xp":      m_rewards[i].xp     = reward.xp;    break;
+                case "health":  m_rewards[i].health = reward.health;break;
+            }
+        }
+    }
+
+    public override Reward GetOnKillReward(DyingReason _reason) {
+        return m_rewards[m_probability.GetWeightedRandomElementIdx()];
+	}
+}
