fb8170153eaecf3310cd8eeee39d90c7b7b43ba0
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2018-11-27 14:34:36 +0100

    Special Dates fireworks
    Former-commit-id: 47e74c19637e7af71f71bde9cae136a1acbdbd65

---
+++ Assets/Engine/Generic/InstantiateByDateTime.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InstantiateByDateTime : MonoBehaviour {
+
+    public string m_resourcePath = "";
+    public bool m_useLeveledParticle = false;
+    
+    [Serializable]
+    public struct DayOfTheYear
+    {
+        [Range (1,31)]
+        public int day;
+        
+        [Range (1,12)]
+        public int month;
+    };
+    
+    public List<DayOfTheYear> m_validDays = new List<DayOfTheYear>();
+
+	// Use this for initialization
+	void Awake () 
+    {
+        System.DateTime dateTime = System.DateTime.Now;
+        
+        bool instantiate = false;
+        
+        instantiate = Prefs.GetBoolPlayer(DebugSettings.SPECIAL_DATES, false);
+        
+        int max = m_validDays.Count;
+        for (int i = 0; i < max && !instantiate; i++)
+        {
+            instantiate = m_validDays[i].day == dateTime.Day && m_validDays[i].month == dateTime.Month;
+        }
+        
+        if ( instantiate )
+        {
+            if ( m_useLeveledParticle )
+            {
+                ParticleSystem ps = ParticleManager.InitLeveledParticle(m_resourcePath, transform);
+                if( ps != null )
+                {
+                    ps.gameObject.SetActive(true);
+                    ps.Play();
+                }
+            }
+            else
+            {
+                GameObject go = Resources.Load<GameObject>(m_resourcePath);
+                if (go != null)
+                {
+                    Instantiate<GameObject>(go, transform);
+                }
+            }
+        }
+    }
+}
