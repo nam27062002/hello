b1fc1e7b12bc0219dde8e3b62b3e39cf9f167267
Diego Campos <diego.campos@ubisoft.com>
2018-11-29 11:28:10 +0100

    Snow effect activation
    Former-commit-id: cabb1d7baad721ab362fd71c262b7e2b9c19e098

--- Assets/Game/UI/HUD/HUDSnow.cs
+++ Assets/Game/UI/HUD/HUDSnow.cs
@@ -7,14 +7,17 @@ using TMPro;
 
 public class HUDSnow : MonoBehaviour {
 
-	public bool m_snowActivated = true;
-
     private int m_wposId;
     private int m_aspectId;
 
 	private Material m_snowMaterial = null;
 	private Image m_image = null;
 
+    private static HUDSnow m_instance = null;
+    public bool m_enable = true;
+
+    public float m_fadeTime = 2.0f;
+    private bool m_enable_bk = true;
 
     // Use this for initialization
     void Awake () 
@@ -24,7 +27,7 @@ public class HUDSnow : MonoBehaviour {
             Destroy(gameObject);
             return;
         }
-
+        m_instance = this;
 
 		m_image = gameObject.GetComponent<Image>();
 			
@@ -35,25 +38,52 @@ public class HUDSnow : MonoBehaviour {
 	
 	// Update is called once per frame
 
-
+    public static void enableSnow(bool enable)
+    {
+        m_instance.m_enable = enable;
+        m_instance.m_image.enabled = true;
+    }
 
 
 	void Update()
 	{
-        if (m_snowActivated)
-        {
-			if (InstanceManager.gameCamera != null) {
-				Vector3 pos = InstanceManager.gameCamera.transform.position;
-				m_snowMaterial.SetVector(m_wposId, pos);
-			}
-            float aspect = (float)Screen.height / (float)Screen.width;
-            m_snowMaterial.SetFloat(m_aspectId, aspect);
+		if (InstanceManager.gameCamera != null) {
+			Vector3 pos = InstanceManager.gameCamera.transform.position;
+			m_snowMaterial.SetVector(m_wposId, pos);
+		}
+        float aspect = (float)Screen.height / (float)Screen.width;
+        m_snowMaterial.SetFloat(m_aspectId, aspect);
 
+        if (m_enable != m_enable_bk)
+        {
+            Color col = m_image.color;
+
+            if (m_enable)
+            {
+                col.a += Time.deltaTime / m_fadeTime;
+                if (col.a > 1.0f)
+                {
+                    col.a = 1.0f;
+                    m_enable_bk = m_enable;
+                }
+            }
+            else
+            {
+                col.a -= Time.deltaTime / m_fadeTime;
+                if (col.a < 0.0f)
+                {
+                    col.a = 0.0f;
+                    m_image.enabled = false;
+                    m_enable_bk = m_enable;
+                }
+            }
+
+            m_image.color = col;
         }
 
         //Debug.Log("Device_CurrentProfile: " + FeatureSettingsManager.instance.Device_CurrentProfile);
-			
-	}
+
+    }
 
 
 }
