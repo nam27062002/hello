b6243135dfc9e817804af9d186a5906cd5cb9a3d
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2018-11-29 16:58:59 +0100

    cupido WIP
    Former-commit-id: 26415e6bc2a616c3b58d73e7a429b869e78b7874

---
+++ Assets/Game/Gameplay/Weapons/PetCupidoProjectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetCupidoProjectile : PetProjectile {
+
+	protected override void DealDamage() {
+        Entity entity = m_hitCollider.GetComponent<Entity>();
+        if (entity != null) {
+            
+        }
+    }
+}
