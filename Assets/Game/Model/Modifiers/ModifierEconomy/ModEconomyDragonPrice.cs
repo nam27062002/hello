d5d408d1e37fd3dddfb810b17acb95539ee7e763
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2018-11-29 18:08:05 +0100

    Dragon Price Economy Mod: Development - Exposed dragonSku. Some extra refactoring.
    Former-commit-id: ffb3ff6286c6a2581fa0ca7a19ef20848d3739d5

--- Assets/Game/Model/Modifiers/ModifierEconomy/ModEconomyDragonPrice.cs
+++ Assets/Game/Model/Modifiers/ModifierEconomy/ModEconomyDragonPrice.cs
@@ -4,27 +4,31 @@ public class ModEconomyDragonPrice : ModifierEconomy {
 
 
     //------------------------------------------------------------------------//
-    private string m_sku;
+	private string m_dragonSku;
+	public string dragonSku {
+		get { return m_dragonSku; }
+	}
+
     private float m_percentage;
     private string m_currency;
 
     //------------------------------------------------------------------------//
     public ModEconomyDragonPrice(DefinitionNode _def) : base(_def) {
-        m_sku = _def.Get("param1");
+        m_dragonSku = _def.Get("param1");
         m_percentage = _def.GetAsFloat("param2");
         m_currency = _def.Get("param3");
-        BuildTextParams(m_sku, m_percentage + "%", UIConstants.PET_CATEGORY_DEFAULT.ToHexString("#"));
+        BuildTextParams(m_dragonSku, m_percentage + "%", UIConstants.PET_CATEGORY_DEFAULT.ToHexString("#"));
     }
 
     public ModEconomyDragonPrice(SimpleJSON.JSONNode _data) : base(_data) {
-        m_sku = _data["param1"];
+        m_dragonSku = _data["param1"];
         m_percentage = _data["param2"].AsFloat;
         m_currency = _data["param3"];
-        BuildTextParams(m_sku, m_percentage + "%", UIConstants.PET_CATEGORY_DEFAULT.ToHexString("#"));
+        BuildTextParams(m_dragonSku, m_percentage + "%", UIConstants.PET_CATEGORY_DEFAULT.ToHexString("#"));
     }
 
     public override void Apply() {
-        IDragonData dragonData = DragonManager.GetDragonData(m_sku);
+        IDragonData dragonData = DragonManager.GetDragonData(m_dragonSku);
         if (dragonData != null) {
             switch(m_currency) {
                 case "sc": dragonData.AddPriceSCModifer(m_percentage); break;
@@ -35,7 +39,7 @@ public class ModEconomyDragonPrice : ModifierEconomy {
     }
 
     public override void Remove() {
-        IDragonData dragonData = DragonManager.GetDragonData(m_sku);
+        IDragonData dragonData = DragonManager.GetDragonData(m_dragonSku);
         if (dragonData != null) {
             switch (m_currency) {
                 case "sc": dragonData.AddPriceSCModifer(-m_percentage); break;
@@ -48,7 +52,7 @@ public class ModEconomyDragonPrice : ModifierEconomy {
     protected override SimpleJSON.JSONClass __ToJson() {
         SimpleJSON.JSONClass data = base.__ToJson();
 
-        data.Add("param1", m_sku);
+        data.Add("param1", m_dragonSku);
         data.Add("param2", m_percentage);
         data.Add("param3", m_currency);
 
