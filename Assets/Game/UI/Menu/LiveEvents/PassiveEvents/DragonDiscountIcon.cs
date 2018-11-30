a86cf6935791679f7e8afe553694572e6a18531c
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2018-11-30 09:22:00 +0100

    Dragon Discount Icon: Development - Full implementation. Passive Event Icon: Development - Created abstract root class to handle all shared logic among all passive event types.
    Former-commit-id: fc50aedab042fca8e1f718af448f2d676847de14

---
+++ Assets/Game/UI/Menu/LiveEvents/PassiveEvents/DragonDiscountIcon.cs
@@ -0,0 +1,202 @@
+// DragonDiscountIcon.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 29/11/2018.
+// Copyright (c) 2018 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+using DG.Tweening;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Controller for the dragon discount icon in the menu HUD.
+/// </summary>
+public class DragonDiscountIcon : IPassiveEventIcon {
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
+
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
+	// Exposed
+	[Space]
+	[SerializeField] private Image m_dragonIcon = null;
+	[SerializeField] private Image m_tierIcon = null;
+	[SerializeField] private TextMeshProUGUI m_discountText = null;
+	[Space]
+	[SerializeField] private GameObject m_arrow = null;
+	[Space]
+	[SerializeField] private Graphic[] m_toTint = null;
+
+	// Internal properties for comfort
+	private ModEconomyDragonPrice _mod {
+		get { 
+			if(m_passiveEventManager != null) {
+				if(m_passiveEventManager.m_passiveEventData != null) {
+					return m_passiveEventManager.m_passiveEventDefinition.mainMod as ModEconomyDragonPrice; 
+				}
+			}
+			return null;
+		}
+	}
+
+	private IDragonData _targetDragonData {
+		get {
+			ModEconomyDragonPrice mod = _mod;
+			if(mod != null) {
+				return DragonManager.GetDragonData(mod.dragonSku);
+			}
+			return null;
+		}
+	}
+
+	//------------------------------------------------------------------------//
+	// PARENT OVERRIDES														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// First update call.
+	/// </summary>
+	protected override void Start() {
+		// Call parent
+		base.Start();
+
+		// Subscribe to external events
+		Messenger.AddListener<string>(MessengerEvents.MENU_DRAGON_SELECTED, OnDragonSelected);
+	}
+
+	/// <summary>
+	/// Destructor.
+	/// </summary>
+	protected override void OnDestroy() {
+		// Call parent
+		base.OnDestroy();
+
+		// Unsubscribe from external events
+		Messenger.RemoveListener<string>(MessengerEvents.MENU_DRAGON_SELECTED, OnDragonSelected);
+	}
+
+	//------------------------------------------------------------------------//
+	// IPassiveEventIcon IMPLEMENTATION										  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Get the manager for this specific passive event type.
+	/// </summary>
+	/// <returns>The event manager corresponding to this event type.</returns>
+	protected override HDPassiveEventManager GetEventManager() {
+		return HDLiveEventsManager.instance.m_dragonDiscounts;
+	}
+
+	/// <summary>
+	/// Update visuals when new data has been received.
+	/// </summary>
+	protected override void RefreshDataInternal() {
+		// Aux vars
+		ModEconomyDragonPrice mod = _mod;
+		IDragonData targetDragonData = _targetDragonData;
+
+		// Dragon icon
+		if(m_dragonIcon != null) {
+			bool show = false;
+			if(targetDragonData != null) {
+				show = true;
+				m_dragonIcon.sprite = Resources.Load<Sprite>(UIConstants.DISGUISE_ICONS_PATH + targetDragonData.sku + "/" + IDragonData.DEFAULT_SKIN_ICON);
+			}
+			m_dragonIcon.gameObject.SetActive(show);
+		}
+
+		// Tier icon
+		if(m_tierIcon != null) {
+			bool show = false;
+			if(targetDragonData != null) {
+				show = true;
+				m_tierIcon.sprite = ResourcesExt.LoadFromSpritesheet(UIConstants.UI_SPRITESHEET_PATH, targetDragonData.tierDef.GetAsString("icon"));
+			}
+			m_tierIcon.gameObject.SetActive(show);
+		}
+
+		// Color tint
+		if(m_toTint != null && targetDragonData != null) {
+			for(int i = 0; i < m_toTint.Length; ++i) {
+				if(m_toTint[i] != null) {
+					m_toTint[i].color = UIConstants.GetDragonTierColor(targetDragonData.tier);
+				}
+			}
+		}
+
+		// Discount text
+		if(m_discountText != null) {
+			bool show = false;
+			if(targetDragonData != null) {
+				show = true;
+				m_discountText.text = LocalizationManager.SharedInstance.Localize(
+					"TID_OFFER_DISCOUNT_PERCENTAGE",
+					StringUtils.FormatNumber(Mathf.Abs(targetDragonData.pricePCModifier), 0)
+				);
+			}
+			m_discountText.gameObject.SetActive(show);
+		}
+	}
+
+	/// <summary>
+	/// Do custom visibility checks based on passive event type.
+	/// </summary>
+	/// <returns>Whether the icon can be displayed or not.</returns>
+	protected override bool RefreshVisibilityInternal() {
+		// Only show in the some specific screens
+		if(InstanceManager.menuSceneController == null) return false;
+		MenuScreen currentScreen = InstanceManager.menuSceneController.currentScreen;
+		if(currentScreen != MenuScreen.DRAGON_SELECTION) return false;
+
+		// Don't show if mod not valid
+		ModEconomyDragonPrice mod = _mod;
+		if(mod == null) return false;
+
+		// Don't show if target dragon not valid
+		IDragonData targetDragonData = _targetDragonData;
+		if(targetDragonData == null) return false;
+
+		// Don't show if selected dragon is the target dragon
+		if(InstanceManager.menuSceneController.selectedDragon == targetDragonData.sku) {
+			return false;
+		}
+
+		// Don't show arrow if the target dragon is previous to the current selected dragon
+		if(m_arrow != null) {
+			bool showArrow = InstanceManager.menuSceneController.selectedDragonData.GetOrder() < targetDragonData.GetOrder();
+			m_arrow.SetActive(showArrow);
+		}
+
+		return true;
+	}
+
+	//------------------------------------------------------------------------//
+	// CALLBACKS															  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Selected dragon has changed.
+	/// </summary>
+	/// <param name="_dragonSku">Sku of the newly selected dragon.</param>
+	private void OnDragonSelected(string _dragonSku) {
+		RefreshVisibility();
+	}
+
+	/// <summary>
+	/// The scroll to target button has been pressed.
+	/// </summary>
+	public void OnScrollToTargetDragon() {
+		// Just do it
+		IDragonData targetDragonData = _targetDragonData;
+		if(targetDragonData != null) {
+			InstanceManager.menuSceneController.SetSelectedDragon(targetDragonData.sku);
+		}
+	}
+}
--- Assets/Game/UI/Menu/LiveEvents/PassiveEvents/PassiveEventIcon.cs
+++ Assets/Game/UI/Menu/LiveEvents/PassiveEvents/PassiveEventIcon.cs
@@ -19,12 +19,10 @@ using DG.Tweening;
 /// <summary>
 /// Controller for the passive event icon in the menu HUD.
 /// </summary>
-public class PassiveEventIcon : MonoBehaviour {
+public class PassiveEventIcon : IPassiveEventIcon {
 	//------------------------------------------------------------------------//
 	// CONSTANTS															  //
 	//------------------------------------------------------------------------//
-	private const float UPDATE_FREQUENCY = 1f;	// Seconds
-
 	[Flags]
 	private enum DisplayLocation {
 		// [AOC] Max 32 values (try inheriting from long if more are needed)
@@ -40,93 +38,23 @@ public class PassiveEventIcon : MonoBehaviour {
 	// MEMBERS AND PROPERTIES												  //
 	//------------------------------------------------------------------------//
 	// Exposed
-	[SerializeField] private ShowHideAnimator m_root = null;
 	[SerializeField] private ModifierIcon m_modifierIcon = null;
-	[SerializeField] private TextMeshProUGUI m_timerText = null;
-
-	// Internal
-	private HDPassiveEventManager m_passiveEventManager;
 
 	//------------------------------------------------------------------------//
-	// GENERIC METHODS														  //
+	// IPassiveEventIcon IMPLEMENTATION										  //
 	//------------------------------------------------------------------------//
 	/// <summary>
-	/// Initialization.
+	/// Get the manager for this specific passive event type.
 	/// </summary>
-	private void Awake() {
-		// Get tournament manager
-		m_passiveEventManager = HDLiveEventsManager.instance.m_passive;
+	/// <returns>The event manager corresponding to this event type.</returns>
+	protected override HDPassiveEventManager GetEventManager() {
+		return HDLiveEventsManager.instance.m_passive;
 	}
 
 	/// <summary>
-	/// First update call.
+	/// Update visuals when new data has been received.
 	/// </summary>
-	private void Start() {
-		// Get latest data from the manager
-		RefreshData();
-
-		Messenger.AddListener(MessengerEvents.LIVE_EVENT_STATES_UPDATED, OnEventStateUpdated);
-		Messenger.AddListener<int, HDLiveEventsManager.ComunicationErrorCodes>(MessengerEvents.LIVE_EVENT_NEW_DEFINITION, OnNewEventDefinition);
-		Messenger.AddListener<int, HDLiveEventsManager.ComunicationErrorCodes>(MessengerEvents.LIVE_EVENT_FINISHED, OnEventFinished);
-		Messenger.AddListener<MenuScreen, MenuScreen>(MessengerEvents.MENU_SCREEN_TRANSITION_START, OnMenuScreenTransition);
-	}
-
-	/// <summary>
-	/// Destructor.
-	/// </summary>
-	void OnDestroy() {
-		Messenger.RemoveListener(MessengerEvents.LIVE_EVENT_STATES_UPDATED, OnEventStateUpdated);
-		Messenger.RemoveListener<int, HDLiveEventsManager.ComunicationErrorCodes>(MessengerEvents.LIVE_EVENT_NEW_DEFINITION, OnNewEventDefinition);
-		Messenger.RemoveListener<int, HDLiveEventsManager.ComunicationErrorCodes>(MessengerEvents.LIVE_EVENT_FINISHED, OnEventFinished);
-		Messenger.RemoveListener<MenuScreen, MenuScreen>(MessengerEvents.MENU_SCREEN_TRANSITION_START, OnMenuScreenTransition);
-	}
-
-	/// <summary>
-	/// Component has been enabled.
-	/// </summary>
-	private void OnEnable() {
-		// Program a periodic update
-		InvokeRepeating("UpdatePeriodic", 0f, UPDATE_FREQUENCY);
-
-		// Get latest data from the manager
-		RefreshData();
-	}
-
-	/// <summary>
-	/// Called periodically - to avoid doing stuff every frame.
-	/// </summary>
-	private void UpdatePeriodic() {
-		// Skip if we're not active - probably in a screen we don't belong to
-		if(!isActiveAndEnabled) return;
-
-		// Refresh the timer!
-		RefreshTimer(true);
-	}
-
-	/// <summary>
-	/// Component has been disabled.
-	/// </summary>
-	private void OnDisable() {
-		// Cancel periodic update
-		CancelInvoke();
-	}
-
-	//------------------------------------------------------------------------//
-	// OTHER METHODS														  //
-	//------------------------------------------------------------------------//
-	/// <summary>
-	/// Force a refresh of the offers manager asking for new featured offers to be displayed
-	/// </summary>
-	public void RefreshData() {
-		// Make sure tournament is at the right state
-		m_passiveEventManager.UpdateStateFromTimers();
-
-		// Update visibility
-		bool show = RefreshVisibility();
-
-		// Nothing else to do if there is no active event
-		if(!m_passiveEventManager.EventExists()) return;
-
+	protected override void RefreshDataInternal() {
 		// Make sure icon is showing the right info
 		if(m_modifierIcon != null) {
 			// Init icon
@@ -138,158 +66,94 @@ public class PassiveEventIcon : MonoBehaviour {
 				m_modifierIcon.gameObject.SetActive(false);
 			}
 		}
-
-		// Update the timer for the first time
-		RefreshTimer(false);
 	}
 
 	/// <summary>
-	/// Refresh the timer. To be called periodically.
+	/// Do custom visibility checks based on passive event type.
 	/// </summary>
-	/// <param name="_checkExpiration">Refresh data if timer reaches 0?</param>
-	private void RefreshTimer(bool _checkExpiration) {
-		// Aux vars
-		HDLiveEventData.State state = m_passiveEventManager.data.m_state;
+	/// <returns>Whether the icon can be displayed or not.</returns>
+	protected override bool RefreshVisibilityInternal() {
+		// Check current screen and event's UI settings
+		bool show = true;
+		Modifier mod = m_passiveEventManager.m_passiveEventDefinition.mainMod;
+		if(mod != null) {
+			// Where can this mod be displayed?
+			DisplayLocation location = DisplayLocation.NONE;
+            switch(mod.GetUICategory()) {
+				case "stats": {
+					location = DisplayLocation.PLAY_SCREEN 
+						| DisplayLocation.NORMAL_MODE
+						| DisplayLocation.TOURNAMENT_MODE
+						| DisplayLocation.INGAME;
+				} break;
 
-		// Is tournament still valid?
-		if(!m_passiveEventManager.EventExists()) return;
-		if(state == HDLiveEventData.State.FINALIZED) return;
+				case "metagame": {
+					location = DisplayLocation.PLAY_SCREEN 
+						| DisplayLocation.NORMAL_MODE
+						| DisplayLocation.OPEN_EGG_SCREEN;
+				} break;
 
-		// Update text
-		double remainingSeconds = m_passiveEventManager.data.remainingTime.TotalSeconds;
-		if(m_timerText != null) {
-			// Set text
-			if(m_timerText.gameObject.activeSelf) {
-				m_timerText.text = TimeUtils.FormatTime(
-					System.Math.Max(0, remainingSeconds), // Just in case, never go negative
-					TimeUtils.EFormat.ABBREVIATIONS,
-					4
-				);
-			}
-		}
-
-		// Manage timer expiration when the icon is visible
-		if(_checkExpiration && remainingSeconds <= 0) {
-			RefreshData();
-		}
-	}
-
-	/// <summary>
-	/// Check whether the icon can be displayed or not.
-	/// </summary>
-	private bool RefreshVisibility() {
-		// Never during tutorial
-		if(UsersManager.currentUser.gamesPlayed < GameSettings.ENABLE_TOURNAMENTS_AT_RUN) {
-			m_root.ForceHide(false);
-			return false;
-		}
-
-		// Do we have a valid event?
-		bool show = false;
-		if(m_passiveEventManager.EventExists()) {
-			// Show only for specific states
-			switch(m_passiveEventManager.data.m_state) {
-				//case HDLiveEventData.State.TEASING:		// No teasing for passive events
-				case HDLiveEventData.State.NOT_JOINED:
-				case HDLiveEventData.State.JOINED:
-				//case HDLiveEventData.State.REWARD_AVAILABLE:	// They don't have rewards either
-				{
-					// Must have a valid definition first
-					if(m_passiveEventManager.data.definition != null 
-					&& m_passiveEventManager.data.definition.initialized) {
-						show = true;
-					}
+				case "levelUp": {
+					location = DisplayLocation.PLAY_SCREEN 
+						| DisplayLocation.NORMAL_MODE
+						| DisplayLocation.INGAME;
 				} break;
 			}
 
-			// Check current screen and event's UI settings
-			// Skip if we shouldn't be displayed anyway
-			if(show) {
-				Modifier mod = m_passiveEventManager.m_passiveEventDefinition.mainMod;
-				if(mod != null) {
-					// Where can this mod be displayed?
-					DisplayLocation location = DisplayLocation.NONE;
-                    switch(mod.GetUICategory()) {
-						case "stats": {
-							location = DisplayLocation.PLAY_SCREEN 
-								| DisplayLocation.NORMAL_MODE
-								| DisplayLocation.TOURNAMENT_MODE
-								| DisplayLocation.INGAME;
-						} break;
+			// Are we in the right place?
+			// Loading Screen
+			if(LoadingScreen.isVisible) {
+				// Check tournament mode
+				switch(GameSceneController.mode) {
+					case GameSceneController.Mode.DEFAULT: {
+						show &= CheckLocation(location, DisplayLocation.INGAME | DisplayLocation.NORMAL_MODE);
+					} break;
 
-						case "metagame": {
-							location = DisplayLocation.PLAY_SCREEN 
-								| DisplayLocation.NORMAL_MODE
-								| DisplayLocation.OPEN_EGG_SCREEN;
-						} break;
+					case GameSceneController.Mode.TOURNAMENT: {
+						show &= CheckLocation(location, DisplayLocation.INGAME | DisplayLocation.TOURNAMENT_MODE);
+					} break;
+				}
+			}
 
-						case "levelUp": {
-							location = DisplayLocation.PLAY_SCREEN 
-								| DisplayLocation.NORMAL_MODE
-								| DisplayLocation.INGAME;
-						} break;
-					}
+			// Menu
+			else if(InstanceManager.menuSceneController != null) {
+				// Which screen?
+				MenuScreen currentScreen = InstanceManager.menuSceneController.currentScreen;
+				switch(currentScreen) {
+					case MenuScreen.PLAY: {
+						show &= CheckLocation(location, DisplayLocation.PLAY_SCREEN);
+					} break;
 
-					// Are we in the right place?
-					// Loading Screen
-					if(LoadingScreen.isVisible) {
-						// Check tournament mode
-						switch(GameSceneController.mode) {
-							case GameSceneController.Mode.DEFAULT: {
-								show &= CheckLocation(location, DisplayLocation.INGAME | DisplayLocation.NORMAL_MODE);
-							} break;
+					case MenuScreen.DRAGON_SELECTION: {
+						show &= CheckLocation(location, DisplayLocation.NORMAL_MODE);
+					} break;
 
-							case GameSceneController.Mode.TOURNAMENT: {
-								show &= CheckLocation(location, DisplayLocation.INGAME | DisplayLocation.TOURNAMENT_MODE);
-							} break;
-						}
-					}
+					case MenuScreen.TOURNAMENT_INFO: {
+						show &= CheckLocation(location, DisplayLocation.TOURNAMENT_MODE);
+					} break;
 
-					// Menu
-					else if(InstanceManager.menuSceneController != null) {
-						// Which screen?
-						MenuScreen currentScreen = InstanceManager.menuSceneController.currentScreen;
-						switch(currentScreen) {
-							case MenuScreen.PLAY: {
-								show &= CheckLocation(location, DisplayLocation.PLAY_SCREEN);
-							} break;
+					case MenuScreen.OPEN_EGG: {
+						show &= CheckLocation(location, DisplayLocation.OPEN_EGG_SCREEN);
+					} break;
+				}
+			}
 
-							case MenuScreen.DRAGON_SELECTION: {
-								show &= CheckLocation(location, DisplayLocation.NORMAL_MODE);
-							} break;
+			// Ingame / Results
+			else {
+				// Check tournament mode
+				switch(GameSceneController.mode) {
+					case GameSceneController.Mode.DEFAULT: {
+						show &= CheckLocation(location, DisplayLocation.INGAME | DisplayLocation.NORMAL_MODE);
+					} break;
 
-							case MenuScreen.TOURNAMENT_INFO: {
-								show &= CheckLocation(location, DisplayLocation.TOURNAMENT_MODE);
-							} break;
-
-							case MenuScreen.OPEN_EGG: {
-								show &= CheckLocation(location, DisplayLocation.OPEN_EGG_SCREEN);
-							} break;
-						}
-					}
-
-					// Ingame / Results
-					else {
-						// Check tournament mode
-						switch(GameSceneController.mode) {
-							case GameSceneController.Mode.DEFAULT: {
-								show &= CheckLocation(location, DisplayLocation.INGAME | DisplayLocation.NORMAL_MODE);
-							} break;
-
-							case GameSceneController.Mode.TOURNAMENT: {
-								show &= CheckLocation(location, DisplayLocation.INGAME | DisplayLocation.TOURNAMENT_MODE);
-							} break;
-						}
-					}
+					case GameSceneController.Mode.TOURNAMENT: {
+						show &= CheckLocation(location, DisplayLocation.INGAME | DisplayLocation.TOURNAMENT_MODE);
+					} break;
 				}
 			}
 		}
-        
-        if ( show ) {
-            show = m_passiveEventManager.m_isActive;
-        }
 
-		if(m_root != null) m_root.Set(show);
+		// All checks done!
 		return show;
 	}
 
@@ -309,22 +173,5 @@ public class PassiveEventIcon : MonoBehaviour {
 	//------------------------------------------------------------------------//
 	// CALLBACKS															  //
 	//------------------------------------------------------------------------//
-	/// <summary>
-	/// We got an update on the tournament state.
-	/// </summary>
-    private void OnNewEventDefinition(int _eventId, HDLiveEventsManager.ComunicationErrorCodes _error) {
-		RefreshData();
-	}
 
-    private void OnEventFinished(int _eventId, HDLiveEventsManager.ComunicationErrorCodes _error) {
-        RefreshVisibility();
-    }
-
-    private void OnEventStateUpdated() {
-        RefreshData();
-    }
-
-    private void OnMenuScreenTransition(MenuScreen _from, MenuScreen _to) {
-		RefreshVisibility();
-	}
 }
