56697ab61b73c2351e8c73b9e090ac686f0b4bf1
Marc Saña <marc.sana@ubisoft.com>
2018-11-30 11:55:22 +0100

    Discount event is finished after buying the target dragon and at the event activation, if that dragon is already owned.
    Former-commit-id: 8f41cb60194b6f53422b72f9f314c3eed811e557

--- Assets/Game/Core/GameServerManager.cs
+++ Assets/Game/Core/GameServerManager.cs
@@ -420,7 +420,7 @@ public class GameServerManager
 	public virtual void HDEvents_GetMyReward(int _eventID, ServerCallback _callback) {}
 	public virtual void HDEvents_FinishMyEvent(int _eventID, ServerCallback _callback) {}
 
-	public virtual void HDEvents_GetRefund(int _eventID, ServerCallback _callback) {}
+    public virtual void HDEvents_GetRefund(int _eventID, ServerCallback _callback) {}
 
 	//------------------------------------------------------------------------//
 	// DEBUG ONLY															  //
