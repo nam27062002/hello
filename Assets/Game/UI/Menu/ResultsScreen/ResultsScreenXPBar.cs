b67b15560a7c76eece5d6ab8565f4cbabe8de241
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2018-11-30 09:20:01 +0100

    Dragon Data: Development - Created constant to get default icon name.
    Former-commit-id: ce93ed6961595fbe094270a1a783d748981c1131

--- Assets/Game/UI/Menu/ResultsScreen/ResultsScreenXPBar.cs
+++ Assets/Game/UI/Menu/ResultsScreen/ResultsScreenXPBar.cs
@@ -132,7 +132,7 @@ public class ResultsScreenXPBar : DragonXPBar {
 		// Load and pose next dragon's preview
 		m_nextDragonData = DragonManager.GetNextDragonData(DragonManager.currentDragon.def.sku);
 		if(m_nextDragonData != null) {
-			m_nextDragonIcon.sprite = Resources.Load<Sprite>(UIConstants.DISGUISE_ICONS_PATH + m_nextDragonData.def.sku + "/icon_disguise_0");
+			m_nextDragonIcon.sprite = Resources.Load<Sprite>(UIConstants.DISGUISE_ICONS_PATH + m_nextDragonData.def.sku + "/" + IDragonData.DEFAULT_SKIN_ICON);
 			m_nextDragonRoot.SetActive(true);
 		} else {
 			m_nextDragonRoot.SetActive(false);
