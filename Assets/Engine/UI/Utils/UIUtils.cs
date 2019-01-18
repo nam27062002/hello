cff578ac1c75ac094803c65ee3ccdcaef466f599
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2019-01-18 10:51:11 +0100

    UI Utils: Development - Added formatting options to Ordinal number formatter.
    Former-commit-id: 3f8785b33cc469014fd67d2b227f1e77399da94e

--- Assets/Engine/UI/Utils/UIUtils.cs
+++ Assets/Engine/UI/Utils/UIUtils.cs
@@ -23,6 +23,12 @@ public partial class UIUtils {
 	//------------------------------------------------------------------------//
 	private const string TID_ORDINAL_TEMPLATE = "TID_GEN_ORDINAL_{0}";
 	private const long ORDINAL_SCOPE = 100;
+
+	public enum OrdinalSuffixFormat {
+		DEFAULT,
+		SUPERSCRIPT,
+		SUBSCRIPT
+	}
 	
 	//------------------------------------------------------------------------//
 	// STATIC METHODS														  //
@@ -33,9 +39,46 @@ public partial class UIUtils {
 	/// </summary>
 	/// <returns>The string representing the given number.</returns>
 	/// <param name="_num">The number to be represented.</param>
+	/// <param name="_format">Format for the suffix.</param>
 	/// <param name="_zeroPadding">The minimum amount of digits to be displayed in the integer part of the number. Missing digits will be filled with '0'. Example: 12.3456, 4 -> "0012.3456".</param>
 	/// <param name="_useThousandSeparators">Whether to use thousands separators or not.</param>
-	public static string FormatOrdinalNumber(long _num, int _zeroPadding = 0, bool _useThousandSeparators = true) {
+	public static string FormatOrdinalNumber(long _num, OrdinalSuffixFormat _format, int _zeroPadding = 0, bool _useThousandSeparators = true) {
+		// Format the number
+		string number = StringUtils.FormatNumber(_num, _zeroPadding, _useThousandSeparators);
+
+		// Get the localized ordinal string
+		string ordinal = LocalizationManager.SharedInstance.Localize(
+			GetOrdinalSuffixTid(_num),
+			number
+		);
+
+		// If required, perform formatting transformations
+		switch(_format) {
+			case OrdinalSuffixFormat.SUPERSCRIPT: {
+				// We'll do the bold assumption that the ordinal suffix is always after the number
+				int suffixStartPos = ordinal.IndexOf(number, StringComparison.InvariantCulture) + number.Length;
+				ordinal = ordinal.Insert(suffixStartPos, "<sup>");	// Insert tag opening
+				ordinal += "</sup>";	// Append tag closing
+			} break;
+
+			case OrdinalSuffixFormat.SUBSCRIPT: {
+				// We'll do the bold assumption that the ordinal suffix is always after the number
+					int suffixStartPos = ordinal.IndexOf(number, StringComparison.InvariantCulture) + number.Length;
+					ordinal = ordinal.Insert(suffixStartPos, "<sub>");  // Insert tag opening
+					ordinal += "</sub>";    // Append tag closing
+			} break;
+		}
+
+		// Done!
+		return ordinal;
+	}
+
+	/// <summary>
+	/// Gets the TID corresponding to the ordinal suffix for a given number (st, nd, th...).
+	/// </summary>
+	/// <returns>The TID for the ordinal suffix. Text will have the format "%U0st".</returns>
+	/// <param name="_num">Number.</param>
+	public static string GetOrdinalSuffixTid(long _num) {
 		// Treat negative numbers as positive
 		long num = Math.Abs(_num);
 
@@ -43,13 +86,7 @@ public partial class UIUtils {
 		// If the number is outside our scope, find equivalent within scope
 		long suffixIdx = num % ORDINAL_SCOPE;   // i.e. 3 -> 3, 150 -> 50, 201 -> 1, etc.
 
-		// Format TID string
-		string tid = String.Format(TID_ORDINAL_TEMPLATE, suffixIdx, CultureInfo.InvariantCulture);  // i.e. TID_GEN_ORDINAL_{0} -> TID_GEN_ORDINAL_25
-
-		// Perform the localization and number formatting all at once and return
-		return LocalizationManager.SharedInstance.Localize(
-			tid, 
-			StringUtils.FormatNumber(_num, _zeroPadding, _useThousandSeparators)
-		);
+		// Format TID string and return
+		return String.Format(TID_ORDINAL_TEMPLATE, suffixIdx, CultureInfo.InvariantCulture);  // i.e. TID_GEN_ORDINAL_{0} -> TID_GEN_ORDINAL_25
 	}
 }
