bf014f6c3fa5611470296795ccfb48ec86f3f27e
Nevera 2 <dcampos@ffelius-imac-2.ubisoft.org>
2019-01-24 11:13:23 +0100

    [UBI_PACKAGES_TEST] Added script to link the tests with UbiPackages code
    Former-commit-id: 677e864460839d057dd6a7217eff8d4c60637c22

---
+++ UbiPackagesTests/UbiPackagesInitializer.sh
@@ -0,0 +1,43 @@
+##########################################################
+##
+## UBIPACKAGES
+## INITIALIZER
+##
+##########################################################
+
+echo
+echo "Preparing UbiPackages framework and symbolic linkages..."
+echo
+
+link_ubi_packages()
+{
+	echo "link_ubi_packages" $1
+
+	ubiPackagesFolderName="UbiPackages"
+	pathToUbiPackagesOriginal=$PWD"/../Assets/"$ubiPackagesFolderName
+	pathToUbiPackagesLink="$1/Assets/"$ubiPackagesFolderName
+
+	#echo "pathToUbiPackagesOriginal="$pathToUbiPackagesOriginal
+	#echo "pathToUbiPackagesLink="$pathToUbiPackagesLink
+
+	# Checks if the link already exists 
+	if [ -L $pathToUbiPackagesLink ]
+	then
+		echo "Symbolic link ${pathToUbiPackagesLink} already exists"
+	else
+		createSymbolicLink $pathToUbiPackagesOriginal $pathToUbiPackagesLink	
+	fi
+}
+
+createSymbolicLink()
+{
+	echo "Symbolic linking $1 to $2..."
+	ln -s $1 $2
+
+	if [ -L $2 ]
+	then
+		echo "SUCCESS"
+	fi
+}
+
+link_ubi_packages AssetBundlesUnitTests
