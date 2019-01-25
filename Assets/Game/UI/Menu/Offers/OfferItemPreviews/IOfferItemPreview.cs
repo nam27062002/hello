fc5f91f39c1310fe457b088adfff8a7bde5941cc
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2019-01-25 11:47:14 +0100

    Featured Offer Popup: Fix - Fixed several issues regarding render order.
    Former-commit-id: c454be5eeff85adc5888d61c27645e58db2ed4ef

--- Assets/Game/UI/Menu/Offers/OfferItemPreviews/IOfferItemPreview.cs
+++ Assets/Game/UI/Menu/Offers/OfferItemPreviews/IOfferItemPreview.cs
@@ -99,14 +99,9 @@ public abstract class IOfferItemPreview : MonoBehaviour {
 			// Aux vars
 			ParticleSystem ps = nestedPS[i];
 
-			// Disable VFX whenever a popup is opened in top of this preview (they
-			// don't render well with a popup on top)
-			// [AOC] At this point the popup containing this preview hasn't yet been 
-			// registered into the PopupManager, so we need to count for it in order 
-			// for the disabler to work as expected.
-			// By doing this, we are assuming the item preview belongs ALWAYS to a popup.
+			// Disable VFX whenever a popup is opened in top of this preview (they don't render well with a popup on top)
 			DisableOnPopup disabler = ps.gameObject.AddComponent<DisableOnPopup>();
-			disabler.refPopupCount = PopupManager.openPopupsCount + 1;
+			disabler.refPopupCount = PopupManager.openPopupsCount;
 
 			// Start particle with a couple of frames of delay to give time for the particle scalers to be applied
 			ps.gameObject.SetActive(false);
