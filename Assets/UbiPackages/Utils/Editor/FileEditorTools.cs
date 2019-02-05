48a5d625e153d4281973c179ba49d444e0238841
David Germade <david.germade@ubisoft.com>
2019-02-05 14:07:21 +0100

    [FILE_UTILS] CopyDirectory created.
    Former-commit-id: 40c458bd6ea468349822f6ba794d0118a2d4f381

--- Assets/UbiPackages/Utils/Editor/FileEditorTools.cs
+++ Assets/UbiPackages/Utils/Editor/FileEditorTools.cs
@@ -6,6 +6,33 @@ public class FileEditorTools
 {
     private static bool VERBOSE = false;
 
+    public static bool Exists(string path)
+    {
+        return Directory.Exists(path);
+    }
+
+    public static void CreateDirectory(string path)
+    {
+        if (!string.IsNullOrEmpty(path) && !Exists(path))
+        {            
+            string[] tokens = path.Split(Path.DirectorySeparatorChar);
+            //string[] tokens = path.Split('/');
+            int count = tokens.Length;
+            string pathSoFar = "";
+            string newPath;
+            for (int i = 0; i < count; i++)
+            {
+                newPath = PathCombine(pathSoFar, tokens[i]);
+                if (!Exists(newPath))
+                {
+                    AssetDatabase.CreateFolder(pathSoFar,tokens[i]);
+                }
+
+                pathSoFar = newPath;
+            }
+        }            
+    }
+
     public static void DeleteFileOrDirectory(string path)
     {
         if (VERBOSE)
@@ -20,19 +47,63 @@ public class FileEditorTools
     }
 
     public static string PathCombine(string path1, string path2)
-    {
-        return path1 + "/" + path2;
-        //return Path.Combine(path1, path2);
+    {   
+        /*     
+        if (string.IsNullOrEmpty(path1))
+        {
+            return path2;
+        }
+        else if (string.IsNullOrEmpty(path2))
+        {
+            return path1;
+        }
+        else
+        {
+            return path1 + "/" + path2;
+        }*/
+            
+        return Path.Combine(path1, path2);        
     }
 
-    public static void CopyFileOrDirectory(string srcPath, string dstPath)
+    public static void CopyDirectory(string srcPath, string dstPath)
     {
         if (VERBOSE)
         {
             Debug.Log("Copy " + srcPath + " into " + dstPath);
         }
+      
+        if (Exists(srcPath))
+        {
+            if (!Exists(dstPath))
+            {
+                CreateDirectory(dstPath);
+            }
 
-        FileUtil.CopyFileOrDirectory(srcPath, dstPath);
+            string[] files = Directory.GetFiles(srcPath);
+
+            string fileName;
+            string dstFile;
+
+            // Copies the files and overwrite destination files if they already exist.           
+            foreach (string s in files)
+            {
+                // Uses static Path methods to extract only the file name from the path.
+                fileName = Path.GetFileName(s);
+                dstFile = PathCombine(dstPath, fileName);
+                File.Copy(s, dstFile, true);
+            }
+
+            // Copies the subdirectories            
+            string dstSubdirectoryPath;
+            string[] subdirectories = Directory.GetDirectories(srcPath);
+            foreach (string s in subdirectories)
+            {
+                // Uses static Path methods to extract only the file name from the path.
+                fileName = Path.GetFileName(s);                
+                dstSubdirectoryPath = PathCombine(dstPath, fileName);
+                CopyDirectory(s, dstSubdirectoryPath);
+            }
+        }        
     }
 
     public static void RenameFile(string oldPath, string newPath)
