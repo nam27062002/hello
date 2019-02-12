372472516a30d281ca43d5f15e98291594c10d66
Build Machine <buildmachine@ubisoft.com>
2019-02-12 11:32:39 +0100

    [TOOLS] Some logs added to Build tools.
    Former-commit-id: 2009457b5b161f27d7b5e2e12bbc0917a85f8c8c

--- Assets/Editor/BuildEditorMenu.cs
+++ Assets/Editor/BuildEditorMenu.cs
@@ -8,10 +8,10 @@ public class BuildEditorMenu : MonoBehaviour
     private const string BUILD_MENU = "Tech/Build";
     private const string BUILD_MENU_BUILD_PLAYER = BUILD_MENU + "/Build Player";
     private const string BUILD_MENU_BUILD_ADDRESSABLES_AND_PLAYER = BUILD_MENU + "/Build Addressables and Player";
-
-    [MenuItem(BUILD_MENU_BUILD_PLAYER)]
-    public static void BuildPlayer()
+    
+    private static void InternalBuildPlayer()
     {
+		Debug.Log("Building player...");
         string[] sceneNames = null;
         EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
         if (scenes != null)
@@ -35,8 +35,7 @@ public class BuildEditorMenu : MonoBehaviour
         buildPlayerOptions.options = BuildOptions.None;
         buildPlayerOptions.assetBundleManifestPath = "AssetBundles/" + buildTargetAsString + "/" + buildTargetAsString + ".manifest";
 
-        string result = BuildPipeline.BuildPlayer(buildPlayerOptions);
-        Debug.Log("Build DONE with result " + result);
+        BuildPipeline.BuildPlayer(buildPlayerOptions);        
         /*
 		BuildSummary summary = report.summary;
 
@@ -49,15 +48,29 @@ public class BuildEditorMenu : MonoBehaviour
 		{
 			Debug.Log("Build failed");
 		}
-		*/
+		*/        
+    }
 
+    [MenuItem(BUILD_MENU_BUILD_PLAYER)]
+    private static void BuildPlayer()
+    {
+		InternalBuildPlayer();
         AssetDatabase.Refresh();
+        OnDone(BUILD_MENU_BUILD_PLAYER);
     }
 
     [MenuItem(BUILD_MENU_BUILD_ADDRESSABLES_AND_PLAYER)]
     public static void BuildAddressablesAndPlayer()
     {
+		Debug.Log("Building addressables...");
         AddressablesEditorMenu.Build();
-        BuildPlayer();
+        InternalBuildPlayer();
+        OnDone(BUILD_MENU_BUILD_ADDRESSABLES_AND_PLAYER);
+    }
+
+    private static void OnDone(string taskName)
+    {
+        AssetDatabase.Refresh();
+        Debug.Log(taskName + " done.");
     }
 }
