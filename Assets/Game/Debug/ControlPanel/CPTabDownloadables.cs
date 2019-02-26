f40770696b877b6568f36bffb35b7b4872c56cd5
David Germade <david.germade@ubisoft.com>
2019-02-26 17:50:31 +0100

    [CP] Downloadables tab added.
    Former-commit-id: c4e977feadbb47c45ee476f0d7c4b3e2f4ac24cb

---
+++ Assets/Game/Debug/ControlPanel/CPTabDownloadables.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CPTabDownloadables : MonoBehaviour
+{
+    public class DownloadableView
+    {
+        private TMP_Text m_downloadedNameText;
+        private Slider m_donwloadedSoFarSlider;
+        private TMP_Text m_donwloadedSoFarText;
+
+        Downloadables.CatalogEntryStatus m_entry;
+
+        public void Setup(GameObject go, Downloadables.CatalogEntryStatus entry)
+        {
+            m_entry = entry;
+            if (m_entry != null)
+            {
+                m_downloadedNameText = go.FindComponentRecursive<TMP_Text>("NameText");
+                m_downloadedNameText.text = entry.Id;
+
+                m_donwloadedSoFarSlider = go.FindComponentRecursive<Slider>("Slider");
+                m_donwloadedSoFarSlider.minValue = 0f;
+                m_donwloadedSoFarSlider.maxValue = m_entry.GetTotalBytes();
+
+                m_donwloadedSoFarText = go.FindComponentRecursive<TMP_Text>("Text");
+            }
+        }
+
+        public void Update()
+        {
+            if (m_donwloadedSoFarSlider != null)
+            {
+                m_donwloadedSoFarSlider.value = m_entry.GetBytesDownloadedSoFar();
+                m_donwloadedSoFarText.text = m_entry.GetMbDownloadedSoFar() + "Mb";
+            }
+        }
+    }
+
+    private List<DownloadableView> m_views = new List<DownloadableView>();
+
+    [SerializeField]
+    private GameObject m_downloadableViewPrefab;
+
+    [SerializeField]
+    private Transform m_downloadableViewsRoot;
+
+    void Start()
+    {        
+        DownloadableView view;
+        GameObject go;
+        Dictionary<string, Downloadables.CatalogEntryStatus> catalog = AssetBundlesManager.Instance.GetDownloadablesCatalog();
+        if (catalog != null)
+        {
+            foreach (KeyValuePair<string, Downloadables.CatalogEntryStatus> pair in catalog)
+            {
+                go = Instantiate(m_downloadableViewPrefab, m_downloadableViewsRoot);
+                go.SetActive(true);
+
+                view = new DownloadableView();
+                view.Setup(go, pair.Value);
+                m_views.Add(view);
+            }
+        }
+    }
+
+    void Update()
+    {        
+        int count = m_views.Count;
+        for (int i = 0; i < count; i++)
+        {
+            m_views[i].Update();
+        }        
+    }
+}
