efa432d657c6e10764ad6abe391fcebded8aeb2e
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2019-03-04 15:34:36 +0100

    Lab Screen: Fix - Going back and forth very fast was causing several issues. Resolves issue HDK-4255 among others.
    Former-commit-id: a13f1298d855d7302adef2176eb099f4a9aa664c

--- Assets/Game/UI/Menu/Lab/LabDragonSelectionScreen.cs
+++ Assets/Game/UI/Menu/Lab/LabDragonSelectionScreen.cs
@@ -235,7 +235,11 @@ public class LabDragonSelectionScreen : MonoBehaviour {
     /// Back button has been pressed.
     /// </summary>
     public void OnBackButton() {
-        // Stop lab music
+		// Make sure we are allowed to change screen (prevent spamming)
+		// [AOC] Resolves issue HDK-4255 among others
+		if(!InstanceManager.menuSceneController.transitionManager.transitionAllowed) return;
+        
+		// Stop lab music
         m_labMusicCount--;
         InstanceManager.musicController.Ambience_Stop(LAB_MUSIC, gameObject);
         
