cde6f0a0e4d90649d9190ad04cffde7a7cc5bfa4
David Germade <david.germade@ubisoft.com>
2019-03-14 17:54:13 +0100

    [ADDRESSABLES] HD Custom scripts are called to generate the catalog.
    Former-commit-id: f8b02816a91805ae526e6fa921eb0a0d31776bb7

---
+++ Assets/Editor/Addressables/HDEditorAddressablesManager.cs
@@ -0,0 +1,15 @@
+﻿using SimpleJSON;
+
+public class HDEditorAddressablesManager : EditorAddressablesManager
+{
+    public override void CustomizeEditorCatalog()
+    {
+        Debug.Log("HDEditorAddressablesManager Customizing editor catalog...");
+    }
+
+    protected override JSONNode GetExternalAddressablesCatalogJSON()
+    {
+        // Scripts to generate game related asset bundles here
+        return null;
+    }
+}
