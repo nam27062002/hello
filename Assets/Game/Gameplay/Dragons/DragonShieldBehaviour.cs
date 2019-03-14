a4a76857db9bfd44116b900f7a7c784bdf3a13ea
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2019-03-14 17:19:48 +0100

    shield control
    Former-commit-id: ca2c415decafa6ebe32072ee3231daef013e587b

--- Assets/Game/Gameplay/Dragons/DragonShieldBehaviour.cs
+++ Assets/Game/Gameplay/Dragons/DragonShieldBehaviour.cs
@@ -11,12 +11,29 @@ public class DragonShieldBehaviour : MonoBehaviour {
     public List<DamageType> m_ignoreDamageTypes = new List<DamageType>();
     private DragonPlayer m_dragon;
     private DragonHealthBehaviour m_dragonHealth;
+
+    [Header("Anim Setup")]
+    public string m_animState = "";
+    protected int m_animStateHash;
+    public string m_animLayer = "";
+    protected int m_animLayerId;
+    public float[] m_animThresholds = new float[0];
+    public float[] m_animFrames = new float[0];
+    public float m_animHisteresisPercentage = 5.0f;
+    public float m_animTime = 0.1f;
+    protected Animator m_anim;
     
 
 	// Use this for initialization
 	void Start () {
+        m_anim = GetComponentInChildren<Animator>();
+        
+        m_animStateHash = Animator.StringToHash(m_animState);
+        m_animLayerId = m_anim.GetLayerIndex(m_animLayer);
+    
         m_dragon = GetComponent<DragonPlayer>();
         m_dragonHealth = m_dragon.dragonHealthBehaviour;
+        
 		Messenger.AddListener<Transform, IEntity, Reward>(MessengerEvents.ENTITY_EATEN, OnEntityEaten);
         Messenger.AddListener<Transform, IEntity, Reward>(MessengerEvents.ENTITY_DESTROYED, OnEntityDestroyed);
         Messenger.AddListener<Transform, IEntity, Reward>(MessengerEvents.ENTITY_BURNED, OnEntityBurned);
@@ -72,6 +89,41 @@ public class DragonShieldBehaviour : MonoBehaviour {
         {
             m_currentShield = 0;
         }
+
+        // Update anim time
+        int max = m_animThresholds.Length;
+        float animTarget = 0;
+        float threshold = 0;
+        bool done = false;
+        for (int i = 0; i < max && !done; i++)
+        {
+            if ( m_animThresholds[i] <= m_currentShield )
+            {
+                animTarget = m_animFrames[i];
+                if ( i+1 < m_animThresholds.Length )
+                    threshold = m_animThresholds[i+1];
+            }
+            else
+            {
+                done = true;
+            }
+        }
+        if ( m_animTime < animTarget )
+        {
+            m_animTime = Mathf.Lerp(m_animTime, animTarget, Time.deltaTime * 10);    // Increase fast
+        }
+        else
+        {
+            threshold -= threshold * m_animHisteresisPercentage / 100.0f;
+            if ( m_currentShield <= threshold )
+            {
+                // Break it
+                m_animTime = animTarget;
+            }
+        }
+
+        // Update animation
+        m_anim.PlayInFixedTime(m_animStateHash, m_animLayerId, m_animTime);
 	}
     
     public float RecieveDamage(float _amount, DamageType _type, Transform _source = null, bool _hitAnimation = true, string _damageOrigin = "", Entity _entity = null)
