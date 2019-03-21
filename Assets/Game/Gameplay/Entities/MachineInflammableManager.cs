f90d9a0b2a29f6cb16165eac491b295025fe80b5
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2019-03-21 12:28:07 +0100

    WIP changes
    Former-commit-id: b24c5b0a4258e03087d936325ae7c7dc75ffe875

--- Assets/Game/Gameplay/Entities/MachineInflammableManager.cs
+++ Assets/Game/Gameplay/Entities/MachineInflammableManager.cs
@@ -5,38 +5,72 @@ using UnityEngine;
 public class MachineInflammableManager : UbiBCN.SingletonMonoBehaviour<MachineInflammableManager>, IBroadcastListener { 
 	private const float DISINTEGRATE_TIME = 1.25f;
 
-	private Material m_ashes_wait;	 // starting queue, all renderers will be back until next queue is available
-	private Material m_ashes_disintegrate;
-	private Material m_ashes_end;
+    public struct AshesMaterials
+    {
+        public Material wait;
+        public Material disintegrate;
+        public Material end;
+    }
+    private Dictionary<FireColorSetupManager.FireColorType, AshesMaterials> m_ashesMats = new Dictionary<FireColorSetupManager.FireColorType, AshesMaterials>();
+	private Dictionary<FireColorSetupManager.FireColorType, float> m_timers;
+    private Dictionary<FireColorSetupManager.FireColorType, List<AI.MachineInflammable>> m_list_wait;
+    private Dictionary<FireColorSetupManager.FireColorType, List<AI.MachineInflammable>> m_list_disintegrate;
 
-	private float m_timer;
 
-	private List<AI.MachineInflammable> m_list_wait;
-	private List<AI.MachineInflammable> m_list_disintegrate;
-
-
-	public static void Add(AI.MachineInflammable _machine) {
-		instance.__Add(_machine);
+	public static void Add(AI.MachineInflammable _machine, FireColorSetupManager.FireColorType _fireColor = FireColorSetupManager.FireColorType.RED ) {
+		instance.__Add(_machine, _fireColor);
 	}
 
 	private void Awake() {
-		Material sharedAshesMaterial = Resources.Load("Game/Materials/BurnToAshes") as Material;
+		m_list_wait = new Dictionary<FireColorSetupManager.FireColorType, List<AI.MachineInflammable>>();
+		m_list_disintegrate = new Dictionary<FireColorSetupManager.FireColorType, List<AI.MachineInflammable>>();
+        m_timers = new Dictionary<FireColorSetupManager.FireColorType, float>();
+        int max = (int)FireColorSetupManager.FireColorType.COUNT;
+        for (int i = 0; i < max; i++)
+        {
+            FireColorSetupManager.FireColorType fireColorType = (FireColorSetupManager.FireColorType)i;
+            m_list_wait.Add(fireColorType, new List<AI.MachineInflammable>());
+            m_list_disintegrate.Add(fireColorType, new List<AI.MachineInflammable>());
+            m_timers.Add(fireColorType, 0 );
+        }
+    }
 
-		m_ashes_wait = new Material(sharedAshesMaterial);
-		m_ashes_wait.SetFloat( GameConstants.Material.ASH_LEVEL , 0f);
-		m_ashes_disintegrate = new Material(sharedAshesMaterial);
-		m_ashes_disintegrate.SetFloat( GameConstants.Material.ASH_LEVEL , 0f);
-		m_ashes_end = new Material(sharedAshesMaterial);
-		m_ashes_end.SetFloat( GameConstants.Material.ASH_LEVEL , 1f);
+    public void RegisterColor(FireColorSetupManager.FireColorType _type)
+    {
+        if ( !m_ashesMats.ContainsKey( _type ) )
+        {
+            Material sharedAshesMaterial = null;
+            switch( _type )
+            {
+                case FireColorSetupManager.FireColorType.RED:
+                    {
+                        sharedAshesMaterial = Resources.Load("Game/Materials/BurnToAshes") as Material;
+                    }break;
+                case FireColorSetupManager.FireColorType.BLUE:
+                    {
+                        sharedAshesMaterial = Resources.Load("Game/Materials/BurnToAshes") as Material;
+                    }break;
+                case FireColorSetupManager.FireColorType.ICE:
+                    {
+                        sharedAshesMaterial = Resources.Load("Game/Materials/BurnToAshesIce") as Material;
+                    }break;
+            }
 
-		m_list_wait = new List<AI.MachineInflammable>();
-		m_list_disintegrate = new List<AI.MachineInflammable>();
-	}
+            AshesMaterials ashesMaterials = new AshesMaterials();
+            ashesMaterials.wait = new Material(sharedAshesMaterial);
+            ashesMaterials.wait.SetFloat( GameConstants.Material.ASH_LEVEL , 0f);
+            ashesMaterials.disintegrate = new Material(sharedAshesMaterial);
+            ashesMaterials.disintegrate.SetFloat( GameConstants.Material.ASH_LEVEL , 0f);
+            ashesMaterials.end = new Material(sharedAshesMaterial);
+            ashesMaterials.end.SetFloat( GameConstants.Material.ASH_LEVEL , 1f);
+            m_ashesMats.Add(_type, ashesMaterials);
+        }
+    }
 
-	/// <summary>
-	/// Component enabled.
-	/// </summary>
-	private void OnEnable() {
+    /// <summary>
+    /// Component enabled.
+    /// </summary>
+    private void OnEnable() {
 		// Subscribe to external events
 		Broadcaster.AddListener(BroadcastEventType.GAME_AREA_EXIT, this);
 		Broadcaster.AddListener(BroadcastEventType.GAME_ENDED, this);
@@ -64,26 +98,31 @@ public class MachineInflammableManager : UbiBCN.SingletonMonoBehaviour<MachineIn
     }
 
 	private void ClearQueues() {
-		m_list_wait.Clear();
-		m_list_disintegrate.Clear();
+	    int max = (int)FireColorSetupManager.FireColorType.COUNT;
+        for (int i = 0; i < max; i++)
+        {
+            FireColorSetupManager.FireColorType fireColorType = (FireColorSetupManager.FireColorType)i;
+            m_list_wait[fireColorType].Clear();
+            m_list_disintegrate[fireColorType].Clear();
+        }
 	}
 
-	private void __Add(AI.MachineInflammable _machine) {
-		if (m_timer <= 0.25f) {
-			AddToDisintegrateList(_machine);
+	private void __Add(AI.MachineInflammable _machine,  FireColorSetupManager.FireColorType _fireColorType = FireColorSetupManager.FireColorType.RED ) {
+		if (m_timers[ _fireColorType ] <= 0.25f) {
+			AddToDisintegrateList(_machine, _fireColorType);
 		} else {
-			AddToWaitQueue(_machine);
+			AddToWaitQueue(_machine, _fireColorType);
 		}
 	}
 
-	private void AddToWaitQueue(AI.MachineInflammable _machine) {
-		ChangeMaterials(_machine, m_ashes_wait);
-		m_list_wait.Add(_machine);
+	private void AddToWaitQueue(AI.MachineInflammable _machine, FireColorSetupManager.FireColorType _fireColorType) {
+		ChangeMaterials(_machine, m_ashesMats[_fireColorType].wait);
+		m_list_wait[_fireColorType].Add(_machine);
 	}
 
-	private void AddToDisintegrateList(AI.MachineInflammable _machine) {
-		ChangeMaterials(_machine, m_ashes_disintegrate);
-		m_list_disintegrate.Add(_machine);
+	private void AddToDisintegrateList(AI.MachineInflammable _machine, FireColorSetupManager.FireColorType _fireColorType) {
+		ChangeMaterials(_machine, m_ashesMats[_fireColorType].disintegrate);
+		m_list_disintegrate[_fireColorType].Add(_machine);
 	}
 
 	private void ChangeMaterials(AI.MachineInflammable _machine, Material _material) {
@@ -101,29 +140,37 @@ public class MachineInflammableManager : UbiBCN.SingletonMonoBehaviour<MachineIn
 	private void Update() {
 		float dt = Time.deltaTime;
 
-		// manage the renderers starting to burn
-		if (m_list_disintegrate.Count == 0) {
-			if (m_list_wait.Count > 0) {
-				for (int i = 0; i < m_list_wait.Count; ++i) {
-					AddToDisintegrateList(m_list_wait[i]);
-				}
-				m_list_wait.Clear();
-				m_timer = 0f;
-			}
-		} else {
-			m_timer += dt;
-			Mathf.Clamp(m_timer, 0f, DISINTEGRATE_TIME);
 
-			if (m_timer >= DISINTEGRATE_TIME) {
-				for (int i = 0; i < m_list_disintegrate.Count; ++i) {
-					m_list_disintegrate[i].Burned();
-					ChangeMaterials(m_list_disintegrate[i], m_ashes_end);
-				}
-				m_list_disintegrate.Clear();
-				m_timer = 0f;
-			}
+        int max = (int)FireColorSetupManager.FireColorType.COUNT;
+        for (int c = 0; c < max; c++)
+        {
+            FireColorSetupManager.FireColorType colorType = (FireColorSetupManager.FireColorType)c;
+            // manage the renderers starting to burn
+            if (m_list_disintegrate[colorType].Count == 0) {
+                if (m_list_wait[colorType].Count > 0) {
+                    for (int i = 0; i < m_list_wait[colorType].Count; ++i) {
+                        AddToDisintegrateList(m_list_wait[colorType][i], colorType);
+                    }
+                    m_list_wait[colorType].Clear();
+                    m_timers[ colorType ] = 0f;
+                }
+            } else {
+                m_timers[ colorType ] += dt;
+                Mathf.Clamp(m_timers[ colorType ], 0f, DISINTEGRATE_TIME);
+    
+                if (m_timers[ colorType ] >= DISINTEGRATE_TIME) {
+                    for (int i = 0; i < m_list_disintegrate[colorType].Count; ++i) {
+                        m_list_disintegrate[colorType][i].Burned();
+                        ChangeMaterials(m_list_disintegrate[colorType][i], m_ashesMats[colorType].end);
+                    }
+                    m_list_disintegrate[colorType].Clear();
+                    m_timers[ colorType ] = 0f;
+                }
+    
+                m_ashesMats[colorType].disintegrate.SetFloat( GameConstants.Material.ASH_LEVEL , m_timers[ colorType ] / DISINTEGRATE_TIME);
+            }
+        }
 
-			m_ashes_disintegrate.SetFloat( GameConstants.Material.ASH_LEVEL , m_timer / DISINTEGRATE_TIME);
-		}
+        
 	}
 }
