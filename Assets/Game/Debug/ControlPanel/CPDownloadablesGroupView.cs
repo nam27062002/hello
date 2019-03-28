18e564cd3e54ad69b10039afc01f225f9c16bd95
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2019-03-28 12:45:32 +0100

    Control Panel: Development - Reworked Downloadables tab for a better user experience.
    Former-commit-id: aae12a8453c16845f5958789e19e21843414f8cb

---
+++ Assets/Game/Debug/ControlPanel/CPDownloadablesGroupView.cs
@@ -0,0 +1,150 @@
+// CPDownloadablesGroupView.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 28/03/2019.
+// Copyright (c) 2019 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// 
+/// </summary>
+public class CPDownloadablesGroupView : MonoBehaviour {
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
+
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
+	// Exposed references
+	[SerializeField] private GameObject m_bundleViewPrefab = null;
+	[SerializeField] private Transform m_bundlesRoot = null;
+	[Space]
+	[SerializeField] private TMP_Text m_nameText = null;
+	[SerializeField] private TMP_Text m_priorityText = null;
+	[SerializeField] private Image m_background = null;
+	[Space]
+	[SerializeField] private Toggle m_permissionRequestedToggle = null;
+	[SerializeField] private Toggle m_permissionGrantedToggle = null;
+
+	// Public properties
+	public Downloadables.CatalogGroup Group { get; private set; }
+
+	// Internal
+	private int m_priority = -1;
+	
+	//------------------------------------------------------------------------//
+	// GENERIC METHODS														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Called every frame.
+	/// </summary>
+	private void Update() {
+		// Make sure priority is updated
+		Refresh(false);
+	}
+
+	//------------------------------------------------------------------------//
+	// OTHER METHODS														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Initialize this view with a given group data.
+	/// </summary>
+	/// <param name="_group">Group to be used.</param>
+	public void InitWithGroup(Downloadables.CatalogGroup _group) {
+		// Store data
+		Group = _group;
+
+		// Initialize bundle views
+		if(Group.EntryIds != null && Group.EntryIds.Count > 0) {
+			// Shortcut to catalog
+			Dictionary<string, Downloadables.CatalogEntryStatus> catalog = AssetBundlesManager.Instance.GetDownloadablesCatalog();
+
+			// Create a new view instance for each bundle in the group
+			CPDownloadablesBundleView bundleView = null;
+			GameObject newInstance = null;
+			Downloadables.CatalogEntryStatus entryStatus = null;
+			int count = Group.EntryIds.Count;
+			for(int i = 0; i < count; ++i) {
+				// Get entry data
+				catalog.TryGetValue(Group.EntryIds[i], out entryStatus);
+				if(entryStatus != null) {
+					// Create new instance
+					newInstance = Instantiate<GameObject>(m_bundleViewPrefab, m_bundlesRoot);
+					newInstance.SetActive(true);
+
+					// Initialize view
+					bundleView = newInstance.GetComponent<CPDownloadablesBundleView>();
+					bundleView.InitWithData(entryStatus);
+				}
+			}
+		}
+
+		// Disable prefab instance
+		m_bundleViewPrefab.SetActive(false);
+
+		// Perform a first refresh
+		Refresh(true);
+	}
+
+	/// <summary>
+	/// Update everything with latest data from the group.
+	/// </summary>
+	/// <param name="_forced">If set to <c>false</c>, visuals will only be changed if values are different from the displayed ones.</param>
+	private void Refresh(bool _forced) {
+		// Name
+		m_nameText.text = Group.Id;
+
+		// Priority
+		if(_forced || m_priority != Group.Priority) {
+			m_priority = Group.Priority;
+			m_priorityText.text = m_priority.ToString();
+		}
+
+		// Permission toggles
+		if(_forced || m_permissionRequestedToggle.isOn != Group.PermissionOverCarrierRequested) {
+			m_permissionRequestedToggle.isOn = Group.PermissionOverCarrierRequested;
+		}
+
+		if(_forced || m_permissionGrantedToggle.isOn != Group.PermissionOverCarrierGranted) {
+			m_permissionGrantedToggle.isOn = Group.PermissionOverCarrierGranted;
+		}
+	}
+
+	/// <summary>
+	/// Define the background color for this group.
+	/// </summary>
+	/// <param name="_color">Color.</param>
+	public void SetBackgroundColor(Color _color) {
+		m_background.color = _color;
+	}
+
+	//------------------------------------------------------------------------//
+	// CALLBACKS															  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// The permission request toggle has been changed.
+	/// </summary>
+	/// <param name="_newValue">New value.</param>
+	public void OnPermissionRequestToggle(bool _newValue) {
+		Group.PermissionOverCarrierRequested = _newValue;
+	}
+
+	/// <summary>
+	/// The permission granted toggle has been changed.
+	/// </summary>
+	/// <param name="_newValue">New value.</param>
+	public void OnPermissionGrantedToggle(bool _newValue) {
+		Group.PermissionOverCarrierGranted = _newValue;
+	}
+}
---
+++ Assets/Game/Debug/ControlPanel/CPDownloadablesTab.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CPDownloadablesTab : MonoBehaviour
+{
+    // Exposed references
+	[Separator("Setup")]
+	[SerializeField] private Color[] m_groupBgColors = new Color[] {
+		Colors.WithAlpha(Colors.skyBlue, 0.25f),
+		Colors.WithAlpha(Colors.teal, 0.25f)
+	};
+
+	[Separator("Groups References")]
+	[SerializeField] private GameObject m_groupViewPrefab = null;
+    [SerializeField] private Transform m_groupViewsRoot = null;
+
+	[Separator("Other References")]
+	[SerializeField] private Toggle m_automaticDownloaderAllowedToggle = null;
+    [SerializeField] private TMP_Dropdown m_networkSpeedDropdown = null;
+
+	// Internal
+    private List<Downloadables.CatalogGroup> m_groupsSortedByPriority;
+	private List<CPDownloadablesGroupView> m_views = new List<CPDownloadablesGroupView>();
+	private List<CPDownloadablesGroupView> m_viewsTemp = new List<CPDownloadablesGroupView>();
+
+	/// <summary>
+	/// 
+	/// </summary>
+	private void Start() { 
+		// Create group views     
+        CPDownloadablesGroupView groupView = null;
+        GameObject newInstance = null;
+        List<Downloadables.CatalogGroup> groupsSortedByPriority = AssetBundlesManager.Instance.GetDownloadablesGroupsSortedByPriority();        
+        if(groupsSortedByPriority != null) {
+			// Cache data for further use
+            m_groupsSortedByPriority = groupsSortedByPriority.GetRange(0, groupsSortedByPriority.Count);
+
+			// Create a new view for each group
+            int count = groupsSortedByPriority.Count;
+            for(int i = 0; i < count; ++i) {
+				// Skip if the group doesn't have any bundle assinged
+                if(groupsSortedByPriority[i].EntryIds != null && groupsSortedByPriority[i].EntryIds.Count > 0) {
+					// Create instacne
+                    newInstance = Instantiate(m_groupViewPrefab, m_groupViewsRoot);
+                    newInstance.SetActive(true);
+
+					// Initialize group view
+					groupView = newInstance.GetComponent<CPDownloadablesGroupView>();
+					groupView.InitWithGroup(groupsSortedByPriority[i]);
+
+					// Set alternate row colors
+					groupView.SetBackgroundColor(m_groupBgColors[i % m_groupBgColors.Length]);
+
+					// Store reference for future use
+                    m_views.Add(groupView);
+                }
+            }
+        }
+
+		// Hide prefab instance
+		m_groupViewPrefab.SetActive(false);
+    }    
+
+	/// <summary>
+	/// 
+	/// </summary>
+    private void OnEnable()
+    {
+        if (m_automaticDownloaderAllowedToggle != null)
+        {
+            m_automaticDownloaderAllowedToggle.isOn = HDAddressablesManager.Instance.IsAutomaticDownloaderAllowed();
+        }        
+    }
+
+	/// <summary>
+	/// 
+	/// </summary>
+    private void Update()
+    {                        
+		// Do groups need to be reordered?
+        List<Downloadables.CatalogGroup> groupsSortedByPriority = AssetBundlesManager.Instance.GetDownloadablesGroupsSortedByPriority();
+        int count = groupsSortedByPriority.Count;
+        bool needsToUpdateGroupsOrder = count != m_groupsSortedByPriority.Count;
+        if (!needsToUpdateGroupsOrder)
+        {
+            for (int i = 0; i < count && !needsToUpdateGroupsOrder; i++)
+            {
+                needsToUpdateGroupsOrder = m_groupsSortedByPriority[i] != groupsSortedByPriority[i];
+            }
+        }
+
+        if (needsToUpdateGroupsOrder)
+        {
+            m_groupsSortedByPriority = groupsSortedByPriority.GetRange(0, groupsSortedByPriority.Count);
+            UpdateGroupsOrder();
+        }
+
+#if UNITY_EDITOR
+        m_networkSpeedDropdown.value = NETWORK_SPEED_SLEEP_TIME_BY_MODE.IndexOf(MockNetworkDriver.MockThrottleSleepTime);
+#endif
+    }
+
+    private void UpdateGroupsOrder()
+    {
+        int count = m_views.Count;
+        for (int i = 0; i < count; i++)
+        {
+            m_views[i].transform.parent = null;
+        }
+        
+        count = m_groupsSortedByPriority.Count;
+        for (int i = 0; i < count; i++)
+        {
+            for (int j = 0; j < m_views.Count; j++)
+            {
+                if (m_views[j].Group.Id == m_groupsSortedByPriority[i].Id)
+                {
+                    m_views[j].transform.parent = m_groupViewsRoot;
+                    break;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Deletes all downloadables from device's storage. It's linked to UI in inspector
+    /// </summary>
+    public void DeleteAll()
+    {
+        AssetBundlesManager.Instance.DeleteAllDownloadables();        
+    }
+
+    private static List<int> NETWORK_SPEED_SLEEP_TIME_BY_MODE = new List<int>(new int[] { 0, 16, 80, 800 });
+
+    public void NetworkSpeedSetOptionId(int optionId)
+    {
+        MockNetworkDriver.MockThrottleSleepTime = NETWORK_SPEED_SLEEP_TIME_BY_MODE[optionId];
+    }
+}
