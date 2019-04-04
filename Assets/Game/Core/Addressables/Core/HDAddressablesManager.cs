1e53135f4506199c638bb371735df1b530ecb34d
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2019-04-04 12:05:28 +0200

    Addressables Manager: Fix - Number of owned dragons was not taking in account special dragons.
    Former-commit-id: e132497541041ad91f809fbf45357c4e7a82805a

--- Assets/Game/Core/Addressables/Core/HDAddressablesManager.cs
+++ Assets/Game/Core/Addressables/Core/HDAddressablesManager.cs
@@ -205,16 +205,13 @@ public class HDAddressablesManager : AddressablesManager
     {
         //return UsersManager.currentUser != null && UsersManager.currentUser.IsTutorialStepCompleted(TutorialStep.SECOND_RUN);
 
-        // Automatic downloader gets unlocked when the user buys a dragon or when the user unlocks the second dragon. This is done to save up on traffic since many user won't made it to the second dragon.
-        IDragonData dragonData = DragonManager.biggestOwnedDragon;
-        bool returnValue = (dragonData != null && dragonData.GetOrder() > 0);
-        if (!returnValue)
-        {
-            dragonData = DragonManager.GetClassicDragonsByOrder(1);
-            returnValue = dragonData != null && !dragonData.isLocked;
-        }
+		// Automatic downloader gets unlocked when the user buys a dragon or when the user unlocks the second dragon. This is done to save up on traffic since many user won't made it to the second dragon.
+        if (UsersManager.currentUser.GetNumOwnedDragons() > 1) {
+			return true;
+		}
 
-        return returnValue;
+        IDragonData dragonData = DragonManager.GetClassicDragonsByOrder(1);
+        return dragonData != null && !dragonData.isLocked;
     }
 
 #region ingame
