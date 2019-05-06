8f9340b8bce520f89bb2218f16e8ceb4edfaaa5a
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2019-05-06 14:28:33 +0200

    UI Color FX: Development - Added custom editor class.
    Former-commit-id: 42c1c22fd1ee4713ecfaaa244acd488c613cc3a3

---
+++ Assets/Engine/UI/FX/Editor/UIColorFXEditor.cs
@@ -0,0 +1,101 @@
+// UIColorFXEditor.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 03/05/2019.
+// Copyright (c) 2019 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+using UnityEditor;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Custom editor for the UIColorFX class.
+/// </summary>
+[CustomEditor(typeof(UIColorFX), true)]	// True to be used by heir classes as well
+//[CanEditMultipleObjects]
+public class UIColorFXEditor : Editor {
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
+
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
+	// Casted target object
+	UIColorFX m_targetUIColorFX = null;
+
+	//------------------------------------------------------------------------//
+	// METHODS																  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// The editor has been enabled - target object selected.
+	/// </summary>
+	private void OnEnable() {
+		// Get target object
+		m_targetUIColorFX = target as UIColorFX;
+	}
+
+	/// <summary>
+	/// The editor has been disabled - target object unselected.
+	/// </summary>
+	private void OnDisable() {
+		// Clear target object
+		m_targetUIColorFX = null;
+	}
+
+	/// <summary>
+	/// Draw the inspector.
+	/// </summary>
+	public override void OnInspectorGUI() {
+		// Instead of modifying script variables directly, it's advantageous to use the SerializedObject and 
+		// SerializedProperty system to edit them, since this automatically handles private fields, multi-object 
+		// editing, undo, and prefab overrides.
+
+		// Update the serialized object - always do this in the beginning of OnInspectorGUI.
+		serializedObject.Update();
+
+		// Loop through all serialized properties and work with special ones
+		SerializedProperty p = serializedObject.GetIterator();
+		p.Next(true);	// To get first element
+		do {
+			// Properties requiring special treatment
+			// Unity's "script" property
+			if(p.name == "m_Script") {
+				// Draw the property, disabled
+				EditorGUI.BeginDisabledGroup(true);
+				EditorGUILayout.PropertyField(p, true);
+				EditorGUI.EndDisabledGroup();
+			}
+
+			// Color Ramp
+			else if(p.name == "m_colorRamp---") {
+
+			}
+
+			// Properties we don't want to show
+			else if(p.name == "m_ObjectHideFlags") {
+				// Do nothing
+			}
+
+			// Default property display
+			else {
+				EditorGUILayout.PropertyField(p, true);
+			}
+		} while(p.NextVisible(false));		// Only direct children, not grand-children (will be drawn by default if using the default EditorGUI.PropertyField)
+
+		// Apply changes to the serialized object - always do this in the end of OnInspectorGUI.
+		serializedObject.ApplyModifiedProperties();
+	}
+
+	/// <summary>
+	/// The scene is being refreshed.
+	/// </summary>
+	public void OnSceneGUI() {
+		// Scene-related stuff
+	}
+}
