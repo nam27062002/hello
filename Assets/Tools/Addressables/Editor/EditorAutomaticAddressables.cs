0cd4ab4455a3011cb26f4ed53c6fe82564ceea89
Marc Saña <marc.sana@ubisoft.com>
2019-05-24 18:16:29 +0200

    updated bundles
    Former-commit-id: 4d3bac21825a76e78ab57e7006045b8a09e424fe

--- Assets/Tools/Addressables/Editor/EditorAutomaticAddressables.cs
+++ Assets/Tools/Addressables/Editor/EditorAutomaticAddressables.cs
@@ -145,7 +145,7 @@ public static class EditorAutomaticAddressables {
         System.Type[] instanciableTypes = new System.Type[] { typeof(UnityEngine.GameObject), typeof(UnityEditor.SceneAsset) };
         GetEntriesFromDirectory(new DirectoryInfo("Assets/AI"), false, null, bundlesSet);
         GetEntriesFromDirectory(new DirectoryInfo("Assets/Art/3D/Gameplay/Entities"), false, null, bundlesSet);
-        GetEntriesFromDirectory(new DirectoryInfo("Assets/Art/3D/Gameplay/Entities/PrefabsMenu/"), false, entries, bundlesSet, true);
+        GetEntriesFromDirectory(new DirectoryInfo("Assets/Art/3D/Gameplay/Entities/PrefabsMenu/"), false, entries, bundlesSet, instanciableTypes);
         GetEntriesFromDirectory(new DirectoryInfo("Assets/Art/Particles/"), false, null, bundlesSet);
         GetEntriesFromDirectory(new DirectoryInfo("Assets/Art/VFX/"), false, null, bundlesSet);
         GetEntriesFromDirectory(new DirectoryInfo("Assets/Game/Scenes/Levels/"), false, entries, bundlesSet, instanciableTypes);
