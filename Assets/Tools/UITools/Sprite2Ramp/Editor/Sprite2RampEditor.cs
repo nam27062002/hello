03a92a709b5716784c61bb483b9ff2141e082be9
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2019-06-26 12:46:17 +0200

    Sprite 2 Ramp Tool: Development - Fully functional version :)
    Former-commit-id: 59a6b63f85461f86b6cb05d0deb379d862d079b0

--- Assets/Tools/UITools/Sprite2Ramp/Editor/Sprite2RampEditor.cs
+++ Assets/Tools/UITools/Sprite2Ramp/Editor/Sprite2RampEditor.cs
@@ -7,9 +7,11 @@
 //----------------------------------------------------------------------------//
 // INCLUDES																	  //
 //----------------------------------------------------------------------------//
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 //----------------------------------------------------------------------------//
@@ -95,6 +97,13 @@ public class Sprite2RampEditor : EditorWindow {
 	/// Update the inspector window.
 	/// </summary>
 	public void OnGUI() {
+		// Nothing to do in play mode!
+		if(Application.isPlaying) {
+			// Error message if missing parameters
+			EditorGUILayout.HelpBox("Can't be used in Play mode!", MessageType.Error);
+			return;
+		}
+
 		// Scroll Rect
 		m_scrollPos = EditorGUILayout.BeginScrollView(m_scrollPos); {
 			// Left Margin
@@ -111,16 +120,25 @@ public class Sprite2RampEditor : EditorWindow {
 
 			// Replacements
 			EditorGUILayout.Space();
-			//m_grayscaleBaseSprite = EditorGUILayout.ObjectField("Grayscale Base Sprite", m_grayscaleBaseSprite, typeof(Sprite), true) as Sprite);
-			//m_gradientTex = EditorGUILayout.ObjectField("Gradient Texture", m_gradientTex, typeof(Texture2D), true) as Texture2D;
+			m_grayscaleBaseSprite = EditorGUILayout.ObjectField("Grayscale Base Sprite", m_grayscaleBaseSprite, typeof(Sprite), true) as Sprite;
+			m_gradientTex = EditorGUILayout.ObjectField("Gradient Texture", m_gradientTex, typeof(Texture2D), true) as Texture2D;
 
-
-			// Button
+			// Space
 			EditorGUILayout.Space();
-			if(GUILayout.Button("REPLACE!!")) {
+
+			// Error message if missing parameters
+			bool parameterCheck = m_toReplaceSprite != null && m_grayscaleBaseSprite != null && m_gradientTex != null;
+			if(!parameterCheck) {
+				EditorGUILayout.HelpBox("Missing parameters!", MessageType.Error);
+			}
+
+			// Button - if all parameters are properly filled!
+			EditorGUI.BeginDisabledGroup(!parameterCheck);
+			if(GUILayout.Button("REPLACE!!", GUILayout.Height(30f))) {
 				// Perform the replacement!
 				DoReplace();
 			}
+			EditorGUI.EndDisabledGroup();
 
 			// Bottom margin
 			GUILayout.Space(WINDOW_MARGIN);
@@ -139,7 +157,117 @@ public class Sprite2RampEditor : EditorWindow {
 	/// Perform the replacement in all open sceen!
 	/// </summary>
 	private void DoReplace() {
+		// Aux vars
+		HashSet<GameObject> modifiedObjects = new HashSet<GameObject>();
 
+		// Find all references to the target sprite in the opened scenes
+		// For the purpose of this tool, assume the sprite will be in an Image component
+		//Image[] allImages = GameObject.FindObjectsOfType<Image>();	// [AOC] This skips disabled objects, we want them all!
+		Image[] allImages = Resources.FindObjectsOfTypeAll<Image>();
+
+		// [AOC] Unfortunately Resources.FindObjectsOfTypeAll returns us not only 
+		//		  the objects in the scene, but anything that is loaded in the editor
+		//		  at this moment (Project view, Inspector view, etc)
+		//		  Use EditorUtility.IsPersistent to filter the list
+		//		  Use the same loop to filter only those images targeting the sprite to replace.
+		Image[] filteredImages = allImages.Where((Image _img) => {
+			// Is it in the scene?
+			if(EditorUtility.IsPersistent(_img)) return false;
+
+			// Does it target the sprite to replace? Several ways of figuring out, choose one
+			if(_img.sprite != m_toReplaceSprite) return false;
+			//if(imagesInScene[i].sprite.GetHashCode() == m_toReplaceSprite.GetHashCode()) return false;
+			//if(spriteGUID == toReplaceGUID) return false;
+
+			// All checks passed!
+			return true;
+		}).ToArray();
+
+		// Iterate through all selected Images and perform the replacement
+		bool canceled = false;
+		for(int i = 0; i < filteredImages.Length; ++i) {
+			// Show progress bar
+			if(EditorUtility.DisplayCancelableProgressBar(
+				"Replacing " + m_toReplaceSprite.name + " in active scenes",
+				"Analyizing Image component " + (i + 1).ToString() + "/" + filteredImages.Length + "... (" + filteredImages[i].name + ")",
+				(float)i / (float)filteredImages.Length
+			)) {
+				canceled = true;
+				break;	// Canceled, break loop
+			}
+
+
+			// Perform the replacement!
+			// Easier naming
+			Image targetImage = filteredImages[i];
+
+			// Gather prefab's root for this image
+			GameObject rootObj = PrefabUtility.FindRootGameObjectWithSameParentPrefab(targetImage.gameObject);
+
+			// Replace target sprite by the grayscale one
+			targetImage.sprite = m_grayscaleBaseSprite;
+
+			// Get the UIColorFX component from this object. Add one if the object doesn't have it.
+			UIColorFX targetFX = targetImage.GetComponent<UIColorFX>();
+			if(targetFX == null) {
+				targetFX = targetImage.gameObject.AddComponent<UIColorFX>();
+			}
+
+			// Set up UIColorFX to use the target color ramp
+			targetFX.colorRampEnabled = true;
+			targetFX.colorRamp = m_gradientTex;
+
+			// Update log
+			modifiedObjects.Add(rootObj);
+		}
+
+		// Close progress bar
+		EditorUtility.ClearProgressBar();
+
+		// Process modified objects
+		int processedPrefabsCount = 0;
+		foreach(GameObject obj in modifiedObjects) {
+			// Show progress bar
+			EditorUtility.DisplayProgressBar(
+				canceled ? "Reverting changes" : "Applying changes to prefabs",
+				"Processing prefab " + (processedPrefabsCount + 1).ToString() + "/" + modifiedObjects.Count + "... (" + obj.name + ")",
+				(float)processedPrefabsCount / (float)modifiedObjects.Count
+			);
+
+			// If canceled, just revert changes
+			if(canceled) {
+				PrefabUtility.RevertPrefabInstance(obj);
+			} else {
+				// Make sure root prefab object is active (we want to save the prefabs active)
+				bool wasActive = obj.activeSelf;
+				obj.SetActive(true);
+
+				// Re-apply the prefab
+				PrefabUtility.ReplacePrefab(
+					obj,
+					PrefabUtility.GetPrefabParent(obj),
+					ReplacePrefabOptions.ConnectToPrefab
+				);
+
+				// Restore original activation state
+				obj.SetActive(wasActive);
+			}
+
+			// Done!
+			processedPrefabsCount++;
+		}
+
+		// Close progress bar
+		EditorUtility.ClearProgressBar();
+
+		// Clear modified objects list if canceled
+		if(canceled) {
+			modifiedObjects.Clear();
+		}
+
+		// Display summary dialog
+		Sprite2RampEditorSummaryWindow.OpenWindow(modifiedObjects);
+		GUIUtility.ExitGUI();
 	}
 
 
