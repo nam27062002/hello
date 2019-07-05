9e5ffff43c33d5832cad8c775c7fed55d5c7c529
Milan Metthey <milan.metthey@ubisoft.com>
2019-07-05 15:32:35 +0200

    Added sound when collecting cake slice.
    Former-commit-id: 918fb29c1ad49df6a9cf7a389d765e26d17edca6

--- Assets/Game/Gameplay/HugeMode/1stAnniversaryCakeCollectibles/CollectibleAnniversaryCake.cs
+++ Assets/Game/Gameplay/HugeMode/1stAnniversaryCakeCollectibles/CollectibleAnniversaryCake.cs
@@ -1,8 +1,16 @@
 ﻿using UnityEngine;
 
 public class CollectibleAnniversaryCake : CollectibleEntity {
-	public override Reward GetOnKillReward(DyingReason _reason) {
+
+    [SerializeField] private string m_onCollectSound;
+
+    public override Reward GetOnKillReward(DyingReason _reason) {
 		Messenger.Broadcast<Vector3>(MessengerEvents.ANNIVERSARY_CAKE_SLICE_EATEN, m_machine.position);
-		return reward;
+
+        // play the sfx.         
+        if ( !string.IsNullOrEmpty(m_onCollectSound) )
+            AudioController.Play( m_onCollectSound );
+
+        return reward;
 	}
 }
