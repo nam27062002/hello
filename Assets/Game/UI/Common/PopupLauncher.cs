abbe27e00775467585bcb1bf2eed5df1a750c755
Jose M. Olea <jose.olea@ubisoft.com>
2019-07-17 16:16:42 +0200

    Fix bug [HDK-3406] The "Missions" pop-up appears during gameplay if the user quickly taps to start the run before the pop-up is displayed
    Former-commit-id: 7d1b50c335de7e956ce7be8c422c49ca422e3879

--- Assets/Game/UI/Common/PopupLauncher.cs
+++ Assets/Game/UI/Common/PopupLauncher.cs
@@ -125,7 +125,7 @@ public class PopupLauncher : MonoBehaviour, IBroadcastListener {
 	// INTERNAL																  //
 	//------------------------------------------------------------------------//
 	/// <summary>
-	/// Gather the popup from the manager and open it.
+	/// Send the popup to the popups queue
 	/// Invokes the OnPopupInit event.
 	/// </summary>
 	private void OpenPopupInternal() {
@@ -133,29 +133,18 @@ public class PopupLauncher : MonoBehaviour, IBroadcastListener {
 		m_popup = PopupManager.LoadPopup(m_popupPath);
 		OnPopupInit.Invoke(m_popup);
 
-		// Open (apply delay)
-		if(m_delay > 0f) {
-			UbiBCN.CoroutineManager.DelayedCall(() => DoOpen(), m_delay);
-		} else {
-			DoOpen();
-		}
-	}
+        m_popup.OnOpen.AddListener(DoTracking);
 
-	/// <summary>
-	/// Executes the popup open method and tracking event.
-	/// </summary>
-	private void DoOpen() {
-		// Send tracking event (if any)
-		DoTracking();
+        // Send the popup to the popups queue
+        PopupManager.EnqueuePopup(m_popupPath);
+        
+    }
 
-		// Open popup
-		m_popup.Open();
-	}
 
 	/// <summary>
 	/// Sends the defined tracking action.
 	/// </summary>
-	private void DoTracking() {
+	private void DoTracking(PopupController _controller) {
 		// Ignore if none
 		if(m_trackingAction == TrackingAction.NONE) return;
 
