3a3cc9d2e528349173f209b27a53a0b107319ccf
Marc Saña <marc.sana@ubisoft.com>
2019-07-18 16:12:53 +0200

    fixed the size of gelatos
    Former-commit-id: 99dacc1fc6d9f00b6ec8ff26015f51c740482382

--- Assets/Game/Gameplay/Decorations/Spawners/PetGelatoSpawner.cs
+++ Assets/Game/Gameplay/Decorations/Spawners/PetGelatoSpawner.cs
@@ -258,9 +258,7 @@ public class PetGelatoSpawner : AbstractSpawner, IBroadcastListener  {
 		t.position = m_gelatoPositions[index];
 		t.localScale = GameConstants.Vector3.one * m_gelatoDefinitions[index].GetAsFloat("gelatoSize", 1f);
 		t.rotation = GameConstants.Quaternion.identity;
-
-		t.localScale = Vector3.one;
-
+        
 		if (m_gelatoGolden[index]) {
 			spawning.SetGolden(Spawner.EntityGoldMode.Gold);
 		}
