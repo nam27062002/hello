00badc81256b263dbfec03bf1e00c6d0a9671c21
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2019-07-30 09:35:08 +0200

    Orbital Camera Controller: Development - Implemented simple script to control a camera rotating around an object.
    Former-commit-id: a891eb049d0b8c2cad1b64841a4c0c0950b3f7b9

---
+++ Assets/Tests/OrbitalCameraController.cs
@@ -0,0 +1,116 @@
+// OrbitalCameraController.cs
+// Hungry Dragon
+// 
+// Created by Alger Ortín Castellví on 30/07/2019.
+// Copyright (c) 2019 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Simple controller for a camera orbiting around a point.
+/// </summary>
+public class OrbitalCameraController : MonoBehaviour {
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
+	public enum MouseButton {
+		NONE = -1,
+		LEFT = 0,
+		RIGHT = 1,
+		MIDDLE = 2
+	}
+
+	//------------------------------------------------------------------------//
+	// MEMBERS AND PROPERTIES												  //
+	//------------------------------------------------------------------------//
+	// Exposed
+	public Transform m_lookAt = null;
+	public float m_rotationSpeed = 0.5f;
+	public float m_zoomSpeed = 0.5f;
+
+	[Space]
+	public MouseButton m_holdMouseButtonToRotate = MouseButton.NONE;
+
+	// Internal
+	private Vector3 m_mouseDelta = Vector3.zero;
+	private Vector3 m_lastMouse = Vector3.zero;
+	
+	//------------------------------------------------------------------------//
+	// GENERIC METHODS														  //
+	//------------------------------------------------------------------------//
+	/// <summary>
+	/// Initialization.
+	/// </summary>
+	private void Awake() {
+
+	}
+
+	/// <summary>
+	/// First update call.
+	/// </summary>
+	private void Start() {
+		// Init internal vars
+		m_lastMouse = Input.mousePosition;
+	}
+
+	/// <summary>
+	/// Component has been enabled.
+	/// </summary>
+	private void OnEnable() {
+
+	}
+
+	/// <summary>
+	/// Component has been disabled.
+	/// </summary>
+	private void OnDisable() {
+
+	}
+
+	/// <summary>
+	/// Called every frame.
+	/// </summary>
+	private void Update() {
+		// Only if active!
+		if(!isActiveAndEnabled) return;
+
+		// Track mouse offset
+		m_mouseDelta = m_lastMouse - Input.mousePosition;
+		m_lastMouse = Input.mousePosition;
+	}
+
+	/// <summary>
+	/// Update performed at the end of the frame.
+	/// </summary>
+	private void LateUpdate() {
+		// Only if active!
+		if(!isActiveAndEnabled) return;
+
+		// Apply rotation?
+		if(m_holdMouseButtonToRotate == MouseButton.NONE || Input.GetMouseButton((int)m_holdMouseButtonToRotate)) {
+			transform.RotateAround(m_lookAt.position, Vector3.up, m_mouseDelta.x * -m_rotationSpeed);
+			transform.RotateAround(m_lookAt.position, Vector3.right, m_mouseDelta.y * m_rotationSpeed);
+		}
+	}
+
+	/// <summary>
+	/// Destructor.
+	/// </summary>
+	private void OnDestroy() {
+
+	}
+
+	//------------------------------------------------------------------------//
+	// OTHER METHODS														  //
+	//------------------------------------------------------------------------//
+
+	//------------------------------------------------------------------------//
+	// CALLBACKS															  //
+	//------------------------------------------------------------------------//
+}
