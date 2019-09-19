e402d9ae97f01d8f7947a0485d895b66d819c58d
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2019-09-19 12:38:51 +0200

    freezing bat
    Former-commit-id: 6b4049f7f20898a9eb70ddf250a6ae69869aa8b7

--- Assets/Game/UI/Menu/Shared/MenuDragonBirdView.cs
+++ Assets/Game/UI/Menu/Shared/MenuDragonBirdView.cs
@@ -9,6 +9,11 @@ public class MenuDragonBirdView : MonoBehaviour {
 	private Material m_ashMaterial;
 	private const float DISINTEGRATE_TIME = 1.25f;
 	public bool m_prepareForBurning = false;
+	public bool m_prepareForFreeze = false;
+	private AddressablesOp m_loadingRequest = null;
+	private GameObject m_freezingParticle;
+	public Transform m_anchor;
+
 	void Awake()
 	{
 		// Search renderer
@@ -16,6 +21,10 @@ public class MenuDragonBirdView : MonoBehaviour {
 		m_originalMaterial = m_renderer.material;
 		if ( m_prepareForBurning )
 			m_ashMaterial = Resources.Load("Game/Materials/BurnToAshes") as Material;
+		if ( m_prepareForFreeze )
+		{
+			m_loadingRequest = HDAddressablesManager.Instance.LoadAssetAsync("FX_FrozenSmallNPC", "Master");
+		}
 	}
 
 	void OnEnable()
@@ -24,6 +33,12 @@ public class MenuDragonBirdView : MonoBehaviour {
 		if ( m_prepareForBurning )
 			m_ashMaterial.SetFloat( GameConstants.Materials.Property.ASH_LEVEL , 0);
 		m_renderer.material = m_originalMaterial;
+	}
+
+	void OnDisable()
+	{
+		if ( m_freezingParticle != null )
+			m_freezingParticle.gameObject.SetActive(false);
 	}
 
 	void Burn()
@@ -47,6 +62,38 @@ public class MenuDragonBirdView : MonoBehaviour {
 		}
 		m_ashMaterial.SetFloat( GameConstants.Materials.Property.ASH_LEVEL , 1);
 		m_renderer.enabled = false;
+	}
+
+	public void Freeze()
+	{
+		StartCoroutine(SpawnFreezing());
+	}
+
+	IEnumerator SpawnFreezing()
+	{
+		if ( m_loadingRequest != null )
+		{
+			if (m_freezingParticle == null)
+			{
+				while( !m_loadingRequest.isDone )
+				{
+					yield return null;
+				}
+				GameObject go = m_loadingRequest.GetAsset<GameObject>();
+				m_freezingParticle = Instantiate(go,m_anchor);
+			}
+			m_freezingParticle.SetActive(true);
+			Transform tr = m_freezingParticle.transform;
+			tr.localPosition = GameConstants.Vector3.zero;
+			tr.localRotation = GameConstants.Quaternion.identity;
+			float time = 0;
+			while( time < 0.5f )
+			{
+				tr.localScale = GameConstants.Vector3.one * time / 0.5f;
+				time += Time.deltaTime;
+				yield return null;
+			}
+		}
 	}
 
 	void Disappear()
