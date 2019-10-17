2e91543730f3a23d93c4f83693d86098fcc76c85
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2019-10-17 12:12:22 +0200

    [FIX][HDK-4610]
    Former-commit-id: cf0722b7cff6356a0e8398556e776c1c470617e1

--- Assets/Game/Gameplay/Entities/AI/Brain/Behaviours/Pet/PetSearchTaggedTarget.cs
+++ Assets/Game/Gameplay/Entities/AI/Brain/Behaviours/Pet/PetSearchTaggedTarget.cs
@@ -16,10 +16,12 @@ namespace AI {
         public class PetSearchTaggedTargetData : StateComponentData {
             public IEntity.Tag tag = 0;
             public IEntity.Tag ignoreTag = 0;
+            /*
             [Tooltip("Max tier this pet will consider target.")]
             public DragonTier maxValidTier = DragonTier.TIER_4;
             [Tooltip("Min tier this pet will consider target.")]
             public DragonTier minValidTier = DragonTier.TIER_0;
+            */
             public TargetPriority priority = TargetPriority.Any;
             public CheckType checkType;
             public Signals.Type ignoreSignal = Signals.Type.None;
@@ -46,6 +48,7 @@ namespace AI {
 
             private PetSearchTaggedTargetData m_data;
             EatBehaviour m_eatBehaviour;
+            DragonTier m_playerTier;
 
 
             public override StateComponentData CreateData() {
@@ -67,10 +70,12 @@ namespace AI {
                 base.OnInitialise();
 
                 m_owner = InstanceManager.player;
+
                 m_data = m_pilot.GetComponentData<PetSearchTaggedTargetData>();
                 m_range = m_owner.data.maxScale * m_data.dragonSizeRangeMultiplier;
 
                 m_sensor = (m_machine as Machine).sensor;
+                m_playerTier = InstanceManager.player.data.tier;
             }
 
             // The first element in _param must contain the amount of time without detecting an enemy
@@ -100,7 +105,7 @@ namespace AI {
                                 bool isViable = false;
                                 switch (m_data.checkType) {
                                     case CheckType.Edible: {
-                                            if (entity.IsEdible(m_data.maxValidTier) && entity.edibleFromTier >= m_data.minValidTier) {
+                                            if (entity.IsEdible(m_playerTier) ) {
                                                 if (m_eatBehaviour == null) {
                                                     isViable = true;
                                                 } else {
@@ -113,7 +118,7 @@ namespace AI {
                                         }
                                         break;
                                     case CheckType.Burnable: {
-                                            isViable = entity.burnableFromTier >= m_data.minValidTier && entity.burnableFromTier <= m_data.maxValidTier;
+                                            isViable = entity.IsBurnable(m_playerTier);
                                         }
                                         break;
                                 }
