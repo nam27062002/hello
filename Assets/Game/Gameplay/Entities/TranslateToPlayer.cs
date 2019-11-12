37728869db8ac21ad53710571fcc9c0c971b6cd8
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2019-11-12 10:37:02 +0100

    Shu prototype
    Former-commit-id: 7dcb619fbb2893c773e78a3b19525a266c77d2e6

---
+++ Assets/Game/Gameplay/Entities/TranslateToPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslateToPlayer : MonoBehaviour {
+
+	Transform m_transform;
+	Transform m_target;
+	public float m_speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+		m_transform = transform;
+		m_target = InstanceManager.player.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_transform.position = Vector3.Lerp(m_transform.position, m_target.position, m_speed * Time.deltaTime);
+	}
+}
