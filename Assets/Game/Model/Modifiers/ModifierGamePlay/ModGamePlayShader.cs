2eef67d6475aff385670ce68eca0c42d9216a172
Miguel Ángel Linares <miguel-angel.linares@ubisoft.com>
2019-11-22 17:10:02 +0100

    night mod test
    Former-commit-id: f6ec350a2f71964de02390fd39689d903bb2b729

---
+++ Assets/Game/Model/Modifiers/ModifierGamePlay/ModGamePlayShader.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+public class ModGamePlayShader : ModifierGamePlay {
+	public const string TARGET_CODE = "shader";
+
+	//------------------------------------------------------------------------//
+	string m_subType;
+	string m_value;
+
+	//------------------------------------------------------------------------//
+	public ModGamePlayShader(DefinitionNode _def) : base(TARGET_CODE, _def) {
+		m_subType = _def.GetAsString("param1");
+		m_value = _def.GetAsString("param2");
+	}
+
+	public override void Apply() {
+		switch( m_subType )
+		{
+			case "keyword":
+			{
+				Shader.EnableKeyword(m_value);
+			}break;
+		}
+	}
+
+	public override void Remove () {
+		switch( m_subType )
+		{
+			case "keyword":
+			{
+				Shader.DisableKeyword(m_value);
+			}break;
+		}
+	}
+}
