ba8e206709d1f3436fb3ceac4d6959ca37d9e31f
Alger Ortín Castellví <alger.ortin@ubisoft.com>
2020-02-12 14:38:18 +0100

    Shop Pills Info Button: Development - Added support for tooltip. Properly choose info button behaviour based on pill type.
    Former-commit-id: f51fcc3b128c20e7c7c93578b64ccfa144e62a5a

--- Assets/Game/UI/Popups/Menu/Components/OfferPills/ShopRotationalOfferPill.cs
+++ Assets/Game/UI/Popups/Menu/Components/OfferPills/ShopRotationalOfferPill.cs
@@ -62,10 +62,27 @@ public class ShopRotationalOfferPill : ShopMonoRewardPill {
 		RefreshTimer();
 	}
 
+	/// <summary>
+	/// Get the info button mode for this pill's pack.
+	/// </summary>
+	/// <returns>The desired button mode.</returns>
+	protected override InfoButtonMode GetInfoButtonMode() {
+		// Only for Eggs
+		InfoButtonMode mode = InfoButtonMode.NONE; // None by default with rotationals
+		if(m_pack != null && m_pack.items.Count > 0 && m_pack.items[0] != null) {
+			// Info button mode depends on pack's item type
+			switch(m_pack.items[0].type) {
+				case Metagame.RewardEgg.TYPE_CODE: {
+					mode = InfoButtonMode.TOOLTIP;
+				} break;
+			}
+		}
 
+		return mode;
+	}
 
 	//------------------------------------------------------------------------//
 	// CALLBACKS															  //
 	//------------------------------------------------------------------------//
-	
+
 }
