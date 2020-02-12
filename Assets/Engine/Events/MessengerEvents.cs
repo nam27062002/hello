392b0f405bbe8a1e8cb28652c48eacf1038e7849
Jose M. Olea <jose.olea@ubisoft.com>
2020-02-12 14:40:31 +0100

    Missing class MessengerEvents!
    Former-commit-id: b52e13e70dee4dabd413cfdbc51a7fc0c418f47e

--- Assets/Engine/Events/MessengerEvents.cs
+++ Assets/Engine/Events/MessengerEvents.cs
@@ -232,6 +232,7 @@ public enum MessengerEvents {
 	OFFERS_CHANGED,		// no params
 	OFFER_APPLIED,		// OfferPack _pack
     HC_PACK_ACQUIRED,  // a HC pack was bought by the player. PARAMS: bool _showPopup: opens the happy hour popup immediately, string offerSku: the purchased offer sku
+    HAPPY_HOUR_CHANGED, // a happy hour was activated/deactivated/extended. No params 
 
 	// Live Events
 	LIVE_EVENT_STATES_UPDATED,
