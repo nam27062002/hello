29f4aa6759af2ddd8a314e4ed99469b18cd07f02
Jose M. Olea <jose.olea@ubisoft.com>
2020-02-27 17:24:10 +0100

    Shop - Optimization: do not load item previews until the pill is visible in the scrolling viewport.
    Former-commit-id: 7bfcce5bacf7d4be5ddada48e09feb394f2018f9

--- Assets/Game/UI/Shop/HcCategoryController.cs
+++ Assets/Game/UI/Shop/HcCategoryController.cs
@@ -120,6 +120,9 @@ public class HcCategoryController : CategoryController
             m_offers.Add(_offer);
             m_offerPills.Add(pill);
 
+            // By default dont load the pill preview until needed
+            pill.loadPillPreview = false;
+
             // If there is a callback
             if (m_shopController.purchaseCompletedCallback != null)
                 pill.OnPurchaseSuccess.AddListener(m_shopController.purchaseCompletedCallback);
