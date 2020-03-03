3d4e9c015cee081b26cf50c90e1e495aa61884db
Jose M. Olea <jose.olea@ubisoft.com>
2020-03-03 18:06:05 +0100

    Shop - Adds parallax effect in the 3d background of the shop
    Former-commit-id: a27b11421accae0a2979f73b6fe41cf20157d8bc

---
+++ Assets/Engine/3D/CameraTraveling.cs
@@ -0,0 +1,72 @@
+// CameraScroller.cs
+// Hungry Dragon
+// 
+// Created by J.M.Olea on 03/03/2020.
+// Copyright (c) 2020 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Moves a camera between two positions based in a value [0,1]
+/// </summary>
+public class CameraTraveling : MonoBehaviour {
+
+    //------------------------------------------------------------------------//
+    // MEMBERS AND PROPERTIES												  //
+    //------------------------------------------------------------------------//
+
+    public Camera m_targetCamera;
+
+    public Vector3 m_startingPosition;
+    public Vector3 m_finalPosition;
+
+    // From 0 to 1. Interpolated position between starting and final points
+    public float m_value;
+
+    /*public float Value  {
+        get { return m_value;  }
+        set {
+            m_value = Mathf.Clamp01(value);
+            UpdateCameraPosition();
+        }
+    }*/
+
+
+    //------------------------------------------------------------------------//
+    // GENERIC METHODS														  //
+    //------------------------------------------------------------------------//
+
+	//------------------------------------------------------------------------//
+	// OTHER METHODS														  //
+	//------------------------------------------------------------------------//
+
+    /// <summary>
+    /// Updates the camera position based on the current value
+    /// </summary>
+    public void UpdateCameraPosition ()
+    {
+        if (m_targetCamera != null && m_targetCamera.isActiveAndEnabled)
+        {
+            m_targetCamera.transform.position = Vector3.Lerp(m_startingPosition, m_finalPosition, m_value);
+        }
+    }
+
+    /// <summary>
+    /// Updates the camera position based on the given value
+    /// </summary>
+    public void UpdateCameraPosition(float _value)
+    {
+        m_value = Mathf.Clamp01( _value );
+
+        UpdateCameraPosition();
+    }
+    //------------------------------------------------------------------------//
+    // CALLBACKS															  //
+    //------------------------------------------------------------------------//
+}
---
+++ Assets/Engine/3D/Editor/CameraTravelingEditor.cs
@@ -0,0 +1,84 @@
+// CameraTravelingEditor.cs
+// Hungry Dragon
+// 
+// Created by J.M.Olea on 03/03/2020.
+// Copyright (c) 2020 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+using UnityEditor;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Custom editor for the MonoBehaviourTemplate class.
+/// </summary>
+[CustomEditor(typeof(CameraTraveling))]	
+public class CameraTravelingEditor : Editor {
+    //------------------------------------------------------------------------//
+    // CONSTANTS															  //
+    //------------------------------------------------------------------------//
+
+    //------------------------------------------------------------------------//
+    // MEMBERS AND PROPERTIES												  //
+    //------------------------------------------------------------------------//
+    SerializedProperty m_targetCamera, m_startingPosition, m_finalPosition, m_value;
+
+    //------------------------------------------------------------------------//
+    // METHODS																  //
+    //------------------------------------------------------------------------//
+
+
+    void OnEnable()
+    {
+        m_targetCamera = serializedObject.FindProperty("m_targetCamera");
+        m_startingPosition = serializedObject.FindProperty("m_startingPosition");
+        m_finalPosition = serializedObject.FindProperty("m_finalPosition");
+        m_value = serializedObject.FindProperty("m_value");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(m_targetCamera);
+        EditorGUILayout.PropertyField(m_startingPosition);
+        EditorGUILayout.PropertyField(m_finalPosition);
+        EditorGUILayout.Slider(m_value, 0f, 1f, "Value");
+        serializedObject.ApplyModifiedProperties();
+
+        if (m_targetCamera != null)
+        {
+            if (GUILayout.Button("Update camera position"))
+            {
+                // Update the camera position
+                ((CameraTraveling)serializedObject.targetObject).UpdateCameraPosition();
+            }
+
+            if (GUILayout.Button("Get starting pos from Camera"))
+            {
+                Vector3 newPos = ((Camera)m_targetCamera.objectReferenceValue).transform.position;
+                m_startingPosition.vector3Value = newPos;
+
+                serializedObject.ApplyModifiedProperties();
+
+            }
+
+            if (GUILayout.Button("Get final pos from Camera"))
+            {
+                Vector3 newPos = ((Camera)m_targetCamera.objectReferenceValue).transform.position;
+                m_finalPosition.vector3Value = newPos;
+
+                serializedObject.ApplyModifiedProperties();
+
+            }
+
+
+        }
+
+
+    }
+
+}
