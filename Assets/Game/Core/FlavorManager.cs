61abdbf6d0af6f6441a99339c9fc531593f7ba76
David Germade <david.germade@ubisoft.com>
2020-03-16 09:38:31 +0100

    [FLAVOR] First implementation. Minimal support: it chooses between Facebook and Weibo as social platform depending on the country when the app was installed
    Former-commit-id: cd3d89e53fc19e53a2b3d18b1c815e5253b6a3d0

---
+++ Assets/Game/Core/FlavorManager.cs
@@ -0,0 +1,27 @@
+﻿/// <summary>
+/// This class is responsible for managing app flavors. Following Android Studio approach (https://developer.android.com/studio/build/build-variants)
+/// for handling build variants our game can have different flavors. Some game features are configurable. Every single configuration
+/// is called "flavor". The two main flavors are:
+/// 1)Worldwide (WW): This is the master
+/// 2)China: Build variant for China
+/// </summary>
+public class FlavorManager
+{
+    /// <summary>
+    /// Returns the social platform configured for current flavor. Possible values:
+    ///        Weibo for China
+    ///        Facebook otherwise
+    /// </summary>    
+    public static SocialUtils.EPlatform GetSocialPlatform()
+    {
+        SocialUtils.EPlatform returnValue = SocialUtils.EPlatform.Facebook;
+
+        // In iOS we need to check the user's country to decide the social platform: either Facebook or Weibo (only in China)
+#if UNITY_IOS
+        string countryCode = PlatformUtils.Instance.Country_GetCodeOnInstall();
+        returnValue = (countryCode == PlatformUtils.COUNTRY_CODE_CHINA) ? SocialUtils.EPlatform.Weibo : SocialUtils.EPlatform.Facebook;
+#endif
+
+        return returnValue;
+    }
+}
