b3f0ca3c790a7d680464f5c98afef5deae19e52c
Chema Olea <jose.olea@ubisoft.com>
2020-04-24 17:15:10 +0200

    Shop - Fix bug in the shop popup
    Former-commit-id: 3608457826dcfda9e9293cf09654f40ba4ab42c7

--- Assets/Game/UI/Shop/ShopController.cs
+++ Assets/Game/UI/Shop/ShopController.cs
@@ -575,10 +575,23 @@ public class ShopController : MonoBehaviour
     {
         m_scrollRect.movementType = ScrollRect.MovementType.Unrestricted;
         m_scrollRect.horizontal = false;
-        m_scrollRect.GetComponent<HorizontalLayoutGroup>().enabled = true;
-        m_scrollRect.GetComponent<ContentSizeFitter>().enabled = true;
-        m_scrollRect.viewport.GetComponent<HorizontalLayoutGroup>().enabled = true;
-        m_scrollRect.viewport.GetComponent<ContentSizeFitter>().enabled = true;
+
+        HorizontalLayoutGroup layout = m_scrollRect.GetComponent<HorizontalLayoutGroup>();
+        ContentSizeFitter fitter = m_scrollRect.GetComponent<ContentSizeFitter>();
+        if (layout != null && fitter != null )
+        {
+            layout.enabled = true;
+            fitter.enabled = true;
+        }
+
+        layout = m_scrollRect.viewport.GetComponent<HorizontalLayoutGroup>();
+        fitter = m_scrollRect.viewport.GetComponent<ContentSizeFitter>();
+        if (layout != null && fitter != null)
+        {
+            layout.enabled = true;
+            fitter.enabled = true;
+        }
+
     }
 
 
