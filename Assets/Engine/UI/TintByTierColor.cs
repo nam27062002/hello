7c8d77fb76691fe5537dd7a7ec7867a26efe2150
Chema Olea <jose.olea@ubisoft.com>
2020-05-19 15:22:51 +0200

    Replacing tier icons with localized text
    Former-commit-id: eb2752667d5f50348d878ab736d47826a0282dab

---
+++ Assets/Engine/UI/TintByTierColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(Image))]
+public class TintByTierColor : MonoBehaviour
+{
+
+    [SerializeField]
+    private DragonTier m_tier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Image>().color = UIConstants.GetDragonTierColor(m_tier);
+    }
+
+
+}
