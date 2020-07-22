c31dc221119e6f4e258c6f92a6136e06ecae9231
Jordi Riambau <jordi.riambau@ubisoft.com>
2020-07-22 18:00:29 +0200

    Editor tool - Dragon wizard
    Former-commit-id: e431180f4014ca586b3bf6edc3de7487d901859e

---
+++ Assets/Tools/Editor/DragonWizard.cs
@@ -0,0 +1,189 @@
+﻿// DragonWizard.cs
+// Hungry Dragon
+// 
+// Created by Jordi Riambau on 22/07/2020.
+// Copyright (c) 2020 Ubisoft. All rights reserved.using System.Collections;
+
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class DragonWizard : EditorWindow
+{
+	const string DRAGON_CLASSIC_PATH = "Assets/Art/3D/Gameplay/Dragons/Prefabs/Game/PF_DragonClassic.prefab";
+
+    // Required
+    UnityEngine.Object dragonFBX;
+	UnityEngine.Object lastDragonFBX;
+	Editor gameObjectEditor;
+	string sku;
+	float fbxScale = 1.0f;
+
+    // Optional
+	Material dragonMaterial;
+	Material lastDragonMaterial;
+
+	// GUI
+	Texture2D previewBackgroundTexture = null;
+	GUIStyle previewStyle = new GUIStyle();
+
+	// Menu
+	[MenuItem("Hungry Dragon/Tools/Dragon Wizard...", false, -150)]
+	static void Init()
+	{
+		// Prepare window docked next to Inspector tab
+		System.Type inspectorType = System.Type.GetType("UnityEditor.InspectorWindow,UnityEditor.dll");
+		System.Type[] desiredDockNextTo = new System.Type[] { inspectorType };
+		EditorWindow window = GetWindow<DragonWizard>(desiredDockNextTo);
+		Texture icon = AssetDatabase.LoadAssetAtPath<Texture>("Assets/Art/UI/Common/Icons/icon_btn_animoji.png");
+		window.titleContent = new GUIContent(" Dragon", icon);
+
+		// Show window
+		window.Show();
+	}
+
+	// GUI 
+	void OnGUI()
+	{
+		// Editor checks
+		if (IsEditorBusy())
+			return;
+		EditorGUILayout.HelpBox("Prepare the required XML files when creating a new dragon", MessageType.Info, true);
+		if (GUILayout.Button("Setup new dragon XML tables...", GUILayout.Height(40)))
+		{
+			CreateXMLTables();
+		}
+
+		EditorGUILayout.Space();
+		EditorGUILayout.HelpBox("Creates 4 prefabs for implementing new dragons.\nIt will create the prefabs for Main Menu, Results, Corpse and Gameplay.", MessageType.Info, true);
+		EditorGUILayout.LabelField("Required", EditorStyles.boldLabel);
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField("View FBX:");
+		dragonFBX = EditorGUILayout.ObjectField(dragonFBX, typeof(UnityEngine.Object), true);
+		EditorGUILayout.EndHorizontal();
+		sku = EditorGUILayout.TextField("Dragon SKU:", sku);
+
+		// Optional
+		EditorGUILayout.Space();
+		EditorGUILayout.LabelField("Optional", EditorStyles.boldLabel);
+		fbxScale = EditorGUILayout.FloatField("FBX scale:", fbxScale);
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.LabelField("Material:");
+		dragonMaterial = (Material)EditorGUILayout.ObjectField(dragonMaterial, typeof(Material), true);
+		EditorGUILayout.EndHorizontal();
+
+		// Create prefabs button
+		EditorGUI.BeginDisabledGroup(dragonFBX == null || string.IsNullOrEmpty(sku));
+		if (GUILayout.Button("Create prefabs...", GUILayout.Height(40)))
+		{
+			CreatePrefabs();
+		}
+		EditorGUI.EndDisabledGroup();
+
+		// Preview FBX
+		if (dragonFBX != null)
+		{
+			if (gameObjectEditor == null || lastDragonFBX != dragonFBX)
+			{
+				//AssignSKU();
+				//AssignMaterial();
+				//AssignAnimationControllers();
+				//AssignClonePetBehaviour();
+                
+				CreatePreview();
+			}
+			else if (lastDragonMaterial != dragonMaterial)
+			{
+				CreatePreview();
+			}
+
+			if (previewBackgroundTexture == null)
+			{
+				CreatePreviewBackgroundTexture();
+				previewStyle.normal.background = previewBackgroundTexture;
+			}
+
+			EditorGUILayout.Space();
+			EditorGUILayout.LabelField("Preview", EditorStyles.boldLabel);
+			gameObjectEditor.OnInteractivePreviewGUI(GUILayoutUtility.GetRect(256, 256), previewStyle);
+
+			lastDragonFBX = dragonFBX;
+			lastDragonMaterial = dragonMaterial;
+		}
+		else
+		{
+			gameObjectEditor = null;
+		}
+	}
+
+    void CreateXMLTables()
+    {
+		EditorWindow window = GetWindow<DragonWizardXML>();
+		Texture icon = AssetDatabase.LoadAssetAtPath<Texture>("Assets/Art/UI/Common/Icons/icon_tab_missions.png");
+		window.titleContent = new GUIContent(" New dragon XML setup", icon);
+
+		// Show window
+		window.Show();
+	}
+
+    bool IsEditorBusy()
+    {
+		bool isEditorBusy = false;
+		if (EditorApplication.isCompiling)
+		{
+			EditorGUILayout.HelpBox("Cannot be used while the editor is compiling scripts", MessageType.Warning, true);
+			isEditorBusy = true;
+		}
+
+		if (EditorApplication.isUpdating)
+		{
+			EditorGUILayout.HelpBox("Cannot be used while refreshing the AssetDatabase", MessageType.Warning, true);
+			isEditorBusy = true;
+		}
+
+		if (EditorApplication.isPlayingOrWillChangePlaymode)
+		{
+			EditorGUILayout.HelpBox("Cannot be used in play mode", MessageType.Warning, true);
+			isEditorBusy = true;
+		}
+
+		return isEditorBusy;
+	}
+
+    void CreatePreviewBackgroundTexture()
+    {
+		Color backgroundColor = new Color(0.1568f, 0.1568f, 0.1568f);
+		previewBackgroundTexture = MakeTexture(backgroundColor);
+	}
+
+	Texture2D MakeTexture(Color color, int width = 1, int height = 1)
+	{
+		Color[] pixels = new Color[width * height];
+
+		for (int i = 0; i < pixels.Length; i++)
+			pixels[i] = color;
+
+		Texture2D result = new Texture2D(width, height, TextureFormat.RGB24, false);
+		result.SetPixels(pixels);
+		result.Apply();
+
+		return result;
+	}
+
+	void CreatePreview()
+    {
+		GameObject preview = (GameObject)dragonFBX;
+		Renderer[] renderers = preview.transform.GetComponentsInChildren<Renderer>();
+		for (int i = 0; i < renderers.Length; i++)
+		{
+			renderers[i].material = dragonMaterial;
+		}
+
+		gameObjectEditor = Editor.CreateEditor(preview);
+	}
+
+    void CreatePrefabs()
+    {
+        
+    }
+}
---
+++ Assets/Tools/Editor/DragonWizardXML.cs
@@ -0,0 +1,112 @@
+﻿// DragonWizardXML.cs
+// Hungry Dragon
+// 
+// Created by Jordi Riambau on 22/07/2020.
+// Copyright (c) 2020 Ubisoft. All rights reserved.
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Xml;
+
+public class DragonWizardXML : EditorWindow
+{
+    const string BASE_DEFINITIONS_PATH = "Assets/Resources/Rules/";
+    const string DRAGON_DEFINITIONS_PATH = BASE_DEFINITIONS_PATH + "dragonDefinitions.xml";
+    const string DRAGON_PROGRESSION_DEFINITIONS_PATH = BASE_DEFINITIONS_PATH + "dragonProgressionDefinitions.xml";
+    const string DISGUISES_DEFINITIONS_PATH = BASE_DEFINITIONS_PATH + "disguisesDefinitions.xml";
+    const string DAILY_REWARDS_DRAGON_MODIFIERS_DEFINITIONS_PATH = BASE_DEFINITIONS_PATH + "dailyRewardsDragonModifiersDefinitions.xml";
+    const string MISSION_DRAGON_MODIFIERS_DEFINITIONS_PATH = BASE_DEFINITIONS_PATH + "missionDragonModifiersDefinitions.xml";
+
+    string sku;
+    readonly List<string> skin = new List<string>() { string.Empty };
+
+    void OnGUI()
+    {
+        EditorGUILayout.HelpBox("Prepare the required XML files when creating a new dragon", MessageType.Info, true);
+
+        // Dragon sku
+        EditorGUILayout.LabelField("Configuration", EditorStyles.boldLabel);
+        sku = EditorGUILayout.TextField("Dragon sku:", sku);
+
+        // Skins
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Skins", EditorStyles.boldLabel);
+        for (int i = 0; i < skin.Count; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+            skin[i] = EditorGUILayout.TextField("Skin " + i + " sku", skin[i]);
+            if (GUILayout.Button(" X "))
+            {
+                skin.RemoveAt(i);
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        // Add new skin
+        if (GUILayout.Button("+ Add new skin", GUILayout.Height(20)))
+        {
+            skin.Add(string.Empty);
+        }
+
+        // Add dragon to XMLs
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        EditorGUI.BeginDisabledGroup(string.IsNullOrEmpty(sku) || skin.Count == 0);
+        if (GUILayout.Button("Add new dragon to XMLs", GUILayout.Height(40)))
+        {
+            ProcessXMLs();
+        }
+        EditorGUI.EndDisabledGroup();
+    }
+
+    void ProcessXMLs()
+    {
+        ProcessXML(DRAGON_DEFINITIONS_PATH, sku);
+        ProcessXML(DRAGON_PROGRESSION_DEFINITIONS_PATH, sku + "_progression", new KeyValuePair<string, string>("dragonSku", sku));
+        for (int i = 0; i < skin.Count; i++)
+        {
+            ProcessXML(DISGUISES_DEFINITIONS_PATH, skin[i], new KeyValuePair<string, string>("dragonSku", sku));
+        }
+
+        ProcessXML(DAILY_REWARDS_DRAGON_MODIFIERS_DEFINITIONS_PATH, sku + "_reward", new KeyValuePair<string, string>("dragonSku", sku));
+        ProcessXML(MISSION_DRAGON_MODIFIERS_DEFINITIONS_PATH, sku + "_mission", new KeyValuePair<string, string>("dragonSku", sku));
+
+        EditorUtility.DisplayDialog("Process finished", "Dragon " + sku + " added to XML files", "Close");
+    }
+
+    void ProcessXML(string xmlPath, string newSku, params KeyValuePair<string, string>[] extraAttributes)
+    {
+        // Load XML file
+        XmlDocument doc = new XmlDocument();
+        doc.Load(xmlPath);
+
+        // Select XML node
+        XmlNodeList nodeList = doc.SelectNodes("Definitions");
+        XmlNode lastChildNode = nodeList[nodeList.Count - 1].LastChild;
+
+        // Create new XML element
+        XmlElement xmlElement = doc.CreateElement("Definition");
+
+        // Copy attributes from last XML element
+        XmlAttributeCollection attributes = lastChildNode.Attributes;
+        foreach (XmlAttribute attribute in attributes)
+        {
+            xmlElement.SetAttribute(attribute.Name, attribute.Value);
+        }
+
+        // Override new attributes
+        xmlElement.SetAttribute("sku", newSku);
+        if (extraAttributes.Length > 0)
+        {
+            foreach (KeyValuePair<string, string> attr in extraAttributes)
+            {
+                xmlElement.SetAttribute(attr.Key, attr.Value);
+            }
+        }
+
+        // Append new element and save
+        doc.DocumentElement.AppendChild(xmlElement);
+        doc.Save(xmlPath);
+    }
+}
