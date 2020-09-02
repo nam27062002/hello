b95feb93e7d4f273c754edc2d509346bcd5a5002
Jordi Riambau <jordi.riambau@ubisoft.com>
2020-09-02 09:23:41 +0200

    Vampire damage VFX - Implementation
    Former-commit-id: 9c09736a9afef19963d828b97c7ef7fefd03f00e

--- Assets/Game/Gameplay/Dragons/DragonVampireEffects.cs
+++ Assets/Game/Gameplay/Dragons/DragonVampireEffects.cs
@@ -1,29 +1,66 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DragonVampireEffects : MonoBehaviour
 {
-    [SerializeField]
-    GameObject onDieParticleEffect;
+    [Header("Damage particle")]
+    [SerializeField] ParticleData onDamageParticleEffect;
+    [SerializeField] int maxDamageParticlesAmount = 30;
+    [SerializeField] float secondsBetweenSpawnDamageParticle = 10.0f;
+
+    float m_lastTimeDamageParticleSpawned;
+    GameObject m_damageParticleInstance;
+    ParticleSystem.MainModule m_damageParticleMainModule;
+
+    void Awake()
+    {
+        CreateInstance();
+    }
 
     void OnEnable()
 	{
-        Messenger.AddListener<DamageType, Transform>(MessengerEvents.PLAYER_KO, OnPlayerDeath);
+        Messenger.AddListener<float, DamageType, Transform>(MessengerEvents.PLAYER_DAMAGE_RECEIVED, OnPlayerDamage);
     }
 
     void OnDisable()
     {
-        Messenger.RemoveListener<DamageType, Transform>(MessengerEvents.PLAYER_KO, OnPlayerDeath);
+        Messenger.RemoveListener<float, DamageType, Transform>(MessengerEvents.PLAYER_DAMAGE_RECEIVED, OnPlayerDamage);
     }
 
-    void OnPlayerDeath(DamageType arg1, Transform arg2)
+    private void OnPlayerDamage(float _damage, DamageType _damageType, Transform _transform)
     {
-        onDieParticleEffect.SetActive(true);
+        // Ignore this type of damage
+        if (_damage < 1.0f || _damageType == DamageType.POISON || _damageType == DamageType.CURSE)
+            return;
+
+        // Check if have passed enough time to spawn the particle
+        float elapsedTime = Time.time - m_lastTimeDamageParticleSpawned;
+        if (elapsedTime < secondsBetweenSpawnDamageParticle)
+            return;
+
+        // Calculate the amount of particles to spawn
+        int particlesAmount = Mathf.RoundToInt((int)_damage * maxDamageParticlesAmount / 60); // BIG_DAMAGE = 60
+        int maxParticles = Mathf.Clamp(particlesAmount, 1, maxDamageParticlesAmount);
+        m_damageParticleMainModule.maxParticles = maxParticles;
+
+        // Spawn damage particle
+        m_damageParticleInstance.transform.SetPositionAndRotation(transform.position, transform.rotation);
+        m_damageParticleInstance.SetActive(true);
+
+        // Update last time particle was spawned
+        m_lastTimeDamageParticleSpawned = Time.time;
     }
 
-    private void Update()
+    void CreateInstance()
     {
-       // if (Input.GetKeyDown(KeyCode.J))
-        //    onDieParticleEffect.SetActive(true);
+        if (onDamageParticleEffect != null)
+        {
+            // Pre-cache damage particle
+            m_damageParticleInstance = onDamageParticleEffect.CreateInstance();
+            ParticleSystem particleInstance = m_damageParticleInstance.GetComponent<ParticleSystem>();
+            m_damageParticleMainModule = particleInstance.main;
+            m_damageParticleInstance.SetActive(false);
+            SceneManager.MoveGameObjectToScene(particleInstance.gameObject, gameObject.scene);
+        }
     }
 }
