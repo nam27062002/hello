7f8deea234412f036ad1513e6a1d8bf91739469a
Chema Olea <jose.olea@ubisoft.com>
2020-11-03 10:46:01 +0100

    Welcome Back - Integrate offers popups prefabs
    Former-commit-id: a41cee12a54daf79e1b35dcd9f27f7adcd31416d

---
+++ Assets/Game/UI/Popups/Offers/PopupShopWelcomeBackOfferPack.cs
@@ -0,0 +1,26 @@
+// PopupShopOfferPack.cs
+// Hungry Dragon
+// 
+// Created by Jose M. Olea
+// Copyright (c) 2020 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Controller for the Welcome back Offer Pack Popup.
+/// </summary>
+[RequireComponent(typeof(PopupController))]
+public class PopupShopWelcomeBackOfferPack : PopupShopOfferPack {
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
+	public new const string PATH = "UI/Popups/Economy/PF_PopupShopWelcomeBackOfferPack";
+	
+	
+}
---
+++ Assets/Game/UI/Popups/Offers/PopupShopWelcomeBackOfferPackSkins.cs
@@ -0,0 +1,27 @@
+// PopupShopOfferPack.cs
+// Hungry Dragon
+// 
+// Created by Jose M. Olea
+// Copyright (c) 2020 Ubisoft. All rights reserved.
+
+//----------------------------------------------------------------------------//
+// INCLUDES																	  //
+//----------------------------------------------------------------------------//
+using UnityEngine;
+
+//----------------------------------------------------------------------------//
+// CLASSES																	  //
+//----------------------------------------------------------------------------//
+/// <summary>
+/// Controller for the Welcome back Offer Pack Popup.
+/// </summary>
+[RequireComponent(typeof(PopupController))]
+public class PopupShopWelcomeBackOfferPackSkins : PopupShopOfferPack
+{
+	//------------------------------------------------------------------------//
+	// CONSTANTS															  //
+	//------------------------------------------------------------------------//
+	public new const string PATH = "UI/Popups/Economy/PF_PopupShopWelcomeBackOfferPackSkins";
+
+
+}
